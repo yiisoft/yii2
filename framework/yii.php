201b7068cc04941a74a759cfc4db3e2a2cec0e92
Qiang Xue <qiang.xue@gmail.com>
2011-07-19 23:47:21 -0400

    w

--- framework/yii.php
+++ framework/yii.php
@@ -2,26 +2,20 @@
 /**
  * Yii bootstrap file.
  *
- * @author Qiang Xue <qiang.xue@gmail.com>
  * @link http://www.yiiframework.com/
  * @copyright Copyright &copy; 2008-2012 Yii Software LLC
  * @license http://www.yiiframework.com/license/
- * @version $Id: yii.php 2799 2011-01-01 19:31:13Z qiang.xue $
- * @package yii
- * @since 2.0
  */
 
-require(__DIR__ . '/base/YiiBase.php');
+require(__DIR__ . '/YiiBase.php');
 
 /**
  * Yii is a helper class serving common framework functionalities.
  *
- * It encapsulates {@link YiiBase} which provides the actual implementation.
- * By writing your own Yii class, you can customize some functionalities of YiiBase.
+ * It extends from [[YiiBase]] which provides the actual implementation.
+ * By writing your own Yii class, you can customize some functionalities of [[YiiBase]].
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
- * @version $Id: yii.php 2799 2011-01-01 19:31:13Z qiang.xue $
- * @package system
  * @since 2.0
  */
 class Yii extends YiiBase
