3bb73e54f9aa06b5ccefd63995b3a7a8a2b89369
Alexander Makarov <sam@rmcreative.ru>
2013-02-25 03:20:55 +0400

    more tests

---
+++ tests/unit/framework/validators/EmailValidatorTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace yiiunit\framework\validators;
+use yii\validators\EmailValidator;
+use yiiunit\TestCase;
+
+/**
+ * EmailValidatorTest
+ */
+class EmailValidatorTest extends TestCase
+{
+	public function testValidateValue()
+	{
+		$validator = new EmailValidator();
+
+		$this->assertTrue($validator->validateValue('sam@rmcreative.ru'));
+		$this->assertTrue($validator->validateValue('5011@gmail.com'));
+		$this->assertFalse($validator->validateValue('rmcreative.ru'));
+	}
+
+	public function testValidateValueMx()
+	{
+		$validator = new EmailValidator();
+		$validator->checkMX = true;
+
+		$this->assertTrue($validator->validateValue('sam@rmcreative.ru'));
+		$this->assertFalse($validator->validateValue('test@example.com'));
+	}
+}
