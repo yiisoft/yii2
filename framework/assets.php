a2594b7dcec0fd2c751133a39661d6e2d6116e05
Qiang Xue <qiang.xue@gmail.com>
2013-04-30 11:26:38 -0400

    script WIP

--- framework/assets.php
+++ framework/assets.php
@@ -1,5 +1,24 @@
 <?php
 
 return array(
-	'basePath' => __DIR__ . '/web/assets',
+	'jquery' => array(
+		'sourcePath' => __DIR__ . '/assets',
+		'js' => array(
+			'jquery.min.js',
+		),
+	),
+	'yii' => array(
+		'sourcePath' => __DIR__ . '/assets',
+		'js' => array(
+			'yii.js',
+		),
+		'depends' => array('jquery'),
+	),
+	'yii/form' => array(
+		'sourcePath' => __DIR__ . '/assets',
+		'js' => array(
+			'yii-form.js',
+		),
+		'depends' => array('yii'),
+	),
 );
---
+++ framework/assets/yii.js
@@ -0,0 +1,16 @@
+/**
+ * Yii JavaScript module.
+ *
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+;(function($) {
+
+	$.yii = {
+		version : '2.0'
+	};
+
+})(jQuery);
