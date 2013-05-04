2bdfc27692731da995f61bef103681b5c0aca184
Qiang Xue <qiang.xue@gmail.com>
2013-05-04 08:28:27 -0400

    Fixes for issue #78.

--- framework/helpers/base/Html.php
+++ framework/helpers/base/Html.php
@@ -127,13 +127,15 @@ class Html
 	 * Encodes special characters into HTML entities.
 	 * The [[yii\base\Application::charset|application charset]] will be used for encoding.
 	 * @param string $content the content to be encoded
+	 * @param boolean $doubleEncode whether to encode HTML entities in `$content`. If false,
+	 * HTML entities in `$content` will not be further encoded.
 	 * @return string the encoded content
 	 * @see decode
 	 * @see http://www.php.net/manual/en/function.htmlspecialchars.php
 	 */
-	public static function encode($content)
+	public static function encode($content, $doubleEncode = true)
 	{
-		return htmlspecialchars($content, ENT_QUOTES, Yii::$app->charset);
+		return htmlspecialchars($content, ENT_QUOTES, Yii::$app->charset, $doubleEncode);
 	}
 
 	/**
