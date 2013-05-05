b737e38aff06d04526044c7c0a1fc59adbbec726
Alexander Kochetov <creocoder@gmail.com>
2013-05-05 18:19:15 +0400

    Code style fix

--- framework/console/webapp/default/protected/views/layouts/main.php
+++ framework/console/webapp/default/protected/views/layouts/main.php
@@ -1,17 +1,23 @@
-<?php use yii\helpers\Html as Html; ?>
-<!doctype html>
-<html lang="<?php \Yii::$app->language?>">
-	<head>
-		<meta charset="utf-8" />
-		<title><?php echo Html::encode($this->title)?></title>
-	</head>
-	<body>
-		<h1><?php echo Html::encode($this->title)?></h1>
-		<div class="content">
-			<?php echo $content?>
-		</div>
-		<div class="footer">
-			<?php echo \Yii::powered()?>
-		</div>
-	</body>
+<?php
+/**
+ * @var $this \yii\base\View
+ * @var $content string
+ */
+use yii\helpers\Html as Html;
+?>
+<!DOCTYPE html>
+<html lang="<?php \Yii::$app->language; ?>">
+<head>
+	<meta charset="utf-8">
+	<title><?php echo Html::encode($this->title); ?></title>
+</head>
+<body>
+	<h1><?php echo Html::encode($this->title); ?></h1>
+	<div class="content">
+		<?php echo $content; ?>
+	</div>
+	<div class="footer">
+		<?php echo \Yii::powered(); ?>
+	</div>
+</body>
 </html>
