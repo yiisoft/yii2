2387e59c4e49cf8e3ab2b963002cf5268e360985
Carsten Brandt <mail@cebe.cc>
2013-05-09 13:31:43 +0200

    Renamed yii.php -> Yii.php for class, filename consitency
    See https://github.com/yiisoft/yii2/issues/123#issuecomment-17657374
    Issue #123

--- apps/bootstrap/index.php
+++ apps/bootstrap/index.php
@@ -3,7 +3,7 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 $frameworkPath = __DIR__ . '/../../framework/';
-require($frameworkPath . 'yii.php');
+require($frameworkPath . 'Yii.php');
 
 $config = require(__DIR__ . '/protected/config/main.php');
 $application = new yii\web\Application($config);
--- tests/web/app/index.php
+++ tests/web/app/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require(__DIR__ . '/../../../framework/yii.php');
+require(__DIR__ . '/../../../framework/Yii.php');
 
 $application = new yii\web\Application('test', __DIR__ . '/protected');
 $application->run();
