aad6d7d9922a6eecdd804bb72fccc711e8dce15e
Qiang Xue <qiang.xue@gmail.com>
2013-05-09 10:18:25 -0400

    refactoring.

--- apps/bootstrap/index.php
+++ apps/bootstrap/index.php
@@ -1,14 +1,13 @@
 <?php
-// Set to false to disable debug mode
+// comment out the following line to disable debug mode
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 
-$frameworkPath = __DIR__ . '/../../framework/';
-require($frameworkPath . 'Yii.php');
+$frameworkPath = __DIR__ . '/../../framework';
+
+require($frameworkPath . '/Yii.php');
+// Register Composer autoloader
+@include($frameworkPath . '/vendor/autoload.php');
 
 $config = require(__DIR__ . '/protected/config/main.php');
 $application = new yii\web\Application($config);
-
-// Register Composer autoloader
-@include $frameworkPath . '/vendor/autoload.php';
-
 $application->run();
