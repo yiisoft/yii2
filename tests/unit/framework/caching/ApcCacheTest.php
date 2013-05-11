6a7529505eaf25c42041e2d4d002ae5b1b9c212a
Carsten Brandt <mail@cebe.cc>
2013-05-11 13:16:47 +0200

    cache tests refactoring cherry picked from redis branch

--- tests/unit/framework/caching/ApcCacheTest.php
+++ tests/unit/framework/caching/ApcCacheTest.php
@@ -21,9 +21,17 @@ class ApcCacheTest extends CacheTest
 			$this->markTestSkipped("APC cli is not enabled. Skipping.");
 		}
 
+		if(!ini_get("apc.enabled") || !ini_get("apc.enable_cli")) {
+			$this->markTestSkipped("APC is installed but not enabled. Skipping.");
+		}
+
 		if ($this->_cacheInstance === null) {
 			$this->_cacheInstance = new ApcCache();
 		}
 		return $this->_cacheInstance;
 	}
+
+	// TODO there seems to be a problem with APC returning cached value even if it is expired.
+	// TODO makes test fail on PHP 5.3.10-1ubuntu3.6 with Suhosin-Patch (cli) -- cebe
+	// TODO http://drupal.org/node/1278292
 }
