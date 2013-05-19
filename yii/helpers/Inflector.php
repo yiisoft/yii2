17397a887616a76e17f489ae66c9638bf9925cc1
Antonio Ramirez <amigo.cobos@gmail.com>
2013-05-19 22:19:24 +0200

    Inflector class

---
+++ yii/helpers/Inflector.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @link http://www.yiiframework.com/
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\helpers;
+
+/**
+ * Inflector pluralizes and singularizes English nouns. It also contains other useful methods.
+ *
+ * @author Antonio Ramirez <amigo.cobos@gmail.com>
+ * @since 2.0
+ */
+class Inflector extends base\Inflector
+{
+}
