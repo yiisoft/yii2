2b63dc888a2887dd83db4f6c8eaca136e2d4e2bc
Alexander Kochetov <creocoder@gmail.com>
2013-05-21 23:16:10 +0400

    Bundle names and depency fixes

--- yii/bootstrap/assets.php
+++ yii/bootstrap/assets.php
@@ -1,115 +1,115 @@
 <?php
 
 return array(
-	'yii/bootstrap' => array(
+	'yii/bootstrap/css' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'css' => array(
 			YII_DEBUG ? 'css/bootstrap.css' : 'css/bootstrap.min.css',
 		),
 	),
-	'yii/bootstrap-responsive' => array(
+	'yii/bootstrap/css-responsive' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'css' => array(
 			YII_DEBUG ? 'css/bootstrap-responsive.css' : 'css/bootstrap-responsive.min.css',
 		),
 		'depends' => array('yii/bootstrap'),
 	),
-	'yii/bootstrap-js' => array(
+	'yii/bootstrap/all' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			YII_DEBUG ? 'js/bootstrap.js' : 'js/bootstrap.min.js',
 		),
-		'depends' => array('yii/jquery'),
+		'depends' => array('yii/jquery', 'yii/bootstrap/css'),
 	),
-	'yii/bootstrap-js-affix' => array(
+	'yii/bootstrap/affix' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			'js/bootstrap-affix.js',
 		),
-		'depends' => array('yii/jquery'),
+		'depends' => array('yii/jquery', 'yii/bootstrap/css'),
 	),
-	'yii/bootstrap-js-alert' => array(
+	'yii/bootstrap/alert' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			'js/bootstrap-alert.js',
 		),
-		'depends' => array('yii/jquery'),
+		'depends' => array('yii/jquery', 'yii/bootstrap/css'),
 	),
-	'yii/bootstrap-js-button' => array(
+	'yii/bootstrap/button' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			'js/bootstrap-button.js',
 		),
-		'depends' => array('yii/jquery'),
+		'depends' => array('yii/jquery', 'yii/bootstrap/css'),
 	),
-	'yii/bootstrap-js-carousel' => array(
+	'yii/bootstrap/carousel' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			'js/bootstrap-carousel.js',
 		),
-		'depends' => array('yii/jquery'),
+		'depends' => array('yii/jquery', 'yii/bootstrap/css'),
 	),
-	'yii/bootstrap-js-collapse' => array(
+	'yii/bootstrap/collapse' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			'js/bootstrap-collapse.js',
 		),
-		'depends' => array('yii/jquery'),
+		'depends' => array('yii/jquery', 'yii/bootstrap/css'),
 	),
-	'yii/bootstrap-js-dropdown' => array(
+	'yii/bootstrap/dropdown' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			'js/bootstrap-dropdown.js',
 		),
-		'depends' => array('yii/jquery'),
+		'depends' => array('yii/jquery', 'yii/bootstrap/css'),
 	),
-	'yii/bootstrap-js-modal' => array(
+	'yii/bootstrap/modal' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			'js/bootstrap-modal.js',
 		),
-		'depends' => array('yii/jquery'),
+		'depends' => array('yii/jquery', 'yii/bootstrap/css'),
 	),
-	'yii/bootstrap-js-popover' => array(
+	'yii/bootstrap/popover' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			'js/bootstrap-popover.js',
 		),
-		'depends' => array('yii/jquery','yii/bootstrap-js-tooltip'),
+		'depends' => array('yii/jquery', 'yii/bootstrap/tooltip', 'yii/bootstrap/css'),
 	),
-	'yii/bootstrap-js-scrollspy' => array(
+	'yii/bootstrap/scrollspy' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			'js/bootstrap-scrollspy.js',
 		),
-		'depends' => array('yii/jquery'),
+		'depends' => array('yii/jquery', 'yii/bootstrap/css'),
 	),
-	'yii/bootstrap-js-tab' => array(
+	'yii/bootstrap/tab' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			'js/bootstrap-tab.js',
 		),
-		'depends' => array('yii/jquery'),
+		'depends' => array('yii/jquery', 'yii/bootstrap/css'),
 	),
-	'yii/bootstrap-js-tooltip' => array(
+	'yii/bootstrap/tooltip' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			'js/bootstrap-tooltip.js',
 		),
-		'depends' => array('yii/jquery'),
+		'depends' => array('yii/jquery', 'yii/bootstrap/css'),
 	),
-	'yii/bootstrap-js-transition' => array(
+	'yii/bootstrap/transition' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			'js/bootstrap-transition.js',
 		),
-		'depends' => array('yii/jquery'),
+		'depends' => array('yii/jquery', 'yii/bootstrap/css'),
 	),
-	'yii/bootstrap-js-typeahead' => array(
+	'yii/bootstrap/typeahead' => array(
 		'sourcePath' => __DIR__ . '/assets',
 		'js' => array(
 			'js/bootstrap-typeahead.js',
 		),
-		'depends' => array('yii/jquery'),
+		'depends' => array('yii/jquery', 'yii/bootstrap/css'),
 	),
 );
