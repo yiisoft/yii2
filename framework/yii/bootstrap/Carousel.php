c4c16d88768de87e2fa4a3d78c5b7322accc298f
Alexander Kochetov <creocoder@gmail.com>
2013-05-23 02:14:21 +0400

    yii\bootstrap\Carousel fixes

--- framework/yii/bootstrap/Carousel.php
+++ framework/yii/bootstrap/Carousel.php
@@ -26,7 +26,7 @@ use yii\helpers\Html;
  *         ),
  *         array(
  *             'content' => '<img src="http://twitter.github.io/bootstrap/assets/img/bootstrap-mdo-sfmoma-03.jpg"/>',
- *             'caption' => '<h4>This is title</h5><p>This is the caption text</p>',
+ *             'caption' => '<h4>This is title</h4><p>This is the caption text</p>',
  *             'options' => array(...),
  *         ),
  *     )
