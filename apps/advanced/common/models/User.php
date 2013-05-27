c8e73b42e75c2770ecd69865af9cfa9f7bc9666d
Carsten Brandt <mail@cebe.cc>
2013-05-27 23:45:13 +0200

    fixed AutoTimestamp example in advanced app
    fixes #429

--- apps/advanced/common/models/User.php
+++ apps/advanced/common/models/User.php
@@ -37,8 +37,7 @@ class User extends ActiveRecord implements Identity
 			'timestamp' => array(
 				'class' => 'yii\behaviors\AutoTimestamp',
 				'attributes' => array(
-					ActiveRecord::EVENT_BEFORE_INSERT => 'create_time',
-					ActiveRecord::EVENT_BEFORE_INSERT => 'update_time',
+					ActiveRecord::EVENT_BEFORE_INSERT => array('create_time', 'update_time'),
 					ActiveRecord::EVENT_BEFORE_UPDATE => 'update_time',
 				),
 			),
