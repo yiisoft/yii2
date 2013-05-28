f690f0db13425ca7ed528e590b34d5359fb9b238
Qiang Xue <qiang.xue@gmail.com>
2013-05-28 19:24:08 -0400

    Fixes issue #441: InvalidRequestException should be HttpException

--- framework/yii/base/Controller.php
+++ framework/yii/base/Controller.php
@@ -151,43 +151,13 @@ class Controller extends Component
 	/**
 	 * Binds the parameters to the action.
 	 * This method is invoked by [[Action]] when it begins to run with the given parameters.
-	 * This method will check the parameter names that the action requires and return
-	 * the provided parameters according to the requirement. If there is any missing parameter,
-	 * an exception will be thrown.
 	 * @param Action $action the action to be bound with parameters
 	 * @param array $params the parameters to be bound to the action
 	 * @return array the valid parameters that the action can run with.
-	 * @throws InvalidRequestException if there are missing parameters.
 	 */
 	public function bindActionParams($action, $params)
 	{
-		if ($action instanceof InlineAction) {
-			$method = new \ReflectionMethod($this, $action->actionMethod);
-		} else {
-			$method = new \ReflectionMethod($action, 'run');
-		}
-
-		$args = array();
-		$missing = array();
-		foreach ($method->getParameters() as $param) {
-			$name = $param->getName();
-			if (array_key_exists($name, $params)) {
-				$args[] = $params[$name];
-				unset($params[$name]);
-			} elseif ($param->isDefaultValueAvailable()) {
-				$args[] = $param->getDefaultValue();
-			} else {
-				$missing[] = $name;
-			}
-		}
-
-		if (!empty($missing)) {
-			throw new InvalidRequestException(Yii::t('yii', 'Missing required parameters: {params}', array(
-				'{params}' => implode(', ', $missing),
-			)));
-		}
-
-		return $args;
+		return array();
 	}
 
 	/**
