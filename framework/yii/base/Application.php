4ad23b2f43e269de3acbb49b1304f2f0e51dab99
callmez <callme-z@qq.com>
2013-06-03 22:37:53 +0800

    Pre-initializes Request component, @wwwroot and @www alias then properties can used it.

--- framework/yii/base/Application.php
+++ framework/yii/base/Application.php
@@ -102,18 +102,6 @@ class Application extends Module
 		Yii::setAlias('@app', $this->getBasePath());
 		unset($config['basePath']);
 
-		if (isset($config['vendor'])) {
-			$this->setVendorPath($config['vendor']);
-			unset($config['vendorPath']);
-		}
-		Yii::setAlias('@vendor', $this->getVendorPath());
-
-		if (isset($config['runtime'])) {
-			$this->setRuntimePath($config['runtime']);
-			unset($config['runtime']);
-		}
-		Yii::setAlias('@runtime', $this->getRuntimePath());
-
 		if (isset($config['timeZone'])) {
 			$this->setTimeZone($config['timeZone']);
 			unset($config['timeZone']);
