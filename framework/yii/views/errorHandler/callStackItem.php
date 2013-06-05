d3b75d9ff97a4867af91935a6aa29130c893bdb7
Luciano Baraglia <luciano.baraglia@gmail.com>
2013-06-05 02:25:06 -0300

    Exception page code block in html ordered list - as discussed in #297 and #418

--- framework/yii/views/errorHandler/callStackItem.php
+++ framework/yii/views/errorHandler/callStackItem.php
@@ -13,35 +13,34 @@
  */
 $context = $this->context;
 ?>
-<li class="<?php if (!$context->isCoreFile($file) || $index === 1) echo 'application'; ?> call-stack-item"
-	data-line="<?php echo (int)($line - $begin); ?>">
+
+<li class="<?php if (!$context->isCoreFile($file) || $index === 1) echo 'application '; ?>call-stack-item">
 	<div class="element-wrap">
 		<div class="element">
 			<span class="item-number"><?php echo (int)$index; ?>.</span>
 			<span class="text"><?php if ($file !== null) echo 'in ' . $context->htmlEncode($file); ?></span>
-			<?php if ($method !== null): ?>
-				<span class="call">
-					<?php if ($file !== null) echo '&ndash;' ?>
-					<?php if ($class !== null) echo $context->addTypeLinks($class) . '→'; ?><?php echo $context->addTypeLinks($method . '()'); ?>
-				</span>
-			<?php endif; ?>
+			<?php if ($method !== null): ?><span class="call"><?php if ($file !== null) echo '&ndash;' ?><?php if ($class !== null) echo $context->addTypeLinks($class) . '→'; ?><?php echo $context->addTypeLinks($method . '()'); ?></span><?php endif; ?>
+			
 			<span class="at"><?php if ($line !== null) echo 'at line'; ?></span>
 			<span class="line"><?php if ($line !== null) echo (int)$line + 1; ?></span>
 		</div>
 	</div>
 	<?php if (!empty($lines)): ?>
-		<div class="code-wrap">
-			<div class="error-line"></div>
-			<?php for ($i = $begin; $i <= $end; ++$i): ?><div class="hover-line"></div><?php endfor; ?>
-			<div class="code">
-				<?php for ($i = $begin; $i <= $end; ++$i): ?><span class="lines-item"><?php echo (int)($i + 1); ?></span><?php endfor; ?>
-				<pre><?php
-					// fill empty lines with a whitespace to avoid rendering problems in opera
-					for ($i = $begin; $i <= $end; ++$i) {
-						echo (trim($lines[$i]) == '') ? " \n" : $context->htmlEncode($lines[$i]);
-					}
-				?></pre>
-			</div>
+	
+	<div class="code-wrap">
+		<div class="code">
+			<ol start="<?php echo $begin + 1; ?>">
+			<?php
+				$error_class = '';
+				for ($i = $begin; $i <= $end; ++$i) {
+					$error_class = $i == $line ? ' class="error"' : '';
+					$indent = $i == $begin ? "\t" : "\t\t\t\t"; // html indentation
+					echo "{$indent}<li{$error_class}><pre>" . $context->htmlEncode($lines[$i]) . "</pre></li>\n";
+				}
+			?>
+			</ol>
 		</div>
+	</div>
 	<?php endif; ?>
+	
 </li>
