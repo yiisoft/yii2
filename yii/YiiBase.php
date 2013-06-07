0c9952b1b0f033286db648f95cc0b0dafe781359
Qiang Xue <qiang.xue@gmail.com>
2013-06-07 11:02:26 -0400

    Added Jsonable support.

--- yii/YiiBase.php
+++ yii/YiiBase.php
@@ -611,6 +611,31 @@ class YiiBase
 			return is_array($params) ? strtr($message, $params) : $message;
 		}
 	}
+
+	/**
+	 * Configures an object with the initial property values.
+	 * @param object $object the object to be configured
+	 * @param array $properties the property initial values given in terms of name-value pairs.
+	 */
+	public static function configure($object, $properties)
+	{
+		foreach ($properties as $name => $value) {
+			$object->$name = $value;
+		}
+	}
+
+	/**
+	 * Returns the public member variables of an object.
+	 * This method is provided such that we can get the public member variables of an object.
+	 * It is different from "get_object_vars()" because the latter will return private
+	 * and protected variables if it is called within the object itself.
+	 * @param object $object the object to be handled
+	 * @return array the public member variables of the object
+	 */
+	public static function getObjectVars($object)
+	{
+		return get_object_vars($object);
+	}
 }
 
 YiiBase::$aliases = array(
---
+++ yii/base/Jsonable.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\base;
+
+/**
+ * Jsonable should be implemented by classes that need to be represented in JSON format.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+interface Jsonable
+{
+	/**
+	 * @return string the JSON representation of this object
+	 */
+	function toJson();
+}
