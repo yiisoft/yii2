9d9cbf92538a09995b17d9428fe155ccea663881
Qiang Xue <qiang.xue@gmail.com>
2013-06-12 21:21:19 -0400

    refactored cache dependency methods.

--- yii/caching/DbDependency.php
+++ yii/caching/DbDependency.php
@@ -52,10 +52,11 @@ class DbDependency extends Dependency
 	/**
 	 * Generates the data needed to determine if dependency has been changed.
 	 * This method returns the value of the global state.
-	 * @throws InvalidConfigException
+	 * @param Cache $cache the cache component that is currently evaluating this dependency
 	 * @return mixed the data needed to determine if dependency has been changed.
+	 * @throws InvalidConfigException if [[db]] is not a valid application component ID
 	 */
-	protected function generateDependencyData()
+	protected function generateDependencyData($cache)
 	{
 		$db = Yii::$app->getComponent($this->db);
 		if (!$db instanceof Connection) {
