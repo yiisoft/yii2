d4910ee4f556a23aadbe732d3ba9e6c02aa613c1
Qiang Xue <qiang.xue@gmail.com>
2013-06-14 17:23:37 -0400

    Put back Yii::getObjectVars() and fixed infinite loop in Object::toArray().

--- framework/yii/YiiBase.php
+++ framework/yii/YiiBase.php
@@ -624,6 +624,19 @@ class YiiBase
 			$object->$name = $value;
 		}
 	}
+
+	/**
+	 * Returns the public member variables of an object.
+	 * This method is provided such that we can get the public member variables of an object.
+	 * It is different from "get_object_vars()" because the latter will return private
+	 * and protected variables if it is called within the object itself.
+	 * @param object $object the object to be handled
+	 * @return array the public member variables of the object
+	 */
+	public static function getObjectVars($object)
+	{
+		return get_object_vars($object);
+	}
 }
 
 YiiBase::$aliases = array(
