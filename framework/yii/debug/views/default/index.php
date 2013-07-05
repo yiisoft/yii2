1e93fcf252f6565285d9b4de5c0a579f5ecabe08
Qiang Xue <qiang.xue@gmail.com>
2013-07-05 09:05:00 -0400

    debug toolbar WIP

--- framework/yii/debug/views/default/index.php
+++ framework/yii/debug/views/default/index.php
@@ -9,8 +9,21 @@ use yii\helpers\Html;
  * @var \yii\debug\Panel $activePanel
  */
 ?>
-<?php foreach ($panels as $panel): ?>
-<?php echo Html::a(Html::encode($panel->getName()), array('debug/default/index', 'tag' => $tag, 'panel' => $panel->id)); ?><br>
-<?php endforeach; ?>
+<div class="default-index">
+	<div class="span3">
+		<div class="well sidebar-nav">
+			<ul class="nav nav-list">
+			<?php
+				foreach ($panels as $panel) {
+					$link = Html::a(Html::encode($panel->getName()), array('debug/default/index', 'tag' => $tag, 'panel' => $panel->id));
+					echo Html::tag('li', $link, array('class' => $panel === $activePanel ? 'active' : null));
+				}
+			?>
+			</ul>
+		</div><!--/.well -->
+	</div><!--/span-->
+	<div class="span9">
+		<?php echo $activePanel->getDetail(); ?>
+	</div>
+</div>
 
-<?php echo $activePanel->getDetail(); ?>
