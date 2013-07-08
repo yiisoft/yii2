d120f14cc74c1f301e024323306cd8026148359e
Qiang Xue <qiang.xue@gmail.com>
2013-07-08 10:53:54 -0400

    finished debugger.

--- yii/debug/views/default/index.php
+++ yii/debug/views/default/index.php
@@ -4,11 +4,7 @@ use yii\helpers\Html;
 
 /**
  * @var \yii\base\View $this
- * @var array $meta
- * @var string $tag
  * @var array $manifest
- * @var \yii\debug\Panel[] $panels
- * @var \yii\debug\Panel $activePanel
  */
 
 $this->registerAssetBundle('yii/bootstrap/dropdown');
@@ -22,6 +18,34 @@ $this->title = 'Yii Debugger';
 					Yii Debugger
 				</div>
 			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<h1>Available Debug Data</h1>
+			<table class="table table-condensed table-bordered table-striped table-hover" style="table-layout: fixed;">
+				<thead>
+					<tr>
+						<th style="width: 120px;">Tag</th>
+						<th style="width: 160px;">Time</th>
+						<th style="width: 120px;">IP</th>
+						<th style="width: 60px;">Method</th>
+						<th>URL</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php foreach ($manifest as $tag => $data): ?>
+					<tr>
+						<td><?php echo Html::a($tag, array('view', 'tag' => $tag)); ?></td>
+						<td><?php echo date('Y-m-d h:i:sa', $data['time']); ?></td>
+						<td><?php echo $data['ip']; ?></td>
+						<td><?php echo $data['method']; ?></td>
+						<td><?php echo $data['url']; ?></td>
+					</tr>
+					<?php endforeach; ?>
+				</tbody>
+			</table>
 		</div>
 	</div>
 </div>
