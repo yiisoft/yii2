aa31b56aafaa9ac1ae84c80bcc7de7e6f6b0edca
Qiang Xue <qiang.xue@gmail.com>
2013-07-09 09:34:45 -0400

    Finished unit test of ActiveDataProvider.

--- tests/unit/framework/data/ActiveDataProviderTest.php
+++ tests/unit/framework/data/ActiveDataProviderTest.php
@@ -8,6 +8,7 @@
 namespace yiiunit\framework\data;
 
 use yii\data\ActiveDataProvider;
+use yii\db\Query;
 use yiiunit\data\ar\ActiveRecord;
 use yiiunit\framework\db\DatabaseTestCase;
 use yiiunit\data\ar\Order;
@@ -27,10 +28,12 @@ class ActiveDataProviderTest extends DatabaseTestCase
 	public function testActiveQuery()
 	{
 		$provider = new ActiveDataProvider(array(
-			'query' => Order::find(),
+			'query' => Order::find()->orderBy('id'),
 		));
 		$orders = $provider->getItems();
 		$this->assertEquals(3, count($orders));
+		$this->assertTrue($orders[0] instanceof Order);
+		$this->assertEquals(array(1, 2, 3), $provider->getKeys());
 
 		$provider = new ActiveDataProvider(array(
 			'query' => Order::find(),
@@ -44,7 +47,23 @@ class ActiveDataProviderTest extends DatabaseTestCase
 
 	public function testQuery()
 	{
+		$query = new Query;
+		$provider = new ActiveDataProvider(array(
+			'query' => $query->from('tbl_order')->orderBy('id'),
+		));
+		$orders = $provider->getItems();
+		$this->assertEquals(3, count($orders));
+		$this->assertTrue(is_array($orders[0]));
+		$this->assertEquals(array(0, 1, 2), $provider->getKeys());
 
-
+		$query = new Query;
+		$provider = new ActiveDataProvider(array(
+			'query' => $query->from('tbl_order'),
+			'pagination' => array(
+				'pageSize' => 2,
+			)
+		));
+		$orders = $provider->getItems();
+		$this->assertEquals(2, count($orders));
 	}
 }
