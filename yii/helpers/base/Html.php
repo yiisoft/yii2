d20ed5b60a8456272d28c0ff011eb18a989b3782
Qiang Xue <qiang.xue@gmail.com>
2013-07-17 20:12:19 -0400

    Fixes #632.

--- yii/helpers/base/Html.php
+++ yii/helpers/base/Html.php
@@ -1398,7 +1398,7 @@ class Html
 			return Yii::$app->getRequest()->getUrl();
 		} else {
 			$url = Yii::getAlias($url);
-			if ($url[0] === '/' || $url[0] === '#' || strpos($url, '://')) {
+			if ($url !== '' && ($url[0] === '/' || $url[0] === '#' || strpos($url, '://'))) {
 				return $url;
 			} else {
 				return Yii::$app->getRequest()->getBaseUrl() . '/' . $url;
