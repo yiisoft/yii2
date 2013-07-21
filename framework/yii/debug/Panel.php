fb31d8f6d855e245bceba64db16882464beb331a
Alexander Makarov <sam@rmcreative.ru>
2013-07-21 19:12:52 +0400

    added Panel phpdoc

--- framework/yii/debug/Panel.php
+++ framework/yii/debug/Panel.php
@@ -11,6 +11,9 @@ use Yii;
 use yii\base\Component;
 
 /**
+ * Panel is a base class for debugger panel. It defines how data should be collected,
+ * what should be dispalyed at debug toolbar and on debugger details view.
+ *
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
@@ -24,21 +27,36 @@ class Panel extends Component
 	public $module;
 	public $data;
 
+	/**
+	 * @return string name of the panel
+	 */
 	public function getName()
 	{
 		return '';
 	}
 
+	/**
+	 * @return string content that is displayed at debug toolbar
+	 */
 	public function getSummary()
 	{
 		return '';
 	}
 
+	/**
+	 * @return string content that is displayed in debugger detail view
+	 */
 	public function getDetail()
 	{
 		return '';
 	}
 
+	/**
+	 * Saves data to be later used in debugger detail view.
+	 * This method is called on every page where debugger is enabled.
+	 *
+	 * @return mixed data to be saved
+	 */
 	public function save()
 	{
 		return null;
@@ -49,6 +67,9 @@ class Panel extends Component
 		$this->data = $data;
 	}
 
+	/**
+	 * @return string URL pointing to panel detail view
+	 */
 	public function getUrl()
 	{
 		return Yii::$app->getUrlManager()->createUrl($this->module->id . '/default/view', array(
