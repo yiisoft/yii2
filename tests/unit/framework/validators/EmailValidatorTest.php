1ce7344e184bfe3e8c0eedd473aed4cc4e5806c6
Suralc <thesurwaveing@googlemail.com>
2013-07-31 21:14:14 +0200

    Additional tests in EmailValidator.

--- tests/unit/framework/validators/EmailValidatorTest.php
+++ tests/unit/framework/validators/EmailValidatorTest.php
@@ -1,5 +1,6 @@
 <?php
 namespace yiiunit\framework\validators;
+
 use yii\validators\EmailValidator;
 use yiiunit\TestCase;
 
@@ -24,5 +25,34 @@ class EmailValidatorTest extends TestCase
 
 		$this->assertTrue($validator->validateValue('5011@gmail.com'));
 		$this->assertFalse($validator->validateValue('test@example.com'));
+	}
+
+	public function testValidateAttribute()
+	{
+		$val = new EmailValidator();
+		$model = new FakedValidationModel();
+		$model->attr_email = '5011@gmail.com';
+		$val->validateAttribute($model, 'attr_email');
+		$this->assertFalse($model->hasErrors('attr_email'));
+	}
+
+	public function testValidateValueIdn()
+	{
+		if (!function_exists('idn_to_ascii')) {
+			$this->markTestSkipped('Intl extension required');
+			return;
+		}
+		$val = new EmailValidator(array('enableIDN' => true));
+		$this->assertTrue($val->validateValue('5011@example.com'));
+		$this->assertTrue($val->validateValue('example@äüößìà.de'));
+		$this->assertTrue($val->validateValue('example@xn--zcack7ayc9a.de'));
+	}
+
+	public function testValidateValueWithName()
+	{
+		$val = new EmailValidator(array('allowName' => true));
+		$this->assertTrue($val->validateValue('test@example.com'));
+		$this->assertTrue($val->validateValue('John Smith <john.smith@example.com>'));
+		$this->assertFalse($val->validateValue('John Smith <example.com>'));
 	}
 }
