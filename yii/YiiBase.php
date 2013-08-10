fa837191dc48607c7a0947ac0bb3eaf3882f43ad
Qiang Xue <qiang.xue@gmail.com>
2013-08-10 07:33:24 -0400

    Refactored the basic app.

--- yii/YiiBase.php
+++ yii/YiiBase.php
@@ -567,7 +567,7 @@ class YiiBase
 	 */
 	public static function powered()
 	{
-		return 'Powered by <a href="http://www.yiiframework.com/" rel="external">Yii Framework</a>.';
+		return 'Powered by <a href="http://www.yiiframework.com/" rel="external">Yii Framework</a>';
 	}
 
 	/**
--- yii/views/errorHandler/error.php
+++ yii/views/errorHandler/error.php
@@ -3,14 +3,32 @@
  * @var \Exception $exception
  * @var \yii\base\ErrorHandler $handler
  */
-$title = $handler->htmlEncode($exception instanceof \yii\base\Exception ? $exception->getName() : get_class($exception));
+if ($exception instanceof \yii\web\HttpException) {
+	$code = $exception->statusCode;
+} else {
+	$code = $exception->getCode();
+}
+if ($exception instanceof \yii\base\Exception) {
+	$name = $exception->getName();
+} else {
+	$name = 'Error';
+}
+if ($code) {
+	$name .= " (#$code)";
+}
+
+if ($exception instanceof \yii\base\UserException) {
+	$message = $exception->getMessage();
+} else {
+	$message = 'An internal server error occurred.';
+}
 ?>
 <?php if (method_exists($this, 'beginPage')) $this->beginPage(); ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8" />
-	<title><?php echo $title?></title>
+	<title><?php echo $handler->htmlEncode($name); ?></title>
 
 	<style>
 		body {
@@ -51,8 +69,8 @@ $title = $handler->htmlEncode($exception instanceof \yii\base\Exception ? $excep
 </head>
 
 <body>
-	<h1><?php echo $title?></h1>
-	<h2><?php echo nl2br($handler->htmlEncode($exception->getMessage()))?></h2>
+	<h1><?php echo $handler->htmlEncode($name); ?></h1>
+	<h2><?php echo nl2br($handler->htmlEncode($message)); ?></h2>
 	<p>
 		The above error occurred while the Web server was processing your request.
 	</p>
@@ -60,7 +78,7 @@ $title = $handler->htmlEncode($exception instanceof \yii\base\Exception ? $excep
 		Please contact us if you think this is a server error. Thank you.
 	</p>
 	<div class="version">
-		<?php echo date('Y-m-d H:i:s', time())?>
+		<?php echo date('Y-m-d H:i:s', time()); ?>
 	</div>
 	<?php if (method_exists($this, 'endBody')) $this->endBody(); // to allow injecting code into body (mostly by Yii Debug Toolbar) ?>
 </body>
