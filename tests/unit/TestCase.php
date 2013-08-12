533b1eeb6e7bb7420a4351bbee8c32628f952a72
Alexander Makarov <sam@rmcreative.ru>
2013-08-13 02:47:30 +0400

    fixed phpdoc

--- tests/unit/TestCase.php
+++ tests/unit/TestCase.php
@@ -37,6 +37,7 @@ abstract class TestCase extends \yii\test\TestCase
 	 * Populates Yii::$app with a new application
 	 * The application will be destroyed on tearDown() automatically.
 	 * @param array $config The application configuration, if needed
+	 * @param string $appClass name of the application class to create
 	 */
 	protected function mockApplication($config = array(), $appClass = '\yii\console\Application')
 	{
