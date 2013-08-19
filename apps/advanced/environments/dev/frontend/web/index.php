829c10f4fa30428bf2390d907926df64247b49e3
Qiang Xue <qiang.xue@gmail.com>
2013-08-18 20:33:11 -0400

    Fixed yii autoload precedence.

--- apps/advanced/environments/dev/frontend/web/index.php
+++ apps/advanced/environments/dev/frontend/web/index.php
@@ -3,8 +3,8 @@
 // comment out the following line to disable debug mode
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 
-require(__DIR__ . '/../../vendor/yiisoft/yii2/yii/Yii.php');
 require(__DIR__ . '/../../vendor/autoload.php');
+require(__DIR__ . '/../../vendor/yiisoft/yii2/yii/Yii.php');
 Yii::importNamespaces(require(__DIR__ . '/../../vendor/composer/autoload_namespaces.php'));
 
 $config = yii\helpers\ArrayHelper::merge(
--- apps/basic/web/index.php
+++ apps/basic/web/index.php
@@ -4,8 +4,8 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require(__DIR__ . '/../vendor/yiisoft/yii2/yii/Yii.php');
 require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/yii/Yii.php');
 Yii::importNamespaces(require(__DIR__ . '/../vendor/composer/autoload_namespaces.php'));
 
 $config = require(__DIR__ . '/../config/web.php');
