c189e5ad3f4e7fbdd4f1bc7a2edda1a2b54813f6
Suralc <thesurwaveing@googlemail.com>
2013-08-24 00:05:27 +0200

    UniqueValidator test.

---
+++ tests/unit/framework/validators/UniqueValidatorDriverTests/UniqueValidatorSQliteTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace yiiunit\framework\validators\UniqueValidatorDriverTests;
+
+
+use yiiunit\framework\validators\UniqueValidatorTest;
+
+class UniqueValidatorSQliteTest extends UniqueValidatorTest
+{
+	protected $driverName = 'sqlite';
+}
