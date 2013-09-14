a1af321d38d3df38e6068b64674e4477c32bdea2
Carsten Brandt <mail@cebe.cc>
2013-09-14 12:56:10 +0200

    fixed typo in apc install script

--- tests/unit/data/travis/apc-setup.sh
+++ tests/unit/data/travis/apc-setup.sh
@@ -1,2 +1,2 @@
-echo "extension = <extension>.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+echo "extension = apc.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "apc.enable_cli = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
