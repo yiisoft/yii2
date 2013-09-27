558f499439fe9adfd9416dfde8944181ae35c3aa
Alexander Makarov <sam@rmcreative.ru>
2013-09-28 02:48:54 +0400

    Renamed YiiBase to AbstractYii

--- framework/yii/Yii.php
+++ framework/yii/Yii.php
@@ -12,13 +12,13 @@ require(__DIR__ . '/YiiBase.php');
 /**
  * Yii is a helper class serving common framework functionalities.
  *
- * It extends from [[YiiBase]] which provides the actual implementation.
- * By writing your own Yii class, you can customize some functionalities of [[YiiBase]].
+ * It extends from [[AbstractYii]] which provides the actual implementation.
+ * By writing your own Yii class, you can customize some functionalities of [[AbstractYii]].
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class Yii extends \yii\YiiBase
+class Yii extends \yii\AbstractYii
 {
 }
 
