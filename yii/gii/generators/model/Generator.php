e3359beb55a238746f498575cc2ddd20ae3f8354
Qiang Xue <qiang.xue@gmail.com>
2013-10-03 11:01:52 -0400

    Fixes #916: generation of class name from table name has problem.

--- yii/gii/generators/model/Generator.php
+++ yii/gii/generators/model/Generator.php
@@ -514,7 +514,8 @@ class Generator extends \yii\gii\Generator
 			$patterns[] = '/^' . str_replace('*', '(\w+)', $pattern) . '$/';
 		}
 		if (!empty($db->tablePrefix)) {
-			$patterns[] = "/^{$db->tablePrefix}(.*?)|(.*?){$db->tablePrefix}$/";
+			$patterns[] = "/^{$db->tablePrefix}(.*?)$/";
+			$patterns[] = "/^(.*?){$db->tablePrefix}$/";
 		} else {
 			$patterns[] = "/^tbl_(.*?)$/";
 		}
@@ -523,6 +524,7 @@ class Generator extends \yii\gii\Generator
 		foreach ($patterns as $pattern) {
 			if (preg_match($pattern, $tableName, $matches)) {
 				$className = $matches[1];
+				break;
 			}
 		}
 		return $this->_classNames[$tableName] = Inflector::id2camel($className, '_');
