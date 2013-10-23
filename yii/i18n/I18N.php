d62863a534f769f5171a8a077a0d2fd880f8c0d1
Carsten Brandt <mail@cebe.cc>
2013-10-23 20:48:04 +0200

    typo ITU -> ICU
    they are not responsible for i18n ;)

--- yii/i18n/I18N.php
+++ yii/i18n/I18N.php
@@ -64,7 +64,7 @@ class I18N extends Component
 	 * Translates a message to the specified language.
 	 *
 	 * After translation the message will be formatted using [[MessageFormatter]] if it contains
-	 * ITU message format and `$params` are not empty.
+	 * ICU message format and `$params` are not empty.
 	 *
 	 * @param string $category the message category.
 	 * @param string $message the message to be translated.
