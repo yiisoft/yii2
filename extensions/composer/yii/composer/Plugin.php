0c517ce969829d3f5beafb0fb3230a7a30185667
Qiang Xue <qiang.xue@gmail.com>
2013-10-22 21:06:17 -0400

    Renamed InstallerPlugin to Plugin

--- extensions/composer/yii/composer/Plugin.php
+++ extensions/composer/yii/composer/Plugin.php
@@ -12,12 +12,12 @@ use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
 /**
- * InstallerPlugin is the composer plugin that registers the Yii composer installer.
+ * Plugin is the composer plugin that registers the Yii composer installer.
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class InstallerPlugin implements PluginInterface
+class Plugin implements PluginInterface
 {
 	/**
 	 * @inheritdoc
