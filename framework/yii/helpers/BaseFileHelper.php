40d46f4da48d204737216e506365d10d09cb1154
Christian Nadolle <git@hqx.de>
2013-10-30 10:39:54 +0100

    Find viewfile using extensions from configured viewRenderers

--- framework/yii/helpers/BaseFileHelper.php
+++ framework/yii/helpers/BaseFileHelper.php
@@ -54,10 +54,11 @@ class BaseFileHelper
 	 * If not set, the value of [[\yii\base\Application::language]] will be used.
 	 * @param string $sourceLanguage the language that the original file is in.
 	 * If not set, the value of [[\yii\base\Application::sourceLanguage]] will be used.
+	 * @param array $possibleExtensions optional array of possible fileExtensions to look for
 	 * @return string the matching localized file, or the original file if the localized version is not found.
 	 * If the target and the source language codes are the same, the original file will be returned.
 	 */
-	public static function localize($file, $language = null, $sourceLanguage = null)
+	public static function localize($file, $language = null, $sourceLanguage = null, $possibleExtensions = [])
 	{
 		if ($language === null) {
 			$language = Yii::$app->language;
@@ -69,7 +70,7 @@ class BaseFileHelper
 			return $file;
 		}
 		$desiredFile = dirname($file) . DIRECTORY_SEPARATOR . $sourceLanguage . DIRECTORY_SEPARATOR . basename($file);
-		return is_file($desiredFile) ? $desiredFile : $file;
+		return ($firstExistingFile = FileHelper::findFileByExtensions($desiredFile, $possibleExtensions)) ? $firstExistingFile : $file;
 	}
 
 	/**
@@ -266,6 +267,24 @@ class BaseFileHelper
 		}
 		closedir($handle);
 		return $list;
+	}
+
+	/**
+	 * find first existing file by baseName and an array of possible extensions
+	 *
+	 * @param $baseName string
+	 * @param $extensions array
+	 * @return string first found file or null
+	 */
+	public static function findFileByExtensions($baseName, $extensions)
+	{
+		if (is_file($baseName)) return $baseName;
+		foreach($extensions as $ext) {
+			if (is_file($testFile = $baseName . '.' . $ext)) {
+				return $testFile;
+			}
+		}
+		return null;
 	}
 
 	/**
