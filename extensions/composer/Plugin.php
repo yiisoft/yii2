513037d97d29499d26b0c1b316ee75fde2271d7a
Qiang Xue <qiang.xue@gmail.com>
2013-11-02 23:27:26 -0400

    bug fix.

--- extensions/composer/Plugin.php
+++ extensions/composer/Plugin.php
@@ -29,7 +29,7 @@ class Plugin implements PluginInterface
 		$file = rtrim($composer->getConfig()->get('vendor-dir'), '/') . '/yiisoft/extensions.php';
 		if (!is_file($file)) {
 			@mkdir(dirname($file));
-			file_put_contents($file, "<?php\nreturn [];\n");
+			file_put_contents($file, "<?php\n\nreturn [];\n");
 		}
 	}
 }
