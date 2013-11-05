3cb61cac179c42d614d74f16407cb9d7ee522cf7
Qiang Xue <qiang.xue@gmail.com>
2013-11-05 18:56:33 -0500

    fixed jqueryasset path.

--- framework/yii/web/JqueryAsset.php
+++ framework/yii/web/JqueryAsset.php
@@ -15,7 +15,7 @@ namespace yii\web;
  */
 class JqueryAsset extends AssetBundle
 {
-	public $sourcePath = '@vendor/yiisoft/jquery';
+	public $sourcePath = '@vendor/frameworks/jquery';
 	public $js = [
 		'jquery.js',
 	];
