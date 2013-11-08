bf40015b70a179a45bb420ed776bc822ea1268b9
Qiang Xue <qiang.xue@gmail.com>
2013-11-07 22:33:57 -0500

    Refactored mail.

--- yii/mail/MessageInterface.php
+++ yii/mail/MessageInterface.php
@@ -8,17 +8,20 @@
 namespace yii\mail;
 
 /**
- * MessageInterface is an interface, which email message should apply.
- * Together with application component, which matches the [[MailerInterface]],
- * it introduces following mail sending syntax:
+ * MessageInterface is the interface that should be implemented by mail message classes.
  *
- * ~~~php
+ * A message represents the settings and content of an email, such as the sender, recipient,
+ * subject, body, etc.
+ *
+ * Messages are sent by a [[MailerInterface||mailer]], like the following,
+ *
+ * ~~~
  * Yii::$app->mail->compose()
- *     ->from('from@domain.com')
- *     ->to('to@domain.com')
- *     ->subject('Message Subject')
- *     ->textBody('Plain text content')
- *     ->htmlBody('<b>HTML content</b>')
+ *     ->setFrom('from@domain.com')
+ *     ->setTo($form->email)
+ *     ->setSubject($form->subject)
+ *     ->setTextBody('Plain text content')
+ *     ->setHtmlBody('<b>HTML content</b>')
  *     ->send();
  * ~~~
  *
@@ -30,72 +33,124 @@ namespace yii\mail;
 interface MessageInterface
 {
 	/**
-	 * Set the character set of this message.
+	 * Returns the character set of this message.
+	 * @return string the character set of this message.
+	 */
+	public function getCharset();
+
+	/**
+	 * Sets the character set of this message.
 	 * @param string $charset character set name.
 	 * @return static self reference.
 	 */
-	public function charset($charset);
+	public function setCharset($charset);
 
 	/**
-	 * Sets message sender.
+	 * Returns the message sender.
+	 * @return string the sender
+	 */
+	public function getFrom();
+
+	/**
+	 * Sets the message sender.
 	 * @param string|array $from sender email address.
 	 * You may pass an array of addresses if this message is from multiple people.
 	 * You may also specify sender name in addition to email address using format:
-	 * [email => name].
+	 * `[email => name]`.
 	 * @return static self reference.
 	 */
-	public function from($from);
+	public function setFrom($from);
 
 	/**
-	 * Sets message receiver.
+	 * Returns the message recipient(s).
+	 * @return array the message recipients
+	 */
+	public function getTo();
+
+	/**
+	 * Sets the message recipient(s).
 	 * @param string|array $to receiver email address.
 	 * You may pass an array of addresses if multiple recipients should receive this message.
 	 * You may also specify receiver name in addition to email address using format:
-	 * [email => name].
+	 * `[email => name]`.
 	 * @return static self reference.
 	 */
-	public function to($to);
+	public function setTo($to);
 
 	/**
-	 * Set the Cc (additional copy receiver) addresses of this message.
+	 * Returns the reply-to address of this message.
+	 * @return string the reply-to address of this message.
+	 */
+	public function getReplyTo();
+
+	/**
+	 * Sets the reply-to address of this message.
+	 * @param string|array $replyTo the reply-to address.
+	 * You may pass an array of addresses if this message should be replied to multiple people.
+	 * You may also specify reply-to name in addition to email address using format:
+	 * `[email => name]`.
+	 * @return static self reference.
+	 */
+	public function setReplyTo($replyTo);
+
+	/**
+	 * Returns the Cc (additional copy receiver) addresses of this message.
+	 * @return array the Cc (additional copy receiver) addresses of this message.
+	 */
+	public function getCc();
+
+	/**
+	 * Sets the Cc (additional copy receiver) addresses of this message.
 	 * @param string|array $cc copy receiver email address.
 	 * You may pass an array of addresses if multiple recipients should receive this message.
 	 * You may also specify receiver name in addition to email address using format:
-	 * [email => name].
+	 * `[email => name]`.
 	 * @return static self reference.
 	 */
-	public function cc($cc);
+	public function setCc($cc);
 
 	/**
-	 * Set the Bcc (hidden copy receiver) addresses of this message.
+	 * Returns the Bcc (hidden copy receiver) addresses of this message.
+	 * @return array the Bcc (hidden copy receiver) addresses of this message.
+	 */
+	public function getBcc();
+
+	/**
+	 * Sets the Bcc (hidden copy receiver) addresses of this message.
 	 * @param string|array $bcc hidden copy receiver email address.
 	 * You may pass an array of addresses if multiple recipients should receive this message.
 	 * You may also specify receiver name in addition to email address using format:
-	 * [email => name].
+	 * `[email => name]`.
 	 * @return static self reference.
 	 */
-	public function bcc($bcc);
+	public function setBcc($bcc);
 
 	/**
-	 * Sets message subject.
+	 * Returns the message subject.
+	 * @return string the message subject
+	 */
+	public function getSubject();
+
+	/**
+	 * Sets the message subject.
 	 * @param string $subject message subject
 	 * @return static self reference.
 	 */
-	public function subject($subject);
+	public function setSubject($subject);
 
 	/**
 	 * Sets message plain text content.
 	 * @param string $text message plain text content.
 	 * @return static self reference.
 	 */
-	public function textBody($text);
+	public function setTextBody($text);
 
 	/**
 	 * Sets message HTML content.
 	 * @param string $html message HTML content.
 	 * @return static self reference.
 	 */
-	public function htmlBody($html);
+	public function setHtmlBody($html);
 
 	/**
 	 * Attaches existing file to the email message.
@@ -149,7 +204,7 @@ interface MessageInterface
 
 	/**
 	 * Sends this email message.
-	 * @return boolean success.
+	 * @return boolean whether this message is sent successfully.
 	 */
 	public function send();
 
