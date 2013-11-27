2643de1db8560e698fd8cd3ff13be5eb81bfa767
Alexander Makarov <sam@rmcreative.ru>
2013-11-28 01:19:28 +0400

    Updated advanced application mail layout to contain layout markers

--- apps/advanced/common/mails/layouts/html.php
+++ apps/advanced/common/mails/layouts/html.php
@@ -1,10 +1,24 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<?php
+use yii\helpers\Html;
+use yii\mail\BaseMessage;
+
+/**
+ * @var \yii\web\View $this
+ * @var BaseMessage $message
+ */
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=<?= Yii::$app->charset ?>" />
+	<title><?= Html::encode($this->title) ?></title>
+	<?php $this->head() ?>
 </head>
 <body>
+	<?php $this->beginBody() ?>
 	<?= $message ?>
+	<?php $this->endBody() ?>
 </body>
 </html>
+<?php $this->endPage() ?>
