814e7966e6a2e3fb5cb13335b3cad13bacd329bd
Qiang Xue <qiang.xue@gmail.com>
2013-11-28 09:04:16 -0500

    Use backquotes to quote column and table names for sqlite (related with #1318)

--- yii/db/sqlite/Schema.php
+++ yii/db/sqlite/Schema.php
@@ -50,6 +50,28 @@ class Schema extends \yii\db\Schema
 	];
 
 	/**
+	 * Quotes a table name for use in a query.
+	 * A simple table name has no schema prefix.
+	 * @param string $name table name
+	 * @return string the properly quoted table name
+	 */
+	public function quoteSimpleTableName($name)
+	{
+		return strpos($name, "`") !== false ? $name : "`" . $name . "`";
+	}
+
+	/**
+	 * Quotes a column name for use in a query.
+	 * A simple column name has no prefix.
+	 * @param string $name column name
+	 * @return string the properly quoted column name
+	 */
+	public function quoteSimpleColumnName($name)
+	{
+		return strpos($name, '`') !== false || $name === '*' ? $name : '`' . $name . '`';
+	}
+
+	/**
 	 * Creates a query builder for the MySQL database.
 	 * This method may be overridden by child classes to create a DBMS-specific query builder.
 	 * @return QueryBuilder query builder instance
