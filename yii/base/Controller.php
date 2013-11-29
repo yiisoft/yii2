60f4bb35de9e1602813d5f08ac4c95c88dd19cef
Qiang Xue <qiang.xue@gmail.com>
2013-11-29 12:31:09 -0500

    Fixes #1296: stricter check of dashes in route.

--- yii/base/Controller.php
+++ yii/base/Controller.php
@@ -194,7 +194,7 @@ class Controller extends Component implements ViewContextInterface
 		$actionMap = $this->actions();
 		if (isset($actionMap[$id])) {
 			return Yii::createObject($actionMap[$id], $id, $this);
-		} elseif (preg_match('/^[a-z0-9\\-_]+$/', $id)) {
+		} elseif (preg_match('/^[a-z0-9\\-_]+$/', $id) && strpos($id, '--') === false && trim($id, '-') === $id) {
 			$methodName = 'action' . str_replace(' ', '', ucwords(implode(' ', explode('-', $id))));
 			if (method_exists($this, $methodName)) {
 				$method = new \ReflectionMethod($this, $methodName);
