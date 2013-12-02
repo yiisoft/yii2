1f9660048cccc6e3d65779af413f54aab5f7ebfd
Mark <mark.github@yandex.ru>
2013-12-02 15:25:21 +0400

    tests modified, bootstrap added

---
+++ apps/basic/tests/unit/models/UserTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace tests\unit\models;
+
+#use yii\test\DbTestTrait;
+
+class UserTest extends \PHPUnit_Framework_TestCase
+{
+
+	use DbTestTrait;
+
+	protected function setUp()
+	{
+		/*
+		 * you can load fixtures in this way
+		 *
+		$this->loadFixtures([
+			'tbl_user',
+		]);
+		 */
+	}
+
+	protected function tearDown()
+	{
+	}
+
+}
