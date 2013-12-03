22b956072309def554e3e90647adabf6d2683289
Mark <mark.github@yandex.ru>
2013-12-03 11:07:36 +0400

    added testcase for unit tests, bootstrap files improved, tests inherited from base testcase

--- apps/basic/tests/_bootstrap.php
+++ apps/basic/tests/_bootstrap.php
@@ -1,1 +1,3 @@
 <?php
+
+require_once(__DIR__.'/yii_bootstrap.php');
--- apps/basic/tests/unit/models/ContactFormTest.php
+++ apps/basic/tests/unit/models/ContactFormTest.php
@@ -2,15 +2,7 @@
 
 namespace tests\unit\models;
 
-class ContactFormTest extends \PHPUnit_Framework_TestCase
+class ContactFormTest extends \tests\_helpers\TestCase
 {
-
-	protected function setUp()
-	{
-	}
-
-	protected function tearDown()
-	{
-	}
 
 }
