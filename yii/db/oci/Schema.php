e53ca14a8def7e9e7bfd61ab68d4b995c67b44a2
Qiang Xue <qiang.xue@gmail.com>
2013-12-09 23:11:56 -0500

    clean up oci driver.

--- yii/db/oci/Schema.php
+++ yii/db/oci/Schema.php
@@ -1,137 +1,115 @@
 <?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
 namespace yii\db\oci;
 
 use yii\db\TableSchema;
-use yii\db\oci\ColumnSchema;
+use yii\db\ColumnSchema;
 
 /**
- * Schema is the class for retrieving metadata information from an Oracle database.
- * 
+ * Schema is the class for retrieving metadata from an Oracle database
+ *
+ * @todo mapping from physical types to abstract types
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
  */
-
 class Schema extends \yii\db\Schema
 {
+	private $_defaultSchema;
 
-    private $_defaultSchema = '';
+	/**
+	 * @inheritdoc
+	 */
+	public function quoteSimpleTableName($name)
+	{
+		return '"' . $name . '"';
+	}
 
-    public function createQueryBuilder()
-    {
-        return new QueryBuilder($this->db);
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function quoteSimpleColumnName($name)
+	{
+		return '"' . $name . '"';
+	}
 
-    public function quoteTableName($name)
-    {
-        if (strpos($name, '.') === false) {
-            return $this->quoteSimpleTableName($name);
-        }
-        $parts = explode('.', $name);
-        foreach ($parts as $i => $part) {
-            $parts[$i] = $this->quoteSimpleTableName($part);
-        }
-        return implode('.', $parts);
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function createQueryBuilder()
+	{
+		return new QueryBuilder($this->db);
+	}
 
-    public function quoteSimpleTableName($name)
-    {
-        return '"' . $name . '"';
-        // return $name;
-    }
+	/**
+	 * @inheritdoc
+	 */
+	public function loadTableSchema($name)
+	{
+		$table = new TableSchema();
+		$this->resolveTableNames($table, $name);
 
-    public function quoteColumnName($name)
-    {
-        if (($pos = strrpos($name, '.')) !== false) {
-            $prefix = $this->quoteTableName(substr($name, 0, $pos)) . '.';
-            $name = substr($name, $pos + 1);
-        } else {
-            $prefix = '';
-        }
-        return $prefix . ($name === '*' ? $name : $this->quoteSimpleColumnName($name));
-    }
+		if ($this->findColumns($table)) {
+			$this->findConstraints($table);
+			return $table;
+		} else {
+			return null;
+		}
+	}
 
-    public function quoteSimpleColumnName($name)
-    {
-        return '"' . $name . '"';
-        // return $name;
-    }
+	/**
+	 * Resolves the table name and schema name (if any).
+	 *
+	 * @param TableSchema $table the table metadata object
+	 * @param string $name the table name
+	 */
+	protected function resolveTableNames($table, $name)
+	{
+		$parts = explode('.', str_replace('"', '', $name));
+		if (isset($parts[1])) {
+			$table->schemaName = $parts[0];
+			$table->name = $parts[1];
+		} else {
+			$table->schemaName = $this->getDefaultSchema();
+			$table->name = $parts[0];
+		}
+	}
 
-    public function loadTableSchema($name)
-    {
-        $table = new TableSchema();
-        $this->resolveTableNames($table, $name);
-        // $this->findPrimaryKeys($table);
-        if ($this->findColumns($table)) {
-            // $this->findForeignKeys($table);
-            $this->findConstraints($table);
-            return $table;
-        }
-    }
+	/**
+	 * @return string default schema.
+	 */
+	public function getDefaultSchema()
+	{
+		if ($this->_defaultSchema === null) {
+			$this->setDefaultSchema(strtoupper($this->db->username));
+		}
+		return $this->_defaultSchema;
+	}
 
-    /**
-     * Resolves the table name and schema name (if any).
-     *
-     * @param TableSchema $table
-     *            the table metadata object
-     * @param string $name
-     *            the table name
-     */
-    protected function resolveTableNames($table, $name)
-    {
-        $parts = explode('.', str_replace('"', '', $name));
-        if (isset($parts[1])) {
-            $table->schemaName = $parts[0];
-            $table->name = $parts[1];
-        } else {
-            $table->name = $parts[0];
-        }
-        
-        if ($table->schemaName === null) {
-            $table->schemaName = $this->getDefaultSchema();
-        }
-    }
+	/**
+	 * @param string $schema default schema.
+	 */
+	public function setDefaultSchema($schema)
+	{
+		$this->_defaultSchema = $schema;
+	}
 
-    /**
-     *
-     * @return string default schema.
-     */
-    public function getDefaultSchema()
-    {
-        if (! strlen($this->_defaultSchema)) {
-            $this->setDefaultSchema(strtoupper($this->db->username));
-        }
-        
-        return $this->_defaultSchema;
-    }
+	/**
+	 * Collects the table column metadata.
+	 * @param TableSchema $table the table schema
+	 * @return boolean whether the table exists
+	 */
+	protected function findColumns($table)
+	{
+		$schemaName = $table->schemaName;
+		$tableName = $table->name;
 
-    /**
-     *
-     * @param string $schema
-     *            default schema.
-     */
-    public function setDefaultSchema($schema)
-    {
-        $this->_defaultSchema = $schema;
-    }
-
-    public function getLastInsertID($sequenceName = '')
-    {
-        if ($this->db->isActive) {
-            $sql = "select {$sequenceName}.currval from dual";
-            return $this->db->createCommand($sql)->queryScalar();
-        } else {
-            throw new InvalidCallException('DB Connection is not active.');
-        }
-    }
-
-    /**
-     * Collects the table column metadata.
-     *     
-     */
-    protected function findColumns($table)
-    {
-        $schemaName = $table->schemaName;
-        $tableName = $table->name;
-        
-        $sql = <<<EOD
+		$sql = <<<EOD
 SELECT a.column_name, a.data_type ||
     case
         when data_precision is not null
@@ -160,65 +138,48 @@ WHERE
 	and b.object_name = '{$tableName}'
 ORDER by a.column_id
 EOD;
-        
-        try {
-            $columns = $this->db->createCommand($sql)->queryAll();
-        } catch (\Exception $e) {
-            return false;
-        }
-        
-        foreach ($columns as $column) {
-            $c = $this->createColumn($column);
-            
-            $table->columns[$c->name] = $c;
-            if ($c->isPrimaryKey) {
-                if ($table->primaryKey === null) {
-                    $table->primaryKey = $c->name;
-                } elseif (is_string($table->primaryKey)) {
-                    $table->primaryKey = array(
-                        $table->primaryKey,
-                        $c->name
-                    );
-                } else {
-                    $table->primaryKey[] = $c->name;
-                }
-                $sql = "select sequence_name
-from user_tables tabs
-join user_triggers trigs
-  on trigs.table_name = tabs.table_name
-join user_dependencies deps
-  on deps.name = trigs.trigger_name
-join user_sequences seqs
-  on seqs.sequence_name = deps.referenced_name
-where tabs.table_name = '{$tableName}'
- and trigs.triggering_event = 'INSERT'";
-                try {
-                    $seqName = $this->db->createCommand($sql)->queryScalar();
-                } catch (\Exception $e) {
-                    return false;
-                }
-                $table->sequenceName = $seqName;
-                $c->autoIncrement = true;
-            }
-        }
-        return true;
-    }
 
-    protected function createColumn($column)
-    {
-        $c = new ColumnSchema();
-        $c->name = $column['COLUMN_NAME'];
-        $c->allowNull = $column['NULLABLE'] === 'Y';
-        $c->isPrimaryKey = strpos($column['KEY'], 'P') !== false;
-        $c->extract($column['DATA_TYPE'], $column['DATA_DEFAULT']);
-        $c->comment = $column['COLUMN_COMMENT'] === null ? '' : $column['COLUMN_COMMENT'];
-        
-        return $c;
-    }
+		try {
+			$columns = $this->db->createCommand($sql)->queryAll();
+		} catch (\Exception $e) {
+			return false;
+		}
 
-    protected function findConstraints($table)
-    {
-        $sql = <<<EOD
+		foreach ($columns as $column) {
+			$c = $this->createColumn($column);
+			$table->columns[$c->name] = $c;
+			if ($c->isPrimaryKey) {
+				$table->primaryKey[] = $c->name;
+				$table->sequenceName = '';
+				$c->autoIncrement = true;
+			}
+		}
+		return true;
+	}
+
+	protected function createColumn($column)
+	{
+		$c = new ColumnSchema();
+		$c->name = $column['COLUMN_NAME'];
+		$c->allowNull = $column['NULLABLE'] === 'Y';
+		$c->isPrimaryKey = strpos($column['KEY'], 'P') !== false;
+		$c->comment = $column['COLUMN_COMMENT'] === null ? '' : $column['COLUMN_COMMENT'];
+
+		$this->extractColumnType($c, $column['DATA_TYPE']);
+		$this->extractColumnSize($c, $column['DATA_TYPE']);
+
+		if (stripos($column['DATA_DEFAULT'], 'timestamp') !== false) {
+			$c->defaultValue = null;
+		} else {
+			$c->defaultValue = $c->typecast($column['DATA_DEFAULT']);
+		}
+
+		return $c;
+	}
+
+	protected function findConstraints($table)
+	{
+		$sql = <<<EOD
 		SELECT D.constraint_type as CONSTRAINT_TYPE, C.COLUMN_NAME, C.position, D.r_constraint_name,
                 E.table_name as table_ref, f.column_name as column_ref,
             	C.table_name
@@ -231,134 +192,82 @@ where tabs.table_name = '{$tableName}'
            and D.constraint_type <> 'P'
         order by d.constraint_name, c.position
 EOD;
-        $command = $this->db->createCommand($sql);
-        foreach ($command->queryAll() as $row) {
-            if ($row['CONSTRAINT_TYPE'] === 'R') { // foreign key
-                $name = $row["COLUMN_NAME"];
-                $table->foreignKeys[$name] = array(
-                    $row["TABLE_REF"],
-                    $row["COLUMN_REF"]
-                );
-                /*
-                 * if (isset($table->columns[$name])) { $table->columns[$name]->isForeignKey = true; }
-                 */
-            }
-        }
-    }
+		$command = $this->db->createCommand($sql);
+		foreach ($command->queryAll() as $row) {
+			if ($row['CONSTRAINT_TYPE'] === 'R') {
+				$name = $row["COLUMN_NAME"];
+				$table->foreignKeys[$name] = [$row["TABLE_REF"], $row["COLUMN_REF"]];
+			}
+		}
+	}
 
-    /**
-     * Returns all table names in the database.
-     * 
-     * @param string $schema
-     *            the schema of the tables. Defaults to empty string, meaning the current or default schema.
-     *            If not empty, the returned table names will be prefixed with the schema name.
-     * @return array all table names in the database.
-     */
-    protected function findTableNames($schema = '')
-    {
-        if ($schema === '') {
-            $sql = <<<EOD
+	/**
+	 * @inheritdoc
+	 */
+	protected function findTableNames($schema = '')
+	{
+		if ($schema === '') {
+			$sql = <<<EOD
 SELECT table_name, '{$schema}' as table_schema FROM user_tables
 EOD;
-            $command = $this->db->createCommand($sql);
-        } else {
-            $sql = <<<EOD
+			$command = $this->db->createCommand($sql);
+		} else {
+			$sql = <<<EOD
 SELECT object_name as table_name, owner as table_schema FROM all_objects
 WHERE object_type = 'TABLE' AND owner=:schema
 EOD;
-            $command = $this->db > createCommand($sql);
-            $command->bindParam(':schema', $schema);
-        }
-        
-        $rows = $command->queryAll();
-        $names = array();
-        foreach ($rows as $row) {
-            if ($schema === $this->getDefaultSchema() || $schema === '') {
-                $names[] = $row['TABLE_NAME'];
-            } else {
-                $names[] = $row['TABLE_SCHEMA'] . '.' . $row['TABLE_NAME'];
-            }
-        }
-        return $names;
-    }
+			$command = $this->db->createCommand($sql);
+			$command->bindParam(':schema', $schema);
+		}
 
-    /**
-     * Builds a SQL statement for renaming a DB table.
-     * 
-     * @param string $table
-     *            the table to be renamed. The name will be properly quoted by the method.
-     * @param string $newName
-     *            the new table name. The name will be properly quoted by the method.
-     * @return string the SQL statement for renaming a DB table.
-     * @since 1.1.6
-     */
-    public function renameTable($table, $newName)
-    {
-        return 'ALTER TABLE ' . $this->quoteTableName($table) . ' RENAME TO ' . $this->quoteTableName($newName);
-    }
+		$rows = $command->queryAll();
+		$names = array();
+		foreach ($rows as $row) {
+			$names[] = $row['TABLE_NAME'];
+		}
+		return $names;
+	}
 
-    /**
-     * Builds a SQL statement for changing the definition of a column.
-     * 
-     * @param string $table
-     *            the table whose column is to be changed. The table name will be properly quoted by the method.
-     * @param string $column
-     *            the name of the column to be changed. The name will be properly quoted by the method.
-     * @param string $type
-     *            the new column type. The {@link getColumnType} method will be invoked to convert abstract column type (if any)
-     *            into the physical one. Anything that is not recognized as abstract type will be kept in the generated SQL.
-     *            For example, 'string' will be turned into 'varchar(255)', while 'string not null' will become 'varchar(255) not null'.
-     * @return string the SQL statement for changing the definition of a column.
-     * @since 1.1.6
-     */
-    public function alterColumn($table, $column, $type)
-    {
-        $type = $this->getColumnType($type);
-        $sql = 'ALTER TABLE ' . $this->quoteTableName($table) . ' MODIFY ' . $this->quoteColumnName($column) . ' ' . $this->getColumnType($type);
-        return $sql;
-    }
+	/**
+	 * Extracts the data types for the given column
+	 * @param ColumnSchema $column
+	 * @param string $dbType DB type
+	 */
+	protected function extractColumnType($column, $dbType)
+	{
+		$column->dbType = $dbType;
 
-    /**
-     * Builds a SQL statement for dropping an index.
-     * 
-     * @param string $name
-     *            the name of the index to be dropped. The name will be properly quoted by the method.
-     * @param string $table
-     *            the table whose index is to be dropped. The name will be properly quoted by the method.
-     * @return string the SQL statement for dropping an index.
-     * @since 1.1.6
-     */
-    public function dropIndex($name, $table)
-    {
-        return 'DROP INDEX ' . $this->quoteTableName($name);
-    }
+		if (strpos($dbType, 'FLOAT') !== false) {
+			$column->type = 'double';
+		} elseif (strpos($dbType, 'NUMBER') !== false || strpos($dbType, 'INTEGER') !== false) {
+			if (strpos($dbType, '(') && preg_match('/\((.*)\)/', $dbType, $matches)) {
+				$values = explode(',', $matches[1]);
+				if (isset($values[1]) and (((int)$values[1]) > 0)) {
+					$column->type = 'double';
+				} else {
+					$column->type = 'integer';
+				}
+			} else {
+				$column->type = 'double';
+			}
+		} else {
+			$column->type = 'string';
+		}
+	}
 
-    /**
-     * Resets the sequence value of a table's primary key.
-     * The sequence will be reset such that the primary key of the next new row inserted
-     * will have the specified value or 1.
-     * 
-     * @param CDbTableSchema $table
-     *            the table schema whose primary key sequence will be reset
-     * @param mixed $value
-     *            the value for the primary key of the next new row inserted. If this is not set,
-     *            the next new row's primary key will have a value 1.
-     * @since 1.1.13
-     */
-    public function resetSequence($table, $value = 1)
-    {
-        $seq = $table->name . "_SEQ";
-        if ($table->sequenceName !== null) {
-            $this->db->createCommand("DROP SEQUENCE " . $seq)->execute();
-            
-            $createSequenceSql = <<< SQL
-create sequence $seq
-start with $value
-increment by 1
-nomaxvalue
-nocache
-SQL;
-            $this->db->createCommand($createSequenceSql)->execute();
-        }
-    }
+	/**
+	 * Extracts size, precision and scale information from column's DB type.
+	 * @param ColumnSchema $column
+	 * @param string $dbType the column's DB type
+	 */
+	protected function extractColumnSize($column, $dbType)
+	{
+		if (strpos($dbType, '(') && preg_match('/\((.*)\)/', $dbType, $matches)) {
+			$values = explode(',', $matches[1]);
+			$column->size = $column->precision = (int)$values[0];
+			if (isset($values[1])) {
+				$column->scale = (int)$values[1];
+			}
+		}
+	}
 }
