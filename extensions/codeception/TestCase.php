26dc108b025b01c59326ac11faece6c898b0e661
Mark <mark.github@yandex.ru>
2013-12-13 19:38:25 +0400

    added new extension yii2-codeception, modified composer settings

---
+++ extensions/codeception/TestCase.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace yii\codeception;
+
+use Yii;
+
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+
+	/**
+	 * Your application base config that will be used for creating application each time before test.
+	 * This can be an array or alias, pointing to the config file. For example for console application it can be
+	 * '@tests/unit/console_bootstrap.php' that can be similar to existing unit tests bootstrap file.
+	 * @var mixed
+	 */
+	protected $baseConfig = '@tests/unit/_bootstrap.php';
+
+	/**
+	 * Your application config, will be merged with base config when creating application. Can be an alias too.
+	 * @var mixed
+	 */
+	protected $config = array();
+
+	/**
+	 * Created application class
+	 * @var string
+	 */
+	protected $appClass = '\yii\web\Application';
+
+	protected function setUp()
+	{
+		parent::setUp();
+		$this->mockApplication();
+	}
+
+	protected function tearDown()
+	{
+		$this->destroyApplication();
+		parent::tearDown();
+	}
+
+	protected function mockApplication()
+	{
+		$baseConfig = is_array($this->baseConfig)? $this->baseConfig : require(Yii::getAlias($this->baseConfig, true));
+		$config = is_array($this->config)? $this->config : require(Yii::getAlias($this->config, true));
+		new $this->appClass(\yii\helpers\ArrayHelper::merge($baseConfig,$config));
+	}
+
+	protected function destroyApplication()
+	{
+		\Yii::$app = null;
+	}
+
+}
