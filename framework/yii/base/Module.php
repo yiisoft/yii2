d3a181c6742a135469c627928872901725e3d0c4
Carsten Brandt <mail@cebe.cc>
2013-12-16 11:03:30 +0100

    fixed controllerMap docs in Module
    fixes #1541

--- framework/yii/base/Module.php
+++ framework/yii/base/Module.php
@@ -62,16 +62,16 @@ abstract class Module extends Component
 	 * @var array mapping from controller ID to controller configurations.
 	 * Each name-value pair specifies the configuration of a single controller.
 	 * A controller configuration can be either a string or an array.
-	 * If the former, the string should be the class name or path alias of the controller.
+	 * If the former, the string should be the fully qualified class name of the controller.
 	 * If the latter, the array must contain a 'class' element which specifies
-	 * the controller's class name or path alias, and the rest of the name-value pairs
+	 * the controller's fully qualified class name, and the rest of the name-value pairs
 	 * in the array are used to initialize the corresponding controller properties. For example,
 	 *
 	 * ~~~
 	 * [
-	 *   'account' => '@app/controllers/UserController',
+	 *   'account' => 'app\controllers\UserController',
 	 *   'article' => [
-	 *      'class' => '@app/controllers/PostController',
+	 *      'class' => 'app\controllers\PostController',
 	 *      'pageTitle' => 'something new',
 	 *   ],
 	 * ]
