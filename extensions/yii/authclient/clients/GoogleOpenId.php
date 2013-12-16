ab86990e97db4a82e97e99582ccedf19f36889ac
Paul Klimov <klimov.paul@gmail.com>
2013-12-16 17:24:29 +0200

    "yii\authclient\clients\GoogleOpenId" added as blank.

---
+++ extensions/yii/authclient/clients/GoogleOpenId.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\authclient\clients;
+
+use yii\authclient\OpenId;
+
+/**
+ * Class GoogleOpenId
+ *
+ * @author Paul Klimov <klimov.paul@gmail.com>
+ * @since 2.0
+ */
+class GoogleOpenId extends OpenId
+{
+
+}
