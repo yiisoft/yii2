722c93fc4860b9104e81cf66110465700cd1dd49
Paul Klimov <klimov.paul@gmail.com>
2013-12-20 11:45:20 +0200

    Yandex OpenId client added.

---
+++ extensions/yii/authclient/clients/YandexOpenId.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\authclient\clients;
+
+use yii\authclient\OpenId;
+
+/**
+ * Class YandexOpenId
+ *
+ * @author Paul Klimov <klimov.paul@gmail.com>
+ * @since 2.0
+ */
+class YandexOpenId extends OpenId
+{
+	public function init()
+	{
+		parent::init();
+		$this->setIdentity('http://openid.yandex.ru');
+		$this->requiredAttributes = [
+			'namePerson',
+			'contact/email',
+		];
+	}
+}
