6dab0c27605674c455e4b0654effd9a65eb1d21d
dev-meghraj <dev.meghraj@gmail.com>
2013-12-24 02:16:47 +0530

    add old yii-ext twig renderer's function into this. and as per yii2's view system changed so for now add simple directory of file based file loader for twig

---
+++ extensions/yii/twig/TwigSimpleFileLoader.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace yii\twig;
+
+/**
+ * Twig view file loader class
+ *
+ * @author dev-mraj <dev.meghraj@gmail.com>
+ * @version 1.0.0
+ */
+class TwigSimpleFileLoader implements \Twig_LoaderInterface {
+
+    /**
+     * Path to directory where all file exists
+     * @param $view string
+     */
+    private $dir;
+
+    public function __construct($dir){
+        $this->dir=$dir;
+    }
+
+    public function isFresh($name, $time){
+        return filemtime($this->getFilePath($name))<=$time;
+    }
+    public function getSource($name){
+        return file_get_contents($this->getFilePath($name));
+    }
+    public function getCacheKey($name){
+        return $this->getFilePath($name);
+    }
+
+    protected  function getFilePath($name){
+        return $this->dir.'/'.$name;
+    }
+}
