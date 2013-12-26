8e1eb221033cb59dd685a2347e609d45db93d60b
Qiang Xue <qiang.xue@gmail.com>
2013-12-26 00:29:03 -0500

    doc fix.

--- framework/yii/validators/ExistValidator.php
+++ framework/yii/validators/ExistValidator.php
@@ -27,7 +27,7 @@ use yii\base\InvalidConfigException;
  * // a1 needs to exist, but its value will use a2 to check for the existence
  * ['a1', 'exist', 'targetAttribute' => 'a2']
  * // a1 and a2 need to exist together, and they both will receive error message
- * ['a1, a2', 'exist', 'targetAttribute' => ['a1', 'a2']]
+ * [['a1', 'a2'], 'exist', 'targetAttribute' => ['a1', 'a2']]
  * // a1 and a2 need to exist together, only a1 will receive error message
  * ['a1', 'exist', 'targetAttribute' => ['a1', 'a2']]
  * // a1 needs to exist by checking the existence of both a2 and a3 (using a1 value)
