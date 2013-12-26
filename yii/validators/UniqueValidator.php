1fd2eb7e9c1c312fb1135da99ed45dbd0298d5ca
Qiang Xue <qiang.xue@gmail.com>
2013-12-26 00:29:03 -0500

    doc fix.

--- yii/validators/UniqueValidator.php
+++ yii/validators/UniqueValidator.php
@@ -24,7 +24,7 @@ use yii\db\ActiveRecordInterface;
  * // a1 needs to be unique, but column a2 will be used to check the uniqueness of the a1 value
  * ['a1', 'unique', 'targetAttribute' => 'a2']
  * // a1 and a2 need to unique together, and they both will receive error message
- * ['a1, a2', 'unique', 'targetAttribute' => ['a1', 'a2']]
+ * [['a1', 'a2'], 'unique', 'targetAttribute' => ['a1', 'a2']]
  * // a1 and a2 need to unique together, only a1 will receive error message
  * ['a1', 'unique', 'targetAttribute' => ['a1', 'a2']]
  * // a1 needs to be unique by checking the uniqueness of both a2 and a3 (using a1 value)
