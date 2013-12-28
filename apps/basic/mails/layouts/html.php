9a0e9295e584ebd42ee070c3cae647f0cb2253cb
Mark <mark.github@yandex.ru>
2013-12-29 02:37:32 +0400

    added html layout for mail component in basic app

---
+++ apps/basic/mails/layouts/html.php
@@ -0,0 +1,24 @@
+<?php
+use yii\helpers\Html;
+use yii\mail\BaseMessage;
+
+/**
+ * @var \yii\web\View $this
+ * @var BaseMessage $content
+ */
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=<?= Yii::$app->charset ?>" />
+	<title><?= Html::encode($this->title) ?></title>
+	<?php $this->head() ?>
+</head>
+<body>
+	<?php $this->beginBody() ?>
+	<?= $content ?>
+	<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
