ccd4ae9f3c9d952900238ef86b11a40fa02ac5cc
Qiang Xue <qiang.xue@gmail.com>
2013-12-29 15:31:35 -0500

    Fixes #1691: added “viewport” meta tag to layout views.

--- apps/basic/views/layouts/main.php
+++ apps/basic/views/layouts/main.php
@@ -16,6 +16,7 @@ AppAsset::register($this);
 <html lang="<?= Yii::$app->language ?>">
 <head>
 	<meta charset="<?= Yii::$app->charset ?>"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?= Html::encode($this->title) ?></title>
 	<?php $this->head() ?>
 </head>
