53f7f79b1572cfb37e47d5214378dc87155ce29c
Mark <mark.github@yandex.ru>
2013-12-31 00:17:36 +0400

    modified tests, improved configuration, introduced aspect mock and specify.

--- apps/basic/tests/functional/_bootstrap.php
+++ apps/basic/tests/functional/_bootstrap.php
@@ -1,3 +1,6 @@
 <?php
 
+require_once(__DIR__ . '/../../vendor/yiisoft/yii2/yii/Yii.php');
+Yii::setAlias('@tests', __DIR__ . '/../');
+
 new yii\web\Application(require(__DIR__ . '/_config.php'));
--- apps/basic/tests/unit/models/ContactFormTest.php
+++ apps/basic/tests/unit/models/ContactFormTest.php
@@ -2,9 +2,62 @@
 
 namespace tests\unit\models;
 
+use Yii;
 use yii\codeception\TestCase;
+use app\models\ContactForm;
+use AspectMock\Test as test;
 
 class ContactFormTest extends TestCase
 {
-	// TODO add test methods here
+
+	use \Codeception\Specify;
+
+	protected function setUp()
+	{
+		parent::setUp();
+		Yii::$app->mail->fileTransportCallback = function ($mailer, $message)
+		{
+			return 'testing_message.eml';
+		};
+	}
+
+	protected function tearDown()
+	{
+		unlink(Yii::getAlias(Yii::$app->mail->fileTransportPath) . '/testing_message.eml');
+		test::clean();
+		parent::tearDown();
+	}
+
+	public function testContact()
+	{
+		test::double('app\models\ContactForm',['validate' => true]);
+
+		$model = new ContactForm();
+		$model->attributes = [
+			'name' => 'Tester',
+			'email' => 'tester@example.com',
+			'subject' => 'very important letter subject',
+			'body' => 'body of current message',
+		];
+
+		$model->contact('admin@example.com');
+
+		$this->specify('email should be send', function () {
+			$this->assertFileExists($this->getMessageFile(), 'email file should exist');
+		});
+
+		$this->specify('message should contain correct data', function () use($model) {
+			$emailMessage = file_get_contents($this->getMessageFile());
+			$this->assertContains($model->name, $emailMessage, 'email should contain user name');
+			$this->assertContains($model->email, $emailMessage, 'email should contain sender email');
+			$this->assertContains($model->subject, $emailMessage, 'email should contain subject');
+			$this->assertContains($model->body, $emailMessage, 'email should contain body');
+		});
+	}
+
+	private function getMessageFile()
+	{
+		return Yii::getAlias(Yii::$app->mail->fileTransportPath) . '/testing_message.eml';
+	}
+
 }
