92d861d9178d0719560cd86aab31419179d66cfd
Luciano Baraglia <luciano.baraglia@gmail.com>
2014-01-03 15:41:58 -0300

    Some debug code style fixes, and https://github.com/yiisoft/yii2/issues/1478#issuecomment-31446267 fix

--- extensions/yii/debug/views/default/panels/request/detail.php
+++ extensions/yii/debug/views/default/panels/request/detail.php
@@ -9,22 +9,22 @@ echo Tabs::widget([
 	'items' => [
 		[
 			'label' => 'Parameters',
-			'content' =>  $this->render('panels/request/table', ['caption' => 'Routing', 'values' => ['Route' => $panel->data['route'], 'Action' => $panel->data['action'], 'Parameters' => $panel->data['actionParams']]])
-						. $this->render('panels/request/table', ['caption' => '$_GET', 'values' => $panel->data['GET']])
-						. $this->render('panels/request/table', ['caption' => '$_POST', 'values' => $panel->data['POST']])
-						. $this->render('panels/request/table', ['caption' => '$_FILES', 'values' => $panel->data['FILES']])
-						. $this->render('panels/request/table', ['caption' => '$_COOKIE', 'values' => $panel->data['COOKIE']]),
+			'content' => $this->render('panels/request/table', ['caption' => 'Routing', 'values' => ['Route' => $panel->data['route'], 'Action' => $panel->data['action'], 'Parameters' => $panel->data['actionParams']]])
+				. $this->render('panels/request/table', ['caption' => '$_GET', 'values' => $panel->data['GET']])
+				. $this->render('panels/request/table', ['caption' => '$_POST', 'values' => $panel->data['POST']])
+				. $this->render('panels/request/table', ['caption' => '$_FILES', 'values' => $panel->data['FILES']])
+				. $this->render('panels/request/table', ['caption' => '$_COOKIE', 'values' => $panel->data['COOKIE']]),
 			'active' => true,
 		],
 		[
 			'label' => 'Headers',
-			'content' =>  $this->render('panels/request/table', ['caption' => 'Request Headers', 'values' => $panel->data['requestHeaders']])
-						. $this->render('panels/request/table', ['caption' => 'Response Headers', 'values' => $panel->data['responseHeaders']])
+			'content' => $this->render('panels/request/table', ['caption' => 'Request Headers', 'values' => $panel->data['requestHeaders']])
+				. $this->render('panels/request/table', ['caption' => 'Response Headers', 'values' => $panel->data['responseHeaders']])
 		],
 		[
 			'label' => 'Session',
-			'content' =>  $this->render('panels/request/table', ['caption' => '$_SESSION', 'values' => $panel->data['SESSION']])
-						. $this->render('panels/request/table', ['caption' => 'Flashes', 'values' => $panel->data['flashes']])
+			'content' => $this->render('panels/request/table', ['caption' => '$_SESSION', 'values' => $panel->data['SESSION']])
+				. $this->render('panels/request/table', ['caption' => 'Flashes', 'values' => $panel->data['flashes']])
 		],
 		[
 			'label' => '$_SERVER',
