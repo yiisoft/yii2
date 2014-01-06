fafba3617f480fdcd448209ae3ac9123a8a94fdb
Alexander Makarov <sam@rmcreative.ru>
2014-01-06 15:11:47 +0400

    Fixed code style and comparison

--- yii/web/Request.php
+++ yii/web/Request.php
@@ -668,8 +668,8 @@ class Request extends \yii\base\Request
 	 */
 	public function getIsSecureConnection()
 	{
-		return isset($_SERVER['HTTPS']) && (strcasecmp($_SERVER['HTTPS'],'on') || $_SERVER['HTTPS'] == 1)
-			|| isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strcasecmp($_SERVER['HTTP_X_FORWARDED_PROTO'],'https');
+		return isset($_SERVER['HTTPS']) && (strcasecmp($_SERVER['HTTPS'], 'on') === 0 || $_SERVER['HTTPS'] == 1)
+			|| isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strcasecmp($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https');
 	}
 
 	/**
