44e36b58564da13dc583570904bad7beef8e8c02
Alexander Makarov <sam@rmcreative.ru>
2014-01-08 22:44:33 +0400

    Advanced app template: moved database connection DSN, login and password to -local config not to expose it to VCS

--- apps/advanced/common/config/params.php
+++ apps/advanced/common/config/params.php
@@ -20,9 +20,6 @@ return [
 
 	'components.db' => [
 		'class' => 'yii\db\Connection',
-		'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-		'username' => 'root',
-		'password' => '',
 		'charset' => 'utf8',
 	],
 ];
