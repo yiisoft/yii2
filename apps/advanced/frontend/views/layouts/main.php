6eb75881b1eca59cfcceca4b26cf2e5e43def7c4
Nikola Trifunovic <johonunu@gmail.com>
2014-01-11 16:41:39 +0100

    Added futter. Fixes #1822

--- apps/advanced/frontend/views/layouts/main.php
+++ apps/advanced/frontend/views/layouts/main.php
@@ -23,40 +23,42 @@ AppAsset::register($this);
 </head>
 <body>
 	<?php $this->beginBody() ?>
-	<?php
-		NavBar::begin([
-			'brandLabel' => 'My Company',
-			'brandUrl' => Yii::$app->homeUrl,
-			'options' => [
-				'class' => 'navbar-inverse navbar-fixed-top',
-			],
-		]);
-		$menuItems = [
-			['label' => 'Home', 'url' => ['/site/index']],
-			['label' => 'About', 'url' => ['/site/about']],
-			['label' => 'Contact', 'url' => ['/site/contact']],
-		];
-		if (Yii::$app->user->isGuest) {
-			$menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-			$menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-		} else {
-			$menuItems[] = ['label' => 'Logout (' . Yii::$app->user->identity->username .')' , 'url' => ['/site/logout']];
-		}
-		echo Nav::widget([
-			'options' => ['class' => 'navbar-nav navbar-right'],
-			'items' => $menuItems,
-		]);
-		NavBar::end();
-	?>
+	<div class="wrap">
+		<?php
+			NavBar::begin([
+				'brandLabel' => 'My Company',
+				'brandUrl' => Yii::$app->homeUrl,
+				'options' => [
+					'class' => 'navbar-inverse navbar-fixed-top',
+				],
+			]);
+			$menuItems = [
+				['label' => 'Home', 'url' => ['/site/index']],
+				['label' => 'About', 'url' => ['/site/about']],
+				['label' => 'Contact', 'url' => ['/site/contact']],
+			];
+			if (Yii::$app->user->isGuest) {
+				$menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
+				$menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+			} else {
+				$menuItems[] = ['label' => 'Logout (' . Yii::$app->user->identity->username .')' , 'url' => ['/site/logout']];
+			}
+			echo Nav::widget([
+				'options' => ['class' => 'navbar-nav navbar-right'],
+				'items' => $menuItems,
+			]);
+			NavBar::end();
+		?>
 
-	<div class="container">
-	<?= Breadcrumbs::widget([
-		'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-	]) ?>
-	<?= Alert::widget() ?>
-	<?= $content ?>
+		<div class="container">
+		<?= Breadcrumbs::widget([
+			'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+		]) ?>
+		<?= Alert::widget() ?>
+		<?= $content ?>
+		</div>
 	</div>
-
+	
 	<footer class="footer">
 		<div class="container">
 		<p class="pull-left">&copy; My Company <?= date('Y') ?></p>
