ae08f0ec4c4239fc35550bf41fb3b0eefb236454
Qiang Xue <qiang.xue@gmail.com>
2014-01-11 08:35:43 -0500

    Fixes #1809: Added support for building "EXISTS" and "NOT EXISTS" query conditions

--- framework/db/QueryBuilder.php
+++ framework/db/QueryBuilder.php
@@ -799,6 +799,8 @@ class QueryBuilder extends \yii\base\Object
 			'NOT LIKE' => 'buildLikeCondition',
 			'OR LIKE' => 'buildLikeCondition',
 			'OR NOT LIKE' => 'buildLikeCondition',
+			'EXISTS' => 'buildExistsCondition',
+			'NOT EXISTS' => 'buildExistsCondition',
 		];
 
 		if (!is_array($condition)) {
@@ -1070,5 +1072,22 @@ class QueryBuilder extends \yii\base\Object
 		}
 
 		return implode($andor, $parts);
+	}
+
+	/**
+	 * Creates an SQL expressions with the `EXISTS` operator.
+	 * @param string $operator the operator to use (e.g. `EXISTS` or `NOT EXISTS`)
+	 * @param array $operands contains only the one element. It is sub-query
+	 * @param array $params the binding parameters to be populated
+	 * @return string the generated SQL expression
+	 */
+	public function buildExistsCondition($operator, $operands, &$params)
+	{
+		$subQuery = $operands[0];
+		list($subQuerySql, $subQueryParams) = $this->build($subQuery);
+		foreach ($subQueryParams as $name => $value) {
+			$params[$name] = $value;
+		}
+		return "$operator ($subQuerySql)";
 	}
 }
