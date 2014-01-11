85874dfd668faabb0c0e387248a45672494d9f19
Qiang Xue <qiang.xue@gmail.com>
2014-01-10 23:56:44 -0500

    reordered language code [skip ci]

--- framework/messages/config.php
+++ framework/messages/config.php
@@ -7,7 +7,7 @@ return [
 	'messagePath' => __DIR__,
 	// array, required, list of language codes that the extracted messages
 	// should be translated to. For example, ['zh-CN', 'de'].
-	'languages' => ['da', 'de', 'ru', 'pl', 'pt-BR', 'it', 'es', 'ro', 'zh-CN', 'ja'],
+	'languages' => ['da', 'de', 'es', 'it', 'ja', 'pl', 'pt-BR', 'ro', 'ru', 'zh-CN'],
 	// string, the name of the function for translating messages.
 	// Defaults to 'Yii::t'. This is used as a mark to find the messages to be
 	// translated. You may use a string for single function name or an array for
