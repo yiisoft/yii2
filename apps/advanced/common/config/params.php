955b43c8920704974e8960f3485629b2b2efbfc2
Alexander Makarov <sam@rmcreative.ru>
2014-01-12 22:40:50 +0400

    Used dirname instead of realpath to avoid extra FS operations

--- apps/advanced/common/config/params.php
+++ apps/advanced/common/config/params.php
@@ -1,9 +1,9 @@
 <?php
 
-Yii::setAlias('common', realpath(__DIR__ . '/../'));
-Yii::setAlias('frontend', realpath(__DIR__ . '/../../frontend'));
-Yii::setAlias('backend', realpath(__DIR__ . '/../../backend'));
-Yii::setAlias('console', realpath(__DIR__ . '/../../console'));
+Yii::setAlias('common', dirname(__DIR__));
+Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
+Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
 
 return [
 	'adminEmail' => 'admin@example.com',
