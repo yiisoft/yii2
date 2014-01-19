ab882a3c002f55f410bc21eb07bfe5ab6794b6b5
Carsten Brandt <mail@cebe.cc>
2014-01-19 17:00:14 +0100

    code style

--- apps/advanced/console/migrations/m130524_201442_init.php
+++ apps/advanced/console/migrations/m130524_201442_init.php
@@ -21,8 +21,8 @@ class m130524_201442_init extends \yii\db\Migration
 			'role' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 10',
 
 			'status' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 10',
-			'created_at' => Schema::TYPE_INTEGER.' NOT NULL',
-			'updated_at' => Schema::TYPE_INTEGER.' NOT NULL',
+			'created_at' => Schema::TYPE_INTEGER . ' NOT NULL',
+			'updated_at' => Schema::TYPE_INTEGER . ' NOT NULL',
 		], $tableOptions);
 	}
 
