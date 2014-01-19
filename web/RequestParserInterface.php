1ab56a8022e6dd83a2efa2dd71355073c6289f4c
Daniel Schmidt <danschmidt5189@gmail.com>
2014-01-19 14:36:10 -0800

    Adds customizable parsing functionality to the web\Request class (Issue #2043)

---
+++ web/RequestParserInterface.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\web;
+
+/**
+ * Interface for objects that parse the raw request body into a parameters array
+ *
+ * @author Dan Schmidt <danschmidt5189@gmail.com>
+ * @since 2.0
+ */
+interface RequestParserInterface
+{
+    /**
+     * @param string $rawBody the raw HTTP request body
+     * @return array parameters parsed from the request body
+     */
+	public function parse($rawBody);
+}
