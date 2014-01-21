d7b209777535e4803d63faf66924a446ad7d3257
Jacob Morrison <jomorrison@gmail.com>
2014-01-20 21:02:16 -0800

    Simplify apidoc  to allow for custom renderers via ext

--- extensions/apidoc/commands/RenderController.php
+++ extensions/apidoc/commands/RenderController.php
@@ -103,15 +103,11 @@ class RenderController extends Controller
 	 */
 	protected function findRenderer()
 	{
-		$file = Yii::getAlias('@yii/apidoc/templates/' . $this->template . '/Renderer.php');
-		$reflection = new FileReflector($file, true);
-		$reflection->process();
-		$classes = $reflection->getClasses();
-		if (empty($classes)) {
+		$rendererClass = 'yii\\apidoc\\templates\\' . $this->template . '\\Renderer';
+		if (!class_exists($rendererClass)) {
 			$this->stderr('Renderer not found.' . PHP_EOL);
+			exit(0);
 		}
-		$rendererClass = reset($classes)->getName();
-		require($file);
 		return new $rendererClass();
 	}
 
