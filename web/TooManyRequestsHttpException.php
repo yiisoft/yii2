8041b54a875e0a1f018483d03b9e9bfee9910f8d
Daniel Schmidt <danschmidt5189@gmail.com>
2014-01-21 16:57:31 -0800

    Adds TooManyRequestsHttpException for 429 http errors

---
+++ web/TooManyRequestsHttpException.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\web;
+
+/**
+ * TooManyRequestsHttpException represents a "Too Many Requests" HTTP exception with status code 429.
+ *
+ * @author Dan Schmidt <danschmidt5189@gmail.com>
+ * @since 2.0
+ */
+class TooManyRequestsHttpException extends HttpException
+{
+	/**
+	 * Constructor.
+	 * @param string $message error message
+	 * @param integer $code error code
+	 * @param \Exception $previous The previous exception used for the exception chaining.
+	 */
+	public function __construct($message = null, $code = 0, \Exception $previous = null)
+	{
+		parent::__construct(429, $message, $code, $previous);
+	}
+}
