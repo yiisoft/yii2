e4eed52003ac6062964acd44ba7c4c08a59eac0a
Alexander Kochetov <creocoder@gmail.com>
2014-01-28 22:19:00 +0400

    Aliases moved to aliases.php

--- apps/advanced/environments/prod/frontend/web/index.php
+++ apps/advanced/environments/prod/frontend/web/index.php
@@ -4,6 +4,7 @@ defined('YII_ENV') or define('YII_ENV', 'prod');
 
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../../common/config/aliases.php');
 
 $config = yii\helpers\ArrayHelper::merge(
 	require(__DIR__ . '/../../common/config/main.php'),
