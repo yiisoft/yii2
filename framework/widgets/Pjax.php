d7ab7eb61e05e09122c9759fe96fe0420a120d11
Qiang Xue <qiang.xue@gmail.com>
2014-01-28 23:27:24 -0500

    Fixes #706: Added `yii\widgets\Pjax` and enhanced `GridView` to work with `Pjax` to support AJAX-update

--- framework/widgets/Pjax.php
+++ framework/widgets/Pjax.php
@@ -9,36 +9,120 @@ namespace yii\widgets;
 
 use Yii;
 use yii\base\Widget;
+use yii\helpers\Html;
 use yii\helpers\Json;
+use yii\web\Response;
 
 /**
+ * Pjax is a widget integrating the [pjax](https://github.com/defunkt/jquery-pjax) jQuery plugin.
+ *
+ * Pjax captures the link clicks in the content enclosed between its [[begin()]] and [[end()]] calls,
+ * turns them into AJAX requests, and replaces the enclosed content with the corresponding AJAX response.
+ *
+ * The following example makes the [[yii\gridview\GridView]] widget support updating via AJAX:
+ *
+ * ```php
+ * use yii\widgets\Pjax;
+ *
+ * Pjax::begin();
+ * echo GridView::widget([...]);
+ * Pjax::end();
+ * ```
+ *
+ * Clicking the sorting and pagination links in the grid will trigger AJAX-based updating of the grid content.
+ * Moreover, if the grid view has turned on filtering, the filtering will also be performed via AJAX.
+ *
+ * By default, Pjax enables [[enablePushState|push state]], which means the browser's current URL will
+ * be updated when an AJAX request is made by Pjax.
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
 class Pjax extends Widget
 {
-	public $links = 'a';
+	/**
+	 * @var array the HTML attributes for the widget container tag.
+	 */
+	public $options = [];
+	/**
+	 * @var string the jQuery selector of the links that should trigger pjax requests.
+	 * If not set, all links within the enclosed content of Pjax will trigger pjax requests.
+	 * Note that the pjax response to a link is a full page, a normal request will be sent again.
+	 */
+	public $linkSelector;
+	/**
+	 * @var boolean whether to enable push state.
+	 */
+	public $enablePushState = true;
+	/**
+	 * @var boolean whether to enable replace state.
+	 */
+	public $enableReplaceState = false;
 
+	/**
+	 * @inheritdoc
+	 */
 	public function init()
 	{
+		if (!isset($this->options['id'])) {
+			$this->options['id'] = $this->getId();
+		}
+
 		ob_start();
 		ob_implicit_flush(false);
-		echo '<div id="' . $this->getId() . '">';
+
+		if ($this->requiresPjax()) {
+			$view = $this->getView();
+			$view->clear();
+			$view->beginPage();
+			$view->head();
+			$view->beginBody();
+		}
+		echo Html::beginTag('div', $this->options);
 	}
 
+	/**
+	 * @inheritdoc
+	 */
 	public function run()
 	{
-		echo '</div>';
+		echo Html::endTag('div');
+		if ($requiresPjax = $this->requiresPjax()) {
+			$view = $this->getView();
+			$view->endBody();
+			$view->endPage(true);
+		}
+
 		$content = ob_get_clean();
 
-		$headers = Yii::$app->getRequest()->getHeaders();
-		if ($headers->get('X-Pjax') && ($selector = $headers->get('X-PJax-Container')) === '#' . $this->getId()) {
-			// todo: send the response and terminate the application
+		if ($requiresPjax) {
+			// only need the content enclosed within this widget
+			$response = Yii::$app->getResponse();
+			$level = ob_get_level();
+			$response->clearOutputBuffers();
+			$response->setStatusCode(200);
+			$response->format = Response::FORMAT_HTML;
+			$response->content = $content;
+			$response->send();
+
+			// re-enable output buffer to capture content after this widget
+			for (; $level > 0; --$level) {
+				ob_start();
+				ob_implicit_flush(false);
+			}
 		} else {
 			$this->registerClientScript();
-			return $content;
+			echo $content;
 		}
+	}
+
+	/**
+	 * @return boolean whether the current request requires pjax response from this widget
+	 */
+	protected function requiresPjax()
+	{
+		$headers = Yii::$app->getRequest()->getHeaders();
+		return $headers->get('X-Pjax') && ($selector = $headers->get('X-Pjax-Container')) === '#' . $this->getId();
 	}
 
 	/**
@@ -46,9 +130,15 @@ class Pjax extends Widget
 	 */
 	public function registerClientScript()
 	{
+		$id = $this->options['id'];
+		$options = Json::encode([
+			'push' => $this->enablePushState,
+			'replace' => $this->enableReplaceState,
+		]);
+		$linkSelector = Json::encode($this->linkSelector !== null ? $this->linkSelector : '#' . $id . ' a');
 		$view = $this->getView();
 		PjaxAsset::register($view);
-		$js = 'jQuery(document).pjax("' . Json::encode($this->links) . '", "#' . $this->getId() . '");';
+		$js = "jQuery(document).pjax($linkSelector, \"#$id\", $options);";
 		$view->registerJs($js);
 	}
 }
