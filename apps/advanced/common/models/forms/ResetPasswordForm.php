d98ec80cd14625a53c3b16a20e409aa106420fb2
Alexander Makarov <sam@rmcreative.ru>
2014-02-01 20:32:07 +0400

    Used setter as field

--- apps/advanced/common/models/forms/ResetPasswordForm.php
+++ apps/advanced/common/models/forms/ResetPasswordForm.php
@@ -59,7 +59,7 @@ class ResetPasswordForm extends Model
 	{
 		$user = $this->_user;
 		if ($user->validate()) {
-			$user->setPassword($this->password);
+			$user->password = $this->password;
 			$user->removePasswordResetToken();
 			return $user->save();
 		} else {
