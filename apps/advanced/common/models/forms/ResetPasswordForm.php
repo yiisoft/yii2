e4c98f0fa3ff0e56950e9cea475100907849ba75
Alexander Makarov <sam@rmcreative.ru>
2014-02-01 17:47:18 +0400

    Fixed constructor overriding to allow array configuration

--- apps/advanced/common/models/forms/ResetPasswordForm.php
+++ apps/advanced/common/models/forms/ResetPasswordForm.php
@@ -22,11 +22,11 @@ class ResetPasswordForm extends Model
 	 * Creates a form model given a token
 	 *
 	 * @param string $token
+	 * @param array $config name-value pairs that will be used to initialize the object properties
 	 * @throws \yii\base\InvalidParamException if token is empty or not valid
 	 */
-	public function __construct($token)
+	public function __construct($token, $config = [])
 	{
-		parent::__construct();
 		if (empty($token) || !is_string($token)) {
 			throw new InvalidParamException('Password reset token cannot be blank.');
 		}
@@ -37,6 +37,7 @@ class ResetPasswordForm extends Model
 		if (!$this->_user) {
 			throw new InvalidParamException('Wrong password reset token.');
 		}
+		parent::__construct($config);
 	}
 
 	/**
