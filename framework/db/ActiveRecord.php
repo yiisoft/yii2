dcc112ec9058dfe7d147e719febbcdda1eb9c007
Vladimir Zbrailov <dizews@gmail.com>
2014-02-01 22:31:13 +0400

    override create

--- framework/db/ActiveRecord.php
+++ framework/db/ActiveRecord.php
@@ -275,6 +275,26 @@ class ActiveRecord extends BaseActiveRecord
 	}
 
 	/**
+	 * @param array $row
+	 * @return static
+	 */
+	public static function create($row)
+	{
+		$record = static::instantiate($row);
+		$columns = array_flip($record->attributes());
+		$schema = static::getTableSchema();
+		foreach ($row as $name => $value) {
+			if (isset($columns[$name])) {
+				$record->setAttribute($name, $schema->getColumn($name)->typecast($value));
+			} else {
+				$record->$name = $value;
+			}
+		}
+		$record->setOldAttributes($record->getAttributes());
+		return $record;
+	}
+
+	/**
 	 * Inserts a row into the associated database table using the attribute values of this record.
 	 *
 	 * This method performs the following steps in order:
