646ed1515a5cb85ec754e1cb279ba9e4a3451697
Tobias Munk <schmunk@usrbin.de>
2014-02-05 16:21:18 +0100

    readded phpinfo links and separator, now opens in a new tab

--- extensions/debug/views/default/panels/config/summary.php
+++ extensions/debug/views/default/panels/config/summary.php
@@ -9,6 +9,9 @@ use yii\helpers\Html;
 <div class="yii-debug-toolbar-block">
 	<a href="<?= $panel->getUrl() ?>">
 		<img width="29" height="30" alt="" src="<?= $panel->getYiiLogo() ?>">
-		<span><span class="label"><?= $panel->data['application']['yii'] ?></span> PHP <span class="label"><?= $panel->data['php']['version'] ?></span></span>
+		<span><?= $panel->data['application']['yii'] ?></span>
 	</a>
+</div>
+<div class="yii-debug-toolbar-block">
+	<?= Html::a('PHP ' . $panel->data['php']['version'], ['phpinfo'], ['title' => 'Show phpinfo()', 'target' => 'phpinfo']) ?>
 </div>
