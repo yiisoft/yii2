65c740e91f0ae14cbef7e12dc91a855e1632bc5f
Antonio Ramirez <amigo.cobos@gmail.com>
2014-02-05 17:06:37 +0100

    remove tabs for spaces in js

--- extensions/gii/components/ActiveField.php
+++ extensions/gii/components/ActiveField.php
@@ -68,9 +68,9 @@ class ActiveField extends \yii\widgets\ActiveField
 		}
 		$this->form->getView()->registerJs("
 var datum = new Bloodhound({
-	datumTokenizer: function(d){return Bloodhound.tokenizers.whitespace(d.word);},
-	queryTokenizer: Bloodhound.tokenizers.whitespace,
-	local: " . Json::encode($data) . "
+    datumTokenizer: function(d){return Bloodhound.tokenizers.whitespace(d.word);},
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: " . Json::encode($data) . "
 });
 datum.initialize();
 jQuery('.typeahead-{$counter}').typeahead(null,{displayKey: 'word', source: datum.ttAdapter()});");
