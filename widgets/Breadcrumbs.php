007284baa24e02459a636c5aee1ec8ade80ee471
Qiang Xue <qiang.xue@gmail.com>
2014-02-05 16:08:39 -0500

    Fixes #2335.

--- widgets/Breadcrumbs.php
+++ widgets/Breadcrumbs.php
@@ -60,7 +60,8 @@ class Breadcrumbs extends Widget
 	 */
 	public $encodeLabels = true;
 	/**
-	 * @var string the first hyperlink in the breadcrumbs (called home link).
+	 * @var array the first hyperlink in the breadcrumbs (called home link).
+	 * Please refer to [[links]] on the format of the link.
 	 * If this property is not set, it will default to a link pointing to [[\yii\web\Application::homeUrl]]
 	 * with the label 'Home'. If this property is false, the home link will not be rendered.
 	 */
