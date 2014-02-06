09946158519ad21fce97b92ce535670d52b9502b
Qiang Xue <qiang.xue@gmail.com>
2014-02-05 21:08:13 -0500

    removed duplicated query parameters.

--- framework/assets/yii.gridView.js
+++ framework/assets/yii.gridView.js
@@ -61,17 +61,23 @@
 		applyFilter: function () {
 			var $grid = $(this);
 			var settings = $grid.data('yiiGridView').settings;
-			var data = $(settings.filterSelector).serialize();
-			var url = settings.filterUrl;
-			if (url.indexOf('?') >= 0) {
-				url += '&' + data;
-			} else {
-				url += '?' + data;
-			}
+			var data = {};
+			$.each($(settings.filterSelector).serializeArray(), function () {
+				data[this.name] = this.value;
+			});
+
+			$.each(yii.getQueryParams(settings.filterUrl), function (name, value) {
+				if (data[name] === undefined) {
+					data[name] = value;
+				}
+			});
+
+			var pos = settings.filterUrl.indexOf('?');
+			var url = pos < 0 ? settings.filterUrl : settings.filterUrl.substring(0, pos);
 
 			$grid.find('form.gridview-filter-form').remove();
 			var $form = $('<form action="' + url + '" method="get" class="gridview-filter-form" style="display:none" data-pjax></form>').appendTo($grid);
-			$.each(yii.getQueryParams(url), function (name, value) {
+			$.each(data, function (name, value) {
 				$form.append($('<input type="hidden" name="t" value="" />').attr('name', name).val(value));
 			});
 			$form.submit();
