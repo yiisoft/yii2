157af3a37e78d878feee524871552a4182fb182d
Mark <mark.github@yandex.ru>
2014-02-11 00:17:09 +0400

    added check for file name

--- extensions/debug/controllers/DefaultController.php
+++ extensions/debug/controllers/DefaultController.php
@@ -97,7 +97,7 @@ class DefaultController extends Controller
 	{
 		$filePath = Yii::getAlias($this->module->panels['mail']->mailPath) . '/' . basename($file);
 
-		if (!is_file($filePath)) {
+		if ((mb_strpos($file, '\\') !== false || mb_strpos($file, '/') !== false) || !is_file($filePath)) {
 			throw new NotFoundHttpException('Mail file not found');
 		}
 
