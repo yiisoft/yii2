1cd3f2bbd7753a7afd11082f79ee5c0292570f93
Qiang Xue <qiang.xue@gmail.com>
2014-02-11 10:22:23 -0500

    Fixed test break.

--- framework/web/View.php
+++ framework/web/View.php
@@ -431,6 +431,9 @@ class View extends \yii\base\View
 			$this->jsFiles[$position][$key] = Html::jsFile($url, $options);
 		} else {
 			$am = Yii::$app->getAssetManager();
+			if (strpos($url, '/') !== 0 && strpos($url, '://') === false) {
+				$url = Yii::$app->getRequest()->getBaseUrl() . '/' . $url;
+			}
 			$am->bundles[$key] = new AssetBundle([
 				'js' => [$url],
 				'jsOptions' => $options,
