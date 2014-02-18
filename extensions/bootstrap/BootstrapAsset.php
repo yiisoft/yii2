09f2bfff4e88113cf9bdd0a8423a471d0206d38e
Kartik Visweswaran <kartikv2@gmail.com>
2014-02-18 19:19:35 +0530

    Load minified assets in PROD environment
    Load minified assets by default for all extensions by default in a production environment (based on `YII_DEBUG` which is usually set to false for PROD). Though you can edit this using the asset bundle settings in config - this can be a default functionality for all Yii inbuilt extensions.

--- extensions/bootstrap/BootstrapAsset.php
+++ extensions/bootstrap/BootstrapAsset.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @link http://www.yiiframework.com/
  * @copyright Copyright (c) 2008 Yii Software LLC
@@ -13,10 +14,13 @@ use yii\web\AssetBundle;
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class BootstrapAsset extends AssetBundle
-{
-	public $sourcePath = '@vendor/twbs/bootstrap/dist';
-	public $css = [
-		'css/bootstrap.css',
-	];
+class BootstrapAsset extends AssetBundle {
+
+    public $sourcePath = '@vendor/twbs/bootstrap/dist';
+
+    public function init() {
+        $this->css = YII_DEBUG ? ['css/bootstrap.css'] : ['css/bootstrap.min.css'];
+        parent::init();
+    }
+
 }
