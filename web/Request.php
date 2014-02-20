9e2b53ffa95b901d87d1cad61ab8910b2a2e1ecf
Alexander Makarov <sam@rmcreative.ru>
2014-02-20 20:55:02 +0400

    Fixes #2502: Unclear error message when `$_SERVER['DOCUMENT_ROOT']` is empty

--- web/Request.php
+++ web/Request.php
@@ -547,7 +547,7 @@ class Request extends \yii\base\Request
 				$this->_scriptUrl = $_SERVER['ORIG_SCRIPT_NAME'];
 			} elseif (($pos = strpos($_SERVER['PHP_SELF'], '/' . $scriptName)) !== false) {
 				$this->_scriptUrl = substr($_SERVER['SCRIPT_NAME'], 0, $pos) . '/' . $scriptName;
-			} elseif (isset($_SERVER['DOCUMENT_ROOT']) && strpos($scriptFile, $_SERVER['DOCUMENT_ROOT']) === 0) {
+			} elseif (!empty($_SERVER['DOCUMENT_ROOT']) && strpos($scriptFile, $_SERVER['DOCUMENT_ROOT']) === 0) {
 				$this->_scriptUrl = str_replace('\\', '/', str_replace($_SERVER['DOCUMENT_ROOT'], '', $scriptFile));
 			} else {
 				throw new InvalidConfigException('Unable to determine the entry script URL.');
