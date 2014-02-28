0e2b2fcbe625a9b939d4a4c8dca37264edf34b20
Jacob Morrison <jmorrison@psesd.org>
2014-02-27 16:59:47 -0800

    Fix for logic Query::queryScalar uses to check for SELECT DISTINCT

--- framework/db/Query.php
+++ framework/db/Query.php
@@ -356,7 +356,7 @@ class Query extends Component implements QueryInterface
 		$this->limit = $limit;
 		$this->offset = $offset;
 
-		if (empty($this->groupBy) && $this->distinct !== true) {
+		if (empty($this->groupBy) && !$this->distinct) {
 			return $command->queryScalar();
 		} else {
 			return (new Query)->select([$selectExpression])
