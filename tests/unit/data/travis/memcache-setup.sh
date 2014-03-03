b6e0c2b26fd212c651b6ab4e7dd45080cd479f34
Carsten Brandt <mail@cebe.cc>
2014-03-03 18:41:47 +0100

    skipping memcache on HHVM

--- tests/unit/data/travis/memcache-setup.sh
+++ tests/unit/data/travis/memcache-setup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-echo "extension=memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-echo "extension=memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+if (php --version | grep -i HHVM > /dev/null); then
+  echo "skipping memcache on HHVM"
+else
+  echo "extension=memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+  echo "extension=memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+fi
