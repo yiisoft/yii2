1786789a56f7aa48d7d6469a89e3495297785ab8
Alexander Makarov <sam@rmcreative.ru>
2014-03-04 22:17:11 +0400

    Used ternary operator instead of "or" for constant definition

--- BaseYii.php
+++ BaseYii.php
@@ -14,37 +14,37 @@ use yii\log\Logger;
 /**
  * Gets the application start timestamp.
  */
-defined('YII_BEGIN_TIME') or define('YII_BEGIN_TIME', microtime(true));
+defined('YII_BEGIN_TIME') ?: define('YII_BEGIN_TIME', microtime(true));
 /**
  * This constant defines the framework installation directory.
  */
-defined('YII_PATH') or define('YII_PATH', __DIR__);
+defined('YII_PATH') ?: define('YII_PATH', __DIR__);
 /**
  * This constant defines whether the application should be in debug mode or not. Defaults to false.
  */
-defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_DEBUG') ?: define('YII_DEBUG', false);
 /**
  * This constant defines in which environment the application is running. Defaults to 'prod', meaning production environment.
  * You may define this constant in the bootstrap script. The value could be 'prod' (production), 'dev' (development), 'test', 'staging', etc.
  */
-defined('YII_ENV') or define('YII_ENV', 'prod');
+defined('YII_ENV') ?: define('YII_ENV', 'prod');
 /**
  * Whether the the application is running in production environment
  */
-defined('YII_ENV_PROD') or define('YII_ENV_PROD', YII_ENV === 'prod');
+defined('YII_ENV_PROD') ?: define('YII_ENV_PROD', YII_ENV === 'prod');
 /**
  * Whether the the application is running in development environment
  */
-defined('YII_ENV_DEV') or define('YII_ENV_DEV', YII_ENV === 'dev');
+defined('YII_ENV_DEV') ?: define('YII_ENV_DEV', YII_ENV === 'dev');
 /**
  * Whether the the application is running in testing environment
  */
-defined('YII_ENV_TEST') or define('YII_ENV_TEST', YII_ENV === 'test');
+defined('YII_ENV_TEST') ?: define('YII_ENV_TEST', YII_ENV === 'test');
 
 /**
  * This constant defines whether error handling should be enabled. Defaults to true.
  */
-defined('YII_ENABLE_ERROR_HANDLER') or define('YII_ENABLE_ERROR_HANDLER', true);
+defined('YII_ENABLE_ERROR_HANDLER') ?: define('YII_ENABLE_ERROR_HANDLER', true);
 
 
 /**
