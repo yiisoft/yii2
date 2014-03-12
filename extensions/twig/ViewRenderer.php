2674c5024c9f3f1730b8cafb80173427017a5dc2
khaperets <khaperets@gmail.com>
2014-03-12 04:00:16 +0200

    namespace support for templates

--- extensions/twig/ViewRenderer.php
+++ extensions/twig/ViewRenderer.php
@@ -73,6 +73,10 @@ class ViewRenderer extends BaseViewRenderer
      */
     public $lexerOptions = [];
     /**
+     * @var array list of namespaces with pathes
+     */
+    public $namespaces = [];
+    /**
      * @var \Twig_Environment twig environment object that do all rendering twig templates
      */
     public $twig;
@@ -127,6 +131,16 @@ class ViewRenderer extends BaseViewRenderer
 
         $this->twig->addGlobal('app', \Yii::$app);
 
+        $loader = new \Twig_Loader_Filesystem;
+
+        foreach ($this->namespaces as $dir => $namespace) {
+            if ($path = Yii::getAlias($dir, false)) {
+                $loader->addPath($path, $namespace);
+            }
+        }
+
+        $this->twig->setLoader($loader);
+
         Yii::$app->layout = false;
     }
 
@@ -145,7 +159,7 @@ class ViewRenderer extends BaseViewRenderer
     public function render($view, $file, $params)
     {
         $this->twig->addGlobal('this', $view);
-        $this->twig->setLoader(new TwigSimpleFileLoader(dirname($file)));
+        $this->twig->getLoader()->addPath(dirname($file));
         return $this->twig->render(pathinfo($file, PATHINFO_BASENAME), $params);
     }
 
