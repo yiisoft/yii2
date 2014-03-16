76a7beb67fac1ff0fedeaeca4c9bb8ebff0b321b
Qiang Xue <qiang.xue@gmail.com>
2014-03-16 08:19:27 -0400

    Fixed test break.

--- apps/advanced/common/mail/passwordResetToken.php
+++ apps/advanced/common/mail/passwordResetToken.php
@@ -13,4 +13,4 @@ Hello <?= Html::encode($user->username) ?>,
 
 Follow the link below to reset your password:
 
-<?= Html::a(Html::encode($resetLink), $resetLink)
+<?= Html::a(Html::encode($resetLink), $resetLink) ?>
