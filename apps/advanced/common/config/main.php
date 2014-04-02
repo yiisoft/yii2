280cbba342c2c20f8b374f3d46c46a107e6e7201
Panagiotis Moustafellos <pmoust@gmail.com>
2014-04-03 02:00:51 +0300

    removed bowerPath from advanced template

--- apps/advanced/common/config/main.php
+++ apps/advanced/common/config/main.php
@@ -1,7 +1,6 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-    'bowerPath' => dirname(dirname(__DIR__)) . '/bower_components',
     'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
     'components' => [
         'cache' => [
