6d48bb4b41f1413dd48fd666c79d76465ac668fd
Panagiotis Moustafellos <pmoust@gmail.com>
2014-04-02 19:50:08 +0300

    added proper location of jquery asset

--- framework/web/JqueryAsset.php
+++ framework/web/JqueryAsset.php
@@ -15,7 +15,7 @@ namespace yii\web;
  */
 class JqueryAsset extends AssetBundle
 {
-    public $sourcePath = '@bower_components/jquery';
+    public $sourcePath = '@bower_components/jquery/jquery';
     public $js = [
         'jquery.js',
     ];
