63634b6ce39ced1b1c1705678acf16c5d27ed7bd
miramir <gmiramir@gmail.com>
2014-04-05 23:14:14 +0600

    Add SyslogTarget

---
+++ framework/log/SyslogTarget.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\components;
+
+use Yii;
+use yii\log\Target;
+
+/**
+ * SyslogTarget write log to syslog.
+ *
+ * @author miramir <gmiramir@gmail.com>
+ * @since 2.0
+ */
+class SyslogTarget extends Target
+{
+    /**
+     * @var string Syslog identity
+     */
+    public $ident;
+    /**
+     * @var integer syslog facility.
+     */
+    public $facility = LOG_SYSLOG;
+
+    /**
+     * @var array syslog levels
+     */
+    private $syslogLevels = [
+		'trace' => LOG_DEBUG,
+		'info' => LOG_INFO,
+		'profile' => LOG_NOTICE,
+		'warning' => LOG_WARNING,
+		'error' => LOG_ERR,
+	];
+
+    /**
+     * Writes log messages to a syslog.
+     */
+    public function export()
+    {
+        openlog($this->ident, LOG_ODELAY | LOG_PID, $this->facility);
+        foreach($this->messages as $message){
+            list($text, $level, $category, $timestamp) = $message;
+            syslog($this->syslogLevels[$level], $category . ':' . $text);
+        }
+        closelog();
+    }
+}
