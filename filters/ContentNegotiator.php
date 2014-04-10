ec0d7320ae1b060ce9a3d201a9a65a315d889085
Qiang Xue <qiang.xue@gmail.com>
2014-04-09 21:35:19 -0400

    added more doc [skip ci]

--- filters/ContentNegotiator.php
+++ filters/ContentNegotiator.php
@@ -65,9 +65,11 @@ use yii\web\UnsupportedMediaTypeHttpException;
  *     return [
  *         [
  *             'class' => 'yii\filters\ContentNegotiator',
+ *             'only' => ['view', 'index'],  // in a controller
+ *             // if in a module, use the following IDs for user actions
+ *             // 'only' => ['user/view', 'user/index']
  *             'formats' => [
  *                 'application/json' => Response::FORMAT_JSON,
- *                 'application/xml' => Response::FORMAT_XML,
  *             ],
  *             'languages' => [
  *                 'en',
