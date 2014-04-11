e147e2d8d1ebdf3770f7f4da4bb8f5300f697bf7
Carsten Brandt <mail@cebe.cc>
2014-04-11 17:44:44 +0200

    updated base AR phpdoc for findOne() and findAll()

--- db/BaseActiveRecord.php
+++ db/BaseActiveRecord.php
@@ -93,6 +93,7 @@ abstract class BaseActiveRecord extends Model implements ActiveRecordInterface
 
     /**
      * @inheritdoc
+     * @return static ActiveRecord instance matching the condition, or null if nothing matches.
      */
     public static function findOne($condition)
     {
@@ -113,6 +114,7 @@ abstract class BaseActiveRecord extends Model implements ActiveRecordInterface
 
     /**
      * @inheritdoc
+     * @return static[]|array an array of ActiveRecord instance, or an empty array if nothing matches.
      */
     public static function findAll($condition)
     {
