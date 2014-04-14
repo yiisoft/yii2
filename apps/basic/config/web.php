6a4ed107420dca762d12b685031418695e429fd2
hiltonjanfield <hilton@janfield.ca>
2014-04-14 03:07:01 -0700

    Bootstrap Gii - adjust basic app.

--- apps/basic/config/web.php
+++ apps/basic/config/web.php
@@ -39,9 +39,11 @@ $config = [
 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = 'yii\debug\Module';
     $config['modules']['gii'] = 'yii\gii\Module';
+
+    $config['bootstrap'][] = 'debug';
+    $config['bootstrap'][] = 'gii';
 }
 
 return $config;
