01c56400494f1516255aa18a49a4678f6a46af97
Henrique Dias <heukirne@gmail.com>
2014-04-28 09:20:18 -0300

    Fix formatting issues
    Change tabs to spaces

--- framework/db/mssql/QueryBuilder.php
+++ framework/db/mssql/QueryBuilder.php
@@ -139,7 +139,7 @@ class QueryBuilder extends \yii\db\QueryBuilder
 
         return "ALTER TABLE {$table} {$enable} CONSTRAINT ALL";
     }
-	
+    
     /**
      * Generates a SELECT SQL statement from a [[Query]] object for SQL Server early than 2012.
      * @param Query $query the [[Query]] object from which the SQL statement will be generated.
@@ -163,11 +163,11 @@ class QueryBuilder extends \yii\db\QueryBuilder
             $this->buildGroupBy($query->groupBy),
             $this->buildHaving($query->having, $params),
             $this->buildOrderBy($query->orderBy),
-			$this->olderMssql()?'':$this->buildLimit($query->limit, $query->offset),
+            $this->olderMssql()?'':$this->buildLimit($query->limit, $query->offset),
         ];
 
         $sql = implode($this->separator, array_filter($clauses));
-		if ($this->olderMssql()) $sql = $this->applyLimit($sql, $query->limit, $query->offset);
+        if ($this->olderMssql()) $sql = $this->applyLimit($sql, $query->limit, $query->offset);
 
         $union = $this->buildUnion($query->union, $params);
         if ($union !== '') {
@@ -175,187 +175,187 @@ class QueryBuilder extends \yii\db\QueryBuilder
         }
 
 
-		
+        
         return [$sql, $params];
     }
-	
-	/**
-	 * This is a port from Prado Framework.
-	 *
-	 * Overrides parent implementation. Alters the sql to apply $limit and $offset.
-	 * The idea for limit with offset is done by modifying the sql on the fly
-	 * with numerous assumptions on the structure of the sql string.
-	 * The modification is done with reference to the notes from
-	 * http://troels.arvin.dk/db/rdbms/#select-limit-offset
-	 *
-	 * <code>
-	 * SELECT * FROM (
-	 *  SELECT TOP n * FROM (
-	 *    SELECT TOP z columns      -- (z=n+skip)
-	 *    FROM tablename
-	 *    ORDER BY key ASC
-	 *  ) AS FOO ORDER BY key DESC -- ('FOO' may be anything)
-	 * ) AS BAR ORDER BY key ASC    -- ('BAR' may be anything)
-	 * </code>
-	 *
-	 * <b>Regular expressions are used to alter the SQL query. The resulting SQL query
-	 * may be malformed for complex queries.</b> The following restrictions apply
-	 *
-	 * <ul>
-	 *   <li>
-	 * In particular, <b>commas</b> should <b>NOT</b>
-	 * be used as part of the ordering expression or identifier. Commas must only be
-	 * used for separating the ordering clauses.
-	 *   </li>
-	 *   <li>
-	 * In the ORDER BY clause, the column name should NOT be be qualified
-	 * with a table name or view name. Alias the column names or use column index.
-	 *   </li>
-	 *   <li>
-	 * No clauses should follow the ORDER BY clause, e.g. no COMPUTE or FOR clauses.
-	 *   </li>
-	 * </ul>
-	 *
-	 * @param string $sql SQL query string.
-	 * @param integer $limit maximum number of rows, -1 to ignore limit.
-	 * @param integer $offset row offset, -1 to ignore offset.
-	 * @return string SQL with limit and offset.
-	 *
-	 * @author Wei Zhuo <weizhuo[at]gmail[dot]com>
-	 */
-	public function applyLimit($sql, $limit, $offset)
-	{
-		$limit = $limit!==null ? (int)$limit : -1;
-		$offset = $offset!==null ? (int)$offset : -1;
-		if ($limit > 0 && $offset <= 0) //just limit
-			$sql = preg_replace('/^([\s(])*SELECT( DISTINCT)?(?!\s*TOP\s*\()/i',"\\1SELECT\\2 TOP $limit", $sql);
-		elseif($limit > 0 && $offset > 0)
-			$sql = $this->rewriteLimitOffsetSql($sql, $limit,$offset);
-		return $sql;
-	}
+    
+    /**
+     * This is a port from Prado Framework.
+     *
+     * Overrides parent implementation. Alters the sql to apply $limit and $offset.
+     * The idea for limit with offset is done by modifying the sql on the fly
+     * with numerous assumptions on the structure of the sql string.
+     * The modification is done with reference to the notes from
+     * http://troels.arvin.dk/db/rdbms/#select-limit-offset
+     *
+     * <code>
+     * SELECT * FROM (
+     *  SELECT TOP n * FROM (
+     *    SELECT TOP z columns      -- (z=n+skip)
+     *    FROM tablename
+     *    ORDER BY key ASC
+     *  ) AS FOO ORDER BY key DESC -- ('FOO' may be anything)
+     * ) AS BAR ORDER BY key ASC    -- ('BAR' may be anything)
+     * </code>
+     *
+     * <b>Regular expressions are used to alter the SQL query. The resulting SQL query
+     * may be malformed for complex queries.</b> The following restrictions apply
+     *
+     * <ul>
+     *   <li>
+     * In particular, <b>commas</b> should <b>NOT</b>
+     * be used as part of the ordering expression or identifier. Commas must only be
+     * used for separating the ordering clauses.
+     *   </li>
+     *   <li>
+     * In the ORDER BY clause, the column name should NOT be be qualified
+     * with a table name or view name. Alias the column names or use column index.
+     *   </li>
+     *   <li>
+     * No clauses should follow the ORDER BY clause, e.g. no COMPUTE or FOR clauses.
+     *   </li>
+     * </ul>
+     *
+     * @param string $sql SQL query string.
+     * @param integer $limit maximum number of rows, -1 to ignore limit.
+     * @param integer $offset row offset, -1 to ignore offset.
+     * @return string SQL with limit and offset.
+     *
+     * @author Wei Zhuo <weizhuo[at]gmail[dot]com>
+     */
+    public function applyLimit($sql, $limit, $offset)
+    {
+        $limit = $limit!==null ? (int)$limit : -1;
+        $offset = $offset!==null ? (int)$offset : -1;
+        if ($limit > 0 && $offset <= 0) //just limit
+            $sql = preg_replace('/^([\s(])*SELECT( DISTINCT)?(?!\s*TOP\s*\()/i',"\\1SELECT\\2 TOP $limit", $sql);
+        elseif($limit > 0 && $offset > 0)
+            $sql = $this->rewriteLimitOffsetSql($sql, $limit,$offset);
+        return $sql;
+    }
 
-	/**
-	 * Rewrite sql to apply $limit > and $offset > 0 for MSSQL database.
-	 * See http://troels.arvin.dk/db/rdbms/#select-limit-offset
-	 * @param string $sql sql query
-	 * @param integer $limit $limit > 0
-	 * @param integer $offset $offset > 0
-	 * @return string modified sql query applied with limit and offset.
-	 *
-	 * @author Wei Zhuo <weizhuo[at]gmail[dot]com>
-	 */
-	protected function rewriteLimitOffsetSql($sql, $limit, $offset)
-	{
-		$fetch = $limit+$offset;
-		$sql = preg_replace('/^([\s(])*SELECT( DISTINCT)?(?!\s*TOP\s*\()/i',"\\1SELECT\\2 TOP $fetch", $sql);
-		$ordering = $this->findOrdering($sql);
-		$orginalOrdering = $this->joinOrdering($ordering, '[__outer__]');
-		$reverseOrdering = $this->joinOrdering($this->reverseDirection($ordering), '[__inner__]');
-		$sql = "SELECT * FROM (SELECT TOP {$limit} * FROM ($sql) as [__inner__] {$reverseOrdering}) as [__outer__] {$orginalOrdering}";
-		return $sql;
-	}
+    /**
+     * Rewrite sql to apply $limit > and $offset > 0 for MSSQL database.
+     * See http://troels.arvin.dk/db/rdbms/#select-limit-offset
+     * @param string $sql sql query
+     * @param integer $limit $limit > 0
+     * @param integer $offset $offset > 0
+     * @return string modified sql query applied with limit and offset.
+     *
+     * @author Wei Zhuo <weizhuo[at]gmail[dot]com>
+     */
+    protected function rewriteLimitOffsetSql($sql, $limit, $offset)
+    {
+        $fetch = $limit+$offset;
+        $sql = preg_replace('/^([\s(])*SELECT( DISTINCT)?(?!\s*TOP\s*\()/i',"\\1SELECT\\2 TOP $fetch", $sql);
+        $ordering = $this->findOrdering($sql);
+        $orginalOrdering = $this->joinOrdering($ordering, '[__outer__]');
+        $reverseOrdering = $this->joinOrdering($this->reverseDirection($ordering), '[__inner__]');
+        $sql = "SELECT * FROM (SELECT TOP {$limit} * FROM ($sql) as [__inner__] {$reverseOrdering}) as [__outer__] {$orginalOrdering}";
+        return $sql;
+    }
 
-	/**
-	 * Base on simplified syntax http://msdn2.microsoft.com/en-us/library/aa259187(SQL.80).aspx
-	 *
-	 * @param string $sql $sql
-	 * @return array ordering expression as key and ordering direction as value
-	 *
-	 * @author Wei Zhuo <weizhuo[at]gmail[dot]com>
-	 */
-	protected function findOrdering($sql)
-	{
-		if(!preg_match('/ORDER BY/i', $sql))
-			return array();
-		$matches=array();
-		$ordering=array();
-		preg_match_all('/(ORDER BY)[\s"\[](.*)(ASC|DESC)?(?:[\s"\[]|$|COMPUTE|FOR)/i', $sql, $matches);
-		if(count($matches)>1 && count($matches[2]) > 0)
-		{
-			$parts = explode(',', $matches[2][0]);
-			foreach($parts as $part)
-			{
-				$subs=array();
-				if(preg_match_all('/(.*)[\s"\]](ASC|DESC)$/i', trim($part), $subs))
-				{
-					if(count($subs) > 1 && count($subs[2]) > 0)
-					{
-						$name='';
-						foreach(explode('.', $subs[1][0]) as $p)
-						{
-							if($name!=='')
-								$name.='.';
-							$name.='[' . trim($p, '[]') . ']';
-						}
-						$ordering[$name] = $subs[2][0];
-					}
-					//else what?
-				}
-				else
-					$ordering[trim($part)] = 'ASC';
-			}
-		}
+    /**
+     * Base on simplified syntax http://msdn2.microsoft.com/en-us/library/aa259187(SQL.80).aspx
+     *
+     * @param string $sql $sql
+     * @return array ordering expression as key and ordering direction as value
+     *
+     * @author Wei Zhuo <weizhuo[at]gmail[dot]com>
+     */
+    protected function findOrdering($sql)
+    {
+        if(!preg_match('/ORDER BY/i', $sql))
+            return array();
+        $matches=array();
+        $ordering=array();
+        preg_match_all('/(ORDER BY)[\s"\[](.*)(ASC|DESC)?(?:[\s"\[]|$|COMPUTE|FOR)/i', $sql, $matches);
+        if(count($matches)>1 && count($matches[2]) > 0)
+        {
+            $parts = explode(',', $matches[2][0]);
+            foreach($parts as $part)
+            {
+                $subs=array();
+                if(preg_match_all('/(.*)[\s"\]](ASC|DESC)$/i', trim($part), $subs))
+                {
+                    if(count($subs) > 1 && count($subs[2]) > 0)
+                    {
+                        $name='';
+                        foreach(explode('.', $subs[1][0]) as $p)
+                        {
+                            if($name!=='')
+                                $name.='.';
+                            $name.='[' . trim($p, '[]') . ']';
+                        }
+                        $ordering[$name] = $subs[2][0];
+                    }
+                    //else what?
+                }
+                else
+                    $ordering[trim($part)] = 'ASC';
+            }
+        }
 
-		// replacing column names with their alias names
-		foreach($ordering as $name => $direction)
-		{
-			$matches = array();
-			$pattern = '/\s+'.str_replace(array('[',']'), array('\[','\]'), $name).'\s+AS\s+(\[[^\]]+\])/i';
-			preg_match($pattern, $sql, $matches);
-			if(isset($matches[1]))
-			{
-				$ordering[$matches[1]] = $ordering[$name];
-				unset($ordering[$name]);
-			}
-		}
+        // replacing column names with their alias names
+        foreach($ordering as $name => $direction)
+        {
+            $matches = array();
+            $pattern = '/\s+'.str_replace(array('[',']'), array('\[','\]'), $name).'\s+AS\s+(\[[^\]]+\])/i';
+            preg_match($pattern, $sql, $matches);
+            if(isset($matches[1]))
+            {
+                $ordering[$matches[1]] = $ordering[$name];
+                unset($ordering[$name]);
+            }
+        }
 
-		return $ordering;
-	}
+        return $ordering;
+    }
 
-	/**
-	 * @param array $orders ordering obtained from findOrdering()
-	 * @param string $newPrefix new table prefix to the ordering columns
-	 * @return string concat the orderings
-	 *
-	 * @author Wei Zhuo <weizhuo[at]gmail[dot]com>
-	 */
-	protected function joinOrdering($orders, $newPrefix)
-	{
-		if(count($orders)>0)
-		{
-			$str=array();
-			foreach($orders as $column => $direction)
-				$str[] = $column.' '.$direction;
-			$orderBy = 'ORDER BY '.implode(', ', $str);
-			return preg_replace('/\s+\[[^\]]+\]\.(\[[^\]]+\])/i', ' '.$newPrefix.'.\1', $orderBy);
-		}
-	}
+    /**
+     * @param array $orders ordering obtained from findOrdering()
+     * @param string $newPrefix new table prefix to the ordering columns
+     * @return string concat the orderings
+     *
+     * @author Wei Zhuo <weizhuo[at]gmail[dot]com>
+     */
+    protected function joinOrdering($orders, $newPrefix)
+    {
+        if(count($orders)>0)
+        {
+            $str=array();
+            foreach($orders as $column => $direction)
+                $str[] = $column.' '.$direction;
+            $orderBy = 'ORDER BY '.implode(', ', $str);
+            return preg_replace('/\s+\[[^\]]+\]\.(\[[^\]]+\])/i', ' '.$newPrefix.'.\1', $orderBy);
+        }
+    }
 
-	/**
-	 * @param array $orders original ordering
-	 * @return array ordering with reversed direction.
-	 *
-	 * @author Wei Zhuo <weizhuo[at]gmail[dot]com>
-	 */
-	protected function reverseDirection($orders)
-	{
-		foreach($orders as $column => $direction)
-			$orders[$column] = strtolower(trim($direction))==='desc' ? 'ASC' : 'DESC';
-		return $orders;
-	}
-	
+    /**
+     * @param array $orders original ordering
+     * @return array ordering with reversed direction.
+     *
+     * @author Wei Zhuo <weizhuo[at]gmail[dot]com>
+     */
+    protected function reverseDirection($orders)
+    {
+        foreach($orders as $column => $direction)
+            $orders[$column] = strtolower(trim($direction))==='desc' ? 'ASC' : 'DESC';
+        return $orders;
+    }
+    
     /**
      * Returns if MSSQL version has Limit/Offset support (versions older than 2012).
-	 * How to determine the version and edition of SQL Server (http://support.microsoft.com/kb/321185)
+     * How to determine the version and edition of SQL Server (http://support.microsoft.com/kb/321185)
      * @return boolean if mssql support limit/offset.
-	 *
-	 * @author Henrique Dias <heukirne[at]gmail[dot]com>
-	 */
+     *
+     * @author Henrique Dias <heukirne[at]gmail[dot]com>
+     */
     protected function olderMssql()
     {
         $this->db->open();
-		$version = preg_split("/\./",$this->db->pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
-		return $version[0] < 11;
+        $version = preg_split("/\./",$this->db->pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
+        return $version[0] < 11;
     }
-	
+    
 }
