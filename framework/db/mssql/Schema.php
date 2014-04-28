650b2341542d5b39054c9f363d4208b37797578c
Henrique Dias <heukirne@gmail.com>
2014-04-28 09:13:42 -0300

    Fix MSSQL Information Schema Case
    In MSSQL information schema are upper case by deafult

--- framework/db/mssql/Schema.php
+++ framework/db/mssql/Schema.php
@@ -22,10 +22,6 @@ class Schema extends \yii\db\Schema
      */
     public $defaultSchema = 'dbo';
     /**
-     * @var string the collation used for the current database.
-     */
-    protected $collation = null;
-    /**
      * @var array mapping from physical column types (keys) to abstract column types (values)
      */
     public $typeMap = [
@@ -239,7 +235,7 @@ class Schema extends \yii\db\Schema
      */
     protected function findColumns($table)
     {
-        $columnsTableName = $this->schemaCase('information_schema.columns');
+        $columnsTableName = 'INFORMATION_SCHEMA.COLUMNS';
         $whereSql = "[t1].[table_name] = '{$table->name}'";
         if ($table->catalogName !== null) {
             $columnsTableName = "{$table->catalogName}.{$columnsTableName}";
@@ -296,8 +292,8 @@ SQL;
      */
     protected function findPrimaryKeys($table)
     {
-        $keyColumnUsageTableName = $this->schemaCase('information_schema.key_column_usage');
-        $tableConstraintsTableName = $this->schemaCase('information_schema.table_constraints');
+        $keyColumnUsageTableName = 'INFORMATION_SCHEMA.KEY_COLUMN_USAGE';
+        $tableConstraintsTableName = 'INFORMATION_SCHEMA.TABLE_CONSTRAINTS';
         if ($table->catalogName !== null) {
             $keyColumnUsageTableName = $table->catalogName . '.' . $keyColumnUsageTableName;
             $tableConstraintsTableName = $table->catalogName . '.' . $tableConstraintsTableName;
@@ -329,8 +325,8 @@ SQL;
      */
     protected function findForeignKeys($table)
     {
-        $referentialConstraintsTableName = $this->schemaCase('information_schema.referential_constraints');
-        $keyColumnUsageTableName = $this->schemaCase('information_schema.key_column_usage');
+        $referentialConstraintsTableName = 'INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS';
+        $keyColumnUsageTableName = 'INFORMATION_SCHEMA.KEY_COLUMN_USAGE';
         if ($table->catalogName !== null) {
             $referentialConstraintsTableName = $table->catalogName . '.' . $referentialConstraintsTableName;
             $keyColumnUsageTableName = $table->catalogName . '.' . $keyColumnUsageTableName;
@@ -378,37 +374,10 @@ SQL;
 
         $sql = <<<SQL
 SELECT [t].[table_name]
-FROM {$this->schemaCase('[information_schema].[tables]')} AS [t]
+FROM [INFORMATION_SCHEMA].[TABLES] AS [t]
 WHERE [t].[table_schema] = :schema AND [t].[table_type] = 'BASE TABLE'
 SQL;
 
         return $this->db->createCommand($sql, [':schema' => $schema])->queryColumn();
     }
-	
-    /**
-     * Returns the right schema case-sensitive for MSSQL (versions older than 2012).
-	 * View Collation Information (http://msdn.microsoft.com/en-us/library/hh230914.aspx)
-	 * Selecting a SQL Server Collation (http://msdn.microsoft.com/en-us/library/ms144250(v=sql.105).aspx)
-     * @return string right schema case-sensitive.
-	 *
-	 * @author Henrique Dias <heukirne[at]gmail[dot]com>
-	 */
-    protected function schemaCase($str)
-    {
-	
-		if ($this->collation == null)  {			
-			$sql = <<<SQL
-SELECT collation_name FROM sys.databases
-WHERE name = :db;
-SQL;
-		
-			$this->collation = $this->db->createCommand($sql, [':db' => 'master'])->queryColumn();		
-		}
-	
-
-		
-		if (strpos($this->collation[0], 'CI') === false) return strtoupper($str);
-		else $str;
-    }	
-
 }
