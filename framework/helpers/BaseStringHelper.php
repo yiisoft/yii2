489699b65be09f79c1295b5c66957948bdaca462
DrMabuse <pascalbrewing@googlemail.com>
2014-04-30 22:41:01 +0200

    refactor code to us the truncatHtml over the exist methods truncate an truncateWords

--- framework/helpers/BaseStringHelper.php
+++ framework/helpers/BaseStringHelper.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @link http://www.yiiframework.com/
+ * @link      http://www.yiiframework.com/
  * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
+ * @license   http://www.yiiframework.com/license/
  */
 
 namespace yii\helpers;
@@ -14,14 +14,16 @@ namespace yii\helpers;
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @author Alex Makarov <sam@rmcreative.ru>
- * @since 2.0
+ * @since  2.0
  */
 class BaseStringHelper
 {
     /**
      * Returns the number of bytes in the given string.
      * This method ensures the string is treated as a byte array by using `mb_strlen()`.
+     *
      * @param string $string the string being measured for length
+     *
      * @return integer the number of bytes in the given string.
      */
     public static function byteLength($string)
@@ -32,9 +34,11 @@ class BaseStringHelper
     /**
      * Returns the portion of string specified by the start and length parameters.
      * This method ensures the string is treated as a byte array by using `mb_substr()`.
-     * @param string $string the input string. Must be one character or longer.
-     * @param integer $start the starting position
+     *
+     * @param string  $string the input string. Must be one character or longer.
+     * @param integer $start  the starting position
      * @param integer $length the desired portion length
+     *
      * @return string the extracted part of string, or FALSE on failure or an empty string.
      * @see http://www.php.net/manual/en/function.substr.php
      */
@@ -51,18 +55,19 @@ class BaseStringHelper
      * file paths, php's `basename()` should work fine for you.
      * Note: this method is not aware of the actual filesystem, or path components such as "..".
      *
-     * @param string $path A path string.
+     * @param string $path   A path string.
      * @param string $suffix If the name component ends in suffix this will also be cut off.
+     *
      * @return string the trailing name component of the given path.
      * @see http://www.php.net/manual/en/function.basename.php
      */
     public static function basename($path, $suffix = '')
     {
-        if (($len = mb_strlen($suffix)) > 0 && mb_substr($path, -$len) == $suffix) {
+        if ( ($len = mb_strlen($suffix)) > 0 && mb_substr($path, -$len) == $suffix ) {
             $path = mb_substr($path, 0, -$len);
         }
         $path = rtrim(str_replace('\\', '/', $path), '/\\');
-        if (($pos = mb_strrpos($path, '/')) !== false) {
+        if ( ($pos = mb_strrpos($path, '/')) !== false ) {
             return mb_substr($path, $pos + 1);
         }
 
@@ -75,49 +80,58 @@ class BaseStringHelper
      * both \ and / as directory separators, independent of the operating system.
      *
      * @param string $path A path string.
+     *
      * @return string the parent directory's path.
      * @see http://www.php.net/manual/en/function.basename.php
      */
     public static function dirname($path)
     {
         $pos = mb_strrpos(str_replace('\\', '/', $path), '/');
-        if ($pos !== false) {
+        if ( $pos !== false ) {
             return mb_substr($path, 0, $pos);
         } else {
             return '';
         }
     }
-    
+
     /**
      * Truncates a string to the number of characters specified.
      *
-     * @param string $string The string to truncate.
-     * @param integer $length How many characters from original string to include into truncated string.
-     * @param string $suffix String to append to the end of truncated string.
-     * @param string $encoding The charset to use, defaults to charset currently used by application.
+     * @param string  $string   The string to truncate.
+     * @param integer $length   How many characters from original string to include into truncated string.
+     * @param string  $suffix   String to append to the end of truncated string.
+     * @param string  $encoding The charset to use, defaults to charset currently used by application.
+     *
      * @return string the truncated string.
      */
-    public static function truncate($string, $length, $suffix = '...', $encoding = null)
+    public static function truncate($string, $length, $suffix = '...', $encoding = null, $asHtml = false)
     {
-        if (mb_strlen($string, $encoding ?: \Yii::$app->charset) > $length) {
-            return trim(mb_substr($string, 0, $length, $encoding ?: \Yii::$app->charset)) . $suffix;
+        if ( $asHtml )
+            return self::truncateHtml($string, $length, $suffix, true);
+
+        if ( mb_strlen($string, $encoding ? : \Yii::$app->charset) > $length ) {
+            return trim(mb_substr($string, 0, $length, $encoding ? : \Yii::$app->charset)) . $suffix;
         } else {
             return $string;
         }
     }
-    
+
     /**
      * Truncates a string to the number of words specified.
      *
-     * @param string $string The string to truncate.
-     * @param integer $count How many words from original string to include into truncated string.
-     * @param string $suffix String to append to the end of truncated string.
+     * @param string  $string The string to truncate.
+     * @param integer $count  How many words from original string to include into truncated string.
+     * @param string  $suffix String to append to the end of truncated string.
+     *
      * @return string the truncated string.
      */
-    public static function truncateWords($string, $count, $suffix = '...')
+    public static function truncateWords($string, $count, $suffix = '...', $asHtml = false)
     {
+        if ( $asHtml )
+            return self::truncateHtml($string, $count, $suffix, false);
+
         $words = preg_split('/(\s+)/u', trim($string), null, PREG_SPLIT_DELIM_CAPTURE);
-        if (count($words) / 2 > $count) {
+        if ( count($words) / 2 > $count ) {
             return implode('', array_slice($words, 0, ($count * 2) - 1)) . $suffix;
         } else {
             return $string;
@@ -126,101 +140,97 @@ class BaseStringHelper
 
     /**
      * Truncates a html string of the number of words or exact
+     *
      * @static
-     * @param string $text
-     * @param int $length
+     *
+     * @param string $string
+     * @param int    $count
      * @param string $ending
-     * @param bool $exact
-     * @param bool $considerHtml
+     * @param bool   $exact
+     * @param bool   $considerHtml
+     *
      * @return string
      */
 
-    public static function truncateHtml($text, $length = 100, $ending = '...', $exact = false, $considerHtml = true) {
-        if ($considerHtml) {
-            // if the plain text is shorter than the maximum length, return the whole text
-            if (strlen(preg_replace('/<.*?>/', '', $text)) <= $length) {
-                return $text;
-            }
-            // splits all html-tags to scanable lines
-            preg_match_all('/(<.+?>)?([^<>]*)/s', $text, $lines, PREG_SET_ORDER);
-            $total_length = strlen($ending);
-            $open_tags = array();
-            $truncate = '';
-            foreach ($lines as $line_matchings) {
-                // if there is any html-tag in this line, handle it and add it (uncounted) to the output
-                if (!empty($line_matchings[1])) {
-                    // if it's an "empty element" with or without xhtml-conform closing slash
-                    if (preg_match('/^<(\s*.+?\/\s*|\s*(img|br|input|hr|area|base|basefont|col|frame|isindex|link|meta|param)(\s.+?)?)>$/is', $line_matchings[1])) {
-                        // do nothing
-                        // if tag is a closing tag
-                    } else if (preg_match('/^<\s*\/([^\s]+?)\s*>$/s', $line_matchings[1], $tag_matchings)) {
-                        // delete tag from $open_tags list
-                        $pos = array_search($tag_matchings[1], $open_tags);
-                        if ($pos !== false) {
-                            unset($open_tags[$pos]);
-                        }
-                        // if tag is an opening tag
-                    } else if (preg_match('/^<\s*([^\s>!]+).*?>$/s', $line_matchings[1], $tag_matchings)) {
-                        // add tag to the beginning of $open_tags list
-                        array_unshift($open_tags, strtolower($tag_matchings[1]));
+    private static function truncateHtml($string, $count, $suffix, $exact = false)
+    {
+        // if the plain text is shorter than the maximum length, return the whole text
+        if ( strlen(preg_replace('/<.*?>/', '', $string)) <= $count ) {
+            return $string;
+        }
+
+        // splits all html-tags to scanable lines
+        preg_match_all('/(<.+?>)?([^<>]*)/s', $string, $lines, PREG_SET_ORDER);
+
+        $totalCount = strlen($suffix);
+        $openTags   = [];
+        $truncate   = '';
+        foreach ( $lines as $lineMap ) {
+            // if there is any html-tag in this line, handle it and add it (uncounted) to the output
+            if ( !empty($lineMap[1]) ) {
+                // if it's an "empty element" with or without xhtml-conform closing slash
+                if ( preg_match('/^<(\s*.+?\/\s*|\s*(img|br|input|hr|area|base|basefont|col|frame|isindex|link|meta|param)(\s.+?)?)>$/is', $lineMap[1]) ) {
+                    // do nothing if tag is a closing tag
+                } else if ( preg_match('/^<\s*\/([^\s]+?)\s*>$/s', $lineMap[1], $tagMap) ) {
+                    // delete tag from $open_tags list
+                    $pos = array_search($tagMap[1], $openTags);
+                    if ( $pos !== false ) {
+                        ArrayHelper::remove($openTags,$pos);
                     }
-                    // add html-tag to $truncate'd text
-                    $truncate .= $line_matchings[1];
+                    // if tag is an opening tag
+                } else if ( preg_match('/^<\s*([^\s>!]+).*?>$/s', $lineMap[1], $tagMap) ) {
+                    // add tag to the beginning of $open_tags list
+                    array_unshift($openTags, strtolower($tagMap[1]));
                 }
-                // calculate the length of the plain text part of the line; handle entities as one character
-                $content_length = strlen(preg_replace('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|[0-9a-f]{1,6};/i', ' ', $line_matchings[2]));
-                if ($total_length+$content_length> $length) {
-                    // the number of characters which are left
-                    $left = $length - $total_length;
-                    $entities_length = 0;
-                    // search for html entities
-                    if (preg_match_all('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|[0-9a-f]{1,6};/i', $line_matchings[2], $entities, PREG_OFFSET_CAPTURE)) {
-                        // calculate the real length of all entities in the legal range
-                        foreach ($entities[0] as $entity) {
-                            if ($entity[1]+1-$entities_length <= $left) {
-                                $left--;
-                                $entities_length += strlen($entity[0]);
-                            } else {
-                                // no more characters left
-                                break;
-                            }
+                // add html-tag to $truncate'd text
+                $truncate .= $lineMap[1];
+            }
+            // calculate the length of the plain text part of the line; handle entities as one character
+            $stringLength     = strlen(preg_replace('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|[0-9a-f]{1,6};/i', ' ', $lineMap[2]));
+            if ( ($totalCount + $stringLength) > $count ) {
+                // the number of characters which are left
+                $left           = $count - $totalCount;
+                $entitiesCount  = 0;
+                // search for html entities
+                if ( preg_match_all('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|[0-9a-f]{1,6};/i', $lineMap[2], $entities, PREG_OFFSET_CAPTURE) ) {
+                    // calculate the real length of all entities in the legal range
+                    foreach ( $entities[0] as $entity ) {
+                        if ( $entity[1] + 1 - $entitiesCount <= $left ) {
+                            $left--;
+                            $entitiesCount += strlen($entity[0]);
+                        } else {
+                            // no more characters left
+                            break;
                         }
                     }
-                    $truncate .= substr($line_matchings[2], 0, $left+$entities_length);
-                    // maximum lenght is reached, so get off the loop
-                    break;
-                } else {
-                    $truncate .= $line_matchings[2];
-                    $total_length += $content_length;
                 }
-                // if the maximum length is reached, get off the loop
-                if($total_length>= $length) {
-                    break;
-                }
-            }
-        } else {
-            if (strlen($text) <= $length) {
-                return $text;
+                $truncate   .= substr($lineMap[2], 0, $left + $entitiesCount);
+                // maximum lenght is reached, so get off the loop
+                break;
             } else {
-                $truncate = substr($text, 0, $length - strlen($ending));
+                $truncate   .= $lineMap[2];
+                $totalCount += $stringLength;
+            }
+            // if the maximum length is reached, get off the loop
+            if ( $totalCount >= $count ) {
+                break;
             }
         }
+
         // if the words shouldn't be cut in the middle...
-        if (!$exact) {
+        if ( !$exact ) {
             // ...search the last occurance of a space...
-            $spacepos = strrpos($truncate, ' ');
-            if (isset($spacepos)) {
+            $space          = strrpos($truncate, ' ');
+            if ( isset($space) ) {
                 // ...and cut the text in this position
-                $truncate = substr($truncate, 0, $spacepos);
+                $truncate   = substr($truncate, 0, $space);
             }
         }
         // add the defined ending to the text
-        $truncate .= $ending;
-        if($considerHtml) {
-            // close all unclosed html-tags
-            foreach ($open_tags as $tag) {
-                $truncate .= '</' . $tag . '>';
-            }
+        $truncate       .= $suffix;
+        // close all unclosed html-tags
+        foreach ( $openTags as $tag ) {
+            $truncate   .= Html::endTag($tag);
         }
         return $truncate;
     }
