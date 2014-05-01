899eb64d91a938d5da886b5babe9a2a6fd149ea9
DrMabuse <pascalbrewing@googlemail.com>
2014-05-01 07:40:38 +0200

    whitespace removed

--- framework/helpers/BaseStringHelper.php
+++ framework/helpers/BaseStringHelper.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @link      http://www.yiiframework.com/
+ * @link http://www.yiiframework.com/
  * @copyright Copyright (c) 2008 Yii Software LLC
- * @license   http://www.yiiframework.com/license/
+ * @license http://www.yiiframework.com/license/
  */
 
 namespace yii\helpers;
@@ -14,16 +14,14 @@ namespace yii\helpers;
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @author Alex Makarov <sam@rmcreative.ru>
- * @since  2.0
+ * @since 2.0
  */
 class BaseStringHelper
 {
     /**
      * Returns the number of bytes in the given string.
      * This method ensures the string is treated as a byte array by using `mb_strlen()`.
-     *
      * @param string $string the string being measured for length
-     *
      * @return integer the number of bytes in the given string.
      */
     public static function byteLength($string)
@@ -34,11 +32,9 @@ class BaseStringHelper
     /**
      * Returns the portion of string specified by the start and length parameters.
      * This method ensures the string is treated as a byte array by using `mb_substr()`.
-     *
-     * @param string  $string the input string. Must be one character or longer.
-     * @param integer $start  the starting position
+     * @param string $string the input string. Must be one character or longer.
+     * @param integer $start the starting position
      * @param integer $length the desired portion length
-     *
      * @return string the extracted part of string, or FALSE on failure or an empty string.
      * @see http://www.php.net/manual/en/function.substr.php
      */
@@ -55,19 +51,18 @@ class BaseStringHelper
      * file paths, php's `basename()` should work fine for you.
      * Note: this method is not aware of the actual filesystem, or path components such as "..".
      *
-     * @param string $path   A path string.
+     * @param string $path A path string.
      * @param string $suffix If the name component ends in suffix this will also be cut off.
-     *
      * @return string the trailing name component of the given path.
      * @see http://www.php.net/manual/en/function.basename.php
      */
     public static function basename($path, $suffix = '')
     {
-        if ( ($len = mb_strlen($suffix)) > 0 && mb_substr($path, -$len) == $suffix ) {
+        if (($len = mb_strlen($suffix)) > 0 && mb_substr($path, -$len) == $suffix) {
             $path = mb_substr($path, 0, -$len);
         }
         $path = rtrim(str_replace('\\', '/', $path), '/\\');
-        if ( ($pos = mb_strrpos($path, '/')) !== false ) {
+        if (($pos = mb_strrpos($path, '/')) !== false) {
             return mb_substr($path, $pos + 1);
         }
 
@@ -80,64 +75,58 @@ class BaseStringHelper
      * both \ and / as directory separators, independent of the operating system.
      *
      * @param string $path A path string.
-     *
      * @return string the parent directory's path.
      * @see http://www.php.net/manual/en/function.basename.php
      */
     public static function dirname($path)
     {
         $pos = mb_strrpos(str_replace('\\', '/', $path), '/');
-        if ( $pos !== false ) {
+        if ($pos !== false) {
             return mb_substr($path, 0, $pos);
         } else {
             return '';
         }
     }
-
+    
     /**
      * Truncates a string to the number of characters specified.
      *
-     * @param string  $string   The string to truncate.
-     * @param integer $length   How many characters from original string to include into truncated string.
-     * @param string  $suffix   String to append to the end of truncated string.
-     * @param string  $encoding The charset to use, defaults to charset currently used by application.
-     *
+     * @param string $string The string to truncate.
+     * @param integer $length How many characters from original string to include into truncated string.
+     * @param string $suffix String to append to the end of truncated string.
+     * @param string $encoding The charset to use, defaults to charset currently used by application.
      * @return string the truncated string.
      */
-    public static function truncate($string, $length, $suffix = '...', $encoding = null, $asHtml = false)
+    public static function truncate($string, $length, $suffix = '...', $encoding = null,$asHtml = false)
     {
         if ( $asHtml )
             return self::truncateHtml($string, $length, $suffix, true);
-
-        if ( mb_strlen($string, $encoding ? : \Yii::$app->charset) > $length ) {
-            return trim(mb_substr($string, 0, $length, $encoding ? : \Yii::$app->charset)) . $suffix;
+        if (mb_strlen($string, $encoding ?: \Yii::$app->charset) > $length) {
+            return trim(mb_substr($string, 0, $length, $encoding ?: \Yii::$app->charset)) . $suffix;
         } else {
             return $string;
         }
     }
-
+    
     /**
      * Truncates a string to the number of words specified.
      *
-     * @param string  $string The string to truncate.
-     * @param integer $count  How many words from original string to include into truncated string.
-     * @param string  $suffix String to append to the end of truncated string.
-     *
+     * @param string $string The string to truncate.
+     * @param integer $count How many words from original string to include into truncated string.
+     * @param string $suffix String to append to the end of truncated string.
      * @return string the truncated string.
      */
-    public static function truncateWords($string, $count, $suffix = '...', $asHtml = false)
+    public static function truncateWords($string, $count, $suffix = '...',$asHtml = false)
     {
         if ( $asHtml )
             return self::truncateHtml($string, $count, $suffix, false);
-
         $words = preg_split('/(\s+)/u', trim($string), null, PREG_SPLIT_DELIM_CAPTURE);
-        if ( count($words) / 2 > $count ) {
+        if (count($words) / 2 > $count) {
             return implode('', array_slice($words, 0, ($count * 2) - 1)) . $suffix;
         } else {
             return $string;
         }
     }
-
 
     /**
      * Truncates a html string of the number of words or exact
