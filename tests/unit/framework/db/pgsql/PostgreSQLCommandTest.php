48dbde3911ef7f11865e7b025f414c0a1a06fb9c
Alexander Makarov <sam@rmcreative.ru>
2014-05-06 19:14:10 +0400

    Fixed PostgreSQL test

--- tests/unit/framework/db/pgsql/PostgreSQLCommandTest.php
+++ tests/unit/framework/db/pgsql/PostgreSQLCommandTest.php
@@ -6,4 +6,13 @@ use yiiunit\framework\db\CommandTest;
 class PostgreSQLCommandTest extends CommandTest
 {
     public $driverName = 'pgsql';
+
+    public function testAutoQuoting()
+    {
+        $db = $this->getConnection(false);
+
+        $sql = 'SELECT [[id]], [[t.name]] FROM {{customer}} t';
+        $command = $db->createCommand($sql);
+        $this->assertEquals('SELECT "id", "t"."name" FROM "customer" t', $command->sql);
+    }
 } 
