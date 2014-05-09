47960e8f0bf07034434d6614d0366eb7cb5ecc59
Paul Klimov <klimov.paul@gmail.com>
2014-05-09 15:35:14 +0300

    Fix a typo

--- extensions/authclient/clients/VKontakte.php
+++ extensions/authclient/clients/VKontakte.php
@@ -21,7 +21,7 @@ use yii\authclient\OAuth2;
  *     'authClientCollection' => [
  *         'class' => 'yii\authclient\Collection',
  *         'clients' => [
- *             'google' => [
+ *             'vkontakte' => [
  *                 'class' => 'yii\authclient\clients\VKontakte',
  *                 'clientId' => 'vkontakte_client_id',
  *                 'clientSecret' => 'vkontakte_client_secret',
