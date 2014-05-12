d0cd3889832352ec3c8cfd83c7536d55ef29c551
Carsten Brandt <mail@cebe.cc>
2014-05-12 13:30:38 +0200

    added errorInfo to exception views

--- views/errorHandler/previousException.php
+++ views/errorHandler/previousException.php
@@ -18,5 +18,8 @@
     </h2>
     <h3><?= nl2br($handler->htmlEncode($exception->getMessage())) ?></h3>
     <p>in <span class="file"><?= $exception->getFile() ?></span> at line <span class="line"><?= $exception->getLine() ?></span></p>
+    <?php if ($exception instanceof \yii\db\Exception && !empty($exception->errorInfo)) {
+        echo '<pre>Error Info: ' . print_r($exception->errorInfo, true) . '</pre>';
+    } ?>
     <?= $handler->renderPreviousExceptions($exception) ?>
 </div>
