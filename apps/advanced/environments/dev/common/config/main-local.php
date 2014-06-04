df65a0f9de8d9530656a27f057dd380f9806201c
Carsten Brandt <mail@cebe.cc>
2014-06-04 17:43:09 +0200

    added explicit comment about file transport to mail config
    fixes #3694

--- apps/advanced/environments/dev/common/config/main-local.php
+++ apps/advanced/environments/dev/common/config/main-local.php
@@ -11,6 +11,9 @@ return [
         'mail' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
             'useFileTransport' => true,
         ],
     ],
--- apps/basic/config/web.php
+++ apps/basic/config/web.php
@@ -19,6 +19,9 @@ $config = [
         ],
         'mail' => [
             'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
             'useFileTransport' => true,
         ],
         'log' => [
