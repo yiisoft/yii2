1cef60db17a95c6f682db7a7a75d088295562710
Carsten Brandt <mail@cebe.cc>
2014-06-25 00:35:03 +0200

    debug toolbar: use textbox filter as fallback
    see https://github.com/yiisoft/yii2/commit/bf599b96bb47f006a0f6fb4859ead30648fc5dde

--- extensions/debug/views/default/index.php
+++ extensions/debug/views/default/index.php
@@ -41,7 +41,7 @@ if (isset($this->context->module->panels['db']) && isset($this->context->module-
         }
     }
     $codes = array_unique($codes, SORT_NUMERIC);
-    $statusCodes = !empty($codes) ? array_combine($codes, $codes) : false;
+    $statusCodes = !empty($codes) ? array_combine($codes, $codes) : null;
 
     echo GridView::widget([
         'dataProvider' => $dataProvider,
