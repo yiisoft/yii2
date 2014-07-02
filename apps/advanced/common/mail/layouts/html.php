00e2540c5f0368f4507ad25d8eb74dd4bc7f757e
Carsten Brandt <mail@cebe.cc>
2014-07-02 17:05:00 +0200

    typo

--- apps/advanced/common/mail/layouts/html.php
+++ apps/advanced/common/mail/layouts/html.php
@@ -2,7 +2,7 @@
 use yii\helpers\Html;
 
 /* @var $this \yii\web\View view component instance */
-/* @var $message \yii\mail\MessageInterface the message bing composed */
+/* @var $message \yii\mail\MessageInterface the message being composed */
 /* @var $content string main view render result */
 ?>
 <?php $this->beginPage() ?>
