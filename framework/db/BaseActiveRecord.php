67bd4972ba6b5bff2f84960698b7bf02887996e0
miramir <gmiramir@gmail.com>
2014-07-07 13:38:02 +0600

    Check availabilty field in BaseActiveRecord::populateRecord
    check with Object::canSetProperty()
    
    fixes #4305, close #4211

--- framework/db/BaseActiveRecord.php
+++ framework/db/BaseActiveRecord.php
@@ -1042,7 +1042,7 @@ abstract class BaseActiveRecord extends Model implements ActiveRecordInterface
         foreach ($row as $name => $value) {
             if (isset($columns[$name])) {
                 $record->_attributes[$name] = $value;
-            } else {
+            } elseif ($record->canSetProperty($name)) {
                 $record->$name = $value;
             }
         }
