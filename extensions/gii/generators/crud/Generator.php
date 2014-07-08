492337de2d19a967c693c1bfdba9a8510103506d
Alexander Makarov <sam@rmcreative.ru>
2014-07-08 16:59:15 +0400

    else if → elseif

--- extensions/gii/generators/crud/Generator.php
+++ extensions/gii/generators/crud/Generator.php
@@ -255,7 +255,7 @@ class Generator extends \yii\gii\Generator
                 }
                 return "\$form->field(\$model, '$attribute')->dropDownList("
                     . preg_replace("/\n\s*/", ' ', VarDumper::export($dropDownOptions)).", ['prompt' => ''])";
-            } else if ($column->phpType !== 'string' || $column->size === null) {
+            } elseif ($column->phpType !== 'string' || $column->size === null) {
                 return "\$form->field(\$model, '$attribute')->$input()";
             } else {
                 return "\$form->field(\$model, '$attribute')->$input(['maxlength' => $column->size])";
