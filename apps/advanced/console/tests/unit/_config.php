d2370fe5eba2a4d25f5bd159b351017f35d1c386
London <London@LondonPC>
2014-07-10 10:39:29 +0400

    added common tier config

--- apps/advanced/console/tests/unit/_config.php
+++ apps/advanced/console/tests/unit/_config.php
@@ -1,6 +1,8 @@
 <?php
 
 return yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../../../common/config/main.php'),
+    require(__DIR__ . '/../../../common/config/main-local.php'),
     require(__DIR__ . '/../../config/main.php'),
     require(__DIR__ . '/../../config/main-local.php'),
     require(__DIR__ . '/../_config.php'),
