7ef0d760486552541b9a78de8b8ff37ffa21946c
Alexander Makarov <sam@rmcreative.ru>
2014-07-16 00:35:44 +0400

    Corrected app/link for Windows once again

--- build/controllers/AppController.php
+++ build/controllers/AppController.php
@@ -29,7 +29,7 @@ class AppController extends Controller
      */
     protected function unlink($file)
     {
-        if (is_dir($file) && !is_link($file)) {
+        if (is_dir($file) && strncasecmp(PHP_OS, 'WIN', 3) === 0) {
             rmdir($file);
         } else {
             unlink($file);
