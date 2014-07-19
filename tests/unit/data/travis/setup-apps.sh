ab9d9063510d068a63c15284c653076b0b46d4b4
Qiang Xue <qiang.xue@gmail.com>
2014-07-19 19:51:44 -0400

    Fixed test break.

--- tests/unit/data/travis/setup-apps.sh
+++ tests/unit/data/travis/setup-apps.sh
@@ -8,6 +8,7 @@ else
 
     composer install --dev --prefer-dist -d apps/basic
     cd apps/basic && composer require --dev codeception/codeception:2.0.* codeception/specify:* codeception/verify:*
+    sed -i "s/'cookieValidationKey' => ''/'cookieValidationKey' => 'testkey'/" config/web.php
     php vendor/bin/codecept build && cd ../..
 
 
@@ -17,6 +18,8 @@ else
     cd apps/advanced && composer require --dev codeception/codeception:2.0.* codeception/specify:* codeception/verify:*
     ./init --env=Development
     sed -i s/root/travis/ common/config/main-local.php
+    sed -i "s/'cookieValidationKey' => ''/'cookieValidationKey' => 'testkey'/" backend/config/main.php
+    sed -i "s/'cookieValidationKey' => ''/'cookieValidationKey' => 'testkey'/" backend/config/main.php
     cd backend && php ../vendor/bin/codecept build
     cd ../common && php ../vendor/bin/codecept build
     cd ../frontend && php ../vendor/bin/codecept build && cd ../../..
