084403cf9f15d0daf37a51dd25428a46aae16891
Philippe Gaultier <pgaultier@gmail.com>
2014-07-21 15:18:25 +0200

    Fix bad default behavior with Access-Control-Allow-Credentials

--- framework/filters/Cors.php
+++ framework/filters/Cors.php
@@ -79,7 +79,7 @@ class Cors extends ActionFilter
         'Origin' => ['*'],
         'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
         'Access-Control-Request-Headers' => ['*'],
-        'Access-Control-Allow-Credentials' => true,
+        'Access-Control-Allow-Credentials' => null,
         'Access-Control-Max-Age' => 86400,
     ];
 
