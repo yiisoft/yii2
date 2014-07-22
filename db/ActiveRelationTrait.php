fa1aeb7b7b948a2fbd039470650de3bf337b8989
Carsten Brandt <mail@cebe.cc>
2014-07-23 01:25:33 +0200

    add support for array valued attr relations to unlink and link

--- db/ActiveRelationTrait.php
+++ db/ActiveRelationTrait.php
@@ -239,7 +239,7 @@ trait ActiveRelationTrait
 
             $link = array_values(isset($viaQuery) ? $viaQuery->link : $this->link);
             foreach ($primaryModels as $i => $primaryModel) {
-                if ($this->multiple && count($link) < 2 && is_array($keys = $primaryModel->{reset($link)})) {
+                if ($this->multiple && count($link) == 1 && is_array($keys = $primaryModel->{reset($link)})) {
                     $value = [];
                     foreach ($keys as $key) {
                         if (isset($buckets[$key])) {
