71776df5c97831380de57ab0c59a76b484b3bebc
Thiago Talma <thiagotalma@users.noreply.github.com>
2014-07-26 02:04:09 -0300

    Set popup diomensions

--- extensions/authclient/clients/Facebook.php
+++ extensions/authclient/clients/Facebook.php
@@ -78,6 +78,17 @@ class Facebook extends OAuth2
     /**
      * @inheritdoc
      */
+    protected function defaultViewOptions()
+    {
+        return [
+            'popupWidth' => 450,
+            'popupHeight' => 380,
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
     protected function defaultName()
     {
         return 'facebook';
