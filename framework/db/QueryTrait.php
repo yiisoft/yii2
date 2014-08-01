47ee0b87d41cf229f913fe4616b4a3a8f3c02861
Alexander Makarov <sam@rmcreative.ru>
2014-08-01 21:18:48 +0400

    Removed unnecessary code

--- framework/db/QueryTrait.php
+++ framework/db/QueryTrait.php
@@ -261,16 +261,6 @@ trait QueryTrait
                     }
                 }
                 break;
-            case 'IN':
-            case 'NOT IN':
-            case 'LIKE':
-            case 'OR LIKE':
-            case 'NOT LIKE':
-            case 'OR NOT LIKE':
-            case 'ILIKE': // PostgreSQL operator for case insensitive LIKE
-            case 'OR ILIKE':
-            case 'NOT ILIKE':
-            case 'OR NOT ILIKE':
             default:
                 if (array_key_exists(1, $condition) && $this->isEmpty($condition[1])) {
                     return [];
