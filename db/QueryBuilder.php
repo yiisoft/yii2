52c0183982e43f5156825dc937ae7c088446e53d
Alexander Makarov <sam@rmcreative.ru>
2014-08-01 11:35:27 +0400

    Fixes #2315: Any operator now could be used with `yii\db\Query::->where()` operand format

--- db/QueryBuilder.php
+++ db/QueryBuilder.php
@@ -868,7 +868,6 @@ class QueryBuilder extends \yii\base\Object
      * on how to specify a condition.
      * @param array $params the binding parameters to be populated
      * @return string the generated SQL expression
-     * @throws InvalidParamException if the condition is in bad format
      */
     public function buildCondition($condition, &$params)
     {
@@ -882,11 +881,11 @@ class QueryBuilder extends \yii\base\Object
             $operator = strtoupper($condition[0]);
             if (isset($this->conditionBuilders[$operator])) {
                 $method = $this->conditionBuilders[$operator];
-                array_shift($condition);
-                return $this->$method($operator, $condition, $params);
             } else {
-                throw new InvalidParamException('Found unknown operator in query: ' . $operator);
+                $method = 'buildSimpleCondition';
             }
+            array_shift($condition);
+            return $this->$method($operator, $condition, $params);
         } else { // hash format: 'column1' => 'value1', 'column2' => 'value2', ...
             return $this->buildHashCondition($condition, $params);
         }
@@ -1193,5 +1192,30 @@ class QueryBuilder extends \yii\base\Object
         } else {
             throw new InvalidParamException('Subquery for EXISTS operator must be a Query object.');
         }
+    }
+
+    /**
+     * Creates an SQL expressions like `"column" operator value`.
+     * @param string $operator the operator to use. Anything could be used e.g. `>`, `<=`, etc.
+     * @param array $operands contains two column names.
+     * @param array $params the binding parameters to be populated
+     * @return string the generated SQL expression
+     */
+    public function buildSimpleCondition($operator, $operands, &$params)
+    {
+        if (!isset($operands[0], $operands[1])) {
+            throw new InvalidParamException("Operator '$operator' requires two operands.");
+        }
+
+        list($column, $value) = $operands;
+
+        if (strpos($column, '(') === false) {
+            $column = $this->db->quoteColumnName($column);
+        }
+
+        $phName = self::PARAM_PREFIX . 0;
+        $params[$phName] = $value === null ? 'NULL' : $value;
+
+        return "$column $operator $phName";
     }
 }
