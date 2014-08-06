b5bd59ef833a2983219bca050b1a064567792b0c
Alexei Tenitski <alexei@ten.net.nz>
2014-08-07 10:09:03 +1200

    Fix for #2925 and #4290 (Twig does not properly support null attribute and relationship values)

---
+++ extensions/twig/Template.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\twig;
+
+/**
+ * Template base class
+ *
+ * @author Alexei Tenitski <alexei@ten.net.nz>
+ */
+abstract class Template extends \Twig_Template
+{
+    protected function getAttribute($object, $item, array $arguments = array(), $type = \Twig_Template::ANY_CALL, $isDefinedTest = false, $ignoreStrictCheck = false)
+    {
+        // Twig uses isset() to check if attribute exists which does not work when attribute exists but is null
+
+        if ($object instanceof \yii\db\BaseActiveRecord) {
+            return $object->$item;
+        }
+
+        return parent::getAttribute($object, $item, $arguments, $type, $isDefinedTest, $ignoreStrictCheck);
+    }
+}
