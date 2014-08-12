d362af6ede066825bb256834bfdb855b84b6e337
Klimov Paul <klimov.paul@gmail.com>
2014-08-12 11:36:50 +0300

    `yii\behaviors\Sluggable` optimized

--- tests/unit/framework/behaviors/SluggableBehaviorTest.php
+++ tests/unit/framework/behaviors/SluggableBehaviorTest.php
@@ -218,7 +218,7 @@ class ActiveRecordSluggableUnique extends ActiveRecordSluggable
             'sluggable' => [
                 'class' => SluggableBehavior::className(),
                 'attribute' => 'name',
-                'unique' => true,
+                'ensureUnique' => true,
             ],
         ];
     }
