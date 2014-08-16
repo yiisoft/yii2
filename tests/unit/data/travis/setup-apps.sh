3913d64d903a4fc485d4698eea58929118dc0025
Alexander Makarov <sam@rmcreative.ru>
2014-08-16 19:08:24 +0400

    Attempt to fix travis

--- tests/unit/data/travis/setup-apps.sh
+++ tests/unit/data/travis/setup-apps.sh
@@ -7,22 +7,20 @@ else
     # basic application:
 
     composer install --dev --prefer-dist -d apps/basic
-    cd apps/basic && composer require --dev codeception/codeception:2.0.* codeception/specify:* codeception/verify:*
     sed -i "s/'cookieValidationKey' => ''/'cookieValidationKey' => 'testkey'/" config/web.php
-    cd tests && php vendor/bin/codecept build && cd ../../..
+    cd tests && codecept build && cd ../../..
 
 
     # advanced application:
 
     composer install --dev --prefer-dist -d apps/advanced
-    cd apps/advanced && composer require --dev codeception/codeception:2.0.* codeception/specify:* codeception/verify:*
     ./init --env=Development
     sed -i s/root/travis/ common/config/main-local.php
     sed -i "s/'cookieValidationKey' => ''/'cookieValidationKey' => 'testkey'/" frontend/config/main.php
     sed -i "s/'cookieValidationKey' => ''/'cookieValidationKey' => 'testkey'/" backend/config/main.php
-    cd tests/codeception/backend && php ../../../vendor/bin/codecept build
-    cd ../../../common && php ../../../vendor/bin/codecept build
-    cd ../../../frontend && php ../../../vendor/bin/codecept build && cd ../../../../../..
+    cd tests/codeception/backend && codecept build
+    cd ../../../common && codecept build
+    cd ../../../frontend && codecept build && cd ../../../../../..
 
     # boot server
     cd apps && php -S localhost:8080 > /dev/null 2>&1 &
