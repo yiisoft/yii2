b1437b167d97f6aa56ee6e1f7f2f7a8bade87aca
restyler <russiancss@gmail.com>
2014-08-18 18:05:04 +0000

    addFlash method was implemented

--- framework/web/Session.php
+++ framework/web/Session.php
@@ -717,13 +717,35 @@ class Session extends Component implements \IteratorAggregate, \ArrayAccess, \Co
     {
         $counters = $this->get($this->flashParam, []);
         $counters[$key] = $removeAfterAccess ? -1 : 0;
+        $_SESSION[$key] = $value;
+        $_SESSION[$this->flashParam] = $counters;
+    }
 
+    /**
+     * Appends new flash message to the specified key.
+     * @param string $key the key identifying the flash message.
+     * @param mixed $value flash message
+     * @param boolean $removeAfterAccess whether the flash message should be automatically removed only if
+     * it is accessed. If false, the flash message will be automatically removed after the next request,
+     * regardless if it is accessed or not. If true (default value), the flash message will remain until after
+     * it is accessed.
+     * @throws InvalidParamException if exising session variable is not array.
+     * @see getFlash()
+     * @see removeFlash()
+     */
+    public function addFlash($key, $value = true, $removeAfterAccess = true)
+    {
+        $counters = $this->get($this->flashParam, []);
+        $counters[$key] = $removeAfterAccess ? -1 : 0;
         if (!empty($_SESSION[$key])) {
+            // If it's not an array, convert it to array 
+            if (!is_array($_SESSION[$key])) {
+                $_SESSION[$key] = [$_SESSION[$key]];
+            }
             $_SESSION[$key][] = $value;
         } else {
             $_SESSION[$key] = [$value];
         }
-
         $_SESSION[$this->flashParam] = $counters;
     }
 
