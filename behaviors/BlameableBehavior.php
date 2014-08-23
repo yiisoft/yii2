9296552ec86fb7c86c5c3a31ad6526dd42aa864f
Qiang Xue <qiang.xue@gmail.com>
2014-08-23 18:32:24 -0400

    Fixes #4736

--- behaviors/BlameableBehavior.php
+++ behaviors/BlameableBehavior.php
@@ -102,12 +102,8 @@ class BlameableBehavior extends AttributeBehavior
     protected function getValue($event)
     {
         if ($this->value === null) {
-            if (Yii::$app->hasMethod('getUser')) {
-                $user = Yii::$app->getUser();
-                return $user && !$user->isGuest ? $user->id : null;
-            } else {
-                return null;
-            }
+            $user = Yii::$app->get('user', false);
+            return $user && !$user->isGuest ? $user->id : null;
         } else {
             return call_user_func($this->value, $event);
         }
