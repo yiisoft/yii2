fb6696c4ce0681e76c7fad9688e8940e8149889f
Alexander Makarov <sam@rmcreative.ru>
2014-08-27 12:55:49 +0400

    Fixes #4829: Removed `PARSING_ONLY` from `\yii\rest\UrlRule` to allow creating non-GET URLs for forms and tests

--- rest/UrlRule.php
+++ rest/UrlRule.php
@@ -201,9 +201,6 @@ class UrlRule extends CompositeUrlRule
         $config['verb'] = $verbs;
         $config['pattern'] = rtrim($prefix . '/' . strtr($pattern, $this->tokens), '/');
         $config['route'] = $action;
-        if (!in_array('GET', $verbs)) {
-            $config['mode'] = \yii\web\UrlRule::PARSING_ONLY;
-        }
         $config['suffix'] = $this->suffix;
 
         return Yii::createObject($config);
