83f5f49469b45193e1e6a8a5d55d31d62d1ac5ea
Carsten Brandt <mail@cebe.cc>
2014-08-28 14:01:29 +0200

    try mongodb without sudo

--- tests/unit/data/travis/mongodb-setup.sh
+++ tests/unit/data/travis/mongodb-setup.sh
@@ -2,10 +2,12 @@
 #
 # install mongodb
 
-echo "extension = mongo.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-sudo sh -c 'echo "setParameter = textSearchEnabled=true" >> /etc/mongodb.conf'
-cat /etc/mongodb.conf
-
 mongod --version
 
-sudo service mongodb restart
+echo "extension = mongo.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+
+# version 2.6 has this enabled by default
+#sudo sh -c 'echo "setParameter = textSearchEnabled=true" >> /etc/mongodb.conf'
+cat /etc/mongodb.conf
+
+#sudo service mongodb restart
