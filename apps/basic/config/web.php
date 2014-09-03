46a8e5585b17b4d3604fafa8166f43288486e60a
Qiang Xue <qiang.xue@gmail.com>
2014-09-03 10:02:31 -0400

    cleaned up the key so that it can be filled up with a random key automatically when installed.

--- apps/basic/config/web.php
+++ apps/basic/config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'thisIsAKey',
+            'cookieValidationKey' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
