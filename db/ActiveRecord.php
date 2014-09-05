8da4f3bc20de232f9e924d1e5980c278e38e7e22
Carsten Brandt <mail@cebe.cc>
2014-09-05 14:59:22 +0200

    changed syntax for referencing the guide in apidoc
    New syntax:
    
    ```
    [link to guide](guide:file-name.md)
    [link to guide](guide:file-name.md#subsection)
    ```
    
    fixes #4719

--- db/ActiveRecord.php
+++ db/ActiveRecord.php
@@ -45,7 +45,7 @@ use yii\helpers\StringHelper;
  *
  * The `tableName` method only has to return the name of the database table associated with the class.
  *
- * > Tip: You may also use the [Gii code generator][guide-gii] to generate ActiveRecord classes from your
+ * > Tip: You may also use the [Gii code generator](guide:start-gii) to generate ActiveRecord classes from your
  * > database tables.
  *
  * Class instances are obtained in one of two ways:
@@ -67,7 +67,7 @@ use yii\helpers\StringHelper;
  * $orders = $user->orders;
  * ```
  *
- * For more details and usage information on ActiveRecord, see the [guide article on ActiveRecord][guide-active-record].
+ * For more details and usage information on ActiveRecord, see the [guide article on ActiveRecord](guide:db-active-record).
  *
  * @method ActiveQuery hasMany(string $class, array $link) see BaseActiveRecord::hasMany() for more info
  * @method ActiveQuery hasOne(string $class, array $link) see BaseActiveRecord::hasOne() for more info
