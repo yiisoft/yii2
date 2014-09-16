6526d7f91bb1816452354564557ec6d00379c33c
Qiang Xue <qiang.xue@gmail.com>
2014-09-16 16:28:49 -0400

    reverted a prior change.

--- apps/basic/config/web.php
+++ apps/basic/config/web.php
@@ -38,10 +38,6 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-        ],
     ],
     'params' => $params,
 ];
