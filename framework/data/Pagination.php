a3cfafc761351e18705d5a394f0b8259a11b337f
Qiang Xue <qiang.xue@gmail.com>
2014-09-17 11:14:05 -0400

    Fixes #5058: Added `$pageSize` parameter to `Pagination::createUrl()` to allow creating URLs with arbitrary page sizes

--- framework/data/Pagination.php
+++ framework/data/Pagination.php
@@ -246,13 +246,16 @@ class Pagination extends Object implements Linkable
      * Creates the URL suitable for pagination with the specified page number.
      * This method is mainly called by pagers when creating URLs used to perform pagination.
      * @param integer $page the zero-based page number that the URL should point to.
+     * @param integer $pageSize the number of items on each page. If 0, the value of [[pageSize]] will be used.
      * @param boolean $absolute whether to create an absolute URL. Defaults to `false`.
      * @return string the created URL
      * @see params
      * @see forcePageParam
      */
-    public function createUrl($page, $absolute = false)
+    public function createUrl($page, $pageSize = 0, $absolute = false)
     {
+        $page = (int) $page;
+        $pageSize = (int) $pageSize;
         if (($params = $this->params) === null) {
             $request = Yii::$app->getRequest();
             $params = $request instanceof Request ? $request->getQueryParams() : [];
@@ -262,7 +265,9 @@ class Pagination extends Object implements Linkable
         } else {
             unset($params[$this->pageParam]);
         }
-        $pageSize = $this->getPageSize();
+        if ($pageSize <= 0) {
+            $pageSize = $this->getPageSize();
+        }
         if ($pageSize != $this->defaultPageSize) {
             $params[$this->pageSizeParam] = $pageSize;
         } else {
