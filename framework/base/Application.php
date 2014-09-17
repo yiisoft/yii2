c9e23ce374344aa25637eedf77d51534bd47bb4e
Basil_Snowman <basil.snowman@gmail.com>
2014-09-17 17:13:34 +0400

    Update Application.php
    Fixed path to bower-asset package

--- framework/base/Application.php
+++ framework/base/Application.php
@@ -449,7 +449,7 @@ abstract class Application extends Module
     {
         $this->_vendorPath = Yii::getAlias($path);
         Yii::setAlias('@vendor', $this->_vendorPath);
-        Yii::setAlias('@bower', $this->_vendorPath . DIRECTORY_SEPARATOR . 'bower');
+        Yii::setAlias('@bower', $this->_vendorPath . DIRECTORY_SEPARATOR . 'bower-asset');
         Yii::setAlias('@npm', $this->_vendorPath . DIRECTORY_SEPARATOR . 'npm');
     }
 
