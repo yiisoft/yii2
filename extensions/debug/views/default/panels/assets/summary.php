4b32837455f1a01db7a60d257353f907daf5dced
artur <artur.fursa@binn.com.ua>
2014-09-18 01:08:23 +0300

    Improvements for debug toolbar (#838) - add panel to display used asset bundles

---
+++ extensions/debug/views/default/panels/assets/summary.php
@@ -0,0 +1,6 @@
+<?php
+/* @var $panel yii\debug\panels\AssetPanel */
+?>
+<div class="yii-debug-toolbar-block">
+    <a href="<?= $panel->getUrl() ?>" title="Asset bundles count">Asset bundles <span class="label label-info"><?= $panel->data['totalBundles'] ?></span></a>
+</div>
