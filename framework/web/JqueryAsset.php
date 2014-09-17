ecdb41e04619e904e1bc7db066b84351be7c409f
Qiang Xue <qiang.xue@gmail.com>
2014-09-17 09:59:44 -0400

    Fixes #5061: Switched to use jquery composer package.

--- framework/web/JqueryAsset.php
+++ framework/web/JqueryAsset.php
@@ -15,7 +15,7 @@ namespace yii\web;
  */
 class JqueryAsset extends AssetBundle
 {
-    public $sourcePath = '@bower/jquery';
+    public $sourcePath = '@vendor/yiisoft/jquery';
     public $js = [
         'dist/jquery.js',
     ];
