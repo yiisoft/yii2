8e2f75af5be83f3b43393159ea1e067839715961
Carsten Brandt <mail@cebe.cc>
2014-09-24 13:39:03 +0200

    reverted addition of $where property

--- db/QueryTrait.php
+++ db/QueryTrait.php
@@ -21,7 +21,7 @@ use yii\base\NotSupportedException;
 trait QueryTrait
 {
     /**
-     * @var array query condition. This refers to the WHERE clause in a SQL statement.
+     * @var string|array query condition. This refers to the WHERE clause in a SQL statement.
      * For example, `['age' => 31, 'team' => 1]`.
      * @see where() for valid syntax on specifying this value.
      */
