d6b797e236c9b6397d1dbd73ea41f80fa6bd2492
Qiang Xue <qiang.xue@gmail.com>
2014-10-03 09:30:26 -0400

    Fixes #5252: Null values are not properly handled by `RangeValidator`

--- framework/validators/RangeValidator.php
+++ framework/validators/RangeValidator.php
@@ -66,7 +66,7 @@ class RangeValidator extends Validator
 
         $in = true;
 
-        foreach ((array)$value as $v) {
+        foreach ((is_array($value) ? $value : [$value]) as $v) {
             if (!in_array($v, $this->range, $this->strict)) {
                 $in = false;
                 break;
