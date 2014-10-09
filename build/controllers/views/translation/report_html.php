048f56a1234be71d0953bdcd16f76813a48fa06f
pana1990 <pana_1990@hotmail.com>
2014-10-09 17:54:27 +0200

    fix code style php5.4 syntax

--- build/controllers/views/translation/report_html.php
+++ build/controllers/views/translation/report_html.php
@@ -27,20 +27,20 @@ use yii\helpers\Html;
         </style>
     </head>
     <body>
-        <h1><?php echo Html::encode($title)?></h1>
+        <h1><?= Html::encode($title)?></h1>
 
         <ul>
-            <li><strong>Source:</strong> <?php echo Html::encode($sourcePath)?></li>
-            <li><strong>Translation:</strong> <?php echo Html::encode($translationPath)?></li>
+            <li><strong>Source:</strong> <?= Html::encode($sourcePath)?></li>
+            <li><strong>Translation:</strong> <?= Html::encode($translationPath)?></li>
         </ul>
 
         <?php foreach($results as $name => $result):?>
-            <h2 class="<?php echo empty($result['errors']) ? 'ok' : 'errors'?>"><?php echo $name?></h2>
+            <h2 class="<?= empty($result['errors']) ? 'ok' : 'errors'?>"><?= $name?></h2>
             <?php foreach($result['errors'] as $error):?>
-                <p><?php echo Html::encode($error)?></p>
+                <p><?= Html::encode($error)?></p>
             <?php endforeach ?>
             <?php if(!empty($result['diff'])):?>
-                <code class="diff"><pre><?php echo $this->context->highlightDiff($result['diff'])?></pre></code>
+                <code class="diff"><pre><?= $this->context->highlightDiff($result['diff'])?></pre></code>
             <?php endif?>
         <?php endforeach ?>
     </body>
