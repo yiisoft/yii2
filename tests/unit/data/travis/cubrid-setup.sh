50c0449d1ae0a4ed1e4231c805eb990fcf8f22da
Carsten Brandt <mail@cebe.cc>
2014-10-11 17:30:41 +0200

    ensure tests are failing when build scripts fail

--- tests/unit/data/travis/cubrid-setup.sh
+++ tests/unit/data/travis/cubrid-setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 #
 # install CUBRID DBMS
 
@@ -71,7 +71,7 @@ install_pdo_cubrid() {
     return $?
 }
 
-install_pdo_cubrid > ~/pdo_cubrid.log || ( echo "=== PDO CUBRID BUILD FAILED ==="; cat ~/pdo_cubrid.log )
+install_pdo_cubrid > ~/pdo_cubrid.log || ( echo "=== PDO CUBRID BUILD FAILED ==="; cat ~/pdo_cubrid.log ) && exit 1
 
 echo ""
 echo "Installed CUBRID PDO $CUBRID_PDO_VERSION"
