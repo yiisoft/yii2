2f239f9027de17a9f756db611a7f6388698f3459
Serge Postrash <jexy.ru@gmail.com>
2014-10-12 16:56:38 +0400

    Update Sort.php

--- framework/data/Sort.php
+++ framework/data/Sort.php
@@ -340,7 +340,7 @@ class Sort extends Object
         }
         $params[$this->sortParam] = $this->createSortParam($attribute);
         $params[0] = $this->route === null ? Yii::$app->controller->getRoute() : $this->route;
-        $urlManager = $this->urlManager === null ? Yii::$app->getUrlManager() : $this->urlManager;
+        $urlManager = $this->getUrlManager;
         if ($absolute) {
             return $urlManager->createAbsoluteUrl($params);
         } else {
@@ -392,5 +392,14 @@ class Sort extends Object
     public function hasAttribute($name)
     {
         return isset($this->attributes[$name]);
+    }
+    
+    protected function getUrlManager()
+    {
+        if ($this->urlManager === null) {
+            $this->urlManager = Yii::$app->getUrlManager();
+        }
+        
+        return $this->urlManager;
     }
 }
