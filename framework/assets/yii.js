7fa27f37575e6d57337e6b706b625879323b7e36
Vadim <4ws@mail.ru>
2014-10-14 19:10:16 +0400

    Fix interpret inner arrays in request string.
    https://github.com/yiisoft/yii2/issues/5546

--- framework/assets/yii.js
+++ framework/assets/yii.js
@@ -182,9 +182,20 @@ yii = (function ($) {
                 return {};
             }
             var qs = url.substring(pos + 1).split('&');
+            var qn,
+                prevqn,
+                qncounter = 0;
             for (var i = 0, result = {}; i < qs.length; i++) {
                 qs[i] = qs[i].split('=');
-                result[decodeURIComponent(qs[i][0])] = decodeURIComponent(qs[i][1]);
+                qn = decodeURIComponent(qs[i][0]);
+                if(prevqn===qn && '[]'===qn.substr(-2)) {
+                    qncounter++;
+                    result[qn.replace('[]','[' + qncounter + ']')] = decodeURIComponent(qs[i][1]);
+                } else {
+                    result[qn] = decodeURIComponent(qs[i][1]);
+                    qncounter = 0;
+                }
+                prevqn = qn;
             }
             return result;
         },
