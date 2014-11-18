d5cded55ce6034f6581e20bbe0d8760e580257e9
Anton Andersen <trianman@gmail.com>
2014-11-18 22:43:37 +0300

    Fix url generation in functional and acceptance tests

--- apps/advanced/tests/codeception/backend/_bootstrap.php
+++ apps/advanced/tests/codeception/backend/_bootstrap.php
@@ -4,7 +4,7 @@ defined('YII_ENV') or define('YII_ENV', 'test');
 
 defined('YII_APP_BASE_PATH') or define('YII_APP_BASE_PATH', dirname(dirname(dirname(__DIR__))));
 
-defined('YII_BACKEND_TEST_ENTRY_URL') or define('YII_BACKEND_TEST_ENTRY_URL', \Codeception\Configuration::config()['config']['test_entry_url']);
+defined('YII_BACKEND_TEST_ENTRY_URL') or define('YII_BACKEND_TEST_ENTRY_URL', parse_url(\Codeception\Configuration::config()['config']['test_entry_url'], PHP_URL_PATH));
 defined('YII_TEST_BACKEND_ENTRY_FILE') or define('YII_TEST_BACKEND_ENTRY_FILE', YII_APP_BASE_PATH . '/backend/web/index-test.php');
 
 require_once(YII_APP_BASE_PATH . '/vendor/autoload.php');
@@ -17,6 +17,7 @@ require_once(YII_APP_BASE_PATH . '/backend/config/bootstrap.php');
 // the entry script file path for functional and acceptance tests
 $_SERVER['SCRIPT_FILENAME'] = YII_TEST_BACKEND_ENTRY_FILE;
 $_SERVER['SCRIPT_NAME'] = YII_BACKEND_TEST_ENTRY_URL;
-$_SERVER['SERVER_NAME'] = 'localhost';
+$_SERVER['SERVER_NAME'] =  parse_url(\Codeception\Configuration::config()['config']['test_entry_url'], PHP_URL_HOST);
+$_SERVER['SERVER_PORT'] =  parse_url(\Codeception\Configuration::config()['config']['test_entry_url'], PHP_URL_PORT) ?: '80';
 
 Yii::setAlias('@tests', dirname(dirname(__DIR__)));
