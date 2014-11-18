700e7ecdb283429c3811c77367bd8ffa08adc1c9
Anton Andersen <trianman@gmail.com>
2014-11-18 21:08:17 +0300

    Move log config to common

--- apps/advanced/frontend/config/main.php
+++ apps/advanced/frontend/config/main.php
@@ -9,21 +9,11 @@ $params = array_merge(
 return [
     'id' => 'app-frontend',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
