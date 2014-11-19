690ed147d0c7408167fea4d9298cf9d4e164bbea
Tobias Munk <schmunk@usrbin.de>
2014-11-19 17:29:29 +0100

    improved config table dispay

--- extensions/debug/views/default/panels/config/table.php
+++ extensions/debug/views/default/panels/config/table.php
@@ -16,14 +16,14 @@ use yii\helpers\Html;
     <table class="table table-condensed table-bordered table-striped table-hover" style="table-layout: fixed;">
         <thead>
             <tr>
-                <th style="width: 200px;">Name</th>
+                <th style="nowrap">Name</th>
                 <th>Value</th>
             </tr>
         </thead>
         <tbody>
         <?php foreach ($values as $name => $value): ?>
             <tr>
-                <th style="width: 200px;"><?= Html::encode($name) ?></th>
+                <th><?= Html::encode($name) ?></th>
                 <td style="overflow:auto"><?= Html::encode($value) ?></td>
             </tr>
         <?php endforeach; ?>
