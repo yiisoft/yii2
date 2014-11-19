fca9565c2dff298c4a01ae0442c3d9b5a2ae319b
Anton Andersen <trianman@gmail.com>
2014-11-19 14:37:13 +0300

    Remove not needed config params

--- apps/advanced/tests/codeception/common/_bootstrap.php
+++ apps/advanced/tests/codeception/common/_bootstrap.php
@@ -9,7 +9,7 @@ require_once(YII_APP_BASE_PATH . '/vendor/yiisoft/yii2/Yii.php');
 require_once(YII_APP_BASE_PATH . '/common/config/bootstrap.php');
 
 // set correct script paths
-$_SERVER['SERVER_NAME'] =  parse_url(\Codeception\Configuration::config()['config']['test_entry_url'], PHP_URL_HOST);
-$_SERVER['SERVER_PORT'] =  parse_url(\Codeception\Configuration::config()['config']['test_entry_url'], PHP_URL_PORT) ?: '80';
+$_SERVER['SERVER_NAME'] = 'localhost';
+$_SERVER['SERVER_PORT'] = '80';
 
 Yii::setAlias('@tests', dirname(dirname(__DIR__)));
