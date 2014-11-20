c9e64e175e26a7d9eed3a9d6e3bd9835a1a2e930
Thiago Talma <thiago@thiagomt.com>
2014-11-20 12:42:04 -0200

    PHP version - Tooltip

--- extensions/debug/views/default/panels/config/summary.php
+++ extensions/debug/views/default/panels/config/summary.php
@@ -4,8 +4,6 @@
 <div class="yii-debug-toolbar-block">
     <a href="<?= $panel->getUrl() ?>">
         Yii
-        <span class="label"><?= $panel->data['application']['yii'] ?></span>
-        PHP
-        <span class="label"><?= $panel->data['php']['version'] ?></span>
+        <span class="label" title="Running on PHP <?= $panel->data['php']['version'] ?>"><?= $panel->data['application']['yii'] ?></span>
     </a>
 </div>
