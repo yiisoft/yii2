04f336e6346404b95bc28fc6cf98e2b090ba135b
plandem <plandem@gmail.com>
2014-11-27 20:15:28 +0700

    lastInsertId
    lastInsertId at PDO by default check for NULL. At least Postgres. At documentation of PDO - too.

--- framework/db/Schema.php
+++ framework/db/Schema.php
@@ -309,7 +309,7 @@ abstract class Schema extends Object
      * @throws InvalidCallException if the DB connection is not active
      * @see http://www.php.net/manual/en/function.PDO-lastInsertId.php
      */
-    public function getLastInsertID($sequenceName = '')
+    public function getLastInsertID($sequenceName = null)
     {
         if ($this->db->isActive) {
             return $this->db->pdo->lastInsertId($sequenceName);
