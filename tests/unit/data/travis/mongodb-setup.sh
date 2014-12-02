0633cb0957120829ede6f91d65cdc6b2d44c4462
Carsten Brandt <mail@cebe.cc>
2014-12-02 13:12:04 +0100

    Update mongodb-setup.sh
    show PHP extension version

--- tests/unit/data/travis/mongodb-setup.sh
+++ tests/unit/data/travis/mongodb-setup.sh
@@ -2,13 +2,17 @@
 #
 # install mongodb
 
-mongod --version
-
 if (php --version | grep -i HipHop > /dev/null); then
   echo "mongodb does not work on HHVM currently, skipping"
 else
   echo "extension = mongo.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 fi
+
+# show mongodb version
+mongod --version
+
+# show mongo PHP extension version
+php -i |grep mongo -4 |grep -2 Version
 
 # enable text search
 mongo --eval 'db.adminCommand( { setParameter: true, textSearchEnabled : true})'
