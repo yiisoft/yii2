603169e17e140872bf62513a688ce0a6ed37756e
Alexander Kochetov <creocoder@gmail.com>
2014-12-13 23:40:04 +0300

    Advanced app User model refactoring

--- apps/advanced/common/models/LoginForm.php
+++ apps/advanced/common/models/LoginForm.php
@@ -27,25 +27,15 @@ class LoginForm extends Model
             // rememberMe must be a boolean value
             ['rememberMe', 'boolean'],
             // password is validated by validatePassword()
-            ['password', 'validatePassword'],
+            ['password', function($attribute, $params) {
+                if (!$this->hasErrors()) {
+                    $user = $this->getUser();
+                    if (!$user || !$user->validatePassword($this->password)) {
+                        $this->addError($attribute, 'Incorrect username or password.');
+                    }
+                }
+            }],
         ];
-    }
-
-    /**
-     * Validates the password.
-     * This method serves as the inline validation for password.
-     *
-     * @param string $attribute the attribute currently being validated
-     * @param array $params the additional name-value pairs given in the rule
-     */
-    public function validatePassword($attribute, $params)
-    {
-        if (!$this->hasErrors()) {
-            $user = $this->getUser();
-            if (!$user || !$user->validatePassword($this->password)) {
-                $this->addError($attribute, 'Incorrect username or password.');
-            }
-        }
     }
 
     /**
