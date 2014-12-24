d9228356f711ed3660d9f3512d52b9cf869f96d9
Alexander Kochetov <creocoder@gmail.com>
2014-12-24 15:06:46 +0300

    Remove `defined` from advanced app template `dev` environment

--- apps/advanced/environments/dev/frontend/web/index.php
+++ apps/advanced/environments/dev/frontend/web/index.php
@@ -1,6 +1,7 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+
+define('YII_DEBUG', true);
+define('YII_ENV', 'dev');
 
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
