09e038a8933020f9e22223f9f09c92dee8154552
Alexander Kochetov <creocoder@gmail.com>
2014-12-30 10:09:48 +0300

    yii\base\Model::load() typo fix

--- base/Model.php
+++ base/Model.php
@@ -761,7 +761,7 @@ class Model extends Component implements IteratorAggregate, ArrayAccess, Arrayab
     public function load($data, $formName = null)
     {
         $scope = $formName === null ? $this->formName() : $formName;
-        if ($scope == '' && !empty($data)) {
+        if ($scope === '' && !empty($data)) {
             $this->setAttributes($data);
 
             return true;
