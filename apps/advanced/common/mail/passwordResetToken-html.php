1cf5e47b55576f7b96ed07fa0e1b921a7db28ff5
Carsten Brandt <mail@cebe.cc>
2015-01-10 14:32:23 +0100

    fix broken password reset mail in advanced app
    - ensure HTML version is acutally HTML formatted
    - added valid text version
    
    fixes #6814
    original issue #6748

--- apps/advanced/common/mail/passwordResetToken-html.php
+++ apps/advanced/common/mail/passwordResetToken-html.php
@@ -6,9 +6,10 @@ use yii\helpers\Html;
 
 $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->password_reset_token]);
 ?>
+<div class="password-reset">
+    <p>Hello <?= Html::encode($user->username) ?>,</p>
 
-Hello <?= Html::encode($user->username) ?>,
+    <p>Follow the link below to reset your password:</p>
 
-Follow the link below to reset your password:
-
-<?= Html::a(Html::encode($resetLink), $resetLink) ?>
+    <p><?= Html::a(Html::encode($resetLink), $resetLink) ?></p>
+</div>
---
+++ apps/advanced/common/mail/passwordResetToken-text.php
@@ -0,0 +1,13 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $user common\models\User */
+
+$resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->password_reset_token]);
+?>
+Hello <?= Html::encode($user->username) ?>,
+
+Follow the link below to reset your password:
+
+<?= $resetLink ?>
