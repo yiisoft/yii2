5cb2ce49dde5e9da837982f7361ba2ffe8e7904f
Qiang Xue <qiang.xue@gmail.com>
2015-01-10 12:36:08 -0500

    Fixes #3665: Better default behavior for ModelSearch generated by the crud generator

--- extensions/gii/generators/crud/default/search.php
+++ extensions/gii/generators/crud/default/search.php
@@ -69,7 +69,11 @@ class <?= $searchModelClass ?> extends <?= isset($modelAlias) ? $modelAlias : $m
             'query' => $query,
         ]);
 
-        if (!($this->load($params) && $this->validate())) {
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to any records when validation fails
+            // $query->where('0=1');
             return $dataProvider;
         }
 
