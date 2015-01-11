174843a971d39268010ededd09f77082cb4589de
Alexander Makarov <sam@rmcreative.ru>
2015-01-11 12:58:10 +0800

    Fixes #5690: adjusted paths in message config generated by `yii message/config` to reflect directory structure better

--- framework/views/messageConfig.php
+++ framework/views/messageConfig.php
@@ -2,7 +2,7 @@
 
 return [
     // string, required, root directory of all source files
-    'sourcePath' => __DIR__,
+    'sourcePath' => __DIR__ . DIRECTORY_SEPARATOR . '..',
     // array, required, list of language codes that the extracted messages
     // should be translated to. For example, ['zh-CN', 'de'].
     'languages' => ['de'],
@@ -44,7 +44,7 @@ return [
     // 'php' output format is for saving messages to php files.
     'format' => 'php',
     // Root directory containing message translations.
-    'messagePath' => __DIR__ . DIRECTORY_SEPARATOR . 'messages',
+    'messagePath' => __DIR__,
     // boolean, whether the message file should be overwritten with the merged messages
     'overwrite' => true,
 
