48c0c5fdefc8e9d7329a05c2f803fe2c6fbd6987
Angel Guevara <aguevara@tecnocen.com>
2015-01-12 19:54:02 -0600

    AuthItemChild model

---
+++ framework/rbac/models/AuthItemChild.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\rbac\models;
+
+use Yii;
+use yii\behaviors\TimestampBehavior;
+
+/**
+ * This parent/children structure for items.
+ *
+ * @property string $parent
+ * @property string $child
+ *
+ * @property AuthItem $parentItem
+ * @property AuthItem $childItem
+ *
+ * @author Angel (Faryshta) Guevara <angeldelcaos@gmail.com>
+ * @since 2.0.2
+ */
+class AuthItemChild extends \yii\rbac\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return $this->authManager->itemChildTable;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['parent', 'child'], 'required'],
+            [['parent', 'child'], 'string', 'max' => 64]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'parent' => 'Parent',
+            'child' => 'Child',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getParentItem()
+    {
+        return $this->hasOne(AuthItem::className(), ['name' => 'parent']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getChildItem()
+    {
+        return $this->hasOne(AuthItem::className(), ['name' => 'child']);
+    }
+}
