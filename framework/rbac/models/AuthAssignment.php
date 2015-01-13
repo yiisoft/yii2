7d3d0e6fd626576c8f611980d48e842e4270a5e8
Angel Guevara <aguevara@tecnocen.com>
2015-01-12 18:28:34 -0600

    AuthAssignment model

---
+++ framework/rbac/models/AuthAssignment.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace yii\rbac\models;
+
+use Yii;
+use yii\behaviors\TimestampBehavior;
+
+/**
+ * This is the model class for table "auth_assignment".
+ *
+ * @property string $item_name
+ * @property integer $user_id
+ * @property integer $created_at
+ *
+ * @property User $user
+ * @property AuthItem $itemName
+ */
+class AuthAssignment extends \yii\rbac\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return $this->authManager->assignmentTable;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [TimestampBehavior::className()];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['item_name', 'user_id'], 'required'],
+            [['created_at'], 'integer'],
+            [['item_name'], 'string', 'max' => 64]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'item_name' => 'Item Name',
+            'user_id' => 'User ID',
+            'created_at' => 'Created At',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getItemName()
+    {
+        return $this->hasOne(AuthItem::className(), ['name' => 'item_name']);
+    }
+}
