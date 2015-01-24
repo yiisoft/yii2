7cb3dbde2937b686059b67a8da1307083a36aa47
Constantin Chuprik <constantinchuprik@gmail.com>
2015-01-25 01:58:22 +0600

    Added Instagram auth

---
+++ extensions/authclient/clients/Instagram.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\authclient\clients;
+
+use yii\authclient\OAuth2;
+
+/**
+ * Instagram allows authentication via Instagram OAuth.
+ *
+ * In order to use Instagram OAuth you must register your application at <http://instagram.com/developer/clients/register>.
+ *
+ * Example application configuration:
+ *
+ * ~~~
+ * 'components' => [
+ *     'authClientCollection' => [
+ *         'class' => 'yii\authclient\Collection',
+ *         'clients' => [
+ *             'instagram' => [
+ *                 'class' => 'yii\authclient\clients\Instagram',
+ *                 'clientId' => 'instagram_client_id',
+ *                 'clientSecret' => 'instagram_client_secret',
+ *             ],
+ *         ],
+ *     ]
+ *     ...
+ * ]
+ * ~~~
+ *
+ * @see http://instagram.com/developer/authentication
+ * @see http://instagram.com/developer/clients/register
+ *
+ * @author Constantin Chuprik <constantinchuprik@gmail.com>
+ * @since 2.0
+ */
+class Instagram extends OAuth2
+{
+    /**
+     * @inheritdoc
+     */
+    public $authUrl = 'https://api.instagram.com/oauth/authorize';
+    /**
+     * @inheritdoc
+     */
+    public $tokenUrl = 'https://api.instagram.com/oauth/access_token';
+    /**
+     * @inheritdoc
+     */
+    public $apiBaseUrl = 'https://api.instagram.com/v1';
+
+    /**
+     * @inheritdoc
+     */
+    protected function initUserAttributes()
+    {
+        $response = $this->api('users/' . $this->accessToken->params['user']['id'], 'GET');
+
+        return $response['data'];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function apiInternal($accessToken, $url, $method, array $params, array $headers)
+    {
+        return $this->sendRequest($method, $url . '?access_token=' . $accessToken->getToken(), $params, $headers);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function defaultName()
+    {
+        return 'instagram';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function defaultTitle()
+    {
+        return 'Instagram';
+    }
+}
