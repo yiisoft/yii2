6b847c66ad6b284aadb4cf7e18a9f7587b4483cd
Ivan Yakovlev <uran1980@gmail.com>
2015-01-25 18:48:27 +0200

    Remove extra space in gridView filter cell

--- framework/grid/DataColumn.php
+++ framework/grid/DataColumn.php
@@ -159,15 +159,15 @@ class DataColumn extends Column
         if ($this->filter !== false && $model instanceof Model && $this->attribute !== null && $model->isAttributeActive($this->attribute)) {
             if ($model->hasErrors($this->attribute)) {
                 Html::addCssClass($this->filterOptions, 'has-error');
-                $error = Html::error($model, $this->attribute, $this->grid->filterErrorOptions);
+                $error = ' ' . Html::error($model, $this->attribute, $this->grid->filterErrorOptions);
             } else {
                 $error = '';
             }
             if (is_array($this->filter)) {
                 $options = array_merge(['prompt' => ''], $this->filterInputOptions);
-                return Html::activeDropDownList($model, $this->attribute, $this->filter, $options) . ' ' . $error;
+                return Html::activeDropDownList($model, $this->attribute, $this->filter, $options) . $error;
             } else {
-                return Html::activeTextInput($model, $this->attribute, $this->filterInputOptions) . ' ' . $error;
+                return Html::activeTextInput($model, $this->attribute, $this->filterInputOptions) . $error;
             }
         } else {
             return parent::renderFilterCellContent();
