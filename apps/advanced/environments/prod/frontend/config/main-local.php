106380f4061945971ac81f79e6966c37f0d90d9b
Alexander Kochetov <creocoder@gmail.com>
2015-02-04 02:22:20 +0300

    Updated "prod" configs to use "dotenv" approach

--- apps/advanced/environments/prod/frontend/config/main-local.php
+++ apps/advanced/environments/prod/frontend/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => getenv('COOKIE_VALIDATION_KEY'),
         ],
     ],
 ];
