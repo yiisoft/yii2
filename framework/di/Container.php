43be3b586c28996bd64c3a9dd5c36978b9093bf5
Angel Faryshta Guevara <angeldelcaos@gmail.com>
2015-02-06 03:53:25 -0600

    doc  for the base yii\di namespace

--- framework/di/Container.php
+++ framework/di/Container.php
@@ -238,7 +238,7 @@ class Container extends Component
      * - a string: a class name, an interface name or an alias name.
      * @param array $params the list of constructor parameters. The parameters will be passed to the class
      * constructor when [[get()]] is called.
-     * @return static the container itself
+     * @return $this 
      */
     public function set($class, $definition = [], array $params = [])
     {
@@ -258,7 +258,7 @@ class Container extends Component
      * @param mixed $definition the definition associated with `$class`. See [[set()]] for more details.
      * @param array $params the list of constructor parameters. The parameters will be passed to the class
      * constructor when [[get()]] is called.
-     * @return static the container itself
+     * @return $this 
      * @see set()
      */
     public function setSingleton($class, $definition = [], array $params = [])
