dc00d95aa4bae64d8aa2b9adae3490959e0580d1
Jitendra Joshi <joshijitendra000@gmail.com>
2015-02-08 13:10:42 +0530

    Updated DatePicker.php
    Added example for working with ActiveForm textinput() field
    
    close #7203

--- extensions/jui/DatePicker.php
+++ extensions/jui/DatePicker.php
@@ -38,6 +38,16 @@ use yii\helpers\Json;
  * ]);
  * ```
  *
+ * You can also use this widget in an [[yii\widgets\ActiveForm|ActiveForm]] using the [[yii\widgets\ActiveField::widget()|widget()]]
+ * method, for example like this:
+ *
+ * ```php
+ * <?= $form->field($model, 'from_date')->widget(\yii\jui\DatePicker::classname(), [
+ *     //'language' => 'ru',
+ *     //'dateFormat' => 'yyyy-MM-dd',
+ * ]) ?>
+ * ```
+ *
  * Note that empty values like empty strings and 0 will result in a date displayed as `1970-01-01`.
  * So to make sure empty values result in an empty text field in the datepicker you need to add a
  * validation filter in your model that sets the value to `null` in case when no date has been entered:
