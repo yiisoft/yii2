ada9abe6b105a3f6abe5e204f3339df6ce52c551
nkovacs <nikola.kovacs@gmail.com>
2015-02-09 16:11:39 +0100

    Fix documentation of PhpMessageSource::$basePath

--- framework/i18n/PhpMessageSource.php
+++ framework/i18n/PhpMessageSource.php
@@ -34,8 +34,7 @@ use Yii;
 class PhpMessageSource extends MessageSource
 {
     /**
-     * @var string the base path for all translated messages. Defaults to null, meaning
-     * the "messages" subdirectory of the application directory (e.g. "protected/messages").
+     * @var string the base path for all translated messages. Defaults to '@app/messages'.
      */
     public $basePath = '@app/messages';
     /**
