2b6de9665eb09849862351ea4a45effe23e75f68
Carsten Brandt <mail@cebe.cc>
2015-02-19 13:44:05 +0100

    adjusted docs about #7347

--- framework/console/controllers/MigrateController.php
+++ framework/console/controllers/MigrateController.php
@@ -64,8 +64,9 @@ class MigrateController extends BaseMigrateController
      */
     public $templateFile = '@yii/views/migration.php';
     /**
-     * @var Connection|string the DB connection object or the application
-     * component ID of the DB connection.
+     * @var Connection|array|string the DB connection object or the application component ID of the DB connection to use
+     * when applying migrations. Starting from version 2.0.3, this can also be a configuration array
+     * for creating the object.
      */
     public $db = 'db';
 
