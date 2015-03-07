6359146d2e18ccb3cc306b464b9b23f73ec04c3c
Sidney Lins (sidtj) <sidtj@wmaior.com>
2015-03-06 23:30:09 -0300

    Trace swiftmailer extension logs

--- extensions/swiftmailer/Mailer.php
+++ extensions/swiftmailer/Mailer.php
@@ -142,7 +142,13 @@ class Mailer extends BaseMailer
         }
         Yii::info('Sending email "' . $message->getSubject() . '" to "' . $address . '"', __METHOD__);
 
-        return $this->getSwiftMailer()->send($message->getSwiftMessage()) > 0;
+        $result = $this->getSwiftMailer()->send($message->getSwiftMessage()) > 0;
+
+        if (!isEmpty($this->_logger)) {
+            Yii::trace('Swift Mailer Plugin Logger: "' . $this->_logger->dump() . '"', __METHOD__);
+        }
+
+        return $result;
     }
 
     /**
