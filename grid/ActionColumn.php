6d0afa11bb037433a195099a6aaded706a0c5d47
Carsten Brandt <mail@cebe.cc>
2015-03-08 19:00:10 +0100

    added ActionColumn::$buttonOptions
    allows for configuration of the default buttons like this:
    
    ```php
    [
    	'class' => ActionColumn::className(),
    	'template' => '<div class="btn-group text-center">{view} {update} {delete}</div>',
    	'buttonOptions' => [
    		'class' => 'btn btn-default btn-xs',
    	],
    ],
    ```
    
    close #7633

--- grid/ActionColumn.php
+++ grid/ActionColumn.php
@@ -87,6 +87,11 @@ class ActionColumn extends Column
      * If this property is not set, button URLs will be created using [[createUrl()]].
      */
     public $urlCreator;
+    /**
+     * @var array html options to be applied to the [[initDefaultButtons()|default buttons]].
+     * @since 2.0.4
+     */
+    public $buttonOptions = [];
 
 
     /**
@@ -105,28 +110,28 @@ class ActionColumn extends Column
     {
         if (!isset($this->buttons['view'])) {
             $this->buttons['view'] = function ($url, $model, $key) {
-                return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url, [
+                return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url, array_merge([
                     'title' => Yii::t('yii', 'View'),
                     'data-pjax' => '0',
-                ]);
+                ], $this->buttonOptions));
             };
         }
         if (!isset($this->buttons['update'])) {
             $this->buttons['update'] = function ($url, $model, $key) {
-                return Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, [
+                return Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, array_merge([
                     'title' => Yii::t('yii', 'Update'),
                     'data-pjax' => '0',
-                ]);
+                ], $this->buttonOptions));
             };
         }
         if (!isset($this->buttons['delete'])) {
             $this->buttons['delete'] = function ($url, $model, $key) {
-                return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, [
+                return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, array_merge([
                     'title' => Yii::t('yii', 'Delete'),
                     'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
                     'data-method' => 'post',
                     'data-pjax' => '0',
-                ]);
+                ], $this->buttonOptions));
             };
         }
     }
