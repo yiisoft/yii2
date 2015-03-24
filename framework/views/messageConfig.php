450cc4bc7f608e3366fb8bac7b1b79ece4b38f39
Alexander Nestorov <alexandernst@gmail.com>
2015-03-24 12:24:12 +0100

    Remove duplicated config entries

--- framework/views/messageConfig.php
+++ framework/views/messageConfig.php
@@ -69,11 +69,7 @@ return [
     /*
     // 'po' output format is for saving messages to gettext po files.
     'format' => 'po',
-    // Root directory containing message translations.
-    'messagePath' => __DIR__ . DIRECTORY_SEPARATOR . 'messages',
     // Name of the file that will be used for translations.
     'catalog' => 'messages',
-    // boolean, whether the message file should be overwritten with the merged messages
-    'overwrite' => true,
     */
 ];
