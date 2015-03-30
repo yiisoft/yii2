4d1ca9ff17783fd321c2d770360b31c01c7556a5
John Was <janek.jan@gmail.com>
2015-03-30 19:56:14 +0200

    Fixes #7831: Add order when fetching database table names and constraints

--- framework/db/sqlite/Schema.php
+++ framework/db/sqlite/Schema.php
@@ -98,7 +98,7 @@ class Schema extends \yii\db\Schema
      */
     protected function findTableNames($schema = '')
     {
-        $sql = "SELECT DISTINCT tbl_name FROM sqlite_master WHERE tbl_name<>'sqlite_sequence'";
+        $sql = "SELECT DISTINCT tbl_name FROM sqlite_master WHERE tbl_name<>'sqlite_sequence' ORDER BY tbl_name";
 
         return $this->db->createCommand($sql)->queryColumn();
     }
