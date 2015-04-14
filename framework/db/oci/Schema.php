60aa5f3eb9d324cce132a502f11c10dcc6141d68
Klimov Paul <klimov.paul@gmail.com>
2015-04-14 11:59:58 +0300

    Redundant checks at `yii\db\oci\Schema::insert()` and `yii\db\pgsql\Schema::insert()` removed

--- framework/db/oci/Schema.php
+++ framework/db/oci/Schema.php
@@ -445,7 +445,7 @@ SQL;
                 } else {
                     $returnParams[$phName]['dataType'] = \PDO::PARAM_INT;
                 }
-                $returning[] = $name === '*' ? $name : $this->quoteColumnName($name);
+                $returning[] = $this->quoteColumnName($name);
             }
             $sql .= ' RETURNING ' . implode(', ', $returning) . ' INTO ' . implode(', ', array_keys($returnParams));
         }
@@ -454,7 +454,7 @@ SQL;
         $command->prepare(false);
 
         foreach ($returnParams as $name => &$value) {
-            $command->pdoStatement->bindParam($name, $value['value']);
+            $command->pdoStatement->bindParam($name, $value['value'], $value['dataType']);
         }
 
         if (!$command->execute()) {
