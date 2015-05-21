8acd90d8aa665578797a787188b4cca7827dc801
Abayomi Osamiluyi <yomexzo@gmail.com>
2015-05-21 12:17:18 +0100

    Fixed issue with adding "onCondition" to "where".
    Developers should know when the want to add a `onCondition` different from when they want to add a `where` clause

--- framework/db/ActiveQuery.php
+++ framework/db/ActiveQuery.php
@@ -201,10 +201,6 @@ class ActiveQuery extends Query implements ActiveQueryInterface
             $this->where = $where;
         }
 
-        if (!empty($this->on)) {
-            $query->andWhere($this->on);
-        }
-
         return $query;
     }
 
