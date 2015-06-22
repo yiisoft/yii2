7c15679086af51bdb9a1015fd53854029972627e
Alexander Kochetov <creocoder@gmail.com>
2015-06-22 17:16:54 +0300

    Updated \yii\queue\Job

--- framework/queue/Job.php
+++ framework/queue/Job.php
@@ -7,6 +7,7 @@
 
 namespace yii\queue;
 
+use Yii;
 use yii\base\Object;
 
 /**
@@ -17,4 +18,25 @@ use yii\base\Object;
  */
 abstract class Job extends Object
 {
+    /**
+     * Returns the queue connection used by this Job class.
+     * By default, the "queue" application component is used as the queue connection.
+     * You may override this method if you want to use a different queue connection.
+     * @return Queue the queue connection used by this Job class.
+     */
+    public static function getQueue()
+    {
+        return Yii::$app->getQueue();
+    }
+
+    /**
+     * Push this job onto the queue.
+     *
+     * @param string|null $queue
+     * @param integer $delay
+     */
+    public static function push($queue = null, $delay = 0)
+    {
+        // implementation
+    }
 }
