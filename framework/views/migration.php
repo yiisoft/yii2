04accb6dcc16eabf6858bdca435f33605ae0f491
Felix Schindler <fschindler@weluse.de>
2015-07-14 14:16:20 +0200

    Trim some trailing whitespaces.

--- framework/views/migration.php
+++ framework/views/migration.php
@@ -24,13 +24,13 @@ class <?= $className ?> extends Migration
 
         return false;
     }
-    
+
     /*
     // Use safeUp/safeDown to run migration code within a transaction
     public function safeUp()
     {
     }
-    
+
     public function safeDown()
     {
     }
