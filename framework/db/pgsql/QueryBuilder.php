8cfc89b21c5866e191a43b5e3d05f00c2ba7ba1b
Carsten Brandt <mail@cebe.cc>
2015-08-02 23:20:52 +0200

    adjusted pgsql/QueryBuilder batchInsert to match parent implementation
    this also fixes #8551

--- framework/db/pgsql/QueryBuilder.php
+++ framework/db/pgsql/QueryBuilder.php
@@ -232,7 +232,7 @@ class QueryBuilder extends \yii\db\QueryBuilder
         foreach ($rows as $row) {
             $vs = [];
             foreach ($row as $i => $value) {
-                if (!is_array($value) && isset($columnSchemas[$columns[$i]])) {
+                if (isset($columns[$i], $columnSchemas[$columns[$i]]) && !is_array($value)) {
                     $value = $columnSchemas[$columns[$i]]->dbTypecast($value);
                 }
                 if (is_string($value)) {
