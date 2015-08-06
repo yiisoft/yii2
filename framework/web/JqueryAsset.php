aa12df10e8eea4e03be4c2f0c7c846c5877906e9
muhammadcahya <muhammadcahyax@gmail.com>
2015-08-06 15:34:10 +0700

    Use minify assets

--- framework/web/JqueryAsset.php
+++ framework/web/JqueryAsset.php
@@ -17,6 +17,6 @@ class JqueryAsset extends AssetBundle
 {
     public $sourcePath = '@bower/jquery/dist';
     public $js = [
-        'jquery.js',
+        'jquery'.( YII_ENV_DEV ? '.js' : '.min.js' ),
     ];
 }
