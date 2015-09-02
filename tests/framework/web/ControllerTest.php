c9e2898a7fde7227e94b00d0a23b94c0346cc36f
mdmunir <misbahuldmunir@gmail.com>
2015-09-02 09:49:21 +0700

    #9476 DI injection via method signature

--- tests/framework/web/ControllerTest.php
+++ tests/framework/web/ControllerTest.php
@@ -21,7 +21,7 @@ use yii\base\Object;
 class ControllerTest extends TestCase
 {
 
-    protected function setUp()
+    public function testBindActionParams()
     {
         $this->mockApplication([
             'components'=>[
@@ -30,28 +30,26 @@ class ControllerTest extends TestCase
                     'foo'=>'belong_app'
                 ],
                 'quxApp'=>[
-                    'class' => Qux::className(),
-                    'a' => 'belong_app'
+                    'class' => OtherQux::className(),
+                    'b' => 'belong_app'
                 ]
             ]
         ]);
-    }
 
-    public function testBindActionParams()
-    {
         $controller = new FakeController('fake', Yii::$app);
         $aksi1 = new InlineAction('aksi1', $controller, 'actionAksi1');
         $aksi2 = new InlineAction('aksi2', $controller, 'actionAksi2');
+        $aksi3 = new InlineAction('aksi3', $controller, 'actionAksi3');
 
         Yii::$container->set('yiiunit\framework\di\stubs\QuxInterface', [
             'class' => Qux::className(),
-            'a' => 'mdm'
+            'a' => 'D426'
         ]);
         Yii::$container->set(Bar::className(),[
             'foo' => 'independent'
         ]);
         
-        $params = ['fromGet'=>'from query params'];
+        $params = ['fromGet'=>'from query params','q'=>'d426'];
 
         list($bar, $fromGet, $other) = $controller->bindActionParams($aksi1, $params);
         $this->assertTrue($bar instanceof Bar);
@@ -60,25 +58,41 @@ class ControllerTest extends TestCase
         $this->assertEquals('from query params', $fromGet);
         $this->assertEquals('default', $other);
 
-        list($barBelongApp, $qux, $quxApp) = $controller->bindActionParams($aksi2, $params);
+        list($barBelongApp, $qux) = $controller->bindActionParams($aksi2, $params);
         $this->assertTrue($barBelongApp instanceof Bar);
         $this->assertEquals($barBelongApp, Yii::$app->barBelongApp);
         $this->assertEquals('belong_app', $barBelongApp->foo);
-        $this->assertTrue($qux instanceof QuxInterface);
-        $this->assertEquals('mdm', $qux->a);
-        $this->assertTrue($quxApp instanceof QuxInterface);
-        $this->assertEquals('belong_app', $quxApp->a);
+        $this->assertTrue($qux instanceof Qux);
+        $this->assertEquals('D426', $qux->a);
+
+        list($quxApp) = $controller->bindActionParams($aksi3, $params);
+        $this->assertTrue($quxApp instanceof OtherQux);
+        $this->assertEquals($quxApp, Yii::$app->quxApp);
+        $this->assertEquals('belong_app', $quxApp->b);
+
+        $result = $controller->runAction('aksi4', $params);
+        $this->assertEquals(['independent', 'other_qux', 'd426'], $result);
     }
 }
 
 class FakeController extends Controller
 {
+    public $enableCsrfValidation = false;
     public function actionAksi1(Bar $bar, $fromGet, $other='default')
     {
     }
 
-    public function actionAksi2(Bar $barBelongApp, QuxInterface $qux, QuxInterface $quxApp)
+    public function actionAksi2(Bar $barBelongApp, QuxInterface $qux)
     {
+    }
+
+    public function actionAksi3(QuxInterface $quxApp)
+    {
+    }
+
+    public function actionAksi4(Bar $bar, QuxInterface $quxApp, $q)
+    {
+        return [$bar->foo, $quxApp->quxMethod(), $q];
     }
 }
 
@@ -87,3 +101,11 @@ class Bar extends Object
     public $foo;
 }
 
+class OtherQux extends Object implements QuxInterface
+{
+    public $b;
+    public function quxMethod()
+    {
+        return 'other_qux';
+    }
+}
