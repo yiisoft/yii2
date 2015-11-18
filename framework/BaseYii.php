6d7627c2a51690175875fb8f21d444a3fb656ba6
Sam Mousa <sam@mousa.nl>
2015-11-18 16:36:25 +0200

    Yii::powered() now uses Yii::t()

--- framework/BaseYii.php
+++ framework/BaseYii.php
@@ -464,7 +464,10 @@ class BaseYii
      */
     public static function powered()
     {
-        return 'Powered by <a href="http://www.yiiframework.com/" rel="external">Yii Framework</a>';
+        return \Yii::t('yii', 'Powered by {yii}', [
+            'yii' => '<a href="http://www.yiiframework.com/" rel="external">' . \Yii::t('yii',
+                    'Yii Framework') . '</a>'
+        ]);
     }
 
     /**
