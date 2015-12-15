d134dbe1b54c2028ff5fa7dd17718229ec20bade
AnatolyRugalev <anatoly.rugalev@gmail.com>
2015-12-15 10:26:20 +0500

    Reformatting issue fixed

--- framework/i18n/MessageFormatter.php
+++ framework/i18n/MessageFormatter.php
@@ -399,12 +399,11 @@ class MessageFormatter extends Component
                     $selector = trim($plural[$i++]);
 
                     if ($i == 1 && strncmp($selector, 'offset:', 7) === 0) {
-                        $offset = (int)trim(mb_substr($selector, 7,
-                            ($pos = mb_strpos(str_replace(["\n", "\r", "\t"], ' ', $selector), ' ', 7, $charset)) - 7, $charset));
+                        $offset = (int)trim(mb_substr($selector, 7, ($pos = mb_strpos(str_replace(["\n", "\r", "\t"], ' ', $selector), ' ', 7, $charset)) - 7, $charset));
                         $selector = trim(mb_substr($selector, $pos + 1, null, $charset));
                     }
                     if ($message === false && $selector === 'other' ||
-                        $selector[0] === '=' && (int)mb_substr($selector, 1, null, $charset) === $arg ||
+                        $selector[0] === '=' && (int) mb_substr($selector, 1, null, $charset) === $arg ||
                         $selector === 'one' && $arg - $offset == 1
                     ) {
                         $message = implode(',', str_replace('#', $arg - $offset, $plural[$i]));
