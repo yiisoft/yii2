86b4915f204b05a81200904301c442d50c4dfe7b
Sam Mousa <sam@mousa.nl>
2016-02-03 13:55:32 +0100

    Fixed issue #7674

--- framework/db/Connection.php
+++ framework/db/Connection.php
@@ -337,6 +337,13 @@ class Connection extends Component
      * @see masterConfig
      */
     public $masters = [];
+
+    /**
+     * @var string the class name of the [[command]] object.
+     */
+    public $commandClass = '\yii\db\Command';
+
+
     /**
      * @var array the configuration that should be merged with every master configuration listed in [[masters]].
      * For example,
@@ -618,9 +625,10 @@ class Connection extends Component
      */
     public function createCommand($sql = null, $params = [])
     {
-        $command = new Command([
+        $command = \Yii::createObject([
+            'class' => $this->commandClass,
             'db' => $this,
-            'sql' => $sql,
+            'sql' => $sql
         ]);
 
         return $command->bindValues($params);
