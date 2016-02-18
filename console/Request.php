1c72ef6fcc87366c7250064785b803bd99504664
Daniel Gomez Pan <pana_1990@hotmail.com>
2016-02-18 12:48:37 +0100

    WIP #10840: refactor regexp

--- console/Request.php
+++ console/Request.php
@@ -66,15 +66,15 @@ class Request extends \yii\base\Request
 
         $params = [];
         foreach ($rawParams as $param) {
-            if (preg_match('/^--(\w+)(=(.*))?$/', $param, $matches)) {
+            if (preg_match('/^--(\w+)=(.*)?$/', $param, $matches)) {
                 $name = $matches[1];
                 if ($name !== Application::OPTION_APPCONFIG) {
-                    $params[$name] = isset($matches[3]) ? $matches[3] : true;
+                    $params[$name] = isset($matches[2]) ? $matches[2] : true;
                 }
-            } elseif (preg_match('/^-(\w+)(=(.*))?$/', $param, $matches)) {
+            } elseif (preg_match('/^-(\w+)=(.*)?$/', $param, $matches)) {
                 $name = $matches[1];
                 if ($name !== Application::OPTION_APPCONFIG) {
-                    $params['alias'][$name] = isset($matches[3]) ? $matches[3] : true;
+                    $params['alias'][$name] = isset($matches[2]) ? $matches[2] : true;
                 }
             } else {
                 $params[] = $param;
