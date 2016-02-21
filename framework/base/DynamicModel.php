c99b5b69c6cd0fd5d26c5ef6a470a9014222dd44
DrDeath72 <dr.death.ru@gmail.com>
2016-02-22 01:11:27 +0500

    Update DynamicModel.php

--- framework/base/DynamicModel.php
+++ framework/base/DynamicModel.php
@@ -156,7 +156,14 @@ class DynamicModel extends Model
     {
         $validators = $this->getValidators();
         foreach($rules as $rule) {
-            $validators->append(Validator::createValidator($rule[1], $this, (array) $rule[0], array_slice($rule, 2)));
+             if ($rule instanceof Validator) {
+                  $validators->append($rule);
+              } elseif (is_array($rule) && isset($rule[0], $rule[1])) { // attributes, validator type
+                 $validator = Validator::createValidator($rule[1], $this, (array) $rule[0], array_slice($rule, 2));
+                 $validators->append($validator);
+             } else {
+                 throw new InvalidConfigException('Invalid validation rule: a rule must specify both attribute names and validator type.');
+             }
         }
 
         return $this;
