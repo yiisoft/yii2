e311442e20f73d83bdf574be72e371af520cc419
Chris Harris <chris@buckshotsoftware.com>
2016-02-21 04:08:57 -0800

    AFTER column name is now escaped by safe method

--- db/cubrid/ColumnSchemaBuilder.php
+++ db/cubrid/ColumnSchemaBuilder.php
@@ -7,6 +7,7 @@
 
 namespace yii\db\cubrid;
 
+use Yii;
 use yii\db\ColumnSchemaBuilder as AbstractColumnSchemaBuilder;
 
 /**
@@ -30,7 +31,9 @@ class ColumnSchemaBuilder extends AbstractColumnSchemaBuilder
      */
     protected function buildAfterString()
     {
-        return $this->after !== null ? " AFTER ('{$this->after}')" : '';
+        return $this->after !== null ?
+            ' AFTER (' . Yii::$app->db->quoteColumnName($this->after) . ')' :
+            '';
     }
 
     /**
