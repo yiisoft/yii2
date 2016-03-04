a83bec993f6e07cb5cb6fda477984a8185aa5146
Daniel Gomez Pan <pana_1990@hotmail.com>
2016-03-04 22:52:20 +0100

    add alias option [skip ci]

--- framework/console/controllers/MessageController.php
+++ framework/console/controllers/MessageController.php
@@ -178,7 +178,8 @@ class MessageController extends Controller
             'S' => 'sort',
             't' => 'translator',
             'm' => 'sourceMessageTable',
-            's' => 'sourcePath'
+            's' => 'sourcePath',
+            'r' => 'removeUnused'
         ]);
     }
 
