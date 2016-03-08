6ea97f8d157ae7158ca2a25f0c547e042bfc09df
Robbert Jan van de Velde <rj@vimisol.com>
2016-03-08 10:36:34 +0100

    Actually changing the translation line with the one from the previous condition

--- framework/i18n/Formatter.php
+++ framework/i18n/Formatter.php
@@ -877,7 +877,7 @@ class Formatter extends Component
             $parts[] = Yii::t('yii', '{delta, plural, =1{1 second} other{# seconds}}', ['delta' => $interval->s], $this->locale);
         }
         if ($interval->s === 0 && empty($parts)) {
-            $parts[] = Yii::t('yii', '{delta} seconds', ['delta' => $interval->s], $this->locale);
+            $parts[] = Yii::t('yii', '{delta, plural, =1{1 second} other{# seconds}}', ['delta' => $interval->s], $this->locale);
             $isNegative = false;
         }
 
