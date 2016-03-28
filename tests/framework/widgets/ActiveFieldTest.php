608066b05f79f33479aa6f4400e9892c567ab38b
Carsten Brandt <mail@cebe.cc>
2016-03-28 04:54:52 +0200

    fixed ActiveForm tests for output buffering
    related to  #8779, and 7dc984d35928128b3612fb6aa4ed19b03ee963fd

--- tests/framework/widgets/ActiveFieldTest.php
+++ tests/framework/widgets/ActiveFieldTest.php
@@ -44,7 +44,8 @@ class ActiveFieldTest extends \yiiunit\TestCase
 
         $this->helperModel = new DynamicModel(['attributeName']);
         ob_start();
-        $this->helperForm = new ActiveForm(['action' => '/something']);
+        $this->helperForm = ActiveForm::begin(['action' => '/something', 'enableClientScript' => false]);
+        ActiveForm::end();
         ob_end_clean();
 
         $this->activeField = new ActiveFieldExtend(true);
@@ -347,7 +348,7 @@ EOD;
     public function testEnctype()
     {
         $this->activeField->fileInput();
-        $this->assertEqualsWithoutLE('multipart/form-data', $this->activeField->form->options['enctype']);
+        $this->assertEquals('multipart/form-data', $this->activeField->form->options['enctype']);
     }
 
     /**
