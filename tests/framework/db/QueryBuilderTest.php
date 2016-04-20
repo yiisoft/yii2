2478ba7b206d7ff87b7116505eb8de8b59eb8ccd
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-04-20 09:35:43 +0300

    QueryBuilderTest::testCommentColumn() - improved to use replaceQuotes()

--- tests/framework/db/QueryBuilderTest.php
+++ tests/framework/db/QueryBuilderTest.php
@@ -1548,29 +1548,29 @@ abstract class QueryBuilderTest extends DatabaseTestCase
     {
         $qb = $this->getQueryBuilder();
 
-        $expected = "ALTER TABLE `comment` CHANGE `add_comment` `add_comment` varchar(255) NOT NULL COMMENT 'This is my column.'";
+        $expected = "ALTER TABLE [[comment]] CHANGE [[add_comment]] [[add_comment]] varchar(255) NOT NULL COMMENT 'This is my column.'";
         $sql = $qb->addCommentOnColumn('comment', 'add_comment', 'This is my column.');
-        $this->assertEquals($expected, $sql);
+        $this->assertEquals($this->replaceQuotes($expected), $sql);
 
-        $expected = "ALTER TABLE `comment` CHANGE `replace_comment` `replace_comment` varchar(255) DEFAULT NULL COMMENT 'This is my column.'";
+        $expected = "ALTER TABLE [[comment]] CHANGE [[replace_comment]] [[replace_comment]] varchar(255) DEFAULT NULL COMMENT 'This is my column.'";
         $sql = $qb->addCommentOnColumn('comment', 'replace_comment', 'This is my column.');
-        $this->assertEquals($expected, $sql);
+        $this->assertEquals($this->replaceQuotes($expected), $sql);
 
-        $expected = "ALTER TABLE `comment` CHANGE `delete_comment` `delete_comment` varchar(128) NOT NULL COMMENT ''";
+        $expected = "ALTER TABLE [[comment]] CHANGE [[delete_comment]] [[delete_comment]] varchar(128) NOT NULL COMMENT ''";
         $sql = $qb->dropCommentFromColumn('comment', 'delete_comment');
-        $this->assertEquals($expected, $sql);
+        $this->assertEquals($this->replaceQuotes($expected), $sql);
     }
 
     public function testCommentTable()
     {
         $qb = $this->getQueryBuilder();
 
-        $expected = "ALTER TABLE `comment` COMMENT 'This is my table.'";
+        $expected = "ALTER TABLE [[comment]] COMMENT 'This is my table.'";
         $sql = $qb->addCommentOnTable('comment', 'This is my table.');
-        $this->assertEquals($expected, $sql);
+        $this->assertEquals($this->replaceQuotes($expected), $sql);
 
-        $expected = "ALTER TABLE `comment` COMMENT ''";
+        $expected = "ALTER TABLE [[comment]] COMMENT ''";
         $sql = $qb->dropCommentFromTable('comment');
-        $this->assertEquals($expected, $sql);
+        $this->assertEquals($this->replaceQuotes($expected), $sql);
     }
 }
