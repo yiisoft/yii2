22503ebe97c3280e516e323105f51e368d017959
Carsten Brandt <mail@cebe.cc>
2016-04-22 10:53:12 +0200

    allow setting custom logger in Dispatcher via config
    fixes #11056

---
+++ tests/framework/log/DispatcherTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @author Carsten Brandt <mail@cebe.cc>
+ */
+
+namespace yiiunit\framework\log;
+
+use yii\log\Dispatcher;
+use yii\log\Logger;
+use Yii;
+use yiiunit\TestCase;
+
+/**
+ * @group log
+ */
+class DispatcherTest extends TestCase
+{
+
+    public function testConfigureLogger()
+    {
+        $dispatcher = new Dispatcher();
+        $this->assertSame(Yii::getLogger(), $dispatcher->getLogger());
+
+
+        $logger = new Logger();
+        $dispatcher = new Dispatcher([
+            'logger' => $logger,
+        ]);
+        $this->assertSame($logger, $dispatcher->getLogger());
+
+
+        $dispatcher = new Dispatcher([
+            'logger' => 'yii\log\Logger',
+        ]);
+        $this->assertInstanceOf('yii\log\Logger', $dispatcher->getLogger());
+        $this->assertEquals(0, $dispatcher->getLogger()->traceLevel);
+
+
+        $dispatcher = new Dispatcher([
+            'logger' => [
+                'class' => 'yii\log\Logger',
+                'traceLevel' => 42,
+            ],
+        ]);
+        $this->assertInstanceOf('yii\log\Logger', $dispatcher->getLogger());
+        $this->assertEquals(42, $dispatcher->getLogger()->traceLevel);
+    }
+}
