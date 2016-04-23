449509a38b23f880bd45d2b1b0efe7d97b22cb20
Anatoly Rugalev <anatoly.rugalev@gmail.com>
2016-04-23 15:22:06 +0500

    [#10322] Fixed merge bug with invalid $button usage

--- framework/assets/yii.activeForm.js
+++ framework/assets/yii.activeForm.js
@@ -576,12 +576,13 @@
                 data.submitting = false;
             } else {
                 data.validated = true;
-                var buttonTarget = $button.attr('formtarget');
-                if (data.submitObject && buttonTarget) {
-                    // set target attribute to form tag at submit moment
+                var buttonTarget = data.submitObject ? data.submitObject.attr('formtarget') : null;
+                if (buttonTarget) {
+                    // set target attribute to form tag before submit
                     $form.attr('target', buttonTarget);
                 }
                 $form.submit();
+                // restore original target attribute value
                 $form.attr('target', data.target);
             }
         } else {
