cd140e61afb3ce8891a3c5f779eb9c6f53c86ad3
mdmunir <misbahuldmunir@gmail.com>
2016-04-28 07:32:05 +0700

    Changable default flavor

---
+++ tests/framework/helpers/MarkdownTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace yiiunit\framework\helpers;
+
+use yiiunit\TestCase;
+use yii\helpers\Markdown;
+/**
+ * Description of MarkdownTest
+ *
+ * @author Misbahul D Munir <misbahuldmunir@gmail.com>
+ */
+class MarkdownTest extends TestCase
+{
+    public function testOriginalFlavor()
+    {
+        $text = <<<TEXT
+html
+new line 1
+
+new line 2
+TEXT;
+
+        Markdown::$defaultFlavor = 'original';
+        $this->assertEquals(Markdown::process($text), Markdown::process($text, 'original'));
+
+        Markdown::$defaultFlavor = 'gfm-comment';
+        $this->assertNotEquals(Markdown::process($text), Markdown::process($text, 'original'));
+        $this->assertEquals(Markdown::process($text), Markdown::process($text, 'gfm-comment'));
+    }
+}
