9d327baa8b2c80b53d4d405678f03e6b89ff6e38
Nikola Kovacs <nikola.kovacs@gmail.com>
2016-05-26 11:19:32 +0200

    coding style fixes

--- framework/console/controllers/MessageController.php
+++ framework/console/controllers/MessageController.php
@@ -141,22 +141,22 @@ class MessageController extends Controller
     public function options($actionID)
     {
         return array_merge(parent::options($actionID), [
-                'sourcePath',
-                'messagePath',
-                'languages',
-                'translator',
-                'sort',
-                'overwrite',
-                'removeUnused',
-                'markUnused',
-                'except',
-                'only',
-                'format',
-                'db',
-                'sourceMessageTable',
-                'messageTable',
-                'catalog',
-                'ignoreCategories'
+            'sourcePath',
+            'messagePath',
+            'languages',
+            'translator',
+            'sort',
+            'overwrite',
+            'removeUnused',
+            'markUnused',
+            'except',
+            'only',
+            'format',
+            'db',
+            'sourceMessageTable',
+            'messageTable',
+            'catalog',
+            'ignoreCategories',
         ]);
     }
 
@@ -180,7 +180,7 @@ class MessageController extends Controller
             't' => 'translator',
             'm' => 'sourceMessageTable',
             's' => 'sourcePath',
-            'r' => 'removeUnused'
+            'r' => 'removeUnused',
         ]);
     }
 
