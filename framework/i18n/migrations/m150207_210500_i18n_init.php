f78ac6cd6c5c29bda0f0bd1f241fb02bb7c7e391
artur <artur.fursa@binn.com.ua>
2016-06-12 14:12:54 +0300

    Fix tables prefix use for i18n migrations

--- framework/i18n/migrations/m150207_210500_i18n_init.php
+++ framework/i18n/migrations/m150207_210500_i18n_init.php
@@ -25,26 +25,26 @@ class m150207_210500_i18n_init extends Migration
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 
-        $this->createTable('source_message', [
+        $this->createTable('{{%source_message}}', [
             'id' => $this->primaryKey(),
             'category' => $this->string(),
             'message' => $this->text(),
         ], $tableOptions);
 
-        $this->createTable('message', [
+        $this->createTable('{{%message}}', [
             'id' => $this->integer()->notNull(),
             'language' => $this->string(16)->notNull(),
             'translation' => $this->text(),
         ], $tableOptions);
 
-        $this->addPrimaryKey('pk_message_id_language', 'message', ['id', 'language']);
-        $this->addForeignKey('fk_message_source_message', 'message', 'id', 'source_message', 'id', 'CASCADE', 'RESTRICT');
+        $this->addPrimaryKey('pk_message_id_language', '{{%message}}', ['id', 'language']);
+        $this->addForeignKey('fk_message_source_message', '{{%message}}', 'id', '{{%source_message}}', 'id', 'CASCADE', 'RESTRICT');
     }
 
     public function down()
     {
-        $this->dropForeignKey('fk_message_source_message', 'message');
-        $this->dropTable('message');
-        $this->dropTable('source_message');
+        $this->dropForeignKey('fk_message_source_message', '{{%message}}');
+        $this->dropTable('{{%message}}');
+        $this->dropTable('{{%source_message}}');
     }
 }
