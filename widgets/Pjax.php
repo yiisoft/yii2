14f713b30deee5e0bebfea846cb3a7858ccb39a7
Thiago <thiago.oliveira.gt14@gmail.com>
2016-07-01 05:16:01 -0300

    Fix for #11850

--- widgets/Pjax.php
+++ widgets/Pjax.php
@@ -195,7 +195,7 @@ class Pjax extends Widget
         if ($this->formSelector !== false) {
             $formSelector = Json::htmlEncode($this->formSelector !== null ? $this->formSelector : '#' . $id . ' form[data-pjax]');
             $submitEvent = Json::htmlEncode($this->submitEvent);
-            $js .= "\njQuery(document).on('$submitEvent', $formSelector, function (event) {jQuery.pjax.submit(event, '#$id', $options);});";
+            $js .= "\njQuery(document).on($submitEvent, $formSelector, function (event) {jQuery.pjax.submit(event, '#$id', $options);});";
         }
         $view = $this->getView();
         PjaxAsset::register($view);
