c34f7b0265a0ca76f818c52d29ce598befc15d9c
DrDeath72 <dr.death.ru@gmail.com>
2016-07-01 11:50:29 +0500

    Case-insensitive verbs
    Like here 
    https://github.com/yiisoft/yii2/blob/master/framework/filters/VerbFilter.php#L100

--- framework/filters/AccessRule.php
+++ framework/filters/AccessRule.php
@@ -181,7 +181,7 @@ class AccessRule extends Component
      */
     protected function matchVerb($verb)
     {
-        return empty($this->verbs) || in_array($verb, $this->verbs, true);
+        return empty($this->verbs) || in_array($verb, array_map('strtoupper', $this->verbs), true);
     }
 
     /**
