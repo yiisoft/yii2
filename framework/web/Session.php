b2fbbdf5d703bce6c7c94b41c4be2ba145fe63b3
Alexander Makarov <sam@rmcreative.ru>
2016-07-30 01:09:39 +0300

    Reverted non-suppression of session_start since it seems it's not possible to achieve same behavior w/o it #10583

--- framework/web/Session.php
+++ framework/web/Session.php
@@ -128,9 +128,7 @@ class Session extends Component implements \IteratorAggregate, \ArrayAccess, \Co
 
         $this->setCookieParamsInternal();
 
-        if (!headers_sent()) {
-            YII_DEBUG ? session_start() : @session_start();
-        }
+        @session_start();
 
         if ($this->getIsActive()) {
             Yii::info('Session started', __METHOD__);
