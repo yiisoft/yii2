acfe21b4001b3a7775ea7755266548b96b1fd192
Klimov Paul <klimov.paul@gmail.com>
2016-08-01 10:31:10 +0300

    Fixed `yii\helpers\Json::encode()` encodes empty array returned by `JsonSerializable::jsonSerialize()` as object

--- framework/helpers/BaseJson.php
+++ framework/helpers/BaseJson.php
@@ -142,7 +142,7 @@ class BaseJson
 
                 return $token;
             } elseif ($data instanceof \JsonSerializable) {
-                $data = $data->jsonSerialize();
+                return $data->jsonSerialize();
             } elseif ($data instanceof Arrayable) {
                 $data = $data->toArray();
             } elseif ($data instanceof \SimpleXMLElement) {
