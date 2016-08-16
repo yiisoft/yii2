15a358b9ac49849b56981f2fc9f95d583a4f9d6f
Zulhilmi Rahman <ahmad.zulhilmi.89@gmail.com>
2016-08-16 10:25:42 +0800

    Update yii.php

--- framework/messages/ms/yii.php
+++ framework/messages/ms/yii.php
@@ -37,7 +37,7 @@ return [
     'Please upload a file.' => 'Sila muat naik fail',
     'Showing <b>{begin, number}-{end, number}</b> of <b>{totalCount, number}</b> {totalCount, plural, one{item} other{items}}.' => 
     'Memaparkan <b>{begin, number}-{end, number}</b> daripada <b>{totalCount, number}</b> {totalCount, plural, one{item} other{items}}.',
-    'The file "{file}" is not an image.' => 'Fail ini "{file}" bukan bejenis gambar.',
+    'The file "{file}" is not an image.' => 'Fail ini "{file}" bukan berjenis gambar.',
     'The file "{file}" is too big. Its size cannot exceed {formattedLimit}.' =>
     'Fail ini "{file}" terlalu besar. Saiz tidak boleh lebih besar daripada {formattedLimit}.',
     'The file "{file}" is too small. Its size cannot be smaller than {formattedLimit}.' =>
