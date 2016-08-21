7512e96f3aa4b77ecd80dc03de237a5013079d41
Dmitriy Makarov <makarov.dmitriy@gmail.com>
2016-08-21 07:41:26 +0300

    Added BaseYiiTest tests of the following methods: - BaseYii::info() - BaseYii::warning() - BaseYii::trace() - BaseYii::error() - BaseYii::beginProfile() - BaseYii::endProfile()

--- tests/framework/BaseYiiTest.php
+++ tests/framework/BaseYiiTest.php
@@ -102,4 +102,50 @@ class BaseYiiTest extends TestCase
         $defaultLogger = BaseYii::getLogger();
         $this->assertTrue($defaultLogger instanceof Logger);
     }
+
+    /**
+     * @covers \yii\BaseYii::info()
+     * @covers \yii\BaseYii::warning()
+     * @covers \yii\BaseYii::trace()
+     * @covers \yii\BaseYii::error()
+     * @covers \yii\BaseYii::beginProfile()
+     * @covers \yii\BaseYii::endProfile()
+     */
+    public function testLog()
+    {
+        $logger = $this->getMock('yii\\log\\Logger', ['log']);
+        BaseYii::setLogger($logger);
+
+        $logger->expects($this->exactly(6))
+            ->method('log')
+            ->withConsecutive(
+                [$this->equalTo('info message'), $this->equalTo(Logger::LEVEL_INFO), $this->equalTo('info category')],
+                [
+                    $this->equalTo('warning message'),
+                    $this->equalTo(Logger::LEVEL_WARNING),
+                    $this->equalTo('warning category')
+                ],
+                [$this->equalTo('trace message'), $this->equalTo(Logger::LEVEL_TRACE), $this->equalTo('trace category')],
+                [$this->equalTo('error message'), $this->equalTo(Logger::LEVEL_ERROR), $this->equalTo('error category')],
+                [
+                    $this->equalTo('beginProfile message'),
+                    $this->equalTo(Logger::LEVEL_PROFILE_BEGIN),
+                    $this->equalTo('beginProfile category')
+                ],
+                [
+                    $this->equalTo('endProfile message'),
+                    $this->equalTo(Logger::LEVEL_PROFILE_END),
+                    $this->equalTo('endProfile category')
+                ]
+            );
+
+        BaseYii::info('info message', 'info category');
+        BaseYii::warning('warning message', 'warning category');
+        BaseYii::trace('trace message', 'trace category');
+        BaseYii::error('error message', 'error category');
+        BaseYii::beginProfile('beginProfile message', 'beginProfile category');
+        BaseYii::endProfile('endProfile message', 'endProfile category');
+        
+        BaseYii::setLogger(null);
+    }
 }
