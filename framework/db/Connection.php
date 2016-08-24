ff8c7e75d1d6e4e6894054df444d460c17b56a6d
Roman Grinyov <w3lifer@gmail.com>
2016-08-24 09:31:11 +0300

    Removed duplicate sentence in docblock

--- framework/db/Connection.php
+++ framework/db/Connection.php
@@ -222,7 +222,6 @@ class Connection extends Component
     public $enableQueryCache = true;
     /**
      * @var integer the default number of seconds that query results can remain valid in cache.
-     * Use 0 to indicate that the cached data will never expire.
      * Defaults to 3600, meaning 3600 seconds, or one hour. Use 0 to indicate that the cached data will never expire.
      * The value of this property will be used when [[cache()]] is called without a cache duration.
      * @see enableQueryCache
