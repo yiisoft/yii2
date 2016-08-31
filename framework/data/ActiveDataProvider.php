6600b6862ccedf90f618e94d8bfad779233819dc
Ceana Razvan <razvan@ceana.ro>
2016-08-31 21:48:19 +0300

    Update ActiveDataProvider.php
    Changed to Yii::createObject() because in case of a custom constructor with some parameters and dependencies, the DI container is not called.

--- framework/data/ActiveDataProvider.php
+++ framework/data/ActiveDataProvider.php
@@ -173,7 +173,7 @@ class ActiveDataProvider extends BaseDataProvider
         parent::setSort($value);
         if (($sort = $this->getSort()) !== false && $this->query instanceof ActiveQueryInterface) {
             /* @var $model Model */
-            $model = new $this->query->modelClass;
+            $model = Yii::createObject($this->query->modelClass);
             if (empty($sort->attributes)) {
                 foreach ($model->attributes() as $attribute) {
                     $sort->attributes[$attribute] = [
