1edf562800c727ecf19e8619b92ae175cdfb4e9e
Carsten Brandt <mail@cebe.cc>
2016-09-08 00:42:48 +0200

    better version match for travis memcache

--- tests/data/travis/memcache-setup.sh
+++ tests/data/travis/memcache-setup.sh
@@ -6,7 +6,7 @@ else
   mkdir -p ~/.phpenv/versions/$(phpenv version-name)/etc
 
   # memcache is not available on PHP 7, memcacheD is.
-  if [ $(phpenv version-name) = 5.* ]; then
+  if [ $(phpenv version-name) = '5.4' ] || [ $(phpenv version-name) = '5.5' ] || [ $(phpenv version-name) = '5.6' ]; then
     echo "extension=memcache.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
   else
     echo "skipping memcache on php 7"
