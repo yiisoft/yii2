de8ce6a86011b060669755021872ba9adc887f5f
Daniil Razorenov <daniil@devgroup.ru>
2016-09-13 14:11:46 +0300

    create ArrayFilterHelper

---
+++ framework/helpers/ArrayFilterHelper.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\helpers;
+
+/**
+ * ArrayFilterHelper provide array filter by the array syntax condition functionality
+ *
+ * @author Daniil Razorenov <daniltmb@mail.ru>
+ * @since 2.0
+ */
+class ArrayFilterHelper extends BaseArrayFilterHelper
+{
+}
