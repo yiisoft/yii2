9a7aa8a265c773cdcd1cac483218662a69166a82
mdmunir <misbahuldmunir@gmail.com>
2016-10-04 21:23:43 +0700

    Show alternative when command not found

---
+++ framework/helpers/RouteHelper.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\helpers;
+
+/**
+ *
+ * @author Misbahul D Munir <misbahuldmunir@gmail.com>
+ * @since 2.0.10
+ */
+class RouteHelper extends BaseRouteHelper
+{
+    
+}
