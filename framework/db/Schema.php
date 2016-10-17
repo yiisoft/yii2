58cf98bbb6e37e596457b57dc220f5ce84a373a9
PowerGamer1 <PowerGamer1@users.noreply.github.com>
2016-10-17 16:33:09 +0300

    PHPDoc @return fix for insert()

--- framework/db/Schema.php
+++ framework/db/Schema.php
@@ -440,7 +440,7 @@ abstract class Schema extends Object
      * Executes the INSERT command, returning primary key values.
      * @param string $table the table that new rows will be inserted into.
      * @param array $columns the column data (name => value) to be inserted into the table.
-     * @return array primary key values or false if the command fails
+     * @return array|false primary key values or false if the command fails
      * @since 2.0.4
      */
     public function insert($table, $columns)
