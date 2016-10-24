97272cf57578a11ab3130073b4a14319defc9e49
SG5 <sergeyv89@gmail.com>
2016-10-24 16:43:21 +0300

    remove duplicate parameters

--- framework/widgets/BaseListView.php
+++ framework/widgets/BaseListView.php
@@ -181,6 +181,17 @@ abstract class BaseListView extends Widget
         }
         $summaryOptions = $this->summaryOptions;
         $tag = ArrayHelper::remove($summaryOptions, 'tag', 'div');
+
+        $begin = $end = $totalCount = $page = $pageCount = null;
+        $params = [
+            'begin' => &$begin,
+            'end' => &$end,
+            'count' => $count,
+            'totalCount' => &$totalCount,
+            'page' => &$page,
+            'pageCount' => &$pageCount,
+        ];
+
         if (($pagination = $this->dataProvider->getPagination()) !== false) {
             $totalCount = $this->dataProvider->getTotalCount();
             $begin = $pagination->getPage() * $pagination->pageSize + 1;
@@ -191,38 +202,17 @@ abstract class BaseListView extends Widget
             $page = $pagination->getPage() + 1;
             $pageCount = $pagination->pageCount;
             if (($summaryContent = $this->summary) === null) {
-                return Html::tag($tag, Yii::t('yii', 'Showing <b>{begin, number}-{end, number}</b> of <b>{totalCount, number}</b> {totalCount, plural, one{item} other{items}}.', [
-                        'begin' => $begin,
-                        'end' => $end,
-                        'count' => $count,
-                        'totalCount' => $totalCount,
-                        'page' => $page,
-                        'pageCount' => $pageCount,
-                    ]), $summaryOptions);
+                return Html::tag($tag, Yii::t('yii', 'Showing <b>{begin, number}-{end, number}</b> of <b>{totalCount, number}</b> {totalCount, plural, one{item} other{items}}.', $params), $summaryOptions);
             }
         } else {
             $begin = $page = $pageCount = 1;
             $end = $totalCount = $count;
             if (($summaryContent = $this->summary) === null) {
-                return Html::tag($tag, Yii::t('yii', 'Total <b>{count, number}</b> {count, plural, one{item} other{items}}.', [
-                    'begin' => $begin,
-                    'end' => $end,
-                    'count' => $count,
-                    'totalCount' => $totalCount,
-                    'page' => $page,
-                    'pageCount' => $pageCount,
-                ]), $summaryOptions);
+                return Html::tag($tag, Yii::t('yii', 'Total <b>{count, number}</b> {count, plural, one{item} other{items}}.', $params), $summaryOptions);
             }
         }
 
-        return Yii::$app->getI18n()->format($summaryContent, [
-            'begin' => $begin,
-            'end' => $end,
-            'count' => $count,
-            'totalCount' => $totalCount,
-            'page' => $page,
-            'pageCount' => $pageCount,
-        ], Yii::$app->language);
+        return Yii::$app->getI18n()->format($summaryContent, $params, Yii::$app->language);
     }
 
     /**
