1db72ca4cae518bcb72ecb199333ab398a177d2f
Boudewijn Vahrmeijer <info@dynasource.eu>
2016-11-01 16:05:51 +0100

    setup ActiveQueryTest

---
+++ tests/framework/db/ActiveQueryTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace yiiunit\framework\db;
+
+/**
+ * Class ActiveQueryTest the base class for testing ActiveQuery
+ *
+ */
+abstract class ActiveQueryTest extends DatabaseTestCase
+{
+
+}
