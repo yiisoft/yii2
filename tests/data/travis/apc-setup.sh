590adda294634cc142bb8ff9dc7edbfc87c29310
Alexey Rogachev <arogachev90@gmail.com>
2016-11-10 17:46:05 +0600

    Revert apc-setup.sh

--- tests/data/travis/apc-setup.sh
+++ tests/data/travis/apc-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-if [ "$TRAVIS_PHP_VERSION" = "5.4" ]; then
+if [ "$(expr "$TRAVIS_PHP_VERSION" "<" "5.5")" -eq 1 ]; then
 	echo "extension = apc.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 	echo "apc.enable_cli = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 else
