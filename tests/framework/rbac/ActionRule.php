3f2db47bdb1d08655efe1a2555cbd8ebb44ab61b
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-11-12 15:43:28 +0200

    Added unit tests for rules with private fields

--- tests/framework/rbac/ActionRule.php
+++ tests/framework/rbac/ActionRule.php
@@ -2,13 +2,19 @@
 
 namespace yiiunit\framework\rbac;
 
+use yii\rbac\Rule;
+
 /**
  * Description of ActionRule
  */
-class ActionRule extends \yii\rbac\Rule
+class ActionRule extends Rule
 {
     public $name = 'action_rule';
     public $action = 'read';
+
+    private $somePrivateProperty;
+    protected $someProtectedProperty;
+
     public function execute($user, $item, $params)
     {
         return $this->action === 'all' || $this->action === $params['action'];
