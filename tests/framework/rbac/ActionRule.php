9e129705a7c09ef32fedf55128ada471dd1106b4
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2016-11-12 15:58:57 +0200

    PHPDocs updated

--- tests/framework/rbac/ActionRule.php
+++ tests/framework/rbac/ActionRule.php
@@ -12,6 +12,14 @@ class ActionRule extends Rule
     public $name = 'action_rule';
     public $action = 'read';
 
+    /**
+     * Private and protected properties to ensure that serialized object
+     * does not get corrupted after saving into the DB because of null-bytes
+     * in the string.
+     *
+     * @see https://github.com/yiisoft/yii2/issues/10176
+     * @see https://github.com/yiisoft/yii2/issues/12681
+     */
     private $somePrivateProperty;
     protected $someProtectedProperty;
 
