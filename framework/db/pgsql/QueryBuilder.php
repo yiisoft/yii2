28768895ef0d1e707dbbbb53c574c196347933e0
voroks <voroks@bk.ru>
2016-11-28 11:44:07 +0300

    Updated comment for pgsql/QueryBuilder.php

--- framework/db/pgsql/QueryBuilder.php
+++ framework/db/pgsql/QueryBuilder.php
@@ -245,7 +245,8 @@ class QueryBuilder extends \yii\db\QueryBuilder
     /**
      * Normalizes data to be saved into the table, performing extra preparations and type converting, if necessary.
      * @param string $table the table that data will be saved into.
-     * @param array|\yii\db\Query $columns the column data (name => value) to be saved into the table.
+     * @param array|\yii\db\Query $columns the column data (name => value) to be saved into the table or instance
+     * of \yii\db\Query to perform INSERT INTO ... SELECT SQL statement.
      * Passing of \yii\db\Query is available since version 2.0.11.
      * @return array normalized columns
      * @since 2.0.9
