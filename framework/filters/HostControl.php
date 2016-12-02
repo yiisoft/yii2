64aa297b322e1366b162437dfc42469067ffa36d
Robert Korulczyk <robert@korulczyk.pl>
2016-12-02 23:02:41 +0100

    Update phpdoc.

--- framework/filters/HostControl.php
+++ framework/filters/HostControl.php
@@ -108,7 +108,8 @@ class HostControl extends ActionFilter
     /**
      * @var string|null fallback host info (e.g. `http://www.yiiframework.com`) used when [[\yii\web\Request::$hostInfo|Request::$hostInfo]] is invalid.
      * This value will replace [[\yii\web\Request::$hostInfo|Request::$hostInfo]] before [[$denyCallback]] is called to make sure that
-     * invalid host will not be used for further processing. You can set it to `null` to leave [[\yii\web\Request::$hostInfo|Request::$hostInfo]] untouched.
+     * an invalid host will not be used for further processing. You can set it to `null` to leave [[\yii\web\Request::$hostInfo|Request::$hostInfo]] untouched.
+     * Default value is empty string (this will result creating relative URLs instead of absolute).
      * @see \yii\web\Request::getHostInfo()
      */
     public $fallbackHostInfo = '';
