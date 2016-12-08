53f571cce2b16b8cd96442ebb4cda9b1c2cc479a
Herbert Maschke <thyseus@gmail.com>
2016-12-08 15:26:37 +0100

    Allow single role in filters/AccessRule.php
    Allows
    
    'roles' => '@'
    
    in addition to
    
    'roles' => ['@']
    
    in AccessRule.php

--- framework/filters/AccessRule.php
+++ framework/filters/AccessRule.php
@@ -36,7 +36,7 @@ class AccessRule extends Component
      */
     public $controllers;
     /**
-     * @var array list of roles that this rule applies to. Two special roles are recognized, and
+     * @var array|string list of roles or single role that this rule applies to. Two special roles are recognized, and
      * they are checked via [[User::isGuest]]:
      *
      * - `?`: matches a guest user (not authenticated yet)
@@ -139,6 +139,10 @@ class AccessRule extends Component
         if (empty($this->roles)) {
             return true;
         }
+
+        if(is_string($this->roles))
+            $this->roles = [$this->roles];
+
         foreach ($this->roles as $role) {
             if ($role === '?') {
                 if ($user->getIsGuest()) {
