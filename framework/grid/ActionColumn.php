2be6d41a91722b46b3f3bcc1cc21d4ea872140db
Carsten Brandt <mail@cebe.cc>
2017-02-01 19:41:11 +0100

    Made ActionColumn Yii::t() tags static again
    this was changed in 2.0.11 and cause the messages translation to not
    recognize them anymore.

--- framework/grid/ActionColumn.php
+++ framework/grid/ActionColumn.php
@@ -161,7 +161,19 @@ class ActionColumn extends Column
     {
         if (!isset($this->buttons[$name]) && strpos($this->template, '{' . $name . '}') !== false) {
             $this->buttons[$name] = function ($url, $model, $key) use ($name, $iconName, $additionalOptions) {
-                $title = Yii::t('yii', ucfirst($name));
+                switch($name) {
+                    case 'view':
+                        $title = Yii::t('yii', 'View');
+                        break;
+                    case 'update':
+                        $title = Yii::t('yii', 'Update');
+                        break;
+                    case 'delete':
+                        $title = Yii::t('yii', 'Delete');
+                        break;
+                    default:
+                        $title = ucfirst($name);
+                }
                 $options = array_merge([
                     'title' => $title,
                     'aria-label' => $title,
