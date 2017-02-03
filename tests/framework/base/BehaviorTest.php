yiisoft/yii2

commit 954c771fdbd867500bc5de8498a3a7453716a74a

Code cleanup (#13113)
* Simplified code branching, named variable better

* More simple code cleanup

* More consistent line breaks before return statemetns

framework/base/Component.php
@@ -130,20 +130,21 @@ class Component extends Object
         if (method_exists($this, $getter)) {
             // read property, e.g. getName()
             return $this->$getter();
-        } else {
-            // behavior property
-            $this->ensureBehaviors();
-            foreach ($this->_behaviors as $behavior) {
-                if ($behavior->canGetProperty($name)) {
-                    return $behavior->$name;
-                }
+        }
+
+        // behavior property
+        $this->ensureBehaviors();
+        foreach ($this->_behaviors as $behavior) {
+            if ($behavior->canGetProperty($name)) {
+                return $behavior->$name;
             }
         }
+
         if (method_exists($this, 'set' . $name)) {
             throw new InvalidCallException('Getting write-only property: ' . get_class($this) . '::' . $name);
-        } else {
-            throw new UnknownPropertyException('Getting unknown property: ' . get_class($this) . '::' . $name);
         }
+
+        throw new UnknownPropertyException('Getting unknown property: ' . get_class($this) . '::' . $name);
     }
 
     /**
@@ -182,22 +183,22 @@ class Component extends Object
             $this->attachBehavior($name, $value instanceof Behavior ? $value : Yii::createObject($value));
 
             return;
-        } else {
-            // behavior property
-            $this->ensureBehaviors();
-            foreach ($this->_behaviors as $behavior) {
-                if ($behavior->canSetProperty($name)) {
-                    $behavior->$name = $value;
+        }
 
-                    return;
-                }
+        // behavior property
+        $this->ensureBehaviors();
+        foreach ($this->_behaviors as $behavior) {
+            if ($behavior->canSetProperty($name)) {
+                $behavior->$name = $value;
+                return;
             }
         }
+
         if (method_exists($this, 'get' . $name)) {
             throw new InvalidCallException('Setting read-only property: ' . get_class($this) . '::' . $name);
-        } else {
-            throw new UnknownPropertyException('Setting unknown property: ' . get_class($this) . '::' . $name);
         }
+
+        throw new UnknownPropertyException('Setting unknown property: ' . get_class($this) . '::' . $name);
     }
 
     /**
@@ -219,15 +220,16 @@ class Component extends Object
         $getter = 'get' . $name;
         if (method_exists($this, $getter)) {
             return $this->$getter() !== null;
-        } else {
-            // behavior property
-            $this->ensureBehaviors();
-            foreach ($this->_behaviors as $behavior) {
-                if ($behavior->canGetProperty($name)) {
-                    return $behavior->$name !== null;
-                }
+        }
+
+        // behavior property
+        $this->ensureBehaviors();
+        foreach ($this->_behaviors as $behavior) {
+            if ($behavior->canGetProperty($name)) {
+                return $behavior->$name !== null;
             }
         }
+
         return false;
     }
 
@@ -250,16 +252,17 @@ class Component extends Object
         if (method_exists($this, $setter)) {
             $this->$setter(null);
             return;
-        } else {
-            // behavior property
-            $this->ensureBehaviors();
-            foreach ($this->_behaviors as $behavior) {
-                if ($behavior->canSetProperty($name)) {
-                    $behavior->$name = null;
-                    return;
-                }
+        }
+
+        // behavior property
+        $this->ensureBehaviors();
+        foreach ($this->_behaviors as $behavior) {
+            if ($behavior->canSetProperty($name)) {
+                $behavior->$name = null;
+                return;
             }
         }
+
         throw new InvalidCallException('Unsetting an unknown or read-only property: ' . get_class($this) . '::' . $name);
     }
 
@@ -503,19 +506,19 @@ class Component extends Object
         if ($handler === null) {
             unset($this->_events[$name]);
             return true;
-        } else {
-            $removed = false;
-            foreach ($this->_events[$name] as $i => $event) {
-                if ($event[0] === $handler) {
-                    unset($this->_events[$name][$i]);
-                    $removed = true;
-                }
-            }
-            if ($removed) {
-                $this->_events[$name] = array_values($this->_events[$name]);
-            }
-            return $removed;
         }
+
+        $removed = false;
+        foreach ($this->_events[$name] as $i => $event) {
+            if ($event[0] === $handler) {
+                unset($this->_events[$name][$i]);
+                $removed = true;
+            }
+        }
+        if ($removed) {
+            $this->_events[$name] = array_values($this->_events[$name]);
+        }
+        return $removed;
     }
 
     /**
@@ -621,9 +624,9 @@ class Component extends Object
             unset($this->_behaviors[$name]);
             $behavior->detach();
             return $behavior;
-        } else {
-            return null;
         }
+
+        return null;
     }
 
     /**
@@ -666,13 +669,14 @@ class Component extends Object
         if (is_int($name)) {
             $behavior->attach($this);
             $this->_behaviors[] = $behavior;
-        } else {
-            if (isset($this->_behaviors[$name])) {
-                $this->_behaviors[$name]->detach();
-            }
-            $behavior->attach($this);
-            $this->_behaviors[$name] = $behavior;
         }
+
+        if (isset($this->_behaviors[$name])) {
+            $this->_behaviors[$name]->detach();
+        }
+        $behavior->attach($this);
+        $this->_behaviors[$name] = $behavior;
+
         return $behavior;
     }
 }

framework/BaseYii.php
@@ -139,20 +139,20 @@ class BaseYii
         if (isset(static::$aliases[$root])) {
             if (is_string(static::$aliases[$root])) {
                 return $pos === false ? static::$aliases[$root] : static::$aliases[$root] . substr($alias, $pos);
-            } else {
-                foreach (static::$aliases[$root] as $name => $path) {
-                    if (strpos($alias . '/', $name . '/') === 0) {
-                        return $path . substr($alias, strlen($name));
-                    }
+            }
+
+            foreach (static::$aliases[$root] as $name => $path) {
+                if (strpos($alias . '/', $name . '/') === 0) {
+                    return $path . substr($alias, strlen($name));
                 }
             }
         }
 
         if ($throwException) {
             throw new InvalidParamException("Invalid path alias: $alias");
-        } else {
-            return false;
         }
+
+        return false;
     }
 
     /**
@@ -170,11 +170,11 @@ class BaseYii
         if (isset(static::$aliases[$root])) {
             if (is_string(static::$aliases[$root])) {
                 return $root;
-            } else {
-                foreach (static::$aliases[$root] as $name => $path) {
-                    if (strpos($alias . '/', $name . '/') === 0) {
-                        return $name;
-                    }
+            }
+
+            foreach (static::$aliases[$root] as $name => $path) {
+                if (strpos($alias . '/', $name . '/') === 0) {
+                    return $name;
                 }
             }
         }
@@ -346,9 +346,9 @@ class BaseYii
             return static::$container->invoke($type, $params);
         } elseif (is_array($type)) {
             throw new InvalidConfigException('Object configuration must be an array containing a "class" element.');
-        } else {
-            throw new InvalidConfigException('Unsupported configuration type: ' . gettype($type));
         }
+
+        throw new InvalidConfigException('Unsupported configuration type: ' . gettype($type));
     }
 
     private static $_logger;
@@ -360,9 +360,9 @@ class BaseYii
     {
         if (self::$_logger !== null) {
             return self::$_logger;
-        } else {
-            return self::$_logger = static::createObject('yii\log\Logger');
         }
+
+        return self::$_logger = static::createObject('yii\log\Logger');
     }
 
     /**
@@ -499,14 +499,14 @@ class BaseYii
     {
         if (static::$app !== null) {
             return static::$app->getI18n()->translate($category, $message, $params, $language ?: static::$app->language);
-        } else {
-            $p = [];
-            foreach ((array) $params as $name => $value) {
-                $p['{' . $name . '}'] = $value;
-            }
-
-            return ($p === []) ? $message : strtr($message, $p);
         }
+
+        $placeholders = [];
+        foreach ((array) $params as $name => $value) {
+            $placeholders['{' . $name . '}'] = $value;
+        }
+
+        return ($placeholders === []) ? $message : strtr($message, $placeholders);
     }
 
     /**

framework/base/Controller.php
@@ -186,9 +186,8 @@ class Controller extends Component implements ViewContextInterface
             return $this->runAction($route, $params);
         } elseif ($pos > 0) {
             return $this->module->runAction($route, $params);
-        } else {
-            return Yii::$app->runAction(ltrim($route, '/'), $params);
         }
+        return Yii::$app->runAction(ltrim($route, '/'), $params);
     }
 
     /**
@@ -393,9 +392,8 @@ class Controller extends Component implements ViewContextInterface
         $layoutFile = $this->findLayoutFile($this->getView());
         if ($layoutFile !== false) {
             return $this->getView()->renderFile($layoutFile, ['content' => $content], $this);
-        } else {
-            return $content;
         }
+        return $content;
     }
 
     /**

framework/base/Event.php
@@ -116,20 +116,19 @@ class Event extends Object
         if ($handler === null) {
             unset(self::$_events[$name][$class]);
             return true;
-        } else {
-            $removed = false;
-            foreach (self::$_events[$name][$class] as $i => $event) {
-                if ($event[0] === $handler) {
-                    unset(self::$_events[$name][$class][$i]);
-                    $removed = true;
-                }
-            }
-            if ($removed) {
-                self::$_events[$name][$class] = array_values(self::$_events[$name][$class]);
-            }
-
-            return $removed;
         }
+
+        $removed = false;
+        foreach (self::$_events[$name][$class] as $i => $event) {
+            if ($event[0] === $handler) {
+                unset(self::$_events[$name][$class][$i]);
+                $removed = true;
+            }
+        }
+        if ($removed) {
+            self::$_events[$name][$class] = array_values(self::$_events[$name][$class]);
+        }
+        return $removed;
     }
 
     /**

framework/base/Model.php
@@ -562,9 +562,8 @@ class Model extends Component implements IteratorAggregate, ArrayAccess, Arrayab
     {
         if ($attribute === null) {
             return $this->_errors === null ? [] : $this->_errors;
-        } else {
-            return isset($this->_errors[$attribute]) ? $this->_errors[$attribute] : [];
         }
+        return isset($this->_errors[$attribute]) ? $this->_errors[$attribute] : [];
     }
 
     /**
@@ -578,16 +577,15 @@ class Model extends Component implements IteratorAggregate, ArrayAccess, Arrayab
     {
         if (empty($this->_errors)) {
             return [];
-        } else {
-            $errors = [];
-            foreach ($this->_errors as $name => $es) {
-                if (!empty($es)) {
-                    $errors[$name] = reset($es);
-                }
-            }
-
-            return $errors;
         }
+
+        $errors = [];
+        foreach ($this->_errors as $name => $es) {
+            if (!empty($es)) {
+                $errors[$name] = reset($es);
+            }
+        }
+        return $errors;
     }
 
     /**
@@ -829,9 +827,8 @@ class Model extends Component implements IteratorAggregate, ArrayAccess, Arrayab
             $this->setAttributes($data[$scope]);
 
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
     /**

framework/base/Module.php
@@ -391,9 +391,8 @@ class Module extends ServiceLocator
             $module = $this->getModule(substr($id, 0, $pos));
 
             return $module === null ? false : $module->hasModule(substr($id, $pos + 1));
-        } else {
-            return isset($this->_modules[$id]);
         }
+        return isset($this->_modules[$id]);
     }
 
     /**
@@ -467,9 +466,8 @@ class Module extends ServiceLocator
             }
 
             return $modules;
-        } else {
-            return $this->_modules;
         }
+        return $this->_modules;
     }
 
     /**
@@ -527,10 +525,10 @@ class Module extends ServiceLocator
             }
 
             return $result;
-        } else {
-            $id = $this->getUniqueId();
-            throw new InvalidRouteException('Unable to resolve the request "' . ($id === '' ? $route : $id . '/' . $route) . '".');
         }
+
+        $id = $this->getUniqueId();
+        throw new InvalidRouteException('Unable to resolve the request "' . ($id === '' ? $route : $id . '/' . $route) . '".');
     }
 
     /**
@@ -640,9 +638,8 @@ class Module extends ServiceLocator
             return get_class($controller) === $className ? $controller : null;
         } elseif (YII_DEBUG) {
             throw new InvalidConfigException("Controller class must extend from \\yii\\base\\Controller.");
-        } else {
-            return null;
         }
+        return null;
     }
 
     /**

framework/base/View.php
@@ -347,9 +347,8 @@ class View extends Component
             $this->addDynamicPlaceholder($placeholder, $statements);
 
             return $placeholder;
-        } else {
-            return $this->evaluateDynamicContent($statements);
         }
+        return $this->evaluateDynamicContent($statements);
     }
 
     /**
@@ -465,9 +464,8 @@ class View extends Component
             $this->endCache();
 
             return false;
-        } else {
-            return true;
         }
+        return true;
     }
 
     /**

commit 63c3a56766e7d7d2ba4e157ef36cbec1e2248f0d

fixed duplicate attachment of behaviors
fixes BC break introduced in
https://github.com/yiisoft/yii2/commit/954c771fdbd867500bc5de8498a3a7453716a74a?diff=split#diff-d45d5e14cbb9739c2e1c63b53ba5e363L669

fixes #13508

framework/base/Component.php
@@ -669,13 +669,13 @@ class Component extends Object
         if (is_int($name)) {
             $behavior->attach($this);
             $this->_behaviors[] = $behavior;
+        } else {
+            if (isset($this->_behaviors[$name])) {
+                $this->_behaviors[$name]->detach();
+            }
+            $behavior->attach($this);
+            $this->_behaviors[$name] = $behavior;
         }
-
-        if (isset($this->_behaviors[$name])) {
-            $this->_behaviors[$name]->detach();
-        }
-        $behavior->attach($this);
-        $this->_behaviors[$name] = $behavior;
 
         return $behavior;
     }

tests/framework/base/BehaviorTest.php
@@ -22,6 +22,9 @@ class FooClass extends Component
 
 class BarBehavior extends Behavior
 {
+    public static $attachCount = 0;
+    public static $detachCount = 0;
+
     public $behaviorProperty = 'behavior property';
 
     public function behaviorMethod()
@@ -46,6 +49,18 @@ class BarBehavior extends Behavior
 
         return parent::hasMethod($name);
     }
+
+    public function attach($owner)
+    {
+        self::$attachCount++;
+        parent::attach($owner);
+    }
+
+    public function detach()
+    {
+        self::$detachCount++;
+        parent::detach();
+    }
 }
 
 /**
@@ -59,11 +74,16 @@ class BehaviorTest extends TestCase
         $this->mockApplication();
     }
 
-    public function testAttachAndAccessing()
+    public function testAttachAndAccessingWithName()
     {
+        BarBehavior::$attachCount = 0;
+        BarBehavior::$detachCount = 0;
+
         $bar = new BarClass();
         $behavior = new BarBehavior();
         $bar->attachBehavior('bar', $behavior);
+        $this->assertEquals(1, BarBehavior::$attachCount);
+        $this->assertEquals(0, BarBehavior::$detachCount);
         $this->assertEquals('behavior property', $bar->behaviorProperty);
         $this->assertEquals('behavior method', $bar->behaviorMethod());
         $this->assertEquals('behavior property', $bar->getBehavior('bar')->behaviorProperty);
@@ -71,14 +91,37 @@ class BehaviorTest extends TestCase
 
         $behavior = new BarBehavior(['behaviorProperty' => 'reattached']);
         $bar->attachBehavior('bar', $behavior);
+        $this->assertEquals(2, BarBehavior::$attachCount);
+        $this->assertEquals(1, BarBehavior::$detachCount);
         $this->assertEquals('reattached', $bar->behaviorProperty);
+    }
+
+    public function testAttachAndAccessingAnonymous()
+    {
+        BarBehavior::$attachCount = 0;
+        BarBehavior::$detachCount = 0;
+
+        $bar = new BarClass();
+        $behavior = new BarBehavior();
+        $bar->attachBehaviors([$behavior]);
+        $this->assertEquals(1, BarBehavior::$attachCount);
+        $this->assertEquals(0, BarBehavior::$detachCount);
+        $this->assertEquals('behavior property', $bar->behaviorProperty);
+        $this->assertEquals('behavior method', $bar->behaviorMethod());
     }
 
     public function testAutomaticAttach()
     {
+        BarBehavior::$attachCount = 0;
+        BarBehavior::$detachCount = 0;
+
         $foo = new FooClass();
+        $this->assertEquals(0, BarBehavior::$attachCount);
+        $this->assertEquals(0, BarBehavior::$detachCount);
         $this->assertEquals('behavior property', $foo->behaviorProperty);
         $this->assertEquals('behavior method', $foo->behaviorMethod());
+        $this->assertEquals(1, BarBehavior::$attachCount);
+        $this->assertEquals(0, BarBehavior::$detachCount);
     }
 
     public function testMagicMethods()