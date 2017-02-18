4ceb34f4dddff7eb42c9a6458d407497a2203b71
Vladimir Reznichenko <kalessil@gmail.com>
2017-02-18 08:08:18 +0100

    Php Inspections (EA Extended): refereted instanceof self in a trait

--- framework/db/ActiveRelationTrait.php
+++ framework/db/ActiveRelationTrait.php
@@ -197,7 +197,13 @@ trait ActiveRelationTrait
             throw new InvalidConfigException('Invalid link: it must be an array of key-value pairs.');
         }
 
-        if (is_array($this->via)) {
+        if ($this->via instanceof self) {
+            // via junction table
+            /* @var $viaQuery ActiveRelationTrait */
+            $viaQuery = $this->via;
+            $viaModels = $viaQuery->findJunctionRows($primaryModels);
+            $this->filterByModels($viaModels);
+        } elseif (is_array($this->via)) {
             // via relation
             /* @var $viaQuery ActiveRelationTrait|ActiveQueryTrait */
             list($viaName, $viaQuery) = $this->via;
