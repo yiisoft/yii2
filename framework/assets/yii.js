4239f288dca363cc58bcd29bd430cd390099b145
Vladislav Lyshenko <lyshenko@devoler.com>
2017-03-09 16:24:00 +0200

    GridView filter JS error fix

--- framework/assets/yii.js
+++ framework/assets/yii.js
@@ -279,10 +279,10 @@ window.yii = (function ($) {
             for (var i = 0, len = pairs.length; i < len; i++) {
                 var pair = pairs[i].split('=');
                 var name = decodeURIComponent(pair[0].replace(/\+/g, '%20'));
-                var value = decodeURIComponent(pair[1].replace(/\+/g, '%20'));
                 if (!name.length) {
                     continue;
                 }
+                var value = decodeURIComponent(pair[1].replace(/\+/g, '%20'));
                 if (params[name] === undefined) {
                     params[name] = value || '';
                 } else {
