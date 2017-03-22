c5afc221d68a408bc347d9f4433b8dd1c178bf69
Kolyunya <oleynikovny@mail.ru>
2017-03-22 22:34:46 +0300

    Fix a bug in `yii\web\User::loginRequired()`
    Throw Unauthorized HTTP exception instead of the Forbidden HTTP
    exception when `loginUrl` is not set or a redirect is not applicable.

--- tests/framework/web/UserTest.php
+++ tests/framework/web/UserTest.php
@@ -17,7 +17,7 @@ namespace yiiunit\framework\web;
 use yii\base\NotSupportedException;
 use yii\base\Component;
 use yii\rbac\PhpManager;
-use yii\web\ForbiddenHttpException;
+use yii\web\UnauthorizedHttpException;
 use yii\web\Cookie;
 use yii\web\CookieCollection;
 use yii\web\IdentityInterface;
@@ -226,7 +226,7 @@ class UserTest extends TestCase
         $_SERVER['HTTP_ACCEPT'] = 'text/json, */*; q=0.1';
         try {
             $user->loginRequired();
-        } catch (ForbiddenHttpException $e) {}
+        } catch (UnauthorizedHttpException $e) {}
         $this->assertFalse(Yii::$app->response->getIsRedirection());
 
         $this->reset();
@@ -263,12 +263,12 @@ class UserTest extends TestCase
         $_SERVER['HTTP_ACCEPT'] = 'text/json;q=0.1';
         try {
             $user->loginRequired();
-        } catch (ForbiddenHttpException $e) {}
+        } catch (UnauthorizedHttpException $e) {}
         $this->assertNotEquals('json-only', $user->getReturnUrl());
 
         $this->reset();
         $_SERVER['HTTP_ACCEPT'] = 'text/json;q=0.1';
-        $this->setExpectedException('yii\\web\\ForbiddenHttpException');
+        $this->setExpectedException('yii\\web\\UnauthorizedHttpException');
         $user->loginRequired();
     }
 
@@ -291,7 +291,7 @@ class UserTest extends TestCase
         $this->mockWebApplication($appConfig);
         $this->reset();
         $_SERVER['HTTP_ACCEPT'] = 'text/json,q=0.1';
-        $this->setExpectedException('yii\\web\\ForbiddenHttpException');
+        $this->setExpectedException('yii\\web\\UnauthorizedHttpException');
         Yii::$app->user->loginRequired();
     }
 
