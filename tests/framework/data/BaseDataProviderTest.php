c57b5cd2ff4b9624b6277b458f4965928d4dbf8c
Sam Mousa <sam@mousa.nl>
2017-03-29 15:25:45 +0200

    Fixed test not resetting internal state.

--- tests/framework/data/BaseDataProviderTest.php
+++ tests/framework/data/BaseDataProviderTest.php
@@ -9,6 +9,10 @@ class BaseDataProviderTest extends TestCase
 {
     public function testGenerateId()
     {
+        $rc = new \ReflectionClass(BaseDataProvider::className());
+        $rp = $rc->getProperty('counter');
+        $rp->setAccessible(true);
+        $rp->setValue(null);
 
         $this->assertNull((new ConcreteDataProvider())->id);
         $this->assertNotNull((new ConcreteDataProvider())->id);
