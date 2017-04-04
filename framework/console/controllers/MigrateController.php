867cd9989bed649f97109fc488e7a40ebe34c52a
mixartemev <mixartemev@gmail.com>
2017-04-04 16:45:11 +0300

    comment for tableComment)

--- framework/console/controllers/MigrateController.php
+++ framework/console/controllers/MigrateController.php
@@ -127,9 +127,7 @@ class MigrateController extends BaseMigrateController
      */
     public $db = 'db';
     /**
-     * @var Connection|array|string the DB connection object or the application component ID of the DB connection to use
-     * when applying migrations. Starting from version 2.0.3, this can also be a configuration array
-     * for creating the object.
+     * @var string the comment for created table. Starting from version 2.0.14.
      */
     public $comment = '';
 
