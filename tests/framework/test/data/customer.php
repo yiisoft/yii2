aaa2d4c8d1394d339e8713a1378ee721c8873b80
leandrogehlen <leandrogehlen@gmail.com>
2017-04-17 22:04:02 -0300

    Fixes #5442: Moved `resetTable` to `unload` method

--- tests/framework/test/data/customer.php
+++ tests/framework/test/data/customer.php
@@ -6,11 +6,13 @@ return [
         'name' => 'customer1',
         'address' => 'address1',
         'status' => 1,
+        'profile_id' => 1
     ],
     'customer2' => [
         'email' => 'customer2@example.com',
         'name' => 'customer2',
         'address' => 'address2',
         'status' => 2,
+        'profile_id' => 2
     ],
 ];
---
+++ tests/framework/test/data/profile.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'profile1' => [
+        'id' => 1,
+        'description' => 'profile 3',
+    ],
+    'profile2' => [
+        'id' => 2,
+        'description' => 'profile 4',
+    ],
+];
