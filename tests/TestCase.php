d3f97f7e64f9ced72a418b70d4e2ad9f3f23d902
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2017-04-20 14:46:30 +0300

    Switched to asset-packagist

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -68,6 +68,10 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
             'id' => 'testapp',
             'basePath' => __DIR__,
             'vendorPath' => $this->getVendorPath(),
+            'aliases' => [
+                '@bower' => '@vendor/bower-asset',
+                '@npm'   => '@vendor/npm-asset',
+            ],
             'components' => [
                 'request' => [
                     'cookieValidationKey' => 'wefJDF8sfdsfSDefwqdxj9oq',
