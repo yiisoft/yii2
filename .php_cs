972b156d967d99b4e79bb2fc150d32ff9f7ffd58
Robert Korulczyk <robert@korulczyk.pl>
2017-05-02 12:46:01 +0200

    Extract config to separate classes.

--- .php_cs
+++ .php_cs
@@ -1,14 +1,14 @@
 <?php
 
-if (!class_exists('PhpCsFixer\Config', true)) {
+if (!class_exists('yii\cs\YiisoftConfig', true)) {
+    // @todo change error message
     fwrite(STDERR, "Your php-cs-version is outdated: please upgrade it.\n");
     die(16);
 }
 
-return PhpCsFixer\Config::create()
-    ->setCacheFile(__DIR__ . '/.php_cs.cache')
-    ->setRules([
-        '@PSR2' => true,
+return yii\cs\YiisoftConfig::create()
+    ->setCacheFile(__DIR__ . '/tests/runtime/php_cs.cache')
+    ->mergeRules([
         'braces' => [
             'allow_single_line_closure' => true,
         ],
---
+++ cs/src/YiiConfig.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\cs;
+
+use PhpCsFixer\Config;
+use yii\helpers\ArrayHelper;
+
+/**
+ * Basic rules used by Yii 2 ecosystem.
+ *
+ * @author Robert Korulczyk <robert@korulczyk.pl>
+ * @since 2.0.0
+ */
+class YiiConfig extends Config
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($name = 'yii-cs-config')
+    {
+        parent::__construct($name);
+
+        $this->setRules([
+            '@PSR2' => true,
+        ]);
+    }
+
+    /**
+     * Merge current rules config with provided list of rules.
+     *
+     * @param array $rules
+     * @return $this
+     * @see setRules()
+     * @see ArrayHelper::merge()
+     */
+    public function mergeRules(array $rules)
+    {
+        parent::setRules(ArrayHelper::merge($this->getRules(), $rules));
+
+        return $this;
+    }
+}
