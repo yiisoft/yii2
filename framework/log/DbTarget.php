44d65220d40c6edc4cbedbd3c23ff77b04b5a593
Alexander Makarov <sam@rmcreative.ru>
2017-05-04 21:28:53 +0300

    Use clone keyword as stated in PHP manual [skip ci]

--- framework/log/DbTarget.php
+++ framework/log/DbTarget.php
@@ -61,7 +61,7 @@ class DbTarget extends Target
     public function export()
     {
         if ($this->db->getTransaction()) {
-            $this->db = clone($this->db);
+            $this->db = clone $this->db;
             $this->db->pdo = null;
             $this->db->open();
         }
