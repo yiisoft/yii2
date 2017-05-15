0ce1c230c4114633a949e20257ace89526f3e9f5
qihuajun <qihuajun@social-touch.com>
2017-05-15 19:13:06 +0800

    #14157 Fix Incorrect Value When Using loadDefaultValues with DATETIME Field: add Unit Tests

--- tests/framework/db/mysql/SchemaTest.php
+++ tests/framework/db/mysql/SchemaTest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace yiiunit\framework\db\mysql;
+use yii\db\Expression;
 
 /**
  * @group db
@@ -9,4 +10,25 @@ namespace yiiunit\framework\db\mysql;
 class SchemaTest extends \yiiunit\framework\db\SchemaTest
 {
     public $driverName = 'mysql';
+
+    public function testLoadDefaultDatetimeColumn(){
+        if (version_compare($this->getConnection()->pdo->getAttribute(\PDO::ATTR_SERVER_VERSION), '5.6', '>=')) {
+            $sql = <<<SQL
+CREATE TABLE  IF NOT EXISTS `datetime_test`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+SQL;
+
+            $this->getConnection()->createCommand($sql)->execute();
+
+            $schema = $this->getConnection()->getTableSchema('datetime_test');
+
+            $dt = $schema->columns['dt'];
+
+            $this->assertInstanceOf(Expression::className(),$dt->defaultValue);
+        }
+    }
 }
