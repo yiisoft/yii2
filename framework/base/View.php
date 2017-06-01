0beb59305c638d2c8c0dbff9962a0a860f405ac4
Paul Klimov <klimov.paul@gmail.com>
2017-06-02 01:30:21 +0300

    Fixes #13058: Fixed caught exception thrown during view file rendering produces wrong output

--- framework/base/View.php
+++ framework/base/View.php
@@ -322,12 +322,28 @@ class View extends Component
      */
     public function renderPhpFile($_file_, $_params_ = [])
     {
+        $_obInitialLevel_ = ob_get_level();
         ob_start();
         ob_implicit_flush(false);
         extract($_params_, EXTR_OVERWRITE);
-        require($_file_);
-
-        return ob_get_clean();
+        try {
+            require($_file_);
+            return ob_get_clean();
+        } catch (\Exception $e) {
+            while (ob_get_level() > $_obInitialLevel_) {
+                if (!@ob_end_clean()) {
+                    ob_clean();
+                }
+            }
+            throw $e;
+        } catch (\Throwable $e) {
+            while (ob_get_level() > $_obInitialLevel_) {
+                if (!@ob_end_clean()) {
+                    ob_clean();
+                }
+            }
+            throw $e;
+        }
     }
 
     /**
