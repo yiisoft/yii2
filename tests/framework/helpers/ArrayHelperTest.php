59002a9e1eeac7b490c0fa2bb6f2f6c8320f60f3
Alex <LAV451@gmail.com>
2017-07-16 00:25:44 +0300

    Fixes #6644: Added `yii\helpers\ArrayHelper::setValue()`

--- tests/framework/helpers/ArrayHelperTest.php
+++ tests/framework/helpers/ArrayHelperTest.php
@@ -821,6 +821,246 @@ class ArrayHelperTest extends TestCase
         $this->assertEquals(23, ArrayHelper::getValue($arrayObject, 'nonExisting'));
     }
 
+    /**
+     * Data provider for [[testSetValue()]]
+     * @return array test data
+     */
+    public function dataProviderSetValue()
+    {
+        return [
+            [
+                [
+                    'key1' => 'val1',
+                    'key2' => 'val2',
+                ],
+                'key', 'val',
+                [
+                    'key1' => 'val1',
+                    'key2' => 'val2',
+                    'key' => 'val',
+                ],
+            ],
+            [
+                [
+                    'key1' => 'val1',
+                    'key2' => 'val2',
+                ],
+                'key2', 'val',
+                [
+                    'key1' => 'val1',
+                    'key2' => 'val',
+                ],
+            ],
+
+            [
+                [
+                    'key1' => 'val1',
+                ],
+                'key.in', 'val',
+                [
+                    'key1' => 'val1',
+                    'key' => ['in' => 'val'],
+                ],
+            ],
+            [
+                [
+                    'key' => 'val1',
+                ],
+                'key.in', 'val',
+                [
+                    'key' => [
+                        'val1',
+                        'in' => 'val'
+                    ],
+                ],
+            ],
+            [
+                [
+                    'key' => 'val1',
+                ],
+                'key', ['in' => 'val'],
+                [
+                    'key' => ['in' => 'val'],
+                ],
+            ],
+
+            [
+                [
+                    'key1' => 'val1',
+                ],
+                'key.in.0', 'val',
+                [
+                    'key1' => 'val1',
+                    'key' => [
+                        'in' => ['val']
+                    ],
+                ],
+            ],
+
+            [
+                [
+                    'key1' => 'val1',
+                ],
+                'key.in.arr', 'val',
+                [
+                    'key1' => 'val1',
+                    'key' => [
+                        'in' => [
+                            'arr' => 'val'
+                        ]
+                    ],
+                ],
+            ],
+            [
+                [
+                    'key1' => 'val1',
+                ],
+                'key.in.arr', ['val'],
+                [
+                    'key1' => 'val1',
+                    'key' => [
+                        'in' => [
+                            'arr' => ['val']
+                        ]
+                    ],
+                ],
+            ],
+            [
+                [
+                    'key' => [
+                        'in' => ['val1']
+                    ],
+                ],
+                'key.in.arr', 'val',
+                [
+                    'key' => [
+                        'in' => [
+                            'val1',
+                            'arr' => 'val'
+                        ]
+                    ],
+                ],
+            ],
+
+            [
+                [
+                    'key' => ['in' => 'val1'],
+                ],
+                'key.in.arr', ['val'],
+                [
+                    'key' => [
+                        'in' => [
+                            'val1',
+                            'arr' => ['val']
+                        ]
+                    ],
+                ],
+            ],
+            [
+                [
+                    'key' => [
+                        'in' => [
+                            'val1',
+                            'key' => 'val'
+                        ]
+                    ],
+                ],
+                'key.in.0', ['arr' => 'val'],
+                [
+                    'key' => [
+                        'in' => [
+                            ['arr' => 'val'],
+                            'key' => 'val'
+                        ]
+                    ],
+                ],
+            ],
+            [
+                [
+                    'key' => [
+                        'in' => [
+                            'val1',
+                            'key' => 'val'
+                        ]
+                    ],
+                ],
+                'key.in', ['arr' => 'val'],
+                [
+                    'key' => [
+                        'in' => ['arr' => 'val']
+                    ],
+                ],
+            ],
+            [
+                [
+                    'key' => [
+                        'in' => [
+                            'key' => 'val',
+                            'data' => [
+                                'attr1',
+                                'attr2',
+                                'attr3',
+                            ]
+                        ]
+                    ],
+                ],
+                'key.in.schema', 'array',
+                [
+                    'key' => [
+                        'in' => [
+                            'key' => 'val',
+                            'schema' => 'array',
+                            'data' => [
+                                'attr1',
+                                'attr2',
+                                'attr3',
+                            ]
+                        ]
+                    ],
+                ],
+            ],
+            [
+                [
+                    'key' => [
+                        'in.array' => [
+                            'key' => 'val',
+                        ]
+                    ],
+                ],
+                ['key', 'in.array', 'ok.schema'], 'array',
+                [
+                    'key' => [
+                        'in.array' => [
+                            'key' => 'val',
+                            'ok.schema' => 'array',
+                        ]
+                    ],
+                ],
+            ],
+            [
+                [
+                    'key' => ['val'],
+                ],
+                null, 'data',
+                'data',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderSetValue
+     *
+     * @param array $array_input
+     * @param string|array|null $key
+     * @param mixed $value
+     * @param mixed $expected
+     */
+    public function testSetValue($array_input, $key, $value, $expected)
+    {
+        ArrayHelper::setValue($array_input, $key, $value);
+        $this->assertEquals($expected, $array_input);
+    }
+
     public function testIsAssociative()
     {
         $this->assertFalse(ArrayHelper::isAssociative('test'));
