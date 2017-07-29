4acb4bd052fce2389b91e8e0339376a568b913b8
Boudewijn Vahrmeijer <info@dynasource.eu>
2017-07-29 18:09:02 +0200

    Update apcu-setup.sh

--- tests/data/travis/apcu-setup.sh
+++ tests/data/travis/apcu-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-yes '' | pecl install -f apcu
+pecl install -f apcu-5.1.8
 #echo "extension = apcu.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "apc.enabled = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "apc.enable_cli = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
