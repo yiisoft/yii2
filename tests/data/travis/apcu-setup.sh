19d562ff15c94a087babc10790efff0721fff60c
Alexander Makarov <sam@rmcreative.ru>
2017-07-29 14:34:08 +0300

    Try using APCu explicitly

--- tests/data/travis/apcu-setup.sh
+++ tests/data/travis/apcu-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 
 yes '' | pecl install -f apcu
-#echo "extension = apcu.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+echo "extension = apcu.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "apc.enable_cli = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
