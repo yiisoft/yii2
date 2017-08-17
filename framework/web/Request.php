467203cbebd0fbe1ddc4af71ce0f78cedc167c6e
Sam <sam@mousa.nl>
2017-08-17 09:07:05 +0200

    Disable PHPMD SuperGlobals warning

--- framework/web/Request.php
+++ framework/web/Request.php
@@ -83,6 +83,7 @@ use yii\base\InvalidConfigException;
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
+ * @SuppressWarnings(PHPMD.SuperGlobals)
  */
 class Request extends \yii\base\Request
 {
