a5cd6c6c63833c828d9af68eb0ea96794e4dfd70
Ivan Buttinoni <ivan.buttinoni@cibi.it>
2017-09-07 07:34:16 +0200

    using the migration to add missing index on `auth_assignment.user_id` see yii\rbac\DbManager::getAssignments

---
+++ framework/console/migrations/m170907_052038_rbac_add_index_on_auth_assignment_user_id.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\db\Migration;
+use yii\rbac\DbManager;
+use yii\base\InvalidConfigException;
+
+/**
+ * Class m170907_052038_rbac_add_index_on_auth_assignment_user_id
+ */
+class m170907_052038_rbac_add_index_on_auth_assignment_user_id extends Migration
+{
+    public $column='user_id';
+    public $index='idx-auth_assignment-user_id';
+
+    /**
+     * @throws yii\base\InvalidConfigException
+     * @return DbManager
+     */
+    protected function getAuthManager()
+    {
+        $authManager = Yii::$app->getAuthManager();
+        if (!$authManager instanceof DbManager) {
+            throw new InvalidConfigException('You should configure "authManager" component to use database before executing this migration.');
+        }
+
+        return $authManager;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $authManager = $this->getAuthManager();
+        $this->createIndex($this->index, $authManager->assignmentTable, $this->column);
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $authManager = $this->getAuthManager();
+        $this->dropIndex($this->index, $authManager->assignmentTable);
+    }
+}
