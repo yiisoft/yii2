1a6b776094fa4e5f468def7d498bbb4c0f7e2083
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2017-10-16 22:36:43 +0300

    Anoter try to fix tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,16 +5,8 @@
  * @license http://www.yiiframework.com/license/
  */
 
-if (getenv('TRAVIS') == 'true' && PHP_VERSION_ID >= 70200 && PHP_VERSION_ID < 70300) {
-    // The each() function is deprecated. This message will be suppressed on further calls
-    // in /home/travis/build/yiisoft/yii2/vendor/phpunit/phpunit/src/Util/Getopt.php on line 38
-    // ---
-    // PHPUnit should be updated, but newer PHPUnit versions require PHP 7.
-    // Solution: suspend deprecation messages on Travis for PHP 7.2
-    error_reporting(E_ALL & ~E_DEPRECATED);
-} else {
-    error_reporting(-1); // ensure we get report on all possible php errors
-}
+// ensure we get report on all possible php errors
+error_reporting(-1);
 
 define('YII_ENABLE_ERROR_HANDLER', false);
 define('YII_DEBUG', true);
