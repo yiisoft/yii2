2a7869b0ca165306602fe77fa804ff9607684557
Robert Korulczyk <robert@korulczyk.pl>
2017-10-18 20:58:56 +0200

    Fix PHPDoc

--- tests/ResultPrinter.php
+++ tests/ResultPrinter.php
@@ -10,7 +10,7 @@ namespace yiiunit;
 /**
  * Class ResultPrinter overrides \PHPUnit\TextUI\ResultPrinter constructor
  * to change default output to STDOUT and prevent some tests from fail when
- * they can not be executed after headers have been sent. *
+ * they can not be executed after headers have been sent.
  */
 class ResultPrinter extends \PHPUnit\TextUI\ResultPrinter
 {
