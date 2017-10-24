5cec42a1085c23f8ab6b38857b996125231a1366
bscheshirwork <bscheshir.work@gmail.com>
2017-10-24 15:38:21 +0300

    Fixed error messages in data filters (#15018)

--- framework/rest/DataFilter.php
+++ framework/rest/DataFilter.php
@@ -276,7 +276,7 @@ class DataFilter extends Model
         if (!is_object($this->_searchModel) || $this->_searchModel instanceof \Closure) {
             $model = Yii::createObject($this->_searchModel);
             if (!$model instanceof Model) {
-                throw new InvalidConfigException('`' . get_class($this) . '::$model` should be an instance of `' . Model::className() . '` or its DI compatible configuration.');
+                throw new InvalidConfigException('`' . get_class($this) . '::$searchModel` should be an instance of `' . Model::className() . '` or its DI compatible configuration.');
             }
             $this->_searchModel = $model;
         }
@@ -290,7 +290,7 @@ class DataFilter extends Model
     public function setSearchModel($model)
     {
         if (is_object($model) && !$model instanceof Model && !$model instanceof \Closure) {
-            throw new InvalidConfigException('`' . get_class($this) . '::$model` should be an instance of `' . Model::className() . '` or its DI compatible configuration.');
+            throw new InvalidConfigException('`' . get_class($this) . '::$searchModel` should be an instance of `' . Model::className() . '` or its DI compatible configuration.');
         }
         $this->_searchModel = $model;
     }
