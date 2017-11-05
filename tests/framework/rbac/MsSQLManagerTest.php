4dafdf335c09dd9bc8da2802416ca0821ca692f4
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2017-11-05 11:15:30 +0200

    Added MsSQLManagerTest (failing)

---
+++ tests/framework/rbac/MsSQLManagerTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yiiunit\framework\rbac;
+
+/**
+ * PgSQLManagerTest.
+ * @group db
+ * @group rbac
+ * @group mssql
+ */
+class MsSQLManagerTest extends DbManagerTestCase
+{
+    protected static $driverName = 'sqlsrv';
+}
