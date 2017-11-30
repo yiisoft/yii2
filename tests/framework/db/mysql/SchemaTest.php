62fcaf33c733c28fc7c30626ac63b1f6b2d5237a
bscheshirwork <bscheshir.work@gmail.com>
2017-11-30 15:47:31 +0300

    add versioned data set

--- tests/framework/db/mysql/SchemaTest.php
+++ tests/framework/db/mysql/SchemaTest.php
@@ -7,6 +7,7 @@
 
 namespace yiiunit\framework\db\mysql;
 
+use PDO;
 use yiiunit\framework\db\AnyCaseValue;
 
 /**
@@ -16,6 +17,20 @@ use yiiunit\framework\db\AnyCaseValue;
 class SchemaTest extends \yiiunit\framework\db\SchemaTest
 {
     public $driverName = 'mysql';
+
+    protected $_serverVersion;
+
+    protected function getServerVersion($refresh = false)
+    {
+        if ($refresh || !isset($this->_serverVersion)) {
+            $databases = self::getParam('databases');
+            $this->database = $databases[$this->driverName];
+            $connection = $this->getConnection();
+            $this->_serverVersion = $connection->getSlavePdo()->getAttribute(PDO::ATTR_SERVER_VERSION);
+        }
+
+        return $this->_serverVersion;
+    }
 
     public function testGetSchemaNames()
     {
@@ -34,7 +49,13 @@ class SchemaTest extends \yiiunit\framework\db\SchemaTest
         $result['3: foreign key'][2][0]->foreignTableName = new AnyCaseValue('T_constraints_2');
         $result['3: check'][2] = false;
 
+        // https://github.com/yiisoft/yii2/issues/15248
+        if (version_compare($this->getServerVersion(), '8.0.0', '>=') === true) {
+            $result['3: foreign key'][2][0]->foreignColumnNames = ['c_id_1', 'c_id_2'];
+        }
+
         $result['4: check'][2] = false;
+
         return $result;
     }
 }
