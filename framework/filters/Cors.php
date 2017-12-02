0ecbdf0992779f9a88108529b3b132cd27584bef
Aleksandar Belic <aleks.belic@gmail.com>
2017-12-02 10:00:40 +0100

    Rearranging CORS comments.

--- framework/filters/Cors.php
+++ framework/filters/Cors.php
@@ -43,10 +43,11 @@ use yii\web\Response;
  *             'cors' => [
  *                 // restrict access to
  *                 'Origin' => ['http://www.myserver.com', 'https://www.myserver.com'],
- *                 'Access-Control-Request-Method' => ['POST', 'PUT'],
  *                 // Allow only POST and PUT methods
- *                 'Access-Control-Request-Headers' => ['X-Wsse'],
+ *                 'Access-Control-Request-Method' => ['POST', 'PUT'],
  *                 // Allow only headers 'X-Wsse'
+ *                 'Access-Control-Request-Headers' => ['X-Wsse'],
+ *                 // Allow Credentials
  *                 'Access-Control-Allow-Credentials' => true,
  *                 // Allow OPTIONS caching
  *                 'Access-Control-Max-Age' => 3600,
