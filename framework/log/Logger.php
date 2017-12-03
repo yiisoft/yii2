338c0408d32d4b87b0ff211878adb57b4385d44e
Nelson J Morais <njmorais@proactive-hosters.com>
2017-12-03 18:29:55 +0000

    Extracted LoggerInterface from yii\log\Logger yiisoft/yii2#14997

--- framework/log/Logger.php
+++ framework/log/Logger.php
@@ -41,7 +41,7 @@ use yii\base\Component;
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class Logger extends Component
+class Logger extends Component implements LoggerInterface
 {
     /**
      * Error message level. An error message is one that indicates the abnormal termination of the
@@ -129,15 +129,7 @@ class Logger extends Component
     }
 
     /**
-     * Logs a message with the given type and category.
-     * If [[traceLevel]] is greater than 0, additional call stack information about
-     * the application code will be logged as well.
-     * @param string|array $message the message to be logged. This can be a simple string or a more
-     * complex data structure that will be handled by a [[Target|log target]].
-     * @param int $level the level of the message. This must be one of the following:
-     * `Logger::LEVEL_ERROR`, `Logger::LEVEL_WARNING`, `Logger::LEVEL_INFO`, `Logger::LEVEL_TRACE`,
-     * `Logger::LEVEL_PROFILE_BEGIN`, `Logger::LEVEL_PROFILE_END`.
-     * @param string $category the category of the message.
+     * @inheritdoc
      */
     public function log($message, $level, $category = 'application')
     {
@@ -164,8 +156,7 @@ class Logger extends Component
     }
 
     /**
-     * Flushes log messages from memory to targets.
-     * @param bool $final whether this is a final call during a request.
+     * @inheritdoc
      */
     public function flush($final = false)
     {
@@ -179,11 +170,7 @@ class Logger extends Component
     }
 
     /**
-     * Returns the total elapsed time since the start of the current request.
-     * This method calculates the difference between now and the timestamp
-     * defined by constant `YII_BEGIN_TIME` which is evaluated at the beginning
-     * of [[\yii\BaseYii]] class file.
-     * @return float the total elapsed time in seconds for current request.
+     * @inheritdoc
      */
     public function getElapsedTime()
     {
@@ -191,20 +178,7 @@ class Logger extends Component
     }
 
     /**
-     * Returns the profiling results.
-     *
-     * By default, all profiling results will be returned. You may provide
-     * `$categories` and `$excludeCategories` as parameters to retrieve the
-     * results that you are interested in.
-     *
-     * @param array $categories list of categories that you are interested in.
-     * You can use an asterisk at the end of a category to do a prefix match.
-     * For example, 'yii\db\*' will match categories starting with 'yii\db\',
-     * such as 'yii\db\Connection'.
-     * @param array $excludeCategories list of categories that you want to exclude
-     * @return array the profiling results. Each element is an array consisting of these elements:
-     * `info`, `category`, `timestamp`, `trace`, `level`, `duration`, `memory`, `memoryDiff`.
-     * The `memory` and `memoryDiff` values are available since version 2.0.11.
+     * @inheritdoc
      */
     public function getProfiling($categories = [], $excludeCategories = [])
     {
@@ -244,11 +218,7 @@ class Logger extends Component
     }
 
     /**
-     * Returns the statistical results of DB queries.
-     * The results returned include the number of SQL statements executed and
-     * the total time spent.
-     * @return array the first element indicates the number of SQL statements executed,
-     * and the second element the total time spent in SQL execution.
+     * @inheritdoc
      */
     public function getDbProfiling()
     {
@@ -263,11 +233,7 @@ class Logger extends Component
     }
 
     /**
-     * Calculates the elapsed time for the given log messages.
-     * @param array $messages the log messages obtained from profiling
-     * @return array timings. Each element is an array consisting of these elements:
-     * `info`, `category`, `timestamp`, `trace`, `level`, `duration`, `memory`, `memoryDiff`.
-     * The `memory` and `memoryDiff` values are available since version 2.0.11.
+     * @inheritdoc
      */
     public function calculateTimings($messages)
     {
@@ -305,9 +271,7 @@ class Logger extends Component
 
 
     /**
-     * Returns the text display of the specified level.
-     * @param int $level the message level, e.g. [[LEVEL_ERROR]], [[LEVEL_WARNING]].
-     * @return string the text display of the level
+     * @inheritdoc
      */
     public static function getLevelName($level)
     {
