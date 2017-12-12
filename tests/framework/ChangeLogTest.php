feaa7fab4e154b5e5d508f401738c151a6354329
Sam Mousa <sam@mousa.nl>
2017-12-12 11:13:18 +0100

    Check the order of the lines

--- tests/framework/ChangeLogTest.php
+++ tests/framework/ChangeLogTest.php
@@ -15,6 +15,17 @@ use yiiunit\TestCase;
  */
 class ChangeLogTest extends TestCase
 {
+    /**
+     * @var string[] Ordered list of line types
+     */
+    public $types = ['Bug', 'Enh', 'Chg'];
+    public $regex = '/- (?<type>{types})( #\d+(, #\d+)*)?: .*[^.] \(.*\)$/';
+
+    public function setUp()
+    {
+        $this->regex = strtr($this->regex, ['{types}' => implode('|', $this->types)]);
+    }
+
     public function changeProvider()
     {
 
@@ -23,10 +34,12 @@ class ChangeLogTest extends TestCase
         // Don't check last 1500 lines, they are old and often don't obey the standard.
         $lastIndex = count($lines) - 1500;
         $result = [];
-        foreach($lines as $i => $line) {
+        $previous = null;
+        foreach ($lines as $i => $line) {
             if (strncmp('- ', $line, 2) === 0) {
-                $result[] = [$line];
+                $result[] = [$line, $previous];
             }
+            $previous = $line;
 
             if ($i > $lastIndex) {
                 break;
@@ -38,7 +51,7 @@ class ChangeLogTest extends TestCase
     /**
      * @dataProvider changeProvider
      */
-    public function testContributorLine($line)
+    public function testContributorLine($line, $previous)
     {
         /**
          * Each change line is tested for:
@@ -49,6 +62,20 @@ class ChangeLogTest extends TestCase
          * - Description ends without a "."
          * - Line ends with contributor name between "(" and ")".
          */
-        $this->assertRegExp('/- (Bug|Enh|Chg|New)( #\d+(, #\d+)*)?: .*[^.] \(.*\)$/', $line);
+
+        $this->assertRegExp($this->regex, $line);
+
+
+        if ($previous !== false
+            && preg_match($this->regex, $previous, $matches)
+            && isset($matches['type'])
+        ) {
+            $previousType = $matches['type'];
+            preg_match($this->regex, $line, $matches);
+            $type = $matches['type'];
+            $this->assertGreaterThanOrEqual(array_search($previousType, $this->types), array_search($type, $this->types));
+        }
     }
+
+
 }
