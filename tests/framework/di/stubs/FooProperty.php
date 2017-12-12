00bc1c4843e404dbebdeb3c5361b293b5b2ca7c1
KoJIT2009 <kojit2009@gmail.com>
2017-12-12 20:18:44 +0500

    di-support-instance-for-property
    add support Instance for object property in dic.

---
+++ tests/framework/di/stubs/FooProperty.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @link      http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license   http://www.yiiframework.com/license/
+ */
+
+namespace yiiunit\framework\di\stubs;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since  2.0
+ *
+ * @property BarSetter $bar
+ */
+class FooProperty
+{
+    /**
+     * @var BarSetter
+     */
+    public $bar;
+}
