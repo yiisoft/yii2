cc59d0455a5e4aae47fe6fcd6d89871c00c68afd
Alexander Makarov <sam@rmcreative.ru>
2017-12-20 01:01:42 +0300

    Removed commented out test

--- tests/framework/helpers/BaseConsoleTest.php
+++ tests/framework/helpers/BaseConsoleTest.php
@@ -26,26 +26,4 @@ class BaseConsoleTest extends TestCase
         $expected = "foo";
         $this->assertEquals($expected, $actual);
     }
-    
-    /**
-     * @test
-     */
-    public function progress()
-    {
-        $expected = '50% (1/2) ETA: 1495723175 sec.';
-        /*        ob_start();
-        ob_implicit_flush(false);
-        BaseConsole::endProgress();
-        $actual =ob_get_clean();
-        $actual = $this->getActualOutput();
-        $this->assertEquals($expected, $actual);*/
-        /*        ob_start();
-                ob_implicit_flush(false);
-                BaseConsole::startProgress(0, 1000);
-                BaseConsole::updateProgress(2, 1000);
-                BaseConsole::endProgress();
-                $actual =ob_get_clean();
-                $actual = file_get_contents('application.log');
-                $this->assertEquals($expected, $actual);*/
-    }
 }
