57e699372316f48fc8d6d932c215c132bfa74ec1
Paul Klimov <klimov.paul@gmail.com>
2018-01-04 21:58:28 +0200

    `MiddlewareDispatcher` for web implemented

--- framework/http/server/MiddlewareDispatcher.php
+++ framework/http/server/MiddlewareDispatcher.php
@@ -7,6 +7,7 @@
 
 namespace yii\http\server;
 
+use Yii;
 use yii\base\Component;
 use yii\base\MiddlewareDispatcherInterface;
 
@@ -29,7 +30,19 @@ class MiddlewareDispatcher extends Component implements MiddlewareDispatcherInte
         if (empty($middleware)) {
             return call_user_func($handler, $request);
         }
-        // @todo process middleware stack
-        return call_user_func($handler, $request);
+
+        /* @var $middlewareInstance \Interop\Http\Server\MiddlewareInterface */
+        $middlewareInstance = array_shift($middleware);
+        if (!is_object($middlewareInstance) || $middlewareInstance instanceof \Closure) {
+            $middlewareInstance = Yii::createObject($middlewareInstance);
+        }
+
+        $newHandler = new CallbackRequestHandler([
+            'callback' => function ($request) use ($middleware, $handler) {
+                return $this->dispatch($request, $middleware, $handler);
+            }
+        ]);
+
+        return $middlewareInstance->process($request, $newHandler);
     }
 }
