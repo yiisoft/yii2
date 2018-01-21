08026ab83937a46b9d55ada7172bb8fff63ea9a8
Elvira Sheina <elle.uz@gmail.com>
2018-01-21 12:35:41 +0500

    Fixed missing `WWW-Authenticate` header (#15494)

--- framework/filters/auth/HttpHeaderAuth.php
+++ framework/filters/auth/HttpHeaderAuth.php
@@ -56,6 +56,7 @@ class HttpHeaderAuth extends AuthMethod
             }
             $identity = $user->loginByAccessToken($authHeader, get_class($this));
             if ($identity === null) {
+                $this->challenge($response);
                 $this->handleFailure($response);
             }
 
