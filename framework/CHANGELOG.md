4f7b410230e3ae1966f2e0a72814b32bb1c62dcb
Hanafi Ahmat <ahmat.hanafi@gmail.com>
2018-01-24 22:02:03 +0800

    Fixes #9771: Assign hidden input with its own set of HTML options via `$hiddenOptions` in activeFileInput `$options`

--- framework/CHANGELOG.md
+++ framework/CHANGELOG.md
@@ -4,6 +4,7 @@ Yii Framework 2 Change Log
 2.0.14 under development
 ------------------------
 
+- Enh #9771: Assign hidden input with its own set of HTML options via `$hiddenOptions` in activeFileInput `$options` (HanafiAhmat)
 - Bug #15536: Fixed `yii\widgets\ActiveForm::init()` for call `parent::init()` (panchenkodv)
 - Enh #14806: Added $placeFooterAfterBody option for GridView (terehru)
 - Bug #14711: Fixed `yii\web\ErrorHandler` displaying exception message in non-debug mode (samdark)
--- framework/helpers/BaseHtml.php
+++ framework/helpers/BaseHtml.php
@@ -1430,22 +1430,29 @@ class BaseHtml
      * Generates a file input tag for the given model attribute.
      * This method will generate the "name" and "value" tag attributes automatically for the model attribute
      * unless they are explicitly specified in `$options`.
+     * Additionally, if a separate set of HTML options array is defined inside `$options` with a key named `hiddenOptions`,
+     * it will be passed to the `activeHiddenInput` field as its own `$options` parameter.
      * @param Model $model the model object
      * @param string $attribute the attribute name or expression. See [[getAttributeName()]] for the format
      * about attribute expression.
      * @param array $options the tag options in terms of name-value pairs. These will be rendered as
      * the attributes of the resulting tag. The values will be HTML-encoded using [[encode()]].
      * See [[renderTagAttributes()]] for details on how attributes are being rendered.
+     * If `hiddenOptions` parameter which is another set of HTML options array is defined, it will be extracted 
+     * from `$options` to be used for the hidden input.
      * @return string the generated input tag
      */
     public static function activeFileInput($model, $attribute, $options = [])
     {
-        // add a hidden field so that if a model only has a file field, we can
-        // still use isset($_POST[$modelClass]) to detect if the input is submitted
         $hiddenOptions = ['id' => null, 'value' => ''];
         if (isset($options['name'])) {
             $hiddenOptions['name'] = $options['name'];
         }
+        $hiddenOptions = ArrayHelper::merge($hiddenOptions, ArrayHelper::remove($options, 'hiddenOptions', []));
+        // add a hidden field so that if a model only has a file field, we can
+        // still use isset($_POST[$modelClass]) to detect if the input is submitted.
+        // The hidden input will be assigned its own set of html options via `$hiddenOptions`.
+        // This provides the possibility to interact with the hidden field via client script.
 
         return static::activeHiddenInput($model, $attribute, $hiddenOptions)
             . static::activeInput('file', $model, $attribute, $options);
