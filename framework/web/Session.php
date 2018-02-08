a661379c729c28c87f2181f043c52f936cc12ad6
Robert Korulczyk <robert@korulczyk.pl>
2018-02-08 16:48:54 +0100

    Micro-optimizations.

--- framework/web/Session.php
+++ framework/web/Session.php
@@ -923,7 +923,7 @@ class Session extends Component implements \IteratorAggregate, \ArrayAccess, \Co
      */
     protected function freeze()
     {
-        if ($this->getIsActive() && isset($_SESSION)) {
+        if (isset($_SESSION) && $this->getIsActive()) {
             $this->frozenSessionData = $_SESSION;
         }
         $this->close();
