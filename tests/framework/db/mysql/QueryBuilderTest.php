0ca003343b60f0bbaf7c71a6be43611c69a8b71e
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2018-03-10 10:09:17 +0200

    Fixed test

--- tests/framework/db/mysql/QueryBuilderTest.php
+++ tests/framework/db/mysql/QueryBuilderTest.php
@@ -250,7 +250,7 @@ class QueryBuilderTest extends \yiiunit\framework\db\QueryBuilderTest
             [
                 'id' => 1,
             ],
-            $this->replaceQuotes('UPDATE [[profile]] SET [[description]]=:qp0 WHERE [[id]]=:qp1'),
+            $this->replaceQuotes('UPDATE [[profile]] SET [[description]]=CAST(:qp0 AS JSON) WHERE [[id]]=:qp1'),
             [
                 ':qp0' => '{"abc":"def","0":123,"1":null}',
                 ':qp1' => 1,
