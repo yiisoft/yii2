5eee4ba39962f0983a1f3f6e79395d9e96e3c056
Newerton <newerton.araujo@gmail.com>
2018-05-23 20:35:01 -0300

    Update ArrayDataProvider.php
    count(): Parameter must be an array or an object that implements Countable
    
    https://github.com/kartik-v/yii2-grid/issues/800

--- framework/data/ArrayDataProvider.php
+++ framework/data/ArrayDataProvider.php
@@ -123,7 +123,7 @@ class ArrayDataProvider extends BaseDataProvider
      */
     protected function prepareTotalCount()
     {
-        return count($this->allModels);
+        return !$this->allModels ?: count($this->allModels);
     }
 
     /**
