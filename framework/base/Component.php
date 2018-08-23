yiisoft/yii2

commit e1a49cfb6de5c2e78ec600566544d77bd10ecc3d

Fixes #16247: Cloning components will now clone their behaviors as well

framework/base/Component.php
@@ -302,13 +302,20 @@ class Component extends BaseObject
 
     /**
      * This method is called after the object is created by cloning an existing one.
-     * It removes all behaviors because they are attached to the old object.
+     * It clones all behaviors as well, and attaches them to the new object.
      */
     public function __clone()
     {
         $this->_events = [];
         $this->_eventWildcards = [];
-        $this->_behaviors = null;
+
+        if ($this->_behaviors !== null) {
+            $behaviors = $this->_behaviors;
+            $this->_behaviors = null;
+            foreach ($behaviors as $name => $behavior) {
+                $this->attachBehavior($name, clone $behavior);
+            }
+        }
     }
 
     /**

tests/framework/base/ComponentTest.php
@@ -61,7 +61,8 @@ class ComponentTest extends TestCase
 
         $clone = clone $component;
         $this->assertNotSame($component, $clone);
-        $this->assertNull($clone->getBehavior('a'));
+        $this->assertNotNull($clone->getBehavior('a'));
+        $this->assertNotSame($behavior, $clone->getBehavior('a'));
         $this->assertFalse($clone->hasEventHandlers('test'));
         $this->assertFalse($clone->hasEventHandlers('foo'));
         $this->assertFalse($clone->hasEventHandlers('*'));

tests/framework/behaviors/CloningBehaviorTest.php
@@ -1,0 +1,57 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yiiunit\framework\behaviors;
+
+use yii\base\Behavior;
+use yii\base\Model;
+use yiiunit\TestCase;
+
+/**
+ * Unit test for cloning behaviors when objects are cloned.
+ *
+ * @group behaviors
+ */
+class CloningBehaviorTest extends TestCase
+{
+    public function testCloningObjectsClonesBehaviors()
+    {
+        $model1 = new ModelWithBehavior();
+        $model1->myBehaviorProperty = 'foo';
+        $model2 = clone $model1;
+
+        $this->assertEquals($model1->myBehaviorProperty, $model2->myBehaviorProperty);
+    }
+}
+
+/**
+ * Test Model class with a behavior attached.
+ *
+ * @mixin BehaviorWithProperty
+ */
+class ModelWithBehavior extends Model
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'myBehavior' => BehaviorWithProperty::class
+        ];
+    }
+}
+
+/**
+ * Test Behavior class with property.
+ *
+ */
+class BehaviorWithProperty extends Behavior
+{
+    public $myBehaviorProperty;
+}
+

commit 5abaa47bb3ba1a90031aa8bb6de1c9d10c8c5927

Fixed bug where component behaviors could be double-attached
This fixes a 3.0 bug introduced in #16430.

If a component's behaviors() method returns any behaviors without a name, then cloning it would result in the behavior getting attached twice.

framework/base/Component.php
@@ -311,9 +311,9 @@ class Component extends BaseObject
 
         if ($this->_behaviors !== null) {
             $behaviors = $this->_behaviors;
-            $this->_behaviors = null;
+            $this->_behaviors = [];
             foreach ($behaviors as $name => $behavior) {
-                $this->attachBehavior($name, clone $behavior);
+                $this->attachBehaviorInternal($name, clone $behavior);
             }
         }
     }