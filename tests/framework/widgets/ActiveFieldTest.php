4178b2b3e210efb29258747144eca8a5af579973
Evgeniy Moiseenko <s1lverhron@gmail.com>
2018-09-14 10:56:11 +0300

    Test (#14368)

--- tests/framework/widgets/ActiveFieldTest.php
+++ tests/framework/widgets/ActiveFieldTest.php
@@ -359,9 +359,8 @@ EOD;
 <div class="form-group field-activefieldtestmodel-attributename">
 <label class="control-label">Attribute Name</label>
 <input type="hidden" name="ActiveFieldTestModel[attributeName]" value="">
-<div id="activefieldtestmodel-attributename" role="radiogroup">
-<label><input type="radio" name="ActiveFieldTestModel[attributeName]" value="1"> Item One</label>
-</div>
+<div id="activefieldtestmodel-attributename" role="radiogroup"><label><input type="radio" name="ActiveFieldTestModel[attributeName]" value="1"> Item One</label></div>
+<div class="hint-block">Hint for attributeName attribute</div>
 <div class="help-block"></div>
 </div>
 EOD;
