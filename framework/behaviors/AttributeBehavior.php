510f6971954202373305419706f01a48b78e2a27
崔亮 <admin@yiichina.com>
2018-12-01 11:15:57 +0800

    Update AttributeBehavior.php

--- framework/behaviors/AttributeBehavior.php
+++ framework/behaviors/AttributeBehavior.php
@@ -14,9 +14,9 @@ use yii\db\ActiveRecord;
 
 /**
  * 当某个事件发生的时候，AttributeBehavior 可以自动地给某个或多个 ActiveRecord 对象的属性
- * 分配指定的值
+ * 分配指定的值。
  *
- * 在使用 AttributeBehavior的时候，需要配置 [[attributes]] 属性，这个属性应该指明需要更新的属性列表
+ * 在使用 AttributeBehavior 的时候，需要配置 [[attributes]] 属性，这个属性应该指明需要更新的属性列表
  * 和触发更新时对应的事件。然后配置 [[value]] 属性为一个 PHP 回调函数
  * 该回调函数返回要分配给当前属性的值。
  * 比如，
@@ -42,7 +42,7 @@ use yii\db\ActiveRecord;
  * ```
  *
  * 因为属性值是由行为自动设置的，所以通常不需要用户输入，因此
- * 也不需要验证等。这些属性不应该出现在 [[\yii\base\Model::rules()|rules()]] 方法中。
+ * 也不需要验证等，这些属性不应该出现在 [[\yii\base\Model::rules()|rules()]] 方法中。
  *
  * @author Luciano Baraglia <luciano.baraglia@gmail.com>
  * @author Qiang Xue <qiang.xue@gmail.com>
@@ -53,8 +53,8 @@ class AttributeBehavior extends Behavior
     /**
      * @var array 属性列表，属性的值将由 [[value]] 自动填充。
      * 数组的键是 ActiveRecord 的事件，属性就是更新于这些事件之上，
-     * 数组的值就是要更新的属性。你可以用字符串来表示一个单独的属性
-     * 也可以用一个数组来表示一系列属性。比如，
+     * 数组的值就是要更新的属性。
+     * 你可以用字符串来表示一个单独的属性也可以用一个数组来表示一系列属性。比如，
      *
      * ```php
      * [
@@ -66,9 +66,9 @@ class AttributeBehavior extends Behavior
     public $attributes = [];
     /**
      * @var mixed 要分配给当前属性的值。它可以是一个匿名函数，
-     * 数组格式的 callable (比如 `[$this, 'methodName']`)，一个 [[\yii\db\Expression|Expression]] 对象表示的 DB 表达式
-     * (比如 `new Expression('NOW()')`)，标量，字符串或者一个任意的值。如果是前者，函数的返回值
-     * 将会设置给这些属性。
+     * 数组格式的 callable（比如 `[$this, 'methodName']`），一个 [[\yii\db\Expression|Expression]] 对象表示的 DB 表达式
+     * （比如 `new Expression('NOW()')`），标量，字符串或者一个任意的值。
+     * 如果是前者，函数的返回值将会设置给这些属性。
      * 函数的签名应该像下面这样，
      *
      * ```php
@@ -80,8 +80,8 @@ class AttributeBehavior extends Behavior
      */
     public $value;
     /**
-     * @var bool 当 `$owner` 没有更新的时候是否跳过
-     * 这个行为
+     * @var bool 当 `$owner`
+     * 没有更新的时候是否跳过这个行为
      * @since 2.0.8
      */
     public $skipUpdateOnClean = true;
@@ -120,7 +120,7 @@ class AttributeBehavior extends Behavior
             $attributes = (array) $this->attributes[$event->name];
             $value = $this->getValue($event);
             foreach ($attributes as $attribute) {
-                // 忽略属性名不是字符串的情况 (比如当被 TimestampBehavior::updatedAtAttribute 设置时)
+                // 忽略属性名不是字符串的情况（比如当被 TimestampBehavior::updatedAtAttribute 设置时）
                 if (is_string($attribute)) {
                     if ($this->preserveNonEmptyValues && !empty($this->owner->$attribute)) {
                         continue;
@@ -133,8 +133,8 @@ class AttributeBehavior extends Behavior
 
     /**
      * 返回给当前属性准备的值。
-     * 该方法在 [[evaluateAttributes()]] 里调用。它的返回值
-     * 将会设置到对应触发事件的属性上。
+     * 该方法在 [[evaluateAttributes()]] 里调用。
+     * 它的返回值将会设置到对应触发事件的属性上。
      * @param Event $event 触发当前属性更新的事件
      * @return mixed 属性值
      */
