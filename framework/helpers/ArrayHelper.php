a684e49822ac3457bfef00152de37b5ed37d8fce
fucheng <fucheng@ganhuoche.com>
2018-12-03 15:36:32 +0800

    update
    Signed-off-by: fucheng <fucheng@ganhuoche.com>

--- framework/helpers/ArrayHelper.php
+++ framework/helpers/ArrayHelper.php
@@ -11,7 +11,7 @@ namespace yii\helpers;
  * ArrayHelper 提供了可以在应用程序中使用的额外的数组
  * 功能。
  *
- * 有关 ArrayHelpers 更多的细节和使用信息，可以参阅[关于数组helper的指南](guide:helper-array)。
+ * 有关 ArrayHelpers 更多的细节和使用信息，可以参阅 [关于数组helper的指南](guide:helper-array)。
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
