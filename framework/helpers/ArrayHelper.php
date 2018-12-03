c54a9acd51d4b09f01bffadfae5e63435c340a7d
崔亮 <admin@yiichina.com>
2018-12-03 15:39:49 +0800

    Update ArrayHelper.php

--- framework/helpers/ArrayHelper.php
+++ framework/helpers/ArrayHelper.php
@@ -8,10 +8,10 @@
 namespace yii\helpers;
 
 /**
- * ArrayHelper 提供了可以在应用程序中使用的额外的数组
- * 功能。
+ * ArrayHelper
+ * 提供了可以在应用程序中使用的额外的数组功能。
  *
- * 有关 ArrayHelpers 更多的细节和使用信息，可以参阅 [关于数组helper的指南](guide:helper-array)。
+ * 有关 ArrayHelper 更多的细节和使用信息，可以参阅 [关于数组助手类的指南](guide:helper-array)。
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
