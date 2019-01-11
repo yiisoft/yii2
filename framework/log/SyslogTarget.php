40e11dd50e360d70399de31a4d77ae899dab67bb
koko <wlzyan@gmail.com>
2019-01-11 21:35:36 +0800

    翻译

--- framework/log/SyslogTarget.php
+++ framework/log/SyslogTarget.php
@@ -23,23 +23,18 @@ class SyslogTarget extends Target
      */
     public $identity;
     /**
-     * @var int syslog facility.
      * @var int 系统日志（syslog）类型
      */
     public $facility = LOG_USER;
     /**
-     * @var int openlog options. This is a bitfield passed as the `$option` parameter to [openlog()](http://php.net/openlog).
      * @var int openlog（打开与系统记录器的连接）选项。这是传递给函数 [openlog()](http://php.net/openlog) 中 `$option` 参数的值。
-     * Defaults to `null` which means to use the default options `LOG_ODELAY | LOG_PID`.
      * 当值为 `null` 时，表示使用默认值 `LOG_ODELAY | LOG_PID`。
-     * @see http://php.net/openlog for available options.
      * @see 访问 http://php.net/openlog 可获取更多可用选项。
      * @since 2.0.11
      */
     public $options;
 
     /**
-     * @var array syslog levels
      * @var array 系统日志（syslog）级别
      */
     private $_syslogLevels = [
@@ -65,9 +60,7 @@ class SyslogTarget extends Target
     }
 
     /**
-     * Writes log messages to syslog.
-     * 将日志写入系统日志（syslog）中。
-     * Starting from version 2.0.14, this method throws LogRuntimeException in case the log can not be exported.
+     * 将日志消息写入系统日志（syslog）中。
      * 从版本2.0.14开始，如果日志无法导出，将抛出异常 LogRuntimeException 。
      * @throws LogRuntimeException
      */
@@ -90,7 +83,6 @@ class SyslogTarget extends Target
         list($text, $level, $category, $timestamp) = $message;
         $level = Logger::getLevelName($level);
         if (!is_string($text)) {
-            // exceptions may not be serializable if in the call stack somewhere is a Closure
             //如果某个闭包调用了堆栈，则异常可能无法序列化。
             if ($text instanceof \Throwable || $text instanceof \Exception) {
                 $text = (string) $text;
