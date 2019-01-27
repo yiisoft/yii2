3221217b0de76b7b86c2bf443c4989c1b2f98819
masterliu <872140945@qq.com>
2019-01-27 11:50:17 +0800

    FileDependency 翻译

--- framework/caching/FileDependency.php
+++ framework/caching/FileDependency.php
@@ -11,12 +11,12 @@ use Yii;
 use yii\base\InvalidConfigException;
 
 /**
- * FileDependency represents a dependency based on a file's last modification time.
+ * FileDependency 是基于文件的最后更新时间实现的依赖类。
  *
- * If the last modification time of the file specified via [[fileName]] is changed,
- * the dependency is considered as changed.
+ * 如果由 [[fileName]] 指明的文件最后更新时间发生了变化，
+ * 这个依赖就被认为是发生了变化。
  *
- * For more details and usage information on Cache, see the [guide article on caching](guide:caching-overview).
+ * 在 Cache 上更多的详情和详细的使用信息，请参考 [guide article on caching](guide:caching-overview)。
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
@@ -24,18 +24,18 @@ use yii\base\InvalidConfigException;
 class FileDependency extends Dependency
 {
     /**
-     * @var string the file path or [path alias](guide:concept-aliases) whose last modification time is used to
-     * check if the dependency has been changed.
+     * @var string 文件路径或者 [path alias](guide:concept-aliases) 别名，检测依赖是否发生变化时，
+     * 会使用文件的最后更新时间。
      */
     public $fileName;
 
 
     /**
-     * Generates the data needed to determine if dependency has been changed.
-     * This method returns the file's last modification time.
-     * @param CacheInterface $cache the cache component that is currently evaluating this dependency
-     * @return mixed the data needed to determine if dependency has been changed.
-     * @throws InvalidConfigException if [[fileName]] is not set
+     * 生成在判断依赖是否发生变化时用到的依赖数据。
+     * 该方法返回文件的最后更新时间。
+     * @param CacheInterface $cache 正在计算缓存依赖的缓存组件。
+     * @return mixed 判断依赖是否发生变化时的依赖数据。
+     * @throws InvalidConfigException 如果 [[fileName]] 为空时。
      */
     protected function generateDependencyData($cache)
     {
