ff2c7e7638ebbf235151bafe42c64c009b7c311d
jackSpanrrows <45549144+jackSpanrrows@users.noreply.github.com>
2019-01-28 10:17:03 +0800

    Update Html.php
    多了个空格

--- framework/helpers/Html.php
+++ framework/helpers/Html.php
@@ -11,7 +11,7 @@ namespace yii\helpers;
  * Html 提供了一组用于生成常用 HTML 标记的静态方法。
  *
  * 这个类中的几乎所有方法都允许为它们生成的 html 标记设置额外的 html 属性。 
- * 你能够指定，例如，html 元素中的 `class`，`style` 或者 `id` 作为 `$options` 中的参数来使用 。
+ * 你能够指定，例如，html 元素中的 `class`，`style` 或者 `id` 作为 `$options` 中的参数来使用。
  * 请查看有关于 [[tag()]] 方法的文档获得更多的详情。
  *
  * 获取有关 Html 的更多的细节和使用信息，请参见 [guide article on html helpers](guide:helper-html)。
