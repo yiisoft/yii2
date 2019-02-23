eff826bf6e5e0cc9d1be904bbec79dcb2e87deef
Robert Korulczyk <robert@korulczyk.pl>
2019-02-23 11:10:12 -0500

    Simplified nested parentheses
    Co-Authored-By: evilito <evil@clanuis.net>

--- framework/data/ArrayDataProvider.php
+++ framework/data/ArrayDataProvider.php
@@ -123,7 +123,7 @@ class ArrayDataProvider extends BaseDataProvider
      */
     protected function prepareTotalCount()
     {
-        return (is_array($this->allModels)) ? count($this->allModels) : 0;
+        return is_array($this->allModels) ? count($this->allModels) : 0;
     }
 
     /**
