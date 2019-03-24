3cfdf4a9114ec8377e72c9bd835105ddc79f709a
ipenge <weixupeng999@163.com>
2019-03-24 20:26:25 +0800

    完成 ExistsConditionBuilder 翻译

--- framework/db/conditions/ExistsConditionBuilder.php
+++ framework/db/conditions/ExistsConditionBuilder.php
@@ -12,7 +12,7 @@ use yii\db\ExpressionBuilderTrait;
 use yii\db\ExpressionInterface;
 
 /**
- * Class ExistsConditionBuilder builds objects of [[ExistsCondition]]
+ * 类 ExistsConditionBuilder 构建 [[ExistsCondition]] 的对象
  *
  * @author Dmytro Naumenko <d.naumenko.a@gmail.com>
  * @since 2.0.14
@@ -23,12 +23,12 @@ class ExistsConditionBuilder implements ExpressionBuilderInterface
 
 
     /**
-     * Method builds the raw SQL from the $expression that will not be additionally
-     * escaped or quoted.
+     * 方法从 $expression 构建原始 SQL，
+     * 不会被额外转义或引用。
      *
-     * @param ExpressionInterface|ExistsCondition $expression the expression to be built.
-     * @param array $params the binding parameters.
-     * @return string the raw SQL that will not be additionally escaped or quoted.
+     * @param ExpressionInterface|ExistsCondition $expression 要构建的表达式。
+     * @param array $params 绑定参数。
+     * @return string 原始 SQL 不会被额外转义或引用。
      */
     public function build(ExpressionInterface $expression, array &$params = [])
     {
