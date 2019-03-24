c4d0894f52f96c32ee94f14f4e6becb752fe1eec
ipenge <weixupeng999@163.com>
2019-03-24 22:17:46 +0800

    完成 NotConditionBuilder 翻译

--- framework/db/conditions/NotConditionBuilder.php
+++ framework/db/conditions/NotConditionBuilder.php
@@ -12,7 +12,7 @@ use yii\db\ExpressionBuilderTrait;
 use yii\db\ExpressionInterface;
 
 /**
- * Class NotConditionBuilder builds objects of [[NotCondition]]
+ * 类 NotConditionBuilder 构建 [[NotCondition]] 的对象
  *
  * @author Dmytro Naumenko <d.naumenko.a@gmail.com>
  * @since 2.0.14
@@ -23,12 +23,12 @@ class NotConditionBuilder implements ExpressionBuilderInterface
 
 
     /**
-     * Method builds the raw SQL from the $expression that will not be additionally
-     * escaped or quoted.
+     * 方法从 $expression 构建原始 SQL，
+     * 不会被额外转义或引用。
      *
-     * @param ExpressionInterface|NotCondition $expression the expression to be built.
-     * @param array $params the binding parameters.
-     * @return string the raw SQL that will not be additionally escaped or quoted.
+     * @param ExpressionInterface|NotCondition $expression 构建的表达式。
+     * @param array $params 绑定参数。
+     * @return string 原始 SQL 不会被转义或引用。
      */
     public function build(ExpressionInterface $expression, array &$params = [])
     {
