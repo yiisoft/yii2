ff22f2f7b05810a6496d465391dd1f294971db2d
ipenge <weixupeng999@163.com>
2019-03-24 15:55:01 +0800

    完成 AndCondition 翻译

--- framework/db/conditions/AndCondition.php
+++ framework/db/conditions/AndCondition.php
@@ -8,7 +8,7 @@
 namespace yii\db\conditions;
 
 /**
- * Condition that connects two or more SQL expressions with the `AND` operator.
+ * 使用 `AND` 运算符连接两个或多个 SQL 表达式的条件。
  *
  * @author Dmytro Naumenko <d.naumenko.a@gmail.com>
  * @since 2.0.14
@@ -16,7 +16,7 @@ namespace yii\db\conditions;
 class AndCondition extends ConjunctionCondition
 {
     /**
-     * Returns the operator that is represented by this condition class, e.g. `AND`, `OR`.
+     * 返回由此条件类表示的运算符，比如：`AND`，`OR`。
      *
      * @return string
      */
