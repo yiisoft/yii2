aa15eb5961c95aef5a1aa3d1ea0411af3c6ab75e
ipenge <weixupeng999@163.com>
2019-03-25 14:34:14 +0800

    完成 db\cubrid\ColumnSchemaBuilder 翻译

--- framework/db/cubrid/ColumnSchemaBuilder.php
+++ framework/db/cubrid/ColumnSchemaBuilder.php
@@ -10,7 +10,7 @@ namespace yii\db\cubrid;
 use yii\db\ColumnSchemaBuilder as AbstractColumnSchemaBuilder;
 
 /**
- * ColumnSchemaBuilder is the schema builder for Cubrid databases.
+ * ColumnSchemaBuilder 是 Cubrid 数据库的模式构建器。
  *
  * @author Chris Harris <chris@buckshotsoftware.com>
  * @since 2.0.8
