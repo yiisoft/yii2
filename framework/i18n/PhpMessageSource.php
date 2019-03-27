3da27e345d19279a7356c33bacb427973d7c8ea6
Shiyang <dr@shiyang.me>
2019-03-27 14:14:21 +0800

    Update PhpMessageSource.php

--- framework/i18n/PhpMessageSource.php
+++ framework/i18n/PhpMessageSource.php
@@ -10,14 +10,14 @@ namespace yii\i18n;
 use Yii;
 
 /**
- * PhpMessageSource represents a message source that stores translated messages in PHP scripts.
+ * PhpMessageSource 表示在 PHP 脚本中存储已翻译消息的消息源。
  *
- * PhpMessageSource uses PHP arrays to keep message translations.
+ * PhpMessageSource 使用 PHP 数组来保存消息翻译。
  *
- * - Each PHP script contains one array which stores the message translations in one particular
- *   language and for a single message category;
- * - Each PHP script is saved as a file named as "[[basePath]]/LanguageID/CategoryName.php";
- * - Within each PHP script, the message translations are returned as an array like the following:
+ * - 每个 PHP 脚本都包含一个数组，
+ *   其存储一个特定的语言和用于单个消息类别的消息翻译;
+ * - 每个 PHP 脚本都保存名为 "[[basePath]]/LanguageID/CategoryName.php" 的文件；
+ * - 在每个 PHP 脚本中，消息翻译将以数组形式返回，如下所示：
  *
  * ```php
  * return [
@@ -26,7 +26,7 @@ use Yii;
  * ];
  * ```
  *
- * You may use [[fileMap]] to customize the association between category names and the file names.
+ * 您可以使用 [[fileMap]] 自定义类别名称和文件名之间的关联。
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
@@ -34,12 +34,12 @@ use Yii;
 class PhpMessageSource extends MessageSource
 {
     /**
-     * @var string the base path for all translated messages. Defaults to '@app/messages'.
+     * @var string 所有翻译的消息的基本路径。默认是 '@app/messages'。
      */
     public $basePath = '@app/messages';
     /**
-     * @var array mapping between message categories and the corresponding message file paths.
-     * The file paths are relative to [[basePath]]. For example,
+     * @var array 消息类别与相应的消息文件路径之间的映射。
+     * 文件路径位于 [[basePath]] 相对路径下。例如，
      *
      * ```php
      * [
@@ -52,17 +52,17 @@ class PhpMessageSource extends MessageSource
 
 
     /**
-     * Loads the message translation for the specified $language and $category.
-     * If translation for specific locale code such as `en-US` isn't found it
-     * tries more generic `en`. When both are present, the `en-US` messages will be merged
-     * over `en`. See [[loadFallbackMessages]] for details.
-     * If the $language is less specific than [[sourceLanguage]], the method will try to
-     * load the messages for [[sourceLanguage]]. For example: [[sourceLanguage]] is `en-GB`,
-     * $language is `en`. The method will load the messages for `en` and merge them over `en-GB`.
+     * 加载指定 $language 和 $category 的消息翻译。
+     * 如果找不到特定区域代码（例如 `en-US`）的翻译，则会尝试更通用的 `en`。
+     * 当两者都存在时，`en-US` 消息将合并在 `en` 上。
+     * 有关详细信息，请参见 [[loadFallbackMessages]]。
+     * 如果 $language 不如 [[sourceLanguage]] 更具体，
+     * 则该方法将尝试加载 [[sourceLanguage]] 的消息。例如：[[sourceLanguage]] 是 `en-GB`，
+     * $language 是 `en`。该方法将加载 `en` 的消息并将它们合并到 `en-GB`。
      *
-     * @param string $category the message category
-     * @param string $language the target language
-     * @return array the loaded messages. The keys are original messages, and the values are the translated messages.
+     * @param string $category 消息类别
+     * @param string $language 目标语言
+     * @return array 加载的消息。键是源消息，值是翻译的消息。
      * @see loadFallbackMessages
      * @see sourceLanguage
      */
@@ -88,16 +88,16 @@ class PhpMessageSource extends MessageSource
     }
 
     /**
-     * The method is normally called by [[loadMessages]] to load the fallback messages for the language.
-     * Method tries to load the $category messages for the $fallbackLanguage and adds them to the $messages array.
+     * 该方法由 [[loadMessages]] 调用来为语言加载后备消息。
+     * 方法尝试为 $fallbackLanguage 加载 $category 消息，并将它们添加到 $messages 数组中。
      *
-     * @param string $category the message category
-     * @param string $fallbackLanguage the target fallback language
-     * @param array $messages the array of previously loaded translation messages.
-     * The keys are original messages, and the values are the translated messages.
-     * @param string $originalMessageFile the path to the file with messages. Used to log an error message
-     * in case when no translations were found.
-     * @return array the loaded messages. The keys are original messages, and the values are the translated messages.
+     * @param string $category 消息类别
+     * @param string $fallbackLanguage 目标后备语言
+     * @param array $messages 先前加载的翻译消息的数组。
+     * 键是源消息，值是翻译的消息。
+     * @param string $originalMessageFile 带有消息文件的路径。
+     * 用于在未找到任何翻译时记录错误信息。
+     * @return array 加载的消息。键是源消息，值是翻译的消息。
      * @since 2.0.7
      */
     protected function loadFallbackMessages($category, $fallbackLanguage, $messages, $originalMessageFile)
@@ -126,11 +126,11 @@ class PhpMessageSource extends MessageSource
     }
 
     /**
-     * Returns message file path for the specified language and category.
+     * 返回指定语言和类别的消息文件路径。
      *
-     * @param string $category the message category
-     * @param string $language the target language
-     * @return string path to message file
+     * @param string $category 消息类别
+     * @param string $language 目标语言
+     * @return string 消息文件的路径
      */
     protected function getMessageFilePath($category, $language)
     {
@@ -145,10 +145,10 @@ class PhpMessageSource extends MessageSource
     }
 
     /**
-     * Loads the message translation for the specified language and category or returns null if file doesn't exist.
+     * 加载指定语言和类别的消息翻译，如果文件不存在返回 null。
      *
-     * @param string $messageFile path to message file
-     * @return array|null array of messages or null if file not found
+     * @param string $messageFile 消息文件的路径
+     * @return array|null 消息数组，如果找不到文件，则返回 null
      */
     protected function loadMessagesFromFile($messageFile)
     {
