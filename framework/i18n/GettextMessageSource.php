65bdaf7720be9876e78f5fd93411310de52b74ae
Shiyang <dr@shiyang.me>
2019-03-27 14:10:04 +0800

    Update GettextMessageSource.php

--- framework/i18n/GettextMessageSource.php
+++ framework/i18n/GettextMessageSource.php
@@ -10,18 +10,18 @@ namespace yii\i18n;
 use Yii;
 
 /**
- * GettextMessageSource represents a message source that is based on GNU Gettext.
+ * GettextMessageSource 表示基于 GNU Gettext 的消息源。
  *
- * Each GettextMessageSource instance represents the message translations
- * for a single domain. And each message category represents a message context
- * in Gettext. Translated messages are stored as either a MO or PO file,
- * depending on the [[useMoFile]] property value.
+ * 每个 GettextMessageSource 实例表示单个域的消息翻译。
+ * 每个消息类别表示 Gettext 中的消息上下文。
+ * 翻译后的消息存储为 MO 或 PO 文件，
+ * 具体取决于 [[useMoFile]] 属性值。
  *
- * All translations are saved under the [[basePath]] directory.
+ * 所有翻译都保存在 [[basePath]] 目录下。
  *
- * Translations in one language are kept as MO or PO files under an individual
- * subdirectory whose name is the language ID. The file name is specified via
- * [[catalog]] property, which defaults to 'messages'.
+ * 一种语言的翻译将以 MO 或 PO 文件保存在一个单独的子目录下，
+ * 该子目录的名称是语言 ID。
+ * 文件名通过 [[catalog]] 属性指定，默认为 'messages'。
  *
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
@@ -50,17 +50,17 @@ class GettextMessageSource extends MessageSource
 
 
     /**
-     * Loads the message translation for the specified $language and $category.
-     * If translation for specific locale code such as `en-US` isn't found it
-     * tries more generic `en`. When both are present, the `en-US` messages will be merged
-     * over `en`. See [[loadFallbackMessages]] for details.
-     * If the $language is less specific than [[sourceLanguage]], the method will try to
-     * load the messages for [[sourceLanguage]]. For example: [[sourceLanguage]] is `en-GB`,
-     * $language is `en`. The method will load the messages for `en` and merge them over `en-GB`.
+     * 加载指定 $language 和 $category 的消息翻译。
+     * 如果找不到特定区域代码（例如 `en-US`）的翻译，则会尝试更通用的 `en`。
+     * 当两者都存在时，`en-US` 消息将合并在 `en` 上。
+     * 有关详细信息，请参见 [[loadFallbackMessages]]。
+     * 如果 $language 不如 [[sourceLanguage]] 更具体，
+     * 则该方法将尝试加载 [[sourceLanguage]] 的消息。例如：[[sourceLanguage]] 是 `en-GB`，
+     * $language 是 `en`。该方法将加载 `en` 的消息并将它们合并到 `en-GB`。
      *
-     * @param string $category the message category
-     * @param string $language the target language
-     * @return array the loaded messages. The keys are original messages, and the values are translated messages.
+     * @param string $category 消息类别
+     * @param string $language 目标语言
+     * @return array 加载的消息。键是源消息，值是翻译的消息。
      * @see loadFallbackMessages
      * @see sourceLanguage
      */
@@ -86,16 +86,16 @@ class GettextMessageSource extends MessageSource
     }
 
     /**
-     * The method is normally called by [[loadMessages]] to load the fallback messages for the language.
-     * Method tries to load the $category messages for the $fallbackLanguage and adds them to the $messages array.
+     * 该方法由 [[loadMessages]] 调用来为语言加载后备消息。
+     * 方法尝试为 $fallbackLanguage 加载 $category 消息，并将它们添加到 $messages 数组中。
      *
-     * @param string $category the message category
-     * @param string $fallbackLanguage the target fallback language
-     * @param array $messages the array of previously loaded translation messages.
-     * The keys are original messages, and the values are the translated messages.
-     * @param string $originalMessageFile the path to the file with messages. Used to log an error message
-     * in case when no translations were found.
-     * @return array the loaded messages. The keys are original messages, and the values are the translated messages.
+     * @param string $category 消息类别
+     * @param string $fallbackLanguage 目标后备语言
+     * @param array $messages 先前加载的翻译消息的数组。
+     * 键是源消息，值是翻译的消息。
+     * @param string $originalMessageFile 带有消息文件的路径。
+     * 用于在未找到任何翻译时记录错误信息。
+     * @return array 加载的消息。键是源消息，值是翻译的消息。
      * @since 2.0.7
      */
     protected function loadFallbackMessages($category, $fallbackLanguage, $messages, $originalMessageFile)
@@ -124,10 +124,10 @@ class GettextMessageSource extends MessageSource
     }
 
     /**
-     * Returns message file path for the specified language and category.
+     * 返回指定语言和类别的消息文件路径。
      *
-     * @param string $language the target language
-     * @return string path to message file
+     * @param string $language 目标语言
+     * @return string 消息文件的路径
      */
     protected function getMessageFilePath($language)
     {
@@ -142,11 +142,11 @@ class GettextMessageSource extends MessageSource
     }
 
     /**
-     * Loads the message translation for the specified language and category or returns null if file doesn't exist.
+     * 加载指定语言和类别的信息翻译，如果文件不存在返回 null。
      *
-     * @param string $messageFile path to message file
-     * @param string $category the message category
-     * @return array|null array of messages or null if file not found
+     * @param string $messageFile 消息文件的路径
+     * @param string $category 消息类别
+     * @return array|null 消息数组，如果找不到文件，则返回 null
      */
     protected function loadMessagesFromFile($messageFile, $category)
     {
