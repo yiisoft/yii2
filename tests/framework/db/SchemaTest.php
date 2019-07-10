9f2a87363ea8cfbb3b3d326e3134c4ed74e95baf
Alexander Kartavenko <askemailbox@gmail.com>
2019-07-10 17:29:11 +0300

    Fixes #17420: Fixed loading of column default values for MSSQL

--- tests/framework/db/SchemaTest.php
+++ tests/framework/db/SchemaTest.php
@@ -495,10 +495,6 @@ abstract class SchemaTest extends DatabaseTestCase
 
     public function testNegativeDefaultValues()
     {
-        if ($this->driverName === 'sqlsrv') {
-            $this->markTestSkipped('Should be fixed');
-        }
-
         /* @var $schema Schema */
         $schema = $this->getConnection()->schema;
 
@@ -513,10 +509,6 @@ abstract class SchemaTest extends DatabaseTestCase
 
     public function testColumnSchema()
     {
-        if ($this->driverName === 'sqlsrv') {
-            $this->markTestSkipped('Should be fixed');
-        }
-
         $columns = $this->getExpectedColumns();
 
         $table = $this->getConnection(false)->schema->getTableSchema('type', true);
--- tests/framework/db/mssql/SchemaTest.php
+++ tests/framework/db/mssql/SchemaTest.php
@@ -133,4 +133,51 @@ class SchemaTest extends \yiiunit\framework\db\SchemaTest
             ['dbo.[table.with.special.characters]', 'table.with.special.characters'],
         ];
     }
+
+    public function getExpectedColumns()
+    {
+        $columns = parent::getExpectedColumns();
+        unset($columns['enum_col']);
+        unset($columns['ts_default']);
+        unset($columns['bit_col']);
+        unset($columns['json_col']);
+
+        $columns['int_col']['dbType'] = 'int';
+        $columns['int_col2']['dbType'] = 'int';
+        $columns['tinyint_col']['dbType'] = 'tinyint';
+        $columns['smallint_col']['dbType'] = 'smallint';
+        $columns['float_col']['dbType'] = 'decimal';
+        $columns['float_col']['phpType'] = 'string';
+        $columns['float_col']['type'] = 'decimal';
+        $columns['float_col']['scale'] = null;
+        $columns['float_col2']['dbType'] = 'float';
+        $columns['float_col2']['phpType'] = 'double';
+        $columns['float_col2']['type'] = 'float';
+        $columns['float_col2']['scale'] = null;
+        $columns['blob_col']['dbType'] = 'varbinary';
+        $columns['numeric_col']['dbType'] = 'decimal';
+        $columns['numeric_col']['scale'] = null;
+        $columns['time']['dbType'] = 'datetime';
+        $columns['time']['type'] = 'datetime';
+        $columns['bool_col']['dbType'] = 'tinyint';
+        $columns['bool_col2']['dbType'] = 'tinyint';
+
+        array_walk($columns, static function (&$item) {
+            $item['enumValues'] = [];
+        });
+
+        array_walk($columns, static function (&$item, $name) {
+            if (!in_array($name, ['char_col', 'char_col2', 'char_col3'])) {
+                $item['size'] = null;
+            }
+        });
+
+        array_walk($columns, static function (&$item, $name) {
+            if (!in_array($name, ['char_col', 'char_col2', 'char_col3'])) {
+                $item['precision'] = null;
+            }
+        });
+
+        return $columns;
+    }
 }
