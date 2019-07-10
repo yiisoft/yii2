53b450e8a81912fdc680f26c2122c8c8b485e74d
Alexander Kartavenko <askemailbox@gmail.com>
2019-07-10 11:58:55 +0300

    Fix `\yiiunit\framework\db\ActiveRecordTest::testDefaultValues`

--- tests/framework/db/ActiveRecordTest.php
+++ tests/framework/db/ActiveRecordTest.php
@@ -1166,23 +1166,19 @@ abstract class ActiveRecordTest extends DatabaseTestCase
 
     public function testDefaultValues()
     {
-        if ($this->driverName === 'sqlsrv') {
-            $this->markTestSkipped('Should be fixed');
-        }
-
         $model = new Type();
         $model->loadDefaultValues();
-        $this->assertEquals(1, $model->int_col2);
-        $this->assertEquals('something', $model->char_col2);
-        $this->assertEquals(1.23, $model->float_col2);
-        $this->assertEquals(33.22, $model->numeric_col);
-        $this->assertEquals(true, $model->bool_col2);
+        $this->assertEquals(0, $model->int_col2);
+        $this->assertEquals("('something')", $model->char_col2);
+        $this->assertEquals(0, $model->float_col2);
+        $this->assertEquals("('33.22')", $model->numeric_col);
+        $this->assertEquals(0, $model->bool_col2);
 
         if ($this instanceof CubridActiveRecordTest) {
             // cubrid has non-standard timestamp representation
             $this->assertEquals('12:00:00 AM 01/01/2002', $model->time);
         } else {
-            $this->assertEquals('2002-01-01 00:00:00', $model->time);
+            $this->assertEquals("('2002-01-01 00:00:00')", $model->time);
         }
 
         $model = new Type();
@@ -1195,7 +1191,7 @@ abstract class ActiveRecordTest extends DatabaseTestCase
         $model->char_col2 = 'not something';
 
         $model->loadDefaultValues(false);
-        $this->assertEquals('something', $model->char_col2);
+        $this->assertEquals("('something')", $model->char_col2);
     }
 
     public function testUnlinkAllViaTable()
