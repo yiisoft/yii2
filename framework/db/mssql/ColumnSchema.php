cd7407307c2e699cc37e701635534078849be026
Alexander Kartavenko <askemailbox@gmail.com>
2019-07-23 21:52:20 +0300

    Update ColumnSchema.php

--- framework/db/mssql/ColumnSchema.php
+++ framework/db/mssql/ColumnSchema.php
@@ -24,7 +24,7 @@ class ColumnSchema extends \yii\db\ColumnSchema
     {
         if ($value !== null) {
             // convert from MSSQL column_default format, e.g. ('1') -> 1, ('string') -> string
-            $value = substr(substr($value, 0, 2), 0, -2);
+            $value = substr(substr($value, 2), 0, -2);
         }
 
         return parent::phpTypecast($value);
