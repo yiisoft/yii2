8b3f961135dffb284b0c43e87c3f5ff01b80f5fe
Alexander Kartavenko <askemailbox@gmail.com>
2019-07-23 21:10:55 +0300

    Change trim to substr

--- framework/db/mssql/ColumnSchema.php
+++ framework/db/mssql/ColumnSchema.php
@@ -24,7 +24,7 @@ class ColumnSchema extends \yii\db\ColumnSchema
     {
         if ($value !== null) {
             // convert from MSSQL column_default format, e.g. ('1') -> 1, ('string') -> string
-            $value = trim($value, "'()");
+            $value = substr(substr($value, 0, 2), 0, -2);
         }
 
         return parent::phpTypecast($value);
