a0c9dce868a8aa45eab5ba2b6be2689c561b220e
Alexander Kartavenko <askemailbox@gmail.com>
2019-07-30 14:19:56 +0300

    mysql version

--- tests/data/travis/mysql-setup.sh
+++ tests/data/travis/mysql-setup.sh
@@ -2,7 +2,7 @@
 
 sudo service mysql stop
 
-docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7.27
+docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7
 
 while ! mysqladmin ping -h 127.0.0.1 --silent; do
     sleep 1
