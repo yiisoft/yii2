5073eb15acc900634d97e20ad40d1a80692b27e6
Alexander Kartavenko <askemailbox@gmail.com>
2019-08-01 11:55:03 +0300

    Revert accidentally committed code [skip ci]

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -23,9 +23,6 @@ require_once __DIR__ . '/../framework/Yii.php';
 
 Yii::setAlias('@yiiunit', __DIR__);
 
-//Yii::setAlias('@yiiunit/runtime', '/home/vagrant/yiiunit/runtime');
-//Yii::setAlias('@runtime', getenv('/home/vagrant/yiiunit/runtime'));
-
 if (getenv('TEST_RUNTIME_PATH')) {
     Yii::setAlias('@yiiunit/runtime', getenv('TEST_RUNTIME_PATH'));
     Yii::setAlias('@runtime', getenv('TEST_RUNTIME_PATH'));
