9ac67573b06f9d42c97b10f80b6c18662ec34efd
Alexander Makarov <sam@rmcreative.ru>
2019-08-13 16:19:56 +0300

    Update Behavior.php

--- framework/base/Behavior.php
+++ framework/base/Behavior.php
@@ -90,7 +90,7 @@ class Behavior extends BaseObject
     public function detach()
     {
         if ($this->owner) {
-            foreach ($this->events() as $event => $handler) {
+            foreach ($this->_attachedEvents as $event => $handler) {
                 $this->owner->off($event, is_string($handler) ? [$this, $handler] : $handler);
             }
             $this->owner = null;
