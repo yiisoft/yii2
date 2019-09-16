be181a11f6c324c8840d496aa620f077a3a5c5eb
DrDeath72 <dr.death.ru@gmail.com>
2019-09-16 22:45:18 +0500

    Update DefaultValueValidatorTest.php

--- tests/framework/validators/DefaultValueValidatorTest.php
+++ tests/framework/validators/DefaultValueValidatorTest.php
@@ -44,4 +44,65 @@ class DefaultValueValidatorTest extends TestCase
         $val->validateAttribute($obj, 'attrA');
         $this->assertEquals($objB->attrA, $obj->attrA);
     }
+    
+    public function testValidateAttributeClosure()
+    {
+        $val = new DefaultValueValidator();
+        $val->value = function($model, $attribute) {
+            return 'test_value';
+        };
+        $obj = new \stdclass();
+        $obj->attrA = 'attrA';
+        $obj->attrB = null;
+        $obj->attrC = '';
+        // original values to chek which attritubes where modified
+        $objB = clone $obj;
+        $val->validateAttribute($obj, 'attrB');
+        $this->assertEquals($val->value, $obj->attrB);
+        $this->assertEquals($objB->attrA, $obj->attrA);
+        $val->value = 'new_test_value';
+        $val->value = function($model, $attribute) {
+            return 'new_test_value';
+        };
+        $obj = clone $objB; // get clean object
+        $val->validateAttribute($obj, 'attrC');
+        $this->assertEquals('new_test_value', $obj->attrC);
+        $this->assertEquals($objB->attrA, $obj->attrA);
+        $val->validateAttribute($obj, 'attrA');
+        $this->assertEquals($objB->attrA, $obj->attrA);
+    }
+    
+    public function testValidateAttributeCallable()
+    {
+        $val = new DefaultValueValidator();
+        $val->value = [__CLASS__, 'testValue'];
+        $obj = new \stdclass();
+        $obj->attrA = 'attrA';
+        $obj->attrB = null;
+        $obj->attrC = '';
+        // original values to chek which attritubes where modified
+        $objB = clone $obj;
+        $val->validateAttribute($obj, 'attrB');
+        $this->assertEquals($val->value, $obj->attrB);
+        $this->assertEquals($objB->attrA, $obj->attrA);
+        $val->value = 'new_test_value';
+        $val->value = [__CLASS__, 'newTestValue'];
+        $obj = clone $objB; // get clean object
+        $val->validateAttribute($obj, 'attrC');
+        $this->assertEquals('new_test_value', $obj->attrC);
+        $this->assertEquals($objB->attrA, $obj->attrA);
+        $val->validateAttribute($obj, 'attrA');
+        $this->assertEquals($objB->attrA, $obj->attrA);
+    }
+    
+    public static function testValue($model, $attribute)
+    {
+        return 'test_value';
+    }
+    
+    public static function newTestValue($model, $attribute)
+    {
+        return 'new_test_value';
+    }
+    
 }
