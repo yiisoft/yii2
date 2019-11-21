3897782fcb0e32d8a8c33b9fab835a6e9617ae63
243083df <243083df@dispostable.com>
2019-11-22 02:49:12 +0400

    Fix clone

--- framework/db/oci/Command.php
+++ framework/db/oci/Command.php
@@ -13,8 +13,8 @@ class Command extends \yii\db\Command
     {
         foreach ($this->_pendingParams as $name => $value) {
             if($value[1] === \PDO::PARAM_STR) {
-                $clonedValue = clone $value[0];
-                $this->pdoStatement->bindParam($name, $clonedValue, $value[1], strlen($clonedValue));
+                $val = $value[0];
+                $this->pdoStatement->bindParam($name, $val, $value[1], strlen($val));
             } else {
                 $this->pdoStatement->bindValue($name, $value[0], $value[1]);
             }
