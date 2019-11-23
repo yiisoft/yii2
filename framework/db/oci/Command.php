20dd4d6311ecc08a5e89f78e185f032e26eb974f
243083df <243083df@dispostable.com>
2019-11-23 12:14:29 +0400

    Fix clone 2

--- framework/db/oci/Command.php
+++ framework/db/oci/Command.php
@@ -15,6 +15,7 @@ class Command extends \yii\db\Command
             if($value[1] === \PDO::PARAM_STR) {
                 $val = $value[0];
                 $this->pdoStatement->bindParam($name, $val, $value[1], strlen($val));
+                unset($val);
             } else {
                 $this->pdoStatement->bindValue($name, $value[0], $value[1]);
             }
