3ebbef77055ecfeb839a4289cbad09016c5758ce
diego <d.mathis@firegroup.com>
2019-12-06 09:06:35 +0100

    Fixes #17708 explicitly define precision to be 15

--- tests/framework/i18n/FormatterNumberTest.php
+++ tests/framework/i18n/FormatterNumberTest.php
@@ -528,7 +528,7 @@ class FormatterNumberTest extends TestCase
         // null display
         $this->assertSame($this->formatter->nullDisplay, $this->formatter->asScientific(null));
 
-        $this->assertSame('8.76543210987654E16', $this->formatter->asScientific('87654321098765436'));
+        $this->assertSame('8.76543210987654E16', $this->formatter->asScientific('87654321098765436', 15));
     }
 
     public function testAsScientific()
