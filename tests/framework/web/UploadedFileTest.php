cf0e56907fb4fa2c4ee090c732782c066a72026a
Yusup Hambali <supalpuket@gmail.com>
2020-01-15 04:57:45 +0700

    Fix #17037, Fix #17729: Fix uploaded file saving for multipart forms, add path alias support for UploadFile::saveAs()

--- tests/framework/web/UploadedFileTest.php
+++ tests/framework/web/UploadedFileTest.php
@@ -8,6 +8,7 @@
 namespace yiiunit\framework\web;
 
 use yii\web\UploadedFile;
+use yiiunit\framework\web\mocks\UploadedFileMock;
 use yiiunit\framework\web\stubs\ModelStub;
 use yiiunit\framework\web\stubs\VendorImage;
 use yiiunit\TestCase;
@@ -35,6 +36,17 @@ class UploadedFileTest extends TestCase
         ];
     }
 
+    private function generateTempFileData()
+    {
+        return [
+            'name' => md5(mt_rand()),
+            'tmp_name' => tempnam(sys_get_temp_dir(), ''),
+            'type' => 'image/jpeg',
+            'size' => mt_rand(1000, 10000),
+            'error' => 0,
+        ];
+    }
+
     private function generateFakeFiles()
     {
         $_FILES['ModelStub[prod_image]'] = $this->generateFakeFileData();
@@ -46,6 +58,8 @@ class UploadedFileTest extends TestCase
         $_FILES['ModelStub[vendor_images][]'] = $this->generateFakeFileData();
         $_FILES['ModelStub[vendor_images][]'] = $this->generateFakeFileData();
         $_FILES['ModelStub[vendor_images][]'] = $this->generateFakeFileData();
+
+        $_FILES['ModelStub[temp_image]'] = $this->generateTempFileData();
     }
 
     // Tests :
@@ -71,5 +85,32 @@ class UploadedFileTest extends TestCase
         foreach ($vendorImages as $vendorImage) {
             $this->assertInstanceOf(VendorImage::className(), $vendorImage);
         }
+    }
+
+    public function testSaveAs()
+    {
+        $tmpImage = UploadedFileMock::getInstance(new ModelStub(), 'temp_image');
+        $targetFile = '@runtime/test_saved_uploaded_file_' . time();
+
+        $this->assertEquals(true, $tmpImage->saveAs($targetFile, $deleteTempFile = false));
+        $this->assertEquals(true, $tmpImage->saveAs($targetFile, $deleteTempFile = true));
+        $this->assertEquals(false, $tmpImage->saveAs($targetFile)); // temp file should not exist
+
+        @unlink($targetFile);
+    }
+
+    public function testSaveFileFromMultipartFormDataParser()
+    {
+        $_FILES = [];
+        UploadedFile::reset();
+        $model = new ModelStub();
+        $targetFile = '@runtime/test_saved_uploaded_file_' . time();
+
+        (new MultipartFormDataParserTest)->testParse();
+        $_FILES['ModelStub'] = $_FILES['Item']; // $_FILES[Item] here from testParse() above
+        $tmpFile = UploadedFile::getInstance($model, 'file');
+
+        $this->assertEquals(true, $tmpFile->saveAs($targetFile));
+        @unlink($targetFile);
     }
 }
