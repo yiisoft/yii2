c99edaca71c493eb0111f36fa8e247ffe4003c5f
DarkDef <v.gobachev@infsys.org>
2020-06-06 19:36:48 +0300

    Fix for issue #13828: Get Primary Key Uniqueidentifier Yii 2.0.11 Retrieving inserted data from a primary key request of type uniqueidentifier (for SQL Server 2005 or later) Added OUTPUT construction for getting inserted data OUTPUT clause - The OUTPUT clause is new to SQL Server 2005 and has the ability to access the INSERTED and DELETED tables as is the case with a trigger.

--- framework/db/mssql/Schema.php
+++ framework/db/mssql/Schema.php
@@ -751,4 +751,35 @@ SQL;
 
         return parent::quoteColumnName($name);
     }
+
+    /**
+     * Retrieving inserted data from a primary key request of type uniqueidentifier (for SQL Server 2005 or later)
+     * {@inheritdoc}
+     */
+    public function insert($table, $columns)
+    {
+        $command = $this->db->createCommand()->insert($table, $columns);
+        if (!$command->execute()) {
+            return false;
+        }
+
+        $version2005orLater = version_compare($this->db->getSchema()->getServerVersion(), '9', '>=');
+        $inserted = $version2005orLater ? $command->pdoStatement->fetch() : [];
+
+        $tableSchema = $this->getTableSchema($table);
+        $result = [];
+        foreach ($tableSchema->primaryKey as $name) {
+            if ($tableSchema->columns[$name]->autoIncrement) {
+                $result[$name] = $this->getLastInsertID($tableSchema->sequenceName);
+                break;
+            }
+            if ($tableSchema->columns[$name]->dbType === 'uniqueidentifier' and isset($inserted[$name])) {
+                $result[$name] = $inserted[$name];
+                break;
+            }
+            $result[$name] = isset($columns[$name]) ? $columns[$name] : $tableSchema->columns[$name]->defaultValue;
+        }
+
+        return $result;
+    }
 }
