91fabfd554f651710177778ed56bd63e7d1f5470
Alexander Makarov <sam@rmcreative.ru>
2020-07-20 02:25:23 +0300

    Fix generating property docs by a build script

--- build/controllers/PhpDocController.php
+++ build/controllers/PhpDocController.php
@@ -654,7 +654,7 @@ class PhpDocController extends Controller
         } else {
             $namespace = $namespace['name'];
         }
-        $classes = $this->match('#\n(?:abstract )(?:final )?class (?<name>\w+)( extends .+)?( implements .+)?\n\{(?<content>.*)\n\}(\n|$)#', $file);
+        $classes = $this->match('#\n(?:abstract )?(?:final )?class (?<name>\w+)( extends .+)?( implements .+)?\n\{(?<content>.*)\n\}(\n|$)#', $file);
 
         if (\count($classes) > 1) {
             $this->stderr("[ERR] There should be only one class in a file: $fileName\n", Console::FG_RED);
