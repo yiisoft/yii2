7ea6725917c04dc85b2b5e3d6e77824d32cb9fad
DarkDef <darkdef@gmail.com>
2020-08-04 22:25:33 +0300

    forward code in Schema.php

--- framework/db/mssql/Schema.php
+++ framework/db/mssql/Schema.php
@@ -768,12 +768,8 @@ SQL;
             return false;
         }
 
-        $version2005orLater = version_compare($this->db->getSchema()->getServerVersion(), '9', '>=');
-
-        $inserted = [];
-        if ($version2005orLater) {
-            $inserted = $command->pdoStatement->fetch();
-        }
+        $isVersion2005orLater = version_compare($this->db->getSchema()->getServerVersion(), '9', '>=');
+        $inserted = $isVersion2005orLater ? $command->pdoStatement->fetch() : [];
 
         $tableSchema = $this->getTableSchema($table);
         $result = [];
@@ -783,8 +779,13 @@ SQL;
                 break;
             }
             // @see https://github.com/yiisoft/yii2/issues/13828 & https://github.com/yiisoft/yii2/issues/17474
-            $result[$name] = isset($inserted[$name]) ? $inserted[$name] :
-                (isset($columns[$name]) ? $columns[$name] : $tableSchema->columns[$name]->defaultValue);
+            if (isset($inserted[$name])) {
+                $result[$name] = $inserted[$name];
+            } elseif (isset($columns[$name])) {
+                $result[$name] = $columns[$name];
+            } else {
+                $result[$name] = $tableSchema->columns[$name]->defaultValue;
+            }
         }
 
         return $result;
