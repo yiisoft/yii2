21805541b2259ec7721ddaf8322b8fc202c314fc
Michael Härtl <haertl.mike@gmail.com>
2020-08-07 16:54:48 +0200

    Issue #18217 Render chained exceptions in debug mode only

--- framework/console/ErrorHandler.php
+++ framework/console/ErrorHandler.php
@@ -65,7 +65,7 @@ class ErrorHandler extends \yii\base\ErrorHandler
         } else {
             echo $message . "\n";
         }
-        if (($previous = $exception->getPrevious()) !== null) {
+        if (YII_DEBUG && ($previous = $exception->getPrevious()) !== null) {
             $causedBy = "\n" . $this->formatMessage('Caused by: ', [Console::BOLD]);
             if (PHP_SAPI === 'cli') {
                 Console::stderr($causedBy);
