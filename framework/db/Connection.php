f6880cc8505cc54fff651d9279977665d5b5c1d4
Alexander Makarov <sam@rmcreative.ru>
2020-08-15 17:28:18 +0300

    Update Connection.php

--- framework/db/Connection.php
+++ framework/db/Connection.php
@@ -724,7 +724,7 @@ class Connection extends Component
                 $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, $this->emulatePrepare);
             }
         }
-        if (in_array($this->getDriverName(), ['mssql', 'dblib'], true) and $this->isSybase) {
+        if (!$this->isSybase && in_array($this->getDriverName(), ['mssql', 'dblib'], true)) {
             $this->pdo->exec('SET ANSI_NULL_DFLT_ON ON');
         }
         if ($this->charset !== null && in_array($this->getDriverName(), ['pgsql', 'mysql', 'mysqli', 'cubrid'], true)) {
