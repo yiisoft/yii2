95935352d313602470c05c83adc3d2e111028f04
Basil <git@nadar.io>
2020-08-19 08:43:45 +0200

    Update framework/filters/RateLimiter.php
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

--- framework/filters/RateLimiter.php
+++ framework/filters/RateLimiter.php
@@ -49,7 +49,7 @@ class RateLimiter extends ActionFilter
      */
     public $errorMessage = 'Rate limit exceeded.';
     /**
-     * @var RateLimitInterface|closure the user object that implements the RateLimitInterface. If not set, it will take the value of `Yii::$app->user->getIdentity(false)`.
+     * @var RateLimitInterface|Closure the user object that implements the RateLimitInterface. If not set, it will take the value of `Yii::$app->user->getIdentity(false)`.
      * {@since 2.0.38} It's possible to provide a closure function in order to assign the user identity on runtime. Using a closure to assign the user identity is recommend
      * when you are **not** using the standard `Yii::$app->user` component. See the example below:
      * ```php
