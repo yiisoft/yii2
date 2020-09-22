7c57e3060ccef7331011115b0787a7a3f6972378
yangjx <yangjx@textbooking.com.cn>
2020-09-22 15:43:58 +0800

    Fixed #18160, #18192: Fixed `registerFile` with argument depends set does not use the position and appendTimestamp argument

--- framework/web/View.php
+++ framework/web/View.php
@@ -479,7 +479,6 @@ class View extends \yii\base\View
         $url = Yii::getAlias($url);
         $key = $key ?: $url;
         $depends = ArrayHelper::remove($options, 'depends', []);
-        $position = ArrayHelper::remove($options, 'position', self::POS_END);
 
         try {
             $asssetManagerAppendTimestamp = $this->getAssetManager()->appendTimestamp;
@@ -487,10 +486,11 @@ class View extends \yii\base\View
             $depends = null; // the AssetManager is not available
             $asssetManagerAppendTimestamp = false;
         }
-        $appendTimestamp = ArrayHelper::remove($options, 'appendTimestamp', $asssetManagerAppendTimestamp);
 
         if (empty($depends)) {
             // register directly without AssetManager
+            $position = ArrayHelper::remove($options, 'position', self::POS_END);
+            $appendTimestamp = ArrayHelper::remove($options, 'appendTimestamp', $asssetManagerAppendTimestamp);
             if ($appendTimestamp && Url::isRelative($url) && ($timestamp = @filemtime(Yii::getAlias('@webroot/' . ltrim($url, '/'), false))) > 0) {
                 $url = $timestamp ? "$url?v=$timestamp" : $url;
             }
@@ -504,7 +504,7 @@ class View extends \yii\base\View
                 'class' => AssetBundle::className(),
                 'baseUrl' => '',
                 'basePath' => '@webroot',
-                (string)$type => [!Url::isRelative($url) ? $url : ltrim($url, '/')],
+                (string)$type => ArrayHelper::merge([!Url::isRelative($url) ? $url : ltrim($url, '/')], $options),
                 "{$type}Options" => $options,
                 'depends' => (array)$depends,
             ]);
