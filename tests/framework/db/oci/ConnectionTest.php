ccf56c7ba885aa4dfdc97083479b8bd50a1365c5
Wilmer Arambula <terabytesoftw@gmail.com>
2020-11-15 14:11:02 -0300

    Fix test ConnectionTest::testQuoteValue.

--- tests/framework/db/oci/ConnectionTest.php
+++ tests/framework/db/oci/ConnectionTest.php
@@ -109,4 +109,12 @@ class ConnectionTest extends \yiiunit\framework\db\ConnectionTest
         )->queryScalar();
         $this->assertEquals(1, $profilesCount, 'profile should be inserted in transaction shortcut');
     }
+
+    public function testQuoteValue()
+    {
+        $connection = $this->getConnection(false);
+        $this->assertEquals(123, $connection->quoteValue(123));
+        $this->assertEquals("'string'", $connection->quoteValue('string'));
+        $this->assertEquals("'It''s interesting'", $connection->quoteValue("It's interesting"));
+    }
 }
