f62fc1557c52071688b9c61d99c0ee9b0e4fba97
oakideas <5075187+oakideas@users.noreply.github.com>
2020-11-20 03:54:46 -0300

    add a new kind of FileTarget
    extends FileTarget log changing the output format to JSON. makes it easier to integrate with log parsers.

---
+++ framework/log/JsonFileTarget.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\log;
+
+/**
+ * JsonFileTarget extends FileTarget changing the output to JSON
+ *
+ * @author Fabio Oak <fabio@oakideas.com.br>
+ */
+class JsonFileTarget extends FileTarget
+{
+	/**
+     * Formats a log message for display as a JSON string.
+     * @param array $message the log message to be formatted.
+     * @return string the formatted message
+     */
+	public function formatMessage($message): string {
+
+        list($text, $level, $category, $timestamp) = $message;
+        $level = Logger::getLevelName($level);
+
+        if ($text instanceof \Throwable || $text instanceof \Exception) {
+            $text = (string) $text;
+		}
+
+        $traces = [];
+        if (isset($message[4])) {
+            foreach ($message[4] as $trace) {
+                $traces[] = "in {$trace['file']}:{$trace['line']}";
+            }
+        }
+
+		return json_encode(['timestamp' => $timestamp, 'datetime' => date('Y-m-d\TH:i:s\Z', intval($timestamp)), 'text' => $text, 'level' => $level, 'category' => $category, 'traces' => $traces]);
+    }
+
+}
