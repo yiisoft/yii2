e94f102c8b6749e17062a91b90a5ff34961cf6b6
M.Monem <mmonem@gmail.com>
2020-12-31 09:48:35 +0200

    Compare Validator: Support of compareValue being also a closure

--- framework/validators/CompareValidator.php
+++ framework/validators/CompareValidator.php
@@ -146,6 +146,9 @@ class CompareValidator extends Validator
             return;
         }
         if ($this->compareValue !== null) {
+            if ($this->compareValue instanceof \Closure) {
+                $this->compareValue = call_user_func($this->compareValue, $model, $attribute);
+            }
             $compareLabel = $compareValue = $compareValueOrAttribute = $this->compareValue;
         } else {
             $compareAttribute = $this->compareAttribute === null ? $attribute . '_repeat' : $this->compareAttribute;
@@ -242,6 +245,9 @@ class CompareValidator extends Validator
         ];
 
         if ($this->compareValue !== null) {
+            if ($this->compareValue instanceof \Closure) {
+                $this->compareValue = call_user_func($this->compareValue, $model, $attribute);
+            }
             $options['compareValue'] = $this->compareValue;
             $compareLabel = $compareValue = $compareValueOrAttribute = $this->compareValue;
         } else {
