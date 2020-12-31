0b15a05d45c7f54cf53f83a5503e40867c40c5c2
Edin Omeragic <edin.omeragic@gmail.com>
2020-12-31 22:27:36 +0100

    Refactoring action parameter binding

--- framework/bindings/BindingHelper.php
+++ framework/bindings/BindingHelper.php
@@ -7,23 +7,7 @@
 
 namespace yii\bindings;
 
-use ReflectionNamedType;
-use ReflectionParameter;
-
 class BindingHelper
 {
-    /**
-     * @var ReflectionParameter
-     * @return string | null
-     */
-    public static function getParameterTypeName($param)
-    {
-        if (PHP_VERSION_ID >= 70000 && $param->hasType()) {
-            $paramType = $param->getType();
-            if ($paramType instanceof ReflectionNamedType) {
-                return PHP_VERSION_ID >= 70100 ? $paramType->getName() : (string)$paramType;
-            }
-        }
-        return null;
-    }
+
 }
