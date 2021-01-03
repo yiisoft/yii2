5ce9d81f52e8ea9f4d535891f1a1d55f40922ea1
Edin Omeragic <edin.omeragic@gmail.com>
2021-01-03 17:58:24 +0100

    Updated tests

--- tests/framework/bindings/ActionBindingController.php
+++ tests/framework/bindings/ActionBindingController.php
@@ -15,3 +15,39 @@ class ActionBindingController extends \yii\base\Controller
     {
     }
 }
+
+
+// class TestController extends Controller
+// {
+
+//     public function actionParams(
+//         $mixed,
+//         int $int,
+//         float $float,
+//         bool $bool,
+//         DateTime $dateTime,
+//         DateTimeImmutable $dateTimeImmutable
+//     )
+//     {
+//     }
+
+//     public function actionNoType($value)
+//     {
+//     }
+
+//     public function actionBuiltin(int $int, float $float, bool $bool)
+//     {
+//     }
+
+//     public function actionBuiltinNullable(?int $int, ?float $float, ?bool $bool)
+//     {
+//     }
+
+//     public function actionDateTime(DateTime $dateTime, DateTimeImmutable $dateTimeImmutable)
+//     {
+//     }
+
+//     public function actionDateTimeNullable(?DateTime $dateTime, ?DateTimeImmutable $dateTimeImmutable)
+//     {
+//     }
+// }
