19a2043412d78e3ff2ab98571263d14d3994d7c7
Ikko Ashimine <eltociear@gmail.com>
2021-01-13 02:51:55 +0900

    Fix typo in Connection.php
    avaiable -> available

--- framework/db/Connection.php
+++ framework/db/Connection.php
@@ -1139,7 +1139,7 @@ class Connection extends Component
      *
      * If none of the servers are available the status cache is ignored and connection attempts are made to all
      * servers (Since version 2.0.35). This is to avoid downtime when all servers are unavailable for a short time.
-     * After a successful connection attempt the server is marked as avaiable again.
+     * After a successful connection attempt the server is marked as available again.
      *
      * @param array $pool the list of connection configurations in the server pool
      * @param array $sharedConfig the configuration common to those given in `$pool`.
