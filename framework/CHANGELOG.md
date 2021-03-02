18311bf2f14c7dd76c3084a19c23411b86bd9128
Alexander Makarov <sam@rmcreative.ru>
2021-03-03 00:32:03 +0300

    Fix #9718: Fix user staying authorized despite authKey change

--- framework/CHANGELOG.md
+++ framework/CHANGELOG.md
@@ -18,6 +18,7 @@ Yii Framework 2 Change Log
 - Enh #18487: Allow creating URLs for non-GET-verb rules (bizley)
 - Bug #8750: Fix MySQL support when running in `ANSI`/`ANSI_QUOTES` modes (brandonkelly)
 - Bug #18505: Fixed `yii\helpers\ArrayHelper::getValue()` for ArrayAccess objects with explicitly defined properties (samdark)
+- Bug #9718: Fix user staying authorized despite authKey change (kidol, Charlie Jack, Kunal Mhaske, samdark)
 
 
 2.0.40 December 23, 2020
--- framework/web/IdentityInterface.php
+++ framework/web/IdentityInterface.php
@@ -69,12 +69,6 @@ interface IdentityInterface
     public static function findIdentityByAccessToken($token, $type = null);
 
     /**
-     * Returns an ID that can uniquely identify a user identity.
-     * @return string|int an ID that uniquely identifies a user identity.
-     */
-    public function getId();
-
-    /**
      * Returns a key that can be used to check the validity of a given identity ID.
      *
      * The key should be unique for each individual user, and should be persistent
@@ -82,8 +76,7 @@ interface IdentityInterface
      *
      * The space of such keys should be big enough to defeat potential identity attacks.
      *
-     * This is required if [[User::enableAutoLogin]] is enabled. The returned key will be stored on the
-     * client side as a cookie and will be used to authenticate user even if PHP session has been expired.
+     * The returned key is used to validate session and auto-login (if [[User::enableAutoLogin]] is enabled).
      *
      * Make sure to invalidate earlier issued authKeys when you implement force user logout, password change and
      * other scenarios, that require forceful access revocation for old sessions.
@@ -94,9 +87,14 @@ interface IdentityInterface
     public function getAuthKey();
 
     /**
+     * Returns an ID that can uniquely identify a user identity.
+     * @return string|int an ID that uniquely identifies a user identity.
+     */
+    public function getId();
+
+    /**
      * Validates the given auth key.
      *
-     * This is required if [[User::enableAutoLogin]] is enabled.
      * @param string $authKey the given auth key
      * @return bool whether the given auth key is valid.
      * @see getAuthKey()
