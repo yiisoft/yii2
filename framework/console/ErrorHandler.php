4a5b325160b04bea5583c390a98054098610b37d
Alexander Makarov <sam@rmcreative.ru>
2021-06-05 23:29:10 +0300

    Fix console error handler exception handling condition. Related to #18696
    See https://github.com/yiisoft/yii2/pull/18696#discussion_r645877103

--- framework/console/ErrorHandler.php
+++ framework/console/ErrorHandler.php
@@ -9,6 +9,7 @@ namespace yii\console;
 
 use Yii;
 use yii\base\ErrorException;
+use yii\base\UserException;
 use yii\helpers\Console;
 
 /**
@@ -38,7 +39,7 @@ class ErrorHandler extends \yii\base\ErrorHandler
             } elseif (count($alternatives) > 1) {
                 $message .= "\n\nDid you mean one of these?\n    - " . implode("\n    - ", $alternatives);
             }
-        } elseif ($exception instanceof Exception || !YII_DEBUG) {
+        } elseif ($exception instanceof UserException && ($exception instanceof Exception || !YII_DEBUG)) {
             $message = $this->formatMessage($exception->getName() . ': ') . $exception->getMessage();
         } elseif (YII_DEBUG) {
             if ($exception instanceof Exception) {
