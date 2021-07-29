a53b6f722ec5ef65ccfc2b3d578dbe09aea1f0c2
Anton <info@ensostudio.ru>
2021-07-29 19:45:14 +0300

    Update RequestTest.php

--- tests/framework/web/RequestTest.php
+++ tests/framework/web/RequestTest.php
@@ -12,6 +12,7 @@ use yiiunit\TestCase;
 
 /**
  * @group web
+ * @backupGlobals enabled
  */
 class RequestTest extends TestCase
 {
@@ -1047,6 +1048,31 @@ class RequestTest extends TestCase
         $this->assertSame('value.dot', $request->getBodyParam('param.dot'));
         $this->assertSame(null, $request->getBodyParam('unexisting'));
         $this->assertSame('default', $request->getBodyParam('unexisting', 'default'));
+    }
+
+    public function getBodyParamsDataProvider()
+    {
+        return [
+            'json' => ['application/json', '{"foo":"bar","baz":1}', ['foo'=>'bar', 'baz' => 1]],
+            'jsonp' => ['application/javascript', 'parseResponse({"x":"y"});', ['x'=>'y']],
+            'post' => ['application/x-www-form-urlencoded', 'foo=bar&baz=1', ['foo'=>'bar', 'baz' => 1]],
+        ];
+    }
+
+    /**
+     * @dataProvider getBodyParamsDataProvider
+     */
+    public function testGetBodyParams($contentType, $rawBody, $expected)
+    {
+        $_SERVER['CONTENT_TYPE'] = $contentType;
+        $request = new Request([
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+                'application/javascript' => 'yii\web\JsonParser',
+            ]
+        ]);
+        $request->setRawBody($rawBody);
+        $this->assertSame($expected, $request->getBodyParams());
     }
 
     public function trustedHostAndInjectedXForwardedForDataProvider()
