cbe9154a878a9325ddeea3203bb83e24352c0ac3
Anton <info@ensostudio.ru>
2021-08-07 15:29:01 +0300

    Update BaseStringHelper.php

--- framework/helpers/BaseStringHelper.php
+++ framework/helpers/BaseStringHelper.php
@@ -470,7 +470,8 @@ class BaseStringHelper
         $parts = preg_split('/(\s+[^\w]+\s+|^[^\w]+\s+|\s+)/u', $string, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
         $ucfirstEven = !trim(mb_substr($parts[0], -1, 1, $encoding));
         foreach ($parts as $key => $value) {
-            if ($upEven === (bool)($key % 2)) {
+            $even = (bool) ($key % 2);
+            if ($ucfirstEven === $even) {
                 $parts[$key] = static::mb_ucfirst($value, $encoding);
             }
         }
