719534d81f0a98bce02de5e829d82288b01c6d43
Игорь Тарасов <develop@dicr.org>
2021-08-13 06:07:39 +0500

    fix return of BaseHtml::getAttributeValue

--- framework/helpers/BaseHtml.php
+++ framework/helpers/BaseHtml.php
@@ -2245,13 +2245,13 @@ class BaseHtml
             throw new InvalidArgumentException('Attribute name must contain word characters only.');
         }
         $attribute = $matches[2];
-        $value = $model->$attribute;
+        $value = $model->{$attribute};
         if ($matches[3] !== '') {
             foreach (explode('][', trim($matches[3], '[]')) as $id) {
                 if ((is_array($value) || $value instanceof \ArrayAccess) && isset($value[$id])) {
                     $value = $value[$id];
                 } else {
-                    return null;
+                    return '';
                 }
             }
         }
@@ -2260,17 +2260,17 @@ class BaseHtml
         if (is_array($value)) {
             foreach ($value as $i => $v) {
                 if ($v instanceof ActiveRecordInterface) {
-                    $v = $v->getPrimaryKey(false);
+                    $v = $v->getPrimaryKey();
                     $value[$i] = is_array($v) ? json_encode($v) : $v;
                 }
             }
         } elseif ($value instanceof ActiveRecordInterface) {
-            $value = $value->getPrimaryKey(false);
+            $value = $value->getPrimaryKey();
 
-            return is_array($value) ? json_encode($value) : $value;
+            return is_array($value) ? json_encode($value) :(string)$value;
         }
 
-        return $value;
+        return (string)$value;
     }
 
     /**
