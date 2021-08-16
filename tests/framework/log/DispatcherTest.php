3074b9f501ca57cb404c1ab0107882f4e6130399
Alexandr B <lexizz19@mail.ru>
2021-08-17 02:40:14 +0500

    Global setLogger was added in Dispatcher, if Logger was configured inside Dispatcher #18840

--- tests/framework/log/DispatcherTest.php
+++ tests/framework/log/DispatcherTest.php
@@ -18,6 +18,7 @@ namespace yii\log {
 
 namespace yiiunit\framework\log {
 
+    use yii\BaseYii;
     use yiiunit\framework\log\mocks\TargetMock;
     use Yii;
     use yii\base\UserException;
@@ -87,6 +88,16 @@ namespace yiiunit\framework\log {
             ]);
             $this->assertInstanceOf('yii\log\Logger', $dispatcher->getLogger());
             $this->assertEquals(42, $dispatcher->getLogger()->traceLevel);
+
+            $dispatcher = new Dispatcher([
+                'logger' => [
+                    'class' => 'yii\log\Logger',
+                    'profilingAware' => true,
+                ],
+            ]);
+            $this->assertInstanceOf('yii\log\Logger', $dispatcher->getLogger());
+            $this->assertEquals(true, $dispatcher->getLogger()->profilingAware);
+            $this->assertEquals(true, BaseYii::getLogger()->profilingAware);
         }
 
         /**
