fd50331da782aa4b9422ab9caa1906a94d9db3c6
Alexander Makarov <sam@rmcreative.ru>
2021-08-24 16:59:57 +0300

    Revert string search change

--- framework/console/controllers/MigrateController.php
+++ framework/console/controllers/MigrateController.php
@@ -606,7 +606,7 @@ class MigrateController extends BaseMigrateController
     protected function addDefaultPrimaryKey(&$fields)
     {
         foreach ($fields as $field) {
-            if ($field['property'] === 'id' || false !== stripos($field['decorators'], 'primarykey()')) {
+            if ($field['property'] === 'id' || false !== strripos($field['decorators'], 'primarykey()')) {
                 return;
             }
         }
