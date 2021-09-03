e8c379c257c7d38ff77175751c0a3ef599a4c5d4
Anton <info@ensostudio.ru>
2021-09-03 11:58:04 +0300

    Update framework/helpers/BaseJson.php
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

--- framework/helpers/BaseJson.php
+++ framework/helpers/BaseJson.php
@@ -51,7 +51,6 @@ class BaseJson
         'JSON_ERROR_SYNTAX' => 'Syntax error',
         'JSON_ERROR_UTF8' => 'Malformed UTF-8 characters, possibly incorrectly encoded', // PHP 5.3.3
         'JSON_ERROR_RECURSION' => 'One or more recursive references in the value to be encoded', // PHP 5.5.0
-        'JSON_ERROR_INF_OR_NAN' => 'One or more NAN or INF values in the value to be encoded', // PHP 5.5.0
     ];
 
 
