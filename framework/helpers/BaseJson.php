969526eee858496efc2cbc066db06bc1623758eb
Anton <info@ensostudio.ru>
2021-09-03 15:15:27 +0300

    Update order in `$jsonErrorMessages` to optimize search

--- framework/helpers/BaseJson.php
+++ framework/helpers/BaseJson.php
@@ -45,14 +45,12 @@ class BaseJson
      * @since 2.0.7
      */
     public static $jsonErrorMessages = [
+        'JSON_ERROR_SYNTAX' => 'Syntax error',
+        'JSON_ERROR_UNSUPPORTED_TYPE' => 'Type is not supported',
         'JSON_ERROR_DEPTH' => 'The maximum stack depth has been exceeded',
         'JSON_ERROR_STATE_MISMATCH' => 'Invalid or malformed JSON',
         'JSON_ERROR_CTRL_CHAR' => 'Control character error, possibly incorrectly encoded',
-        'JSON_ERROR_SYNTAX' => 'Syntax error',
         'JSON_ERROR_UTF8' => 'Malformed UTF-8 characters, possibly incorrectly encoded',
-        'JSON_ERROR_RECURSION' => 'One or more recursive references in the value to be encoded',
-        'JSON_ERROR_INF_OR_NAN' => 'One or more NAN or INF values in the value to be encoded',
-        'JSON_ERROR_UNSUPPORTED_TYPE' => 'Type is not supported',
     ];
 
 
