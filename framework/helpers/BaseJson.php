d21d1f633d9dc00ff481a3fb64ec81619efb0b11
Anton <info@ensostudio.ru>
2021-09-03 14:33:33 +0300

    Update BaseJson.php

--- framework/helpers/BaseJson.php
+++ framework/helpers/BaseJson.php
@@ -50,6 +50,8 @@ class BaseJson
         'JSON_ERROR_CTRL_CHAR' => 'Control character error, possibly incorrectly encoded',
         'JSON_ERROR_SYNTAX' => 'Syntax error',
         'JSON_ERROR_UTF8' => 'Malformed UTF-8 characters, possibly incorrectly encoded',
+        'JSON_ERROR_RECURSION' => 'One or more recursive references in the value to be encoded',
+        'JSON_ERROR_INF_OR_NAN' => 'One or more NAN or INF values in the value to be encoded',
         'JSON_ERROR_UNSUPPORTED_TYPE' => 'Type is not supported',
     ];
 
