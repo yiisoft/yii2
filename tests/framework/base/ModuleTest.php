6067608321b0f918bb94aa42dbba1c738dbec406
Anton <info@ensostudio.ru>
2021-12-09 10:46:15 +0300

    Fix #19005: Add `yii\base\Module::setControllerPath()`

--- tests/framework/base/ModuleTest.php
+++ tests/framework/base/ModuleTest.php
@@ -37,11 +37,22 @@ class ModuleTest extends TestCase
         $this->assertEquals('parent', $child2->module->id);
     }
 
-    public function testControllerPath()
+    public function testGetControllerPath()
     {
         $module = new TestModule('test');
+        $controllerPath = __DIR__ . DIRECTORY_SEPARATOR . 'controllers';
+
         $this->assertEquals('yiiunit\framework\base\controllers', $module->controllerNamespace);
-        $this->assertEquals(__DIR__ . DIRECTORY_SEPARATOR . 'controllers', str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $module->controllerPath));
+        $this->assertEquals($controllerPath, str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $module->getControllerPath()));
+    }
+
+    public function testSetControllerPath()
+    {
+        $module = new TestModule('test');
+        $controllerPath = __DIR__ . DIRECTORY_SEPARATOR . 'controllers';
+
+        $module->setControllerPath($controllerPath);
+        $this->assertEquals($controllerPath, $module->getControllerPath());
     }
 
     public function testSetupVersion()
