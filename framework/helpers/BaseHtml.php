e77e7b63fa7e0f3cce2941f2eb9b8f81a08f5e24
Anton <info@ensostudio.ru>
2021-12-21 13:47:38 +0300

    Add BaseHtml::$normalizeClassAttribute

--- framework/helpers/BaseHtml.php
+++ framework/helpers/BaseHtml.php
@@ -94,8 +94,15 @@ class BaseHtml
      * @since 2.0.3
      */
     public static $dataAttributes = ['aria', 'data', 'data-ng', 'ng'];
-
-
+    /**
+     * @var bool whether to removes duplicate class names in tag attribute `class`
+     * @see mergeCssClasses()
+     * @see renderTagAttributes()
+     * @since 2.0.44
+     */
+    public static $normalizeClassAttribute = false;
+    
+    
     /**
      * Encodes special characters into HTML entities.
      * The [[\yii\base\Application::charset|application charset]] will be used for encoding.
@@ -1981,7 +1988,7 @@ class BaseHtml
                     if (empty($value)) {
                         continue;
                     }
-                    if (count($value) > 1) {
+                    if (static::$normalizeClassAttribute === true && count($value) > 1) {
                         // removes duplicate classes
                         $value = explode(' ', implode(' ', $value));
                         $value = array_unique($value);
@@ -2052,7 +2059,7 @@ class BaseHtml
             }
         }
 
-        return array_unique($existingClasses);
+        return static::$normalizeClassAttribute ? array_unique($existingClasses) : $existingClasses;
     }
 
     /**
