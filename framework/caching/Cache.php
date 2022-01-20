ebb1d8bc600e4ba40a42dab44f16a51be79b000f
Aurélien Chretien <achretien@users.noreply.github.com>
2022-01-20 09:22:25 +0100

    Fix #19178: Fix null value in unserialize for PHP 8.1 (#19178)

--- framework/caching/Cache.php
+++ framework/caching/Cache.php
@@ -135,7 +135,7 @@ abstract class Cache extends Component implements CacheInterface
         if ($value === false || $this->serializer === false) {
             return $value;
         } elseif ($this->serializer === null) {
-            $value = unserialize($value);
+            $value = unserialize((string)$value);
         } else {
             $value = call_user_func($this->serializer[1], $value);
         }
