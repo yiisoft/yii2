2cdcf6a85574e02ebc85daf864f3287da23521c5
Dmitrijlin <dmitrij-lin9@rambler.ru>
2022-01-25 13:51:10 +0300

    Apply suggestions from code review
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

--- framework/web/HttpException.php
+++ framework/web/HttpException.php
@@ -42,7 +42,7 @@ class HttpException extends UserException
      * @param int $code error code
      * @param \Throwable|null $previous The previous exception used for the exception chaining.
      */
-    public function __construct($status, $message = null, $code = 0, \Throwable $previous = null)
+    public function __construct($status, $message = null, $code = 0, $previous = null)
     {
         $this->statusCode = $status;
         parent::__construct((string)$message, $code, $previous);
