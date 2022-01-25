af99570d1a61c55e323a208e2d7c1bd92396cda3
Dmitrijlin <dmitrij-lin9@rambler.ru>
2022-01-25 13:51:55 +0300

    Apply suggestions from code review
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

--- framework/web/BadRequestHttpException.php
+++ framework/web/BadRequestHttpException.php
@@ -27,7 +27,7 @@ class BadRequestHttpException extends HttpException
      * @param int $code error code
      * @param \Throwable|null $previous The previous exception used for the exception chaining.
      */
-    public function __construct($message = null, $code = 0, \Throwable $previous = null)
+    public function __construct($message = null, $code = 0, $previous = null)
     {
         parent::__construct(400, $message, $code, $previous);
     }
