5db441d1724c160dd2abe81b020a71ad18588d99
Long Tran <long.thanh.tran@gmail.com>
2022-02-05 11:54:37 +0700

    Fall back to isset check instead

--- framework/db/mysql/Schema.php
+++ framework/db/mysql/Schema.php
@@ -294,7 +294,8 @@ SQL;
              * See details here: https://mariadb.com/kb/en/library/now/#description
              */
             if (($column->type === 'timestamp' || $column->type === 'datetime')
-                && preg_match('/^current_timestamp(?:\(([0-9]*)\))?$/i', (string)$info['default'], $matches)) {
+                && isset($info['default'])
+                && preg_match('/^current_timestamp(?:\(([0-9]*)\))?$/i', $info['default'], $matches)) {
                 $column->defaultValue = new Expression('CURRENT_TIMESTAMP' . (!empty($matches[1]) ? '(' . $matches[1] . ')' : ''));
             } elseif (isset($type) && $type === 'bit') {
                 $column->defaultValue = bindec(trim(isset($info['default']) ? $info['default'] : '', 'b\''));
