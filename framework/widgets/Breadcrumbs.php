7b276bd389fe7b3ca2d1f0d57659ae9ac1349fbe
Anton <info@ensostudio.ru>
2022-05-03 21:00:26 +0300

    Fix `yii\widgets\Breadcrumbs::run()`
    Fix BC break

--- framework/widgets/Breadcrumbs.php
+++ framework/widgets/Breadcrumbs.php
@@ -125,12 +125,12 @@ class Breadcrumbs extends Widget
 
 
     /**
-     * Renders the widget.
+     * {@inheritdoc}
      */
     public function run()
     {
         if (empty($this->links)) {
-            return;
+            return '';
         }
         $links = [];
         if ($this->homeLink === null) {
@@ -147,7 +147,7 @@ class Breadcrumbs extends Widget
             }
             $links[] = $this->renderItem($link, isset($link['url']) ? $this->itemTemplate : $this->activeItemTemplate);
         }
-        echo Html::tag($this->tag, implode('', $links), $this->options);
+        return Html::tag($this->tag, implode('', $links), $this->options);
     }
 
     /**
