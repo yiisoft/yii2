41509d886d5500dfa7a3dc30d6ec59318a953e64
Anton <info@ensostudio.ru>
2022-05-28 11:56:10 +0300

    Fix translators in `messages/config.php`
    #19415

--- framework/messages/config.php
+++ framework/messages/config.php
@@ -17,7 +17,7 @@ return [
     // Defaults to 'Yii::t'. This is used as a mark to find the messages to be
     // translated. You may use a string for single function name or an array for
     // multiple function names.
-    'translator' => 'Yii::t',
+    'translator' => ['\Yii::t', 'Yii::t'],
     // boolean, whether to sort messages by keys when merging new messages
     // with the existing ones. Defaults to false, which means the new (untranslated)
     // messages will be separated from the old (translated) ones.
