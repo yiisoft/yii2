a7a4c33147d278ec6b0d9efb7007ed8db4c7455c
Anton <info@ensostudio.ru>
2022-08-24 19:33:39 +0300

    Update BaseComparator.php

--- framework/helpers/BaseComparator.php
+++ framework/helpers/BaseComparator.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @link http://www.yiiframework.com/
+ * @link https://www.yiiframework.com/
  * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
+ * @license https://www.yiiframework.com/license/
  */
 
 namespace yii\helpers;
@@ -14,7 +14,7 @@ use ReflectionFunction;
  * The flexible comparation.
  *
  * @author info@ensostudio.ru
- * @since 2.0.46
+ * @since 2.0.47
  */
 abstract class BaseComparator
 {
