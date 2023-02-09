da392847b411bde9e54e74e20d6a65ecdc0a6381
Deriabin Sergei <deryabinsergey@gmail.com>
2023-02-09 13:55:08 +0100

    Rollback from mimeType to controller, bad luck for PHP version 7.3 and lower

--- build/controllers/MimeTypeController.php
+++ build/controllers/MimeTypeController.php
@@ -98,15 +98,12 @@ class MimeTypeController extends Controller
             }
         }
         $mimeMap = array_merge($mimeMap, $this->additionalMimeTypes);
+        if (PHP_VERSION_ID >= 80100) {
+            $mimeMap = array_replace($mimeMap, array('xz' => 'application/octet-stream'));
+        }
         ksort($mimeMap);
         $array = VarDumper::export($mimeMap);
-        $array = implode(
-            PHP_EOL,
-            array_map(
-                static fn ($line) => "\t" . $line,
-                explode(PHP_EOL, $array)
-            )
-        );
+
         $content = <<<EOD
 <?php
 /**
@@ -118,12 +115,7 @@ class MimeTypeController extends Controller
  * https://svn.apache.org/viewvc/httpd/httpd/trunk/docs/conf/mime.types?view=markup
  * This file has been placed in the public domain for unlimited redistribution.
  */
-return array_merge(
-        $array,
-    PHP_VERSION_ID >= 80100
-        ? ['xz' => 'application/octet-stream']
-        : [],
-);
+return $array;
 
 EOD;
         file_put_contents($outFile, $content);
