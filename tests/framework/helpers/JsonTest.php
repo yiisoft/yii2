5406e5dad3eaa4e9d58f65bb565d37adc4307f3b
Гордиенко Владислав Юрьевич <gordienkovladis@gmail.com>
2023-02-27 16:48:22 +0500

    Fix #19766: Add support for PHP generators to JSON helper

--- tests/framework/helpers/JsonTest.php
+++ tests/framework/helpers/JsonTest.php
@@ -95,6 +95,15 @@ class JsonTest extends TestCase
         $data = new JsonModel();
         $data->data = (object) null;
         $this->assertSame('{}', Json::encode($data));
+
+        // Generator
+        $data = function () {
+            foreach (['a' => 1, 'b' => 2] as $name  => $value) {
+                yield $name => $value;
+            }
+        };
+
+        $this->assertSame('{"a":1,"b":2}', Json::encode($data()));
     }
 
     public function testHtmlEncode()
