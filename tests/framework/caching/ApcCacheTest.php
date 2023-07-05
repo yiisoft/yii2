bb3fa7cb6438e6a7fd65b61084f9c7afac1e1fab
Wilmer Arambula <terabytesoftw@gmail.com>
2023-07-05 11:34:49 -0400

    Fix tests again.

--- tests/framework/caching/ApcCacheTest.php
+++ tests/framework/caching/ApcCacheTest.php
@@ -23,7 +23,7 @@ class ApcCacheTest extends CacheTestCase
      */
     protected function getCacheInstance()
     {
-        if (!extension_loaded('apc') || !extension_loaded('apcu')) {
+        if (!extension_loaded('apc') && !extension_loaded('apcu')) {
             $this->markTestSkipped('APC not installed. Skipping.');
         } elseif ('cli' === PHP_SAPI && !ini_get('apc.enable_cli')) {
             $this->markTestSkipped('APC cli is not enabled. Skipping.');
