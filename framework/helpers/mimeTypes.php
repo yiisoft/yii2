4b5a2578fdb22512e0ece8f6193c00b07205bb5b
Tobias Munk <schmunk@usrbin.de>
2023-08-24 11:12:36 +0200

    added PHP version check, fixed #19925

--- framework/helpers/mimeTypes.php
+++ framework/helpers/mimeTypes.php
@@ -1001,7 +1001,8 @@ $mimeTypes = [
     'zmm' => 'application/vnd.handheld-entertainment+xml',
 ];
 
-if (PHP_VERSION_ID >= 80100) {
+# fix for bundled libmagic bug, see also https://github.com/yiisoft/yii2/issues/19925
+if ((PHP_VERSION_ID >= 80100 && PHP_VERSION_ID < 80122) || (PHP_VERSION_ID >= 80200 && PHP_VERSION_ID < 80209)) {
     $mimeTypes = array_replace($mimeTypes, array('xz' => 'application/octet-stream'));
 }
 
