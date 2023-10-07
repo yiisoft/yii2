5e37678700f95c756c3d5cf20574dcf5a90c8dd5
Nabi <nabikaz@gmail.com>
2023-10-07 19:26:51 +0330

    Added 'zh' into framework/messages/config.php
    Signed-off-by: Nabi <nabikaz@gmail.com>

--- framework/messages/config.php
+++ framework/messages/config.php
@@ -15,8 +15,8 @@ return [
     'languages' => [
         'af', 'ar', 'az', 'be', 'bg', 'bs', 'ca', 'cs', 'da', 'de', 'el', 'es', 'et', 'fa', 'fi', 'fr', 'he', 'hi',
         'pt-BR', 'ro', 'hr', 'hu', 'hy', 'id', 'it', 'ja', 'ka', 'kk', 'ko', 'kz', 'lt', 'lv', 'ms', 'nb-NO', 'nl',
-        'pl', 'pt', 'ru', 'sk', 'sl', 'sr', 'sr-Latn', 'sv', 'tg', 'th', 'tr', 'uk', 'uz', 'uz-Cy', 'vi', 'zh-CN',
-        'zh-TW'
+        'pl', 'pt', 'ru', 'sk', 'sl', 'sr', 'sr-Latn', 'sv', 'tg', 'th', 'tr', 'uk', 'uz', 'uz-Cy', 'vi', 'zh',
+        'zh-CN', 'zh-TW'
     ],
     // string, the name of the function for translating messages.
     // Defaults to 'Yii::t'. This is used as a mark to find the messages to be
