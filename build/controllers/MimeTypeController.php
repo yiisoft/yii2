4addf02c262c203f482690288027b0be30cd679b
Bizley <pawel.bizley@gmail.com>
2023-10-08 13:03:48 +0200

    Fix mime type generator

--- build/controllers/MimeTypeController.php
+++ build/controllers/MimeTypeController.php
@@ -124,7 +124,8 @@ class MimeTypeController extends Controller
  */
 \$mimeTypes = $array;
 
-if (PHP_VERSION_ID >= 80100) {
+# fix for bundled libmagic bug, see also https://github.com/yiisoft/yii2/issues/19925
+if ((PHP_VERSION_ID >= 80100 && PHP_VERSION_ID < 80122) || (PHP_VERSION_ID >= 80200 && PHP_VERSION_ID < 80209)) {
     \$mimeTypes = array_replace(\$mimeTypes, array('xz' => 'application/octet-stream'));
 }
 
