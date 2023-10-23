63b777e667d69ef6c10fdcce62ba49d262a0402e
Wilmer Arambula <terabytesoftw@gmail.com>
2023-10-23 11:30:58 -0300

    Fix type hint in `BaseObjectTest:class `.

--- tests/framework/base/BaseObjectTest.php
+++ tests/framework/base/BaseObjectTest.php
@@ -170,7 +170,7 @@ class BaseObjectTest extends TestCase
 class NewObject extends BaseObject
 {
     private ?\yiiunit\framework\base\NewObject $_object = null;
-    private string $_text = 'default';
+    private string|null $_text = 'default';
     private array $_items = [];
     public $content;
 
