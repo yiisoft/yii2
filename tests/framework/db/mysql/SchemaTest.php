30c53ddae5d4c1afab431a906e6a6ffea1a1ccaf
Wilmer Arambula <terabytesoftw@gmail.com>
2024-05-18 14:03:39 -0400

    Normalize column names.

--- tests/framework/db/mysql/SchemaTest.php
+++ tests/framework/db/mysql/SchemaTest.php
@@ -77,16 +77,12 @@ SQL;
     public static function constraintsProvider(): array
     {
         $result = parent::constraintsProvider();
-        $result['1: check'][2] = false;
 
         $result['2: primary key'][2]->name = null;
-        $result['2: check'][2] = false;
 
         // Work aroung bug in MySQL 5.1 - it creates only this table in lowercase. O_o
         $result['3: foreign key'][2][0]->foreignTableName = new AnyCaseValue('T_constraints_2');
-        $result['3: check'][2] = false;
 
-        $result['4: check'][2] = false;
         return $result;
     }
 
