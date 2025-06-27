e758a06e733dc26fd2ec9d9ebe0d88784bbffefb
Максим Спирков <spirkov.2001@mail.ru>
2025-06-27 22:10:49 +0400

    self review

--- framework/web/Controller.php
+++ framework/web/Controller.php
@@ -121,8 +121,8 @@ class Controller extends \yii\base\Controller
      * @phpstan-param Action<static> $action
      * @psalm-param Action<static> $action
      *
-     * @phpstan-param array<string, mixed> $params
-     * @psalm-param array<string, mixed> $params
+     * @phpstan-param array<array-key, mixed> $params
+     * @psalm-param array<array-key, mixed> $params
      *
      * @phpstan-return mixed[]
      * @psalm-return mixed[]
