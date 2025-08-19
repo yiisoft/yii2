d50cd9c0ba805a43a8905818584da7948a7878f4
Максим Спирков <spirkov.2001@mail.ru>
2025-08-19 10:33:16 +0400

    cr fix

--- framework/caching/XCache.php
+++ framework/caching/XCache.php
@@ -101,9 +101,7 @@ class XCache extends Cache
     protected function flushValues()
     {
         for ($i = 0, $max = xcache_count(XC_TYPE_VAR); $i < $max; $i++) {
-            if (xcache_clear_cache(XC_TYPE_VAR, $i) === false) {
-                return false;
-            }
+            xcache_clear_cache(XC_TYPE_VAR, $i);
         }
 
         return true;
