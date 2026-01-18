1cf41dcc5535b80d5ceaccf5b709bc8b4a5c04c5
KalimeroMK <zbogoevski@gmail.com>
2026-01-18 15:21:16 +0100

    remove cyrillic comments

--- tests/framework/db/ConnectionSupportTest.php
+++ tests/framework/db/ConnectionSupportTest.php
@@ -2,16 +2,17 @@
 
 namespace yiiunit\framework\db;
 
-use Yii;
 use yii\db\Query;
 use yii\db\Connection;
 use yiiunit\TestCase;
 
-// Дефинираме тест модел надвор од методот за подобра поддршка на Late Static Binding
 class StubAuditLog extends \yii\db\ActiveRecord
 {
     protected static $connection = 'db_logs';
-    public static function tableName() { return 'audit_log'; }
+    public static function tableName()
+    {
+        return 'audit_log';
+    }
 }
 
 class ConnectionSupportTest extends TestCase
@@ -42,11 +43,9 @@ class ConnectionSupportTest extends TestCase
 
     public function testActiveRecordConnectionProperty()
     {
-        // Сега го повикуваме моделот кој експлицитно има дефинирано $connection
         $db = StubAuditLog::getDb();
 
         $this->assertInstanceOf(Connection::class, $db);
-        // Ова ќе потврди дека ActiveRecord ја земал 'db_logs' конекцијата
         $this->assertEquals('mysql:host=localhost;dbname=logs_db_name', $db->dsn);
     }
 }
