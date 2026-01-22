2047c9f0b6acfbc69e750b9cc1d6aff4bcd0aec9
KalimeroMK <zbogoevski@gmail.com>
2026-01-22 17:56:42 +0100

    remoce connection type

--- framework/db/ActiveRecord.php
+++ framework/db/ActiveRecord.php
@@ -129,7 +129,7 @@ class ActiveRecord extends BaseActiveRecord
      * You may override this method if you want to use a different database connection.
      * @return Connection the database connection used by this AR class.
      */
-    public static function getDb(): Connection
+    public static function getDb()
     {
         $modelClass = static::class;
         if (property_exists($modelClass, 'connection')) {
