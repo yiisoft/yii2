80e426f52bd5c9b062f0f714f586a42a1c7f6670
WarLikeLaux <warlikelaux080399@mail.ru>
2026-03-07 23:10:34 +0600

    Fix `OracleMutexIntegrationTest` for unsupported `DBMS_LOCK`

--- tests/framework/mutex/OracleMutexIntegrationTest.php
+++ tests/framework/mutex/OracleMutexIntegrationTest.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 
 namespace yiiunit\framework\mutex;
 
+use PDO;
 use Yii;
 use yii\base\InvalidConfigException;
 use yii\mutex\OracleMutex;
@@ -25,6 +26,48 @@ class OracleMutexIntegrationTest extends DatabaseTestCase
     use MutexTestTrait;
 
     protected $driverName = 'oci';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $db = $this->getConnection(false);
+        $mutexName = 'oracle_mutex_capability_probe';
+        $acquireStatus = null;
+        $releaseStatus = null;
+
+        try {
+            $db->createCommand(
+                'DECLARE
+    handle VARCHAR2(128);
+BEGIN
+    DBMS_LOCK.ALLOCATE_UNIQUE(:name, handle);
+    :result := DBMS_LOCK.REQUEST(handle, DBMS_LOCK.X_MODE, 0, FALSE);
+END;',
+                [':name' => $mutexName]
+            )
+                ->bindParam(':result', $acquireStatus, PDO::PARAM_INT, 1)
+                ->execute();
+
+            $db->createCommand(
+                'DECLARE
+    handle VARCHAR2(128);
+BEGIN
+    DBMS_LOCK.ALLOCATE_UNIQUE(:name, handle);
+    :result := DBMS_LOCK.RELEASE(handle);
+END;',
+                [':name' => $mutexName]
+            )
+                ->bindParam(':result', $releaseStatus, PDO::PARAM_INT, 1)
+                ->execute();
+        } catch (\Throwable $e) {
+            $this->markTestSkipped('Oracle DBMS_LOCK integration is not available: ' . $e->getMessage());
+        }
+
+        if (($acquireStatus !== 0 && $acquireStatus !== '0') || ($releaseStatus !== 0 && $releaseStatus !== '0')) {
+            $this->markTestSkipped('Oracle DBMS_LOCK integration is not supported by this test environment.');
+        }
+    }
 
     /**
      * @return OracleMutex
