96bb922e69d66a7187240e1c3f35f1a2303b1f49
zoran <zoran.bogoevski@zitcha.com>
2026-08-21 20:32:35 +0200

    Add `validateId()` to `SessionHandler`, generate session IDs without `session_create_id()` for PHP 7.4 compatibility

--- framework/web/SessionHandler.php
+++ framework/web/SessionHandler.php
@@ -64,11 +64,44 @@ class SessionHandler implements SessionHandlerInterface
 
     /**
      * @inheritDoc
+     *
+     * Generates the ID the same way as PHP's default session module does, honoring
+     * `session.sid_length` and `session.sid_bits_per_character` ini settings.
+     * `session_create_id()` can't be used here because it fails on PHP < 8 when
+     * a user save handler (such as this class) is registered.
      */
     #[\ReturnTypeWillChange]
+    // phpcs:ignore PSR1.Methods.CamelCapsMethodName.NotCamelCaps -- method name is defined by SessionHandlerInterface
     public function create_sid()
     {
-        return session_create_id();
+        static $charsets = [
+            4 => '0123456789abcdef',
+            5 => '0123456789abcdefghijklmnopqrstuv',
+            6 => '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,-',
+        ];
+
+        $bits = (int) ini_get('session.sid_bits_per_character');
+        $charset = $charsets[$bits] ?? $charsets[5];
+        $length = (int) ini_get('session.sid_length');
+        if ($length < 22 || $length > 256) {
+            $length = 32;
+        }
+
+        $id = '';
+        $maxIndex = strlen($charset) - 1;
+        for ($i = 0; $i < $length; $i++) {
+            $id .= $charset[random_int(0, $maxIndex)];
+        }
+
+        return $id;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function validateId($sessionId): bool
+    {
+        return $this->_session->readSession($sessionId) !== '';
     }
 
     /**
--- tests/framework/web/session/CacheSessionTest.php
+++ tests/framework/web/session/CacheSessionTest.php
@@ -74,6 +74,20 @@ class CacheSessionTest extends TestCase
         $this->assertNotSame($sid, $handler->create_sid());
     }
 
+    /**
+     * @see https://github.com/yiisoft/yii2/issues/21068
+     */
+    public function testSessionHandlerValidateId(): void
+    {
+        $session = new CacheSession();
+        $handler = new SessionHandler($session);
+
+        $session->writeSession('test', 'sessionData');
+        $this->assertTrue($handler->validateId('test'));
+        $this->assertFalse($handler->validateId('not-exists'));
+        $session->destroySession('test');
+    }
+
     public function testUseStrictMode(): void
     {
         $this->useStrictModeTest(CacheSession::class);
